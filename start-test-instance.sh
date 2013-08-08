e0651a7539f1a3dbe86c3edaf100f304286a7307
Christophe Ludet <ludetc@apples-MacBook-Pro.local>
2013-08-08 14:17:55 -0400

    Add a few selenium reg tests

--- start-test-instance.sh
+++ start-test-instance.sh
@@ -3,5 +3,10 @@
 export PORT=3001
 export MONGO_URI=mongodb://localhost/test
 
+/Users/ludetc/dev/mongodb-osx-x86_64-2.4.3/bin/mongo test --eval "db.users.drop()"
+/Users/ludetc/dev/mongodb-osx-x86_64-2.4.3/bin/mongo test --eval "db.dataelements.drop()"
+/Users/ludetc/dev/mongodb-osx-x86_64-2.4.3/bin/mongo test --eval "db.regAuths.drop()"
+/Users/ludetc/dev/mongodb-osx-x86_64-2.4.3/bin/mongo test --eval "db.users.insert({username: 'nlm', password: 'nlm', siteAdmin: true})"
+
 node app
 
