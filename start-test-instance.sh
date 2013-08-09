b7b565061856c7c63af686f6dc6fbb49d6c51dad
Christophe Ludet <ludetc@apples-MacBook-Pro.local>
2013-08-09 10:29:32 -0400

    mongo test init script in a file

--- start-test-instance.sh
+++ start-test-instance.sh
@@ -3,11 +3,6 @@
 export PORT=3001
 export MONGO_URI=mongodb://localhost/test
 
-#/Users/ludetc/dev/mongodb-osx-x86_64-2.4.3/bin/mongo test --eval "db.users.drop()"
-#/Users/ludetc/dev/mongodb-osx-x86_64-2.4.3/bin/mongo test --eval "db.dataelements.drop()"
-#/Users/ludetc/dev/mongodb-osx-x86_64-2.4.3/bin/mongo test --eval "db.regAuths.drop()"
-#/Users/ludetc/dev/mongodb-osx-x86_64-2.4.3/bin/mongo test --eval "db.users.insert({username: 'nlm', password: 'nlm', siteAdmin: true})"
-
 /Users/ludetc/dev/mongodb-osx-x86_64-2.4.3/bin/mongo test test/dbInit.js
 
 node ingester/uploadCadsr test/cadsrTestSeed.xml & 
