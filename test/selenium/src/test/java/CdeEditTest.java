a6e0d869c0f5c879d95021341a6d219ae6182d48
Christophe Ludet <ludetc@apples-MacBook-Pro.local>
2013-09-23 10:15:00 -0400

    nlm updates reg status test

--- test/selenium/src/test/java/CdeEditTest.java
+++ test/selenium/src/test/java/CdeEditTest.java
@@ -14,30 +14,7 @@ import org.testng.annotations.Test;
  *
  * @author ludetc
  */
-public class CdeEditTest extends NlmCdeBaseTest {
-    @Test
-    public void changeRegistrationStatus() {
-        loginAs(cabigAdmin_username, cabigAdmin_password);
-        goToCdeByName("Investigator Identifier java.lang.Integer");
-        Assert.assertTrue(textPresent("Qualified"));
-        findElement(By.cssSelector("dd.ng-binding > i.icon-pencil")).click();
-        new Select(driver.findElement(By.name("registrationStatus"))).selectByVisibleText("Recorded");
-        findElement(By.name("effectiveDate")).sendKeys("9/15/2013");
-        findElement(By.name("untilDate")).sendKeys("10/31/2014");
-        findElement(By.name("administrativeNote")).sendKeys("Admin Note 1");
-        findElement(By.name("unresolvedIssue")).sendKeys("Unresolved Issue 1");
-        findElement(By.cssSelector("button.btn.btn-warning")).click();
-        goToCdeByName("Investigator Identifier java.lang.Integer");
-        Assert.assertTrue(textPresent("Recorded"));
-        findElement(By.linkText("Status")).click();
-        Assert.assertTrue(textPresent("Recorded"));
-        Assert.assertTrue(textPresent("09/15/2013"));
-        Assert.assertTrue(textPresent("10/31/2014"));
-        Assert.assertTrue(textPresent("Admin Note 1"));
-        Assert.assertTrue(textPresent("Unresolved Issue 1"));
-        logout();
-    }
-    
+public class CdeEditTest extends NlmCdeBaseTest {    
     
     @Test
     public void selfRegister() {
---
+++ test/selenium/src/test/java/RegStatusTest.java
@@ -0,0 +1,54 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ludetc
+ */
+public class RegStatusTest extends NlmCdeBaseTest {
+    
+    
+    @Test
+    public void changeRegistrationStatus() {
+        loginAs(cabigAdmin_username, cabigAdmin_password);
+        goToCdeByName("Investigator Identifier java.lang.Integer");
+        Assert.assertTrue(textPresent("Qualified"));
+        findElement(By.cssSelector("dd.ng-binding > i.icon-pencil")).click();
+        new Select(driver.findElement(By.name("registrationStatus"))).selectByVisibleText("Recorded");
+        findElement(By.name("effectiveDate")).sendKeys("9/15/2013");
+        findElement(By.name("untilDate")).sendKeys("10/31/2014");
+        findElement(By.name("administrativeNote")).sendKeys("Admin Note 1");
+        findElement(By.name("unresolvedIssue")).sendKeys("Unresolved Issue 1");
+        findElement(By.cssSelector("button.btn.btn-warning")).click();
+        goToCdeByName("Investigator Identifier java.lang.Integer");
+        Assert.assertTrue(textPresent("Recorded"));
+        findElement(By.linkText("Status")).click();
+        Assert.assertTrue(textPresent("Recorded"));
+        Assert.assertTrue(textPresent("09/15/2013"));
+        Assert.assertTrue(textPresent("10/31/2014"));
+        Assert.assertTrue(textPresent("Admin Note 1"));
+        Assert.assertTrue(textPresent("Unresolved Issue 1"));
+        logout();
+    }
+        
+    @Test
+    public void nlmPromotesToStandard() {
+        loginAs(nlm_username, nlm_password);
+        goToCdeByName("Patient Name");
+        Assert.assertTrue(textPresent("Qualified"));
+        findElement(By.id("editStatus")).click();
+        new Select(driver.findElement(By.name("registrationStatus"))).selectByVisibleText("Standard");
+        findElement(By.cssSelector("button.btn.btn-warning")).click();
+        goToCdeByName("Patient Name");
+        Assert.assertTrue(textPresent("Standard"));
+        logout();
+    }
+    
+}
