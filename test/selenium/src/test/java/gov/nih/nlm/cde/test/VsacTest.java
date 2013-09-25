8bbd8c6422a1370e28dffbe122692ea43ab70ded
Christophe Ludet <ludetc@apples-MacBook-Pro.local>
2013-09-25 13:29:51 -0400

    vsac id test added

--- test/selenium/src/test/java/gov/nih/nlm/cde/test/VsacTest.java
+++ test/selenium/src/test/java/gov/nih/nlm/cde/test/VsacTest.java
@@ -1,9 +1,9 @@
 package gov.nih.nlm.cde.test;
 
-import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.testng.Assert;
+import org.testng.annotations.Test;
 
 /**
  *
@@ -13,30 +13,29 @@ public class VsacTest extends NlmCdeBaseTest {
     
     @Test
     public void assignVsacId() {
+        loginAs(nlm_username, nlm_password);
         goToCdeByName("Patient Ethnic Group Category");
-        findElement(By.linkText("Permissible Values"));
+        findElement(By.linkText("Permissible Values")).click();
         Assert.assertTrue(textPresent("No Value Set specified."));
         findElement(By.linkText("Update O.I.D")).click();
-        findElement(By.name("vsacId")).sendKeys("invalid Id");
-        findElement(By.cssSelector("button.icon-ok")).click();
+        findElement(By.name("vsacId")).sendKeys("invalidId");
+        findElement(By.id("vsacIdCheck")).click();
         Assert.assertTrue(textPresent("Invalid VSAC OID"));
-
         findElement(By.linkText("Update O.I.D")).click();
         findElement(By.name("vsacId")).sendKeys("2.16.840.1.114222.4.11.837");
-        findElement(By.cssSelector("button.icon-ok")).click();
+        findElement(By.id("vsacIdCheck")).click();
         // check that version got fetched.
         Assert.assertTrue(textPresent("20121025"));
-
+        findElement(By.cssSelector("button.btn.btn-primary")).click();
         findElement(By.name("changeNote")).sendKeys("Adding vsac Id");
         Assert.assertTrue(textPresent("This version number has already been used"));
         findElement(By.name("version")).sendKeys(Keys.BACK_SPACE);
         findElement(By.name("version")).sendKeys("3");
         findElement(By.cssSelector("button.btn.btn-warning")).click();
-
         goToCdeByName("Patient Ethnic Group Category");
-        findElement(By.linkText("Permissible Values"));
+        findElement(By.linkText("Permissible Values")).click();
         Assert.assertTrue(textPresent("20121025"));
-
+        logout();
         
     }
     
