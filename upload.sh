02b13b87b35bdd8bd821325e2b01a282a0e65cd0
Christophe Ludet <ludetc@apples-MacBook-Pro.local>
2013-11-07 10:37:54 -0500

    no longer upload users in setup

--- upload.sh
+++ upload.sh
@@ -6,5 +6,5 @@ find ../nlm-seed/ExternalCDEs/caDSR/*.xml -exec node ingester/uploadCadsr {} \;
 
 # node ingester/uploader fitbir ../nlm-seed/ExternalCDEs/FITBIR-Full.xml
 
-node ingester/uploadUsers
+#node ingester/uploadUsers
 
