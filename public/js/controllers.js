5a18a47cef24868328587f2cacd161999525e61e
Jakub Flaska <jakub.flaska@nih.gov>
2014-03-28 12:16:38 -0400

    accordions, last issues

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -534,52 +534,6 @@ function DEListCtrl($scope, $http, $modal, $location) {
     $scope.filter = [];
     $scope.uniqueOrg = false;
     
-    /*$scope.cdeIconAction = function (cde, action, event) {
-        if (event) {
-            event.preventDefault();
-            event.stopPropagation();
-        }
-        switch (action) {
-            case "view":
-                $scope.view(cde);
-            break;
-            case "openPinModal":
-                $scope.openPinModal(cde);
-            break;
-            case "addToCompareCart":
-                $scope.addToCompareCart(cde);
-            break;
-        }        
-    };
-    
-    $scope.openPinModal = function (cde) {
-        var modalInstance = $modal.open({
-          templateUrl: 'selectBoardModalContent.html',
-          controller: SelectBoardModalCtrl,
-          resolve: {
-            boards: function () {
-              return $scope.boards;
-            }
-          }
-        });
-
-        modalInstance.result.then(function (selectedBoard) {
-            $http.put("/pincde/" + cde.uuid + "/" + selectedBoard._id).then(function(response) {
-                if (response.status==200)
-                    $scope.addAlert("success", response.data);
-                else
-                    $scope.addAlert("warning", response.data);
-            }, function (response){
-                $scope.addAlert("danger", response.data);
-            });
-        }, function () {
-        });
-    };
-        
-    $scope.view = function(cde) {       
-        $location.url("deview?cdeId=" + cde._id);
-    };*/
-    
     $scope.setPage = function (pageNo) {
       $scope.currentPage = pageNo;
     };       
