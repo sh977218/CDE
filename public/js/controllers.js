5c43edd8fc5b357ea0f545ac247326e5b3dc4eb0
Jakub Flaska <jakub.flaska@nih.gov>
2014-04-14 10:30:25 -0400

    facets coming in json in de List

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -810,7 +810,6 @@ function DEListCtrl($scope, $http, $modal, $cacheFactory) {
 
         if ($scope.selectedOrg !== undefined) {
             queryStuff.facets.groups = {
-                //terms: {field: "classification.conceptSystem", size: 200}
                 terms: {field: "classification.elements.name", size: 200}
                 , facet_filter: {term: {"classification.stewardOrg.name": $scope.selectedOrg}}
             }
