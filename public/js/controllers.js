be35b6670485503bfb66c2fdd9f7daf684a3bbb5
Jakub Flaska <jakub.flaska@nih.gov>
2014-04-18 16:08:06 -0400

    grid view width

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -190,7 +190,7 @@ function ExportCtrl($scope, $http, $window) {
             line.slice(0, line.Length - 1);
             str += line + '\r\n';
         }
-        $window.open("data:text/csv;charset=utf-8," + escape(str))
+        $window.open("data:text/csv;charset=utf-8," + escape(str),"nlmcde.csv");
     }
     
     $scope.buildElasticQuery(function(query) {
