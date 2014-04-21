c7f6513cf2a5431b289e332c2c2b571060dfcedf
Jakub Flaska <jakub.flaska@nih.gov>
2014-04-21 13:40:57 -0400

    ingestors classpath

--- upload.sh
+++ upload.sh
@@ -2,19 +2,19 @@
 
 mongo nlmcde test/dbInit.js
 
-find ../nlm-seed/ExternalCDEs/caDSR/*.xml -exec groovy -cp groovy/ groovy/UploadCadsr {} \;
+find ../nlm-seed/ExternalCDEs/caDSR/*.xml -exec groovy -cp ./groovy/ groovy/UploadCadsr {} \;
 
 find ../nlm-seed/ExternalCDEs/Vsac/*.xml -exec node ingester/uploadVsac {} \;
 
 find ../nlm-seed/ExternalCDEs/Ahrq -name "de_*.json" -exec node ingester/uploadAhrq {} \;
 
-groovy -cp groovy/ groovy/uploadNinds ../nlm-seed/ExternalCDEs/ninds/all/cdes.xml
+groovy -cp ./groovy/ groovy/uploadNinds ../nlm-seed/ExternalCDEs/ninds/all/cdes.xml
 
 node ingester/matchToVsac.js
 
-groovy -cp groovy/ groovy/PhenXLoad --merge
+groovy -cp ./groovy/ groovy/PhenXLoad --merge
 
-groovy -cp groovy/ groovy/UploadPhri.groovy
+groovy -cp ./groovy/ groovy/UploadPhri.groovy
 
 # node ingester/uploader fitbir ../nlm-seed/ExternalCDEs/FITBIR-Full.xml
 
