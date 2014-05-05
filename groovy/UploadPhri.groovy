413025dc23fdcc116b8ba05811a61bde82bd9378
Christophe Ludet <ludetc@apples-MacBook-Pro.local>
2014-05-05 10:11:11 -0400

    phri upload

--- groovy/UploadPhri.groovy
+++ groovy/UploadPhri.groovy
@@ -225,7 +225,8 @@ def DBObject ParseRow(XSSFRow row, Map xlsMap) {
     parsePatientStory(stewardClassificationsArray, stewardOrg, xlsMap, row);    
     def classificationArray = [stewardClassification];        
     newDE.put("classification", classificationArray);
-   
+
+    newDE.put("classificationBoost", .3f);    
                             
     BasicDBObject registrationState = new BasicDBObject();
     registrationState.put("registrationStatus", "Recorded");
