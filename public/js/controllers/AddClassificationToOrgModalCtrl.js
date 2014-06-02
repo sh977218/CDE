96e6d4ba07fb5b51b00de11c71af3b53415be8ee
Jakub Flaska <jakub.flaska@nih.gov>
2014-06-02 14:44:28 -0400

    working

--- public/js/controllers/AddClassificationToOrgModalCtrl.js
+++ public/js/controllers/AddClassificationToOrgModalCtrl.js
@@ -2,6 +2,7 @@ function AddClassificationToOrgModalCtrl($scope, $modalInstance, $http, org) {
     $scope.classificationType = "orgClass";
     $scope.org = org;
     $scope.newClassification = { categories: [] };
+    $scope.indices = [];
      
     $scope.okCreate = function (classification) {
       $modalInstance.close(classification);
@@ -11,9 +12,16 @@ function AddClassificationToOrgModalCtrl($scope, $modalInstance, $http, org) {
       $modalInstance.dismiss('cancel');
     };
     
-    $scope.getCategories = function(level, choice) {
+    $scope.getCategories = function(level) {
         var elt = org.classifications;
-        for (var i = 0; i < level; i++) { elt = elt[choice]["elements"]; }
+        for (var i = 0; i < level; i++) { 
+            var choice  = 0;
+            for (var j = 0; j < elt.length; j++) {
+                if (elt[j].name == $scope.newClassification.categories[i]) break;
+                choice++;
+            }
+            elt = elt[choice]["elements"]; 
+        }
         return elt;
     };
 }
