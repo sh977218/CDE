4a22a45a92b3c6b05c625109f60082a632a735c3
Jakub Flaska <jakub.flaska@nih.gov>
2014-06-02 16:59:54 -0400

    nested working good!

--- public/js/controllers/AddClassificationToOrgModalCtrl.js
+++ public/js/controllers/AddClassificationToOrgModalCtrl.js
@@ -11,20 +11,7 @@ function AddClassificationToOrgModalCtrl($scope, $modalInstance, $http, org) {
     $scope.cancelCreate = function () {
       $modalInstance.dismiss('cancel');
     };
-    
-    /*$scope.getCategories = function(level) {
-        var elt = org.classifications;
-        for (var i = 0; i < level; i++) { 
-            var choice  = 0;
-            for (var j = 0; j < elt.length; j++) {
-                if (elt[j].name == $scope.newClassification.categories[i]) break;
-                choice++;
-            }
-            elt = elt[choice]["elements"]; 
-        }
-        return elt;
-    };*/
-    
+
     $scope.getCategories = function(level) {
         var elt = org.classifications;
         for (var i = 0; i < level; i++) { 
@@ -36,8 +23,8 @@ function AddClassificationToOrgModalCtrl($scope, $modalInstance, $http, org) {
         }
         return elt;
     };  
-    $scope.getNumber = function(num) {
-        return new Array(num);   
+    $scope.wipeRest = function(num) {
+        $scope.newClassification.categories.splice(num+1,Number.MAX_SAFE_INTEGER);
     };    
 }
 
