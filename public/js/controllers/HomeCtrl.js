90ae44c26e4e700394f90796ba5527dda8d622d6
Yi Zhao <yi1zhao@gmail.com>
2014-06-03 11:18:51 -0400

    Finished quick search and added 3 test cases.

--- public/js/controllers/HomeCtrl.js
+++ public/js/controllers/HomeCtrl.js
@@ -1,10 +1,54 @@
-function HomeCtrl($scope) {
+function HomeCtrl($scope, $http, $location) {
     $scope.setActiveMenu('HOME');
     
-    $scope.orgList = ['x','y'];
+    $scope.orgList = [];
+    $scope.selectedOrg = "";
+    $scope.ftsearch = "";
+    
     
     $scope.getOrgList = function() {
-        $scope.orgList = ['a','b','c','d'];
+        $http.get("/listorgs").then(function(response) {
+            $scope.orgList = response.data;
+        });
     };
+
+    $scope.resetSelectedOrg = function() {
+        $scope.selectedOrg = "All Organizations";
+    };
+    
+    $scope.updateSelectedOrg = function( neworg ) {
+        $scope.selectedOrg = neworg;
+    };
+    
+    $scope.clearSearch = function() {
+        delete $scope.ftsearch;
+        $scope.resetSelectedOrg();
+    };
+    
+    $scope.gotoSearch = function() {
+        $scope.initCache();
+        
+        if( $scope.selectedOrg !== "All Organizations" ) {
+            $scope.cache.put( "selectedOrg", $scope.selectedOrg );
+        }
+        $scope.cache.put( "ftsearch", $scope.ftsearch );
+
+        $location.url("search");
+    };
+    
+    $scope.submitForm = function( isValid ) {
+        if( isValid ) {
+            $scope.gotoSearch();
+        } else {
+            alert( "Please correct form error(s) and resubmit." );
+        }
+    };
+    
+    // Initialize the selectedOrg
+    $scope.resetSelectedOrg();
+
+    // Retrieves list of organizations from backend
+    $scope.getOrgList();
+    
 
 }
