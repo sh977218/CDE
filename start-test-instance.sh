c952976b580e955da1f5faf953227e354abd1798
unknown <ludetc@NLM01889924OCCS.nlm.nih.gov>
2014-06-13 09:33:44 -0400

    fix number

--- start-test-instance.sh
+++ start-test-instance.sh
@@ -16,7 +16,7 @@ groovy -cp ./groovy/ groovy/grdr test/data/grdr.xlsx
 
 sleep 3;
 
-export target='{"count":582,"_shards":{"total":1,"successful":1,"failed":0}}'
+export target='{"count":592,"_shards":{"total":1,"successful":1,"failed":0}}'
 export curl_res=$(curl http://localhost:9200/cdetest/_count)
 
 if [ "$curl_res" == "$target" ] 
