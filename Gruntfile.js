d23ebf49f5c1c6cede59d8284351fa05acf2a93b
Jakub Flaska <jakub.flaska@nih.gov>
2014-06-13 15:21:53 -0400

    grunt working

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,5 @@
-var config = require('./config.js')
-    /*, elastic = require('./deploy/elasticSearchInit.js')*/;
-
 module.exports = function(grunt) {
+
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json')
         , gitpull: {
@@ -11,19 +9,9 @@ module.exports = function(grunt) {
                 }
             }
         }
-        /*, http: {
-            createIndex: {
-                options: {
-                    url: config.elasticUri
-                    , method: 'POST'
-                    , body: function() {
-                        return JSON.stringify(elastic.creadeIndexJson);
-                    }
-                }
-            }
-        }*/
     });
-    grunt.loadNpmTasks(['grunt-git'/*, 'grunt-http'*/]);
-    grunt.registerTask('default', ['gitpull'/*, 'http'*/]);
-    //grunt.registerTask('elastic', ['http']);
+
+    grunt.loadNpmTasks('grunt-git');
+    grunt.registerTask('default', ['gitpull']);
+
 };
