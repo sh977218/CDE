238fbf00e83971454dc116e76639945f5039d139
unknown <ludetc@NLM01889924OCCS.nlm.nih.gov>
2014-06-18 14:02:02 -0400

    all done

--- public/js/controllers/SaveCdeModalCtrl.js
+++ public/js/controllers/SaveCdeModalCtrl.js
@@ -3,6 +3,20 @@ var SaveCdeModalCtrl = function ($scope, $window, $modalInstance, cde, user) {
   $scope.user = user;
 
   $scope.stewardRegStatuses = ['Incomplete', 'Candidate', 'Recorded', 'Qualified', 'Retired'];
+  
+  $scope.helpMessages = {
+      Incomplete: "Incomplete indicates a Data Element that is not fully defined. Incomplete CDEs are not visible to the public"
+      , 'Candidate':  "Candidate CDEs are not visible to the public"
+      , Recorded: "Recorded CDEs are visible to the public"
+      , Qualified: "Qualified CDEs should be well defined and will be visible to the public"
+      , Retired: "Retired Data Elements are not returned in searches"
+      , Standard : "Standard CDEs cannot be edited by their stewards"
+      , "Preferred Standard" : "Preferred Standard CDEs cannot be edited by their stewards"
+  };
+  
+  $scope.setHelpMessage = function() {
+      $scope.helpMessage = $scope.helpMessages[$scope.cde.registrationState.registrationStatus];
+  };
 
   $scope.ok = function () {
     $scope.cde.$save(function (newcde) {
