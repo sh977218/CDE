975fc5bc84f83c22ae0f6fb0c2b419e754a7fe0f
Jakub Flaska <jakub.flaska@nih.gov>
2014-07-16 14:21:05 -0400

    done

--- public/js/controllers/DEListCtrl.js
+++ public/js/controllers/DEListCtrl.js
@@ -3,20 +3,7 @@ function DEListCtrl($scope, $http, $modal, Elastic) {
 
     $scope.registrationStatuses = $scope.cache.get("registrationStatuses");
     if ($scope.registrationStatuses === undefined) {
-        $scope.registrationStatuses = [
-            {name: 'Preferred Standard',
-                help: "Preferred Standard elements are managed by the CDE Working Group and described by Meaninful Use terminology. <br/> Preferred Standard elements can only be editied by the CDE Working Group"}
-            , {name: 'Standard'
-                ,help: "Standard elements are managed by the CDE Working Group. Standard elements can only be editied by the CDE Working Group" }
-            , {name: 'Qualified'
-                , help: "Qualified elements are managed by their Stewards and may be eligible to become Standard."}
-            , {name: 'Recorded'
-                , help: "Recorded elements are managed by their Stewards and indicate elements that have not yet been Qualified to become Standard"}
-            , {name: 'Candidate'
-                , help: "Candidate elements are only visible to their Stewards."}
-            , {name: 'Incomplete'
-                , help: "Incomplete elements are only visible to their Stewards and indicate elements that are still in the process of being defined."}
-        ];
+        $scope.registrationStatuses = regStatusShared.statusList;
     }
 
     $scope.resultPerPage = 20;
