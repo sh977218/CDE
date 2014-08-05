fbbab157418a45e2f65d8fc35acfa11900a32e82
Jakub Flaska <jakub.flaska@nih.gov>
2014-08-05 13:49:03 -0400

    no message

--- modules/form/public/js/controllers/CreateFormCtrl.js
+++ modules/form/public/js/controllers/CreateFormCtrl.js
@@ -1,13 +1,13 @@
 function CreateFormCtrl($scope, $http) {
-    $scope.newForm = {};
+    $scope.newForm = {stewardOrg:{}};
     $scope.validationErrors = function() {
         if (!$scope.newForm.designation) {
             return "Please enter a name for the new form.";
         } else if (!$scope.newForm.definition) {
             return "Please enter a definition for the new form.";
-        } /*else if (!$scope.newForm.stewardOrg) {
+        } else if (!$scope.newForm.stewardOrg.name) {
             return "Please select a steward for the new form.";
-        }*/
+        }
         return null;
     };
 }
