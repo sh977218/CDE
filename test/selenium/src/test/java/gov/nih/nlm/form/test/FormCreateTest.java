8076838f64a58e85ae5a605c1ef68cbe3ada447e
Jakub Flaska <jakub.flaska@nih.gov>
2014-08-07 11:45:14 -0400

    no message

--- test/selenium/src/test/java/gov/nih/nlm/form/test/FormCreateTest.java
+++ test/selenium/src/test/java/gov/nih/nlm/form/test/FormCreateTest.java
@@ -3,12 +3,39 @@ package gov.nih.nlm.form.test;
 import gov.nih.nlm.cde.test.NlmCdeBaseTest;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.Select;
 
 public class FormCreateTest extends NlmCdeBaseTest {
     
+    protected void gotoFormCreate() {
+        findElement(By.linkText("Create")).click();
+        findElement(By.linkText("Form")).click();    
+    }
+    
+    protected void fillInput(String type, String value) {
+        findElement(By.xpath("//label[text()=\""+type+"\"]/following-sibling::input")).sendKeys(value); 
+    }   
+    
+    protected void gotoPublicForms() {
+        findElement(By.linkText("Forms")).click();    
+    }
+    
     @Test
-    public void addRemoveProperty() {
+    public void createForm() {
         mustBeLoggedInAs(ctepCurator_username, ctepCurator_password);
-        Assert.assertTrue( true );
+        gotoFormCreate();
+        Assert.assertTrue(textPresent("Create New Form"));
+        fillInput("Name", "First Form");
+        fillInput("Definition", "Fill out carefully!");
+        fillInput("Version", "0.1alpha");
+        new Select(findElement(By.id("newForm.stewardOrg.name"))).selectByVisibleText("CTEP");
+        findElement(By.xpath("//button[text()='Save']")).click();
+        hangon(1);
+        
+        gotoPublicForms();        
+        Assert.assertTrue(textPresent("First Form"));
+        Assert.assertTrue(textPresent("Fill out carefully!"));
+        Assert.assertTrue(textPresent("0.1alpha"));        
     }
 }
