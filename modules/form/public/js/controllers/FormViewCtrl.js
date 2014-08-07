189c8b7001a352873a3528709f81cc21df240579
Jakub Flaska <jakub.flaska@nih.gov>
2014-08-07 16:10:40 -0400

    no message

--- modules/form/public/js/controllers/FormViewCtrl.js
+++ modules/form/public/js/controllers/FormViewCtrl.js
@@ -1,5 +1,5 @@
 function FormViewCtrl($scope, $routeParams, $http) {
-    $scope.form = {_id: $routeParams._id, name: "123"};
+    $scope.form = {_id: $routeParams._id};
     
     $http.get("/viewForm?_id=" + $scope.form._id).success(function(form) {
         $scope.form = form;
