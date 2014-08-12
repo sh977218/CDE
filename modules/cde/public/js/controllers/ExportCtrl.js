76270fe398401b59bfa5060c54c242cd82075081
Jakub Flaska <jakub.flaska@nih.gov>
2014-08-12 15:15:15 -0400

    - Fixed missing column names.
    - Fixed mismatching order of columns.

--- modules/cde/public/js/controllers/ExportCtrl.js
+++ modules/cde/public/js/controllers/ExportCtrl.js
@@ -15,12 +15,17 @@ function ExportCtrl($scope, Elastic, CsvDownload) {
             , {field: 'permissibleValues', displayName: 'Permissible Values', width: 200}            
             , {field: 'origin', displayName: 'Origin', width: 60}
             , {field: 'version', displayName: 'Version', width: 40}
+            , {field: 'uuid', displayName: 'NLM ID', width: 100}            
             , {field: 'ids', displayName: 'IDs', width: 100}
         ]
     };
-     
+
+    $scope.columnNames = function() {
+        return $scope.gridOptions.columnDefs.map(function(column) {return column.displayName;}).join(", ") + "\n";
+    };
+    
     $scope.exportStr = function() {
-        $scope.encodedStr = "data:text/csv;charset=utf-8," + escape(CsvDownload.export($scope.gridCdes));
+        $scope.encodedStr = "data:text/csv;charset=utf-8," + escape($scope.columnNames() + CsvDownload.export($scope.gridCdes));
     };
 
     Elastic.buildElasticQueryPre($scope);
@@ -36,15 +41,15 @@ function ExportCtrl($scope, Elastic, CsvDownload) {
                 var cde = list[i];
                 var thisCde = 
                 {
-                    uuid: cde.uuid
-                    , version: cde.version
-                    , primaryNameCopy: cde.naming[0].designation
+                    primaryNameCopy: cde.naming[0].designation
                     , primaryDefinitionCopy: cde.naming[0].definition
                     , stewardOrg: cde.stewardOrg.name
-                    , origin: cde.origin
                     , registrationStatus: cde.registrationState.registrationStatus
                     , naming: cde.naming.slice(1,100).map(function(naming) {return naming.designation;}).join(", ")
-                    , permissibleValues: cde.valueDomain.permissibleValues.map(function(pv) {return pv.permissibleValue;}).join(", ")
+                    , permissibleValues: cde.valueDomain.permissibleValues.map(function(pv) {return pv.permissibleValue;}).join(", ")                                 
+                    , origin: cde.origin
+                    , version: cde.version       
+                    , uuid: cde.uuid
                 };              
                 var ids = "";
                 for (var j = 0; j < cde.ids.length; j++) {
