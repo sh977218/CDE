ee8f7a8a7f39d16e2c3344189f972088e7c75f05
Jakub Flaska <jakub.flaska@nih.gov>
2014-09-05 16:11:21 -0400

    x

--- modules/system/node-js/classificationNode.js
+++ modules/system/node-js/classificationNode.js
@@ -7,33 +7,6 @@ var mongo_data_cde = require('../../cde/node-js/mongo-cde')
 
 var classification = this;
 
-//this.classifyElement = function(err, cde) {
-//    var steward = classificationShared.findSteward(cde, body.orgName);
-//    if (!steward) {
-//        cde.classification.push({
-//            stewardOrg: {
-//                name: body.orgName
-//            }
-//            , elements: []
-//        });
-//        steward = classificationShared.findSteward(cde, body.orgName);
-//    }
-//
-//    if( !(body.categories instanceof Array) ) {
-//        body.categories = [body.categories];
-//    }
-//
-//    if (action === classificationShared.actions.create) classificationShared.addCategory(steward.object, body.categories, classification.cdeClassification.saveCdeClassif);
-//    if (action === classificationShared.actions.delete) {
-//        classificationShared.modifyCategory(steward.object, body.categories, {type:"delete"}, classification.cdeClassification.saveCdeClassif);
-//
-//        // Delete the organization from classificaiton if organization doesn't have any descendant elements.
-//        if( steward.object.elements.length === 0 ) {
-//            classificationShared.removeClassification( cde, body.orgName );
-//        }
-//    }
-//};
-
 exports.cdeClassification = function(body, action, cb) {
     this.saveCdeClassif = function(err, cde) {   
         if (err) {
