fc6f59f719efe9688760d85bdd280e1784b17b3d
Jakub Flaska <jakub.flaska@nih.gov>
2014-09-19 16:03:47 -0400

    x

--- test/selenium/src/test/java/gov/nih/nlm/cde/test/ClassificationTest.java
+++ test/selenium/src/test/java/gov/nih/nlm/cde/test/ClassificationTest.java
@@ -10,7 +10,7 @@ import org.testng.annotations.Test;
 public class ClassificationTest extends NlmCdeBaseTest {  
 
     
-    @Test
+    /*@Test
     public void addClassification() {
         mustBeLoggedInAs("classificationMgtUser", "pass");
         goToCdeByName("Surgical Procedure Other Anatomic Site Performed Indicator");
@@ -91,6 +91,28 @@ public class ClassificationTest extends NlmCdeBaseTest {
         addClassificationMethod(new String[]{"NINDS","Disease"});
         List<WebElement> linkList = driver.findElements(By.cssSelector("li[id$='Disease']"));
         Assert.assertTrue(linkList.size() == 1);
-    }
+    }*/
+    
+    @Test
+    public void classifyEntireSearch() {
+        mustBeLoggedInAs(ninds_username, ninds_password);
+        goToCdeSearch();
+        findElement(By.id("li-blank-AECC")).click();
+        textPresent("NCI Standard Template CDEs (7)");
+        findElement(By.id("classifyAll")).click();
+        findElement(By.xpath("//span[text()='Population']")).click();
+        findElement(By.xpath("//div[@id='addClassification-Adult']//button")).click();
+        textPresent("Search result classified");
+        goToCdeByName("Noncompliant Reason Text");
+        findElement(By.linkText("Classification")).click();
+        textPresent("NINDS");
+        textPresent("Population");
+        textPresent("Adult");
+        goToCdeByName("Adverse Event Ongoing Event Indicator");
+        findElement(By.linkText("Classification")).click();
+        textPresent("NINDS");
+        textPresent("Population");
+        textPresent("Adult");        
+    }    
 
 }
