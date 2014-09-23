acd4e4261e860bcd4628bd878dbfe5767aff4d03
unknown <ludetc@NLM01889924OCCS.nlm.nih.gov>
2014-09-23 12:52:42 -0400

    increase lockout

--- modules/system/node-js/authentication.js
+++ modules/system/node-js/authentication.js
@@ -125,7 +125,7 @@ exports.authBeforeVsac = function(req, username, password, done) {
                     }
                 });
             } else { // If user was found in local datastore and password != 'umls'
-                if (user.lockCounter === 8) {
+                if (user.lockCounter === 30) {
                     return done(null, false, { message: 'User is locked out' }); 
                 } else if (user.password !== password) {
                     // Initialize the lockCounter if it hasn't been
