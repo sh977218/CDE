af82abd2fd055fd1a7630850069822810dc7c93f
Jakub Flaska <jakub.flaska@nih.gov>
2014-09-30 14:21:48 -0400

    x

--- modules/cde/node-js/app.js
+++ modules/cde/node-js/app.js
@@ -462,11 +462,4 @@ exports.init = function(app, daoManager) {
     
     app.get('/status/cde', status.status);
     
-    app.post('/pinEntireSearchToBoard', function(req, res) {
-        if (req.isAuthenticated()) {
-            usersvc.pinToBoard(req, res);
-        } else {
-            res.send("Please login first.");
-        }      
-    });    
 };
