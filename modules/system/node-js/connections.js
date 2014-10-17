5ecc9ee04ed6977fb264ff98158e895e5d82c55f
unknown <ludetc@NLM01889924OCCS.nlm.nih.gov>
2014-10-17 09:17:29 -0400

    add missing file

---
+++ modules/system/node-js/connections.js
@@ -0,0 +1,30 @@
+var mongoose = require('mongoose');
+
+var connectionEstablisher = function(uri, type) {
+    this.uri = uri;
+    var _type = type;
+    var conn = mongoose.createConnection(uri);
+    this.connect = function (openCb) {
+        conn.once('open', function () {
+            console.log('MongoDB ' + _type + ' connection open');
+            openCb(conn);
+        });
+        conn.on('error', function (error) {
+            console.error('Error in MongoDb ' + _type + ' connection: ' + error);
+            mongoose.disconnect();
+        });
+        conn.on('reconnected', function () {
+            console.log('MongoDB ' + _type + ' reconnected!');
+        });
+        conn.on('disconnected', function () {
+            console.log('MongoDB ' + _type + ' disconnected!, reconnecting in 10 seconds');
+            setTimeout(function() {
+                conn = mongoose.createConnection(uri);
+                connect(openCb);
+            }, 10 * 1000);
+        });
+    };
+    var connect = this.connect;
+};
+
+exports.connectionEstablisher = connectionEstablisher;
