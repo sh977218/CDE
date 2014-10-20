ac171ecb45716168517e0b2f924c3c89f21f3360
Jakub Flaska <jakub.flaska@nih.gov>
2014-10-20 14:44:58 -0400

    x

---
+++ test/selenium/src/test/java/gov/nih/nlm/cde/common/test/SearchTest.java
@@ -0,0 +1,24 @@
+package gov.nih.nlm.cde.common.test;
+
+import gov.nih.nlm.cde.test.NlmCdeBaseTest;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class SearchTest extends NlmCdeBaseTest {
+
+    @Test
+    public void saveSearchState() {
+        goToCdeSearch();
+        findElement(By.xpath("//i[@id=\"li-blank-CTEP\"]")).click();
+        findElement(By.xpath("//small[text()=\" CATEGORY (50)\"]")).click();
+        findElement(By.xpath("//i[@id=\"li-blank-Qualified\"]")).click();
+        findElement(By.name("ftsearch")).sendKeys("name");
+        findElement(By.id("search.submit")).click();     
+        textPresent("4 results for CTEP : CATEGORY | name | Qualified");
+        findElement(By.linkText("Forms")).click();     
+        textNotPresent("CATEGORY");
+        findElement(By.linkText("CDEs")).click();     
+        textPresent("4 results for CTEP : CATEGORY | name | Qualified");
+    }
+    
+}
