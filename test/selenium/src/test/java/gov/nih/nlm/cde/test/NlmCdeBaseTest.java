616cceb25c16fbeef07dafca8659d1e9fca00aab
unknown <ludetc@NLM01889924OCCS.nlm.nih.gov>
2014-11-05 10:02:50 -0500

    test done

--- test/selenium/src/test/java/gov/nih/nlm/cde/test/NlmCdeBaseTest.java
+++ test/selenium/src/test/java/gov/nih/nlm/cde/test/NlmCdeBaseTest.java
@@ -505,25 +505,6 @@ public class NlmCdeBaseTest {
         findElement(By.xpath("//label[text()=\"" + type + "\"]/following-sibling::input")).sendKeys(value);
     }
 
-    protected void gotoFormCreate() {
-        findElement(By.linkText("Create")).click();
-        findElement(By.linkText("Form")).click();
-    }
-
-    protected void createForm(String name, String definition, String version, String org) {
-        gotoFormCreate();
-        Assert.assertTrue(textPresent("Create New Form"));
-        fillInput("Name", name);
-        fillInput("Description", definition);
-        if (version != null) {
-            fillInput("Version", version);
-        }
-        new Select(findElement(By.id("newForm.stewardOrg.name"))).selectByVisibleText(org);
-        findElement(By.xpath("//button[text()='Save']")).click();
-        textPresent("Form created");
-        closeAlert();
-        hangon(1);
-    }
 
     protected void showSearchFilters() {
         WebElement showHideFilterButton = findElement(By.id("showHideFilters"));
--- test/selenium/src/test/java/gov/nih/nlm/form/test/BaseFormTest.java
+++ test/selenium/src/test/java/gov/nih/nlm/form/test/BaseFormTest.java
@@ -2,6 +2,7 @@ package gov.nih.nlm.form.test;
 
 import gov.nih.nlm.cde.test.NlmCdeBaseTest;
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 
 public class BaseFormTest extends NlmCdeBaseTest {
@@ -25,4 +26,24 @@ public class BaseFormTest extends NlmCdeBaseTest {
         findElement(By.name("ftsearch")).sendKeys("\"" + query + "\"");
         findElement(By.id("search.submit")).click();    
     }
+        protected void gotoFormCreate() {
+        findElement(By.linkText("Create")).click();
+        findElement(By.linkText("Form")).click();
+    }
+
+    public void createForm(String name, String definition, String version, String org) {
+        gotoFormCreate();
+        Assert.assertTrue(textPresent("Create New Form"));
+        fillInput("Name", name);
+        fillInput("Description", definition);
+        if (version != null) {
+            fillInput("Version", version);
+        }
+        new Select(findElement(By.id("newForm.stewardOrg.name"))).selectByVisibleText(org);
+        findElement(By.xpath("//button[text()='Save']")).click();
+        textPresent("Form created");
+        closeAlert();
+        hangon(1);
+    }
+    
 }
