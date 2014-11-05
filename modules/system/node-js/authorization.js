faf515b27a8277dd419863b8ebe48c697c15844e
Yi Zhao <yi1zhao@gmail.com>
2014-11-04 19:09:33 -0500

    Refactored code to authorization.js

---
+++ modules/system/node-js/authorization.js
@@ -0,0 +1,30 @@
+function isCuratorOrAdmin(req, elt) {
+    return (req.user.orgAdmin && req.user.orgAdmin.indexOf(elt.stewardOrg.name) < 0)
+           || (req.user.orgCurator && req.user.orgCurator.indexOf(elt.stewardOrg.name) < 0);
+};
+    
+exports.checkOwnership = function(dao, id, req, cb) {
+    if (req.isAuthenticated()) {
+        dao.byId(id, function (err, elt) {
+            if (err || !elt) {
+                return cb("Element does not exist.", null);
+            }
+            if (!req.user || !isCuratorOrAdmin(req, elt)
+               ) {
+                return cb("You do not own this element.", null);
+            } else {
+                cb(null, elt);
+            }
+        });
+    } else {
+        return cb("You are not authorized.", null);                   
+    }
+};
+
+exports.isOrgOrSiteAdmin = function(req) {
+    if(req.isAuthenticated() && (req.user.siteAdmin || req.user.orgAdmin.indexOf(req.body.org)>=0)) {
+        return true;
+    }
+    
+    return false;
+};
