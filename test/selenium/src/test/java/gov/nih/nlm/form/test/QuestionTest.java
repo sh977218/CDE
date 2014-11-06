9d57a5c4acaea3cefba3e5ba0fb856fa121f9166
Jakub Flaska <jakub.flaska@nih.gov>
2014-11-06 15:12:57 -0500

    x

--- test/selenium/src/test/java/gov/nih/nlm/form/test/QuestionTest.java
+++ test/selenium/src/test/java/gov/nih/nlm/form/test/QuestionTest.java
@@ -299,6 +299,9 @@ public class QuestionTest extends BaseFormTest {
         findElement(By.id("formPreview")).click();
         switchTab(1);
         findElement(By.xpath("//div[@id=\"formRenderSection_Medical History\"]//div[@id=\"formRenderSection_Treatment Details\"]//label[text()='First-Line Therapy Chemotherapy Regimen Name']"));        
+        Assert.assertTrue(driver.findElements(By.xpath("//*[text()=\"Treatment Details\"]")).size() == 1); 
+        findElement(By.xpath("//*[text()=\" Add One\"]").click());
+        Assert.assertTrue(driver.findElements(By.xpath("//*[text()=\"Treatment Details\"]")).size() == 2);              
         
         resizeWindow(currentWindowSize.getWidth(), currentWindowSize.getHeight());        
     }
