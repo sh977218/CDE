204c4bbe925f16a1dc7168ce9d707d74cd04ed39
Yi Zhao <yi1zhao@gmail.com>
2014-11-10 17:32:38 -0500

    fix

--- test/selenium/src/test/java/gov/nih/nlm/form/test/QuestionTest.java
+++ test/selenium/src/test/java/gov/nih/nlm/form/test/QuestionTest.java
@@ -264,7 +264,7 @@ public class QuestionTest extends BaseFormTest {
     @Test
     public void sectionInSection() {
         Dimension currentWindowSize = getWindowSize();
-        resizeWindow(1024, 1150);        
+        resizeWindow(1524, 1150);        
         mustBeLoggedInAs(ctepCurator_username, password);
         String formName = "Cancer Patient Data Collection";
         String formDef = "Section in Section";
