d1f773dc3303d70edcd9d8ccd8efc4dd56a443e2
unknown <ludetc@NLM01889924OCCS.nlm.nih.gov>
2014-11-14 14:22:41 -0500

    forms loader done

--- ingester/nindsFormUpload.js
+++ ingester/nindsFormUpload.js
@@ -17,10 +17,7 @@ var mongoUri = config.mongoUri;
 var conn = mongoose.createConnection(mongoUri);
 conn.on('error', console.error.bind(console, 'connection error:'));
 conn.once('open', function callback () {
-
-    console.log('mongodb connection open');
-    
-
+    console.log('connected to ' + mongoUri);
 });    
 
 // Global variables
@@ -32,7 +29,7 @@ var Form = conn.model('Form', form_schemas.formSchema);
 var DataElement = conn.model('DataElement', cde_schemas.dataElementSchema);
 
 var i = 0;
-var doNext = function(result) {
+var doNextForm = function(result) {
     
     if (i === result.dataStructureExport.dataStructure.length) {
         process.exit();
@@ -58,56 +55,67 @@ var doNext = function(result) {
                 , formElements: []
             }
         ]
+        , classification: [
+            {stewardOrg: {name: "NINDS"}, elements: [{name: "Disease", elements: []}]}
+        ]
     });
-
-    async.each(origFormElt.repeatableGroups[0].mapElements,
-        function(elt, cb){
-            DataElement.find()
-                .where("properties").elemMatch(function(elem) {
-                    elem.where("key").equals("NINDS Variable Name").where("value").equals(new RegExp("^" + elt.dataElement[0].name[0]));
-                })
-                .exec(function(err, result) {
-                    if (result.length > 0) {
-                        newForm.formElements[0].formElements.push({
-                            elementType: "question"
-                            , label: result[0].naming[0].designation
-                            , cardinality: "0.1"
-                            , question: {
-                                cde: {tinyId: result[0].tinyId, version: "" + result[0].version}
-                            }
-                        });
-                    } else {
-                        console.log("CDE: " + elt.dataElement[0].name[0] + " not found");
-                    }
-                    cb();
+    
+    var j = 0;
+    var doNextQuestion = function (elts) {
+        if (j === elts.length) {
+            // Add Classifications
+            for (var h = 0; h < origFormElt.diseaseList[0].disease.length; h++) {
+                newForm.classification[0].elements[0].elements.push({
+                    name: origFormElt.diseaseList[0].disease[h].name[0]
                 });
-        },
-        function(err){
-            if (err) {
-                console.log("ERROR: " + err);
-                console.log("not saving: " + newForm.naming[0].designation);
-            } else {
-                newForm.save(function(err) {
-                    if (err) {
-                        console.log("ERROR: " + err);
-                        console.log("could not save: " + newForm.naming[0].designation);
-                    } else {
-                        console.log("saved " + newForm.naming[0].designation)
-                    }
-                    i++;
-                    doNext(result);
-                });                    
             }
+            
+            //  Copyright
+            newForm.isCopyrighted = (origFormElt.isCopyrighted[0] == "true")
+                
+            newForm.save(function(err) {
+                if (err) {
+                    console.log("ERROR: " + err);
+                    console.log("could not save: " + newForm.naming[0].designation);
+                } else {
+                    console.log("saved " + newForm.naming[0].designation);
+                }
+                i++;
+                doNextForm(result);
+            });                
+        } else {
+            DataElement.find()
+                .where("ids").elemMatch(function (elem) {
+                elem.where("source").equals("NINDS Variable Name").where("id").equals(elts[j].dataElement[0].name[0]);
+            }).exec(function (err, result) {
+                if (result.length > 0) {
+                    newForm.formElements[0].formElements.push({
+                        elementType: "question"
+                        , label: result[0].naming[0].designation
+                        , cardinality: "0.1"
+                        , question: {
+                            cde: {tinyId: result[0].tinyId, version: "" + result[0].version}
+                        }
+                    });
+                } else {
+                    console.log("CDE: " + elt.dataElement[0].name[0] + " not found");
+                }
+                j++;
+                doNextQuestion(elts);
+            });
         }
-    );
+    };
 
-}
+    doNextQuestion(origFormElt.repeatableGroups[0].mapElements);
+};
+
+console.log("Parsing " + process.argv[2]);
 
 fs.readFile(process.argv[2], function(err, data) {
     parser.parseString(data, function (err, result) {
 //        for (var i = 0; i < result.dataStructureExport.dataStructure.length; i++) {
 //        for (var i = 0; i < 2; i++) {
-          doNext(result);  
+          doNextForm(result);  
     });
 });
  
