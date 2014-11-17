ab796fc0f8c1f80ef7ab97aa268ae80f0a5f3888
unknown <ludetc@NLM01889924OCCS.nlm.nih.gov>
2014-11-17 15:08:17 -0500

    split tests

--- test/selenium/src/test/java/gov/nih/nlm/cde/test/FacetSearchTest.java
+++ test/selenium/src/test/java/gov/nih/nlm/cde/test/FacetSearchTest.java
@@ -12,7 +12,7 @@ import org.testng.annotations.Test;
 
 public class FacetSearchTest extends NlmCdeBaseTest {
 
-    private void clickIfDisplayed(String id) {
+    public void clickIfDisplayed(String id) {
         List<WebElement> elts = driver.findElements(By.id(id));
         for (int i = 0; i < elts.size(); i++) {
             if (elts.get(i).isDisplayed()) {
@@ -158,119 +158,4 @@ public class FacetSearchTest extends NlmCdeBaseTest {
         textPresent("Noncompliant Reason Text");
     }
     
-    @Test
-    public void ownerAndAdminCanSeeLowStatus() {
-        mustBeLoggedInAs(ctepCurator_username, password);
-        String cdeName = "Low Status Cde";
-        new CdeCreateTest().createBasicCde(cdeName, "Low Stat Definition", "0.1", "CTEP", "DISEASE", "Lung");
-        goToCdeSearch();
-        findElement(By.id("li-blank-Incomplete")).click();
-        textPresent(cdeName);
-        
-        mustBeLoggedInAs(cabigAdmin_username, password);
-        goToCdeSearch();
-        hangon(1);
-        if (textPresentTrueFalse("Incomplete (")) {
-            findElement(By.id("li-blank-Incomplete")).click();
-            textNotPresent(cdeName);
-        }
-        
-        mustBeLoggedInAs(nlm_username, nlm_password);
-        goToCdeSearch();
-        findElement(By.id("li-blank-Incomplete")).click();
-        textPresent(cdeName);
-
-        mustBeLoggedInAs(ctepCurator_username, password);
-        goToCdeByName(cdeName);
-        findElement(By.id("editStatus")).click();
-        modalHere();
-        new Select(driver.findElement(By.name("registrationStatus"))).selectByVisibleText("Candidate");
-        findElement(By.id("saveRegStatus")).click();
-        hangon(3);
-
-        goToCdeSearch();
-        findElement(By.id("li-blank-Candidate")).click();
-        textPresent(cdeName);
-        
-        mustBeLoggedInAs(cabigAdmin_username, password);
-        goToCdeSearch();
-        hangon(1);
-        if (textPresentTrueFalse("Candidate (")) {
-            findElement(By.id("li-blank-Candidate")).click();
-            textNotPresent(cdeName);
-        }
-        
-        mustBeLoggedInAs(nlm_username, nlm_password);
-        goToCdeSearch();
-        findElement(By.id("ftsearch-input")).sendKeys(cdeName);
-        findElement(By.cssSelector("i.fa-search")).click();
-        hangon(1);
-        findElement(By.id("li-blank-Candidate")).click();
-        textPresent(cdeName);
-        
-    }
-    
-    @Test
-    public void infoBarClassification() {
-        goToCdeSearch();
-        findElement(By.id("resetSearch")).click();
-        hangon(.5);
-        findElement(By.id("li-blank-NINDS")).click();
-        findElement(By.id("li-blank-Disease")).click();
-        findElement(By.id("li-blank-Amyotrophic Lateral Sclerosis")).click();
-        clickIfDisplayed("li-blank-Classification");
-        clickIfDisplayed("li-blank-Core");
-        textPresent( "NINDS : Disease : Amyotrophic Lateral Sclerosis : Classification : Core" );
-        findElement(By.id("resetSearch")).click();
-        textPresent( "All Classifications" );
-    }
-    
-    @Test
-    public void infoBarStatus() {
-        goToCdeSearch();
-        findElement(By.id("resetSearch")).click();
-        hangon(2);
-        findElement(By.id("li-blank-Qualified")).click();
-        textPresent("| Qualified");
-        findElement(By.id("li-blank-Recorded")).click();
-        textPresent( "Qualified, Recorded" );
-        scrollToTop();
-        findElement(By.id("resetSearch")).click();
-        textPresent( "All Status" );
-    }
-    
-        
-    @Test
-    public void infoBarTerms() {
-        goToCdeSearch();
-        findElement(By.id("resetSearch")).click();
-        findElement(By.name("ftsearch")).sendKeys("blah blah blah");
-        findElement(By.cssSelector("i.fa-search")).click();
-        hangon(.5);
-        scrollToTop();
-        textPresent( "blah blah blah" );
-        findElement(By.id("resetSearch")).click();
-        textPresent( "All Terms" );
-    }
-    
-    @Test
-    public void hoverOverClassifications() {
-        goToCdeSearch();
-        hoverOverElement(findElement(By.linkText("CDEs")));
-        textNotPresent("Albert Einstein Cancer Center");
-        hoverOverElement(findElement(By.id("classifications-text-AECC")));
-        textPresent("Albert Einstein Cancer Center");
-        hoverOverElement(findElement(By.id("classifications-text-caBIG")));
-        textNotPresent("Albert Einstein Cancer Center");
-    }
-    
-    @Test
-    public void lowStatusFilter() {
-        mustBeLoggedInAs(acrin_username, password);
-        goToCdeSearch();
-        textPresent("ACRIN (3)");
-        logout();
-        goToCdeSearch();
-        textNotPresent("ACRIN (3");
-    }
 }
