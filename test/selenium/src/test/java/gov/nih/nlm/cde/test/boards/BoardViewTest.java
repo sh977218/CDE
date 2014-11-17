71b37eeab7b861bf2545a32ee6937663887ddb79
subramani <subramani@mail.nih.gov>
2014-11-17 13:23:26 -0500

    CDE-63 Gridview link from board test case

---
+++ test/selenium/src/test/java/gov/nih/nlm/cde/test/boards/BoardViewTest.java
@@ -0,0 +1,41 @@
+package gov.nih.nlm.cde.test.boards;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class BoardViewTest extends BoardTest
+{
+	@Test
+    public void showGridView() {
+		mustBeLoggedInAs(boardUser, password);
+		String boardName = "Test Board";
+		createBoard(boardName, "Test");
+		
+		goToCdeSearch();
+        attachToBoard( "Prostate Cancer American Joint Committee on Cancer (AJCC) Edition 7 Pathologic Regional Lymph Node N Stage" );
+        findElement(By.linkText(boardName)).click();
+        attachToBoard( "Fluorescence in situ Hybridization Anaplastic Lymphoma Kinase Calculation Standard Deviation Value" );
+        findElement(By.linkText(boardName)).click();
+        attachToBoard( "Recurrent Malignant Neoplasm Patient No Cisplatin Interval Month Count" );
+        findElement(By.linkText(boardName)).click();
+        attachToBoard( "Prior BMSCT Administered Indicator" );
+        findElement(By.linkText(boardName)).click();
+        attachToBoard( "Generalized Activities of Daily Living Pain Restricted Scale" );
+        findElement(By.linkText(boardName)).click();
+        
+        goToCdeSearch();
+		
+        goToBoard(boardName);
+        findElement(By.linkText("View Board")).click();
+        findElement(By.id("gridView")).click();
+        Assert.assertEquals(driver.findElements(By.cssSelector("div.ngRow")).size(), 5);
+    }
+	
+	public void attachToBoard(String cdeName)
+	{
+		openCdeInList(cdeName);
+		findElement(By.xpath("//a[@id='pin_0']")).click();
+        hangon(1);
+	}
+}
