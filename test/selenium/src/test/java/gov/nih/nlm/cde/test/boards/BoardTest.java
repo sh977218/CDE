ed06f645145d6772f81b43e5d98ae173fa53d03c
unknown <ludetc@NLM01889924OCCS.nlm.nih.gov>
2014-11-20 13:35:07 -0500

    code done

--- test/selenium/src/test/java/gov/nih/nlm/cde/test/boards/BoardTest.java
+++ test/selenium/src/test/java/gov/nih/nlm/cde/test/boards/BoardTest.java
@@ -2,7 +2,6 @@ package gov.nih.nlm.cde.test.boards;
 
 import gov.nih.nlm.cde.test.NlmCdeBaseTest;
 import org.openqa.selenium.By;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 
 public class BoardTest extends NlmCdeBaseTest {
