39eed552898e0ff948f8cd52e8bbe15bb538d108
Yi Zhao <yi1zhao@gmail.com>
2014-12-18 15:34:29 -0500

    misc changes

--- modules/system/public/js/controllers/ListCtrl.js
+++ modules/system/public/js/controllers/ListCtrl.js
@@ -40,7 +40,7 @@ function ListCtrl($scope, $modal, Elastic, OrgHelpers, $rootScope, $http, screen
         $scope.currentSearchTerm = $scope.searchForm.ftsearch;   
     }
     
-//    $scope.altClassificationFilterMode = false;
+    $scope.altClassificationFilterMode = false;
     $scope.selectedOrg = $scope.cache.get($scope.getCacheName("selectedOrg"));    
     $scope.selectedElements = $scope.cache.get($scope.getCacheName("selectedElements"));
     if (!$scope.selectedElements) {
