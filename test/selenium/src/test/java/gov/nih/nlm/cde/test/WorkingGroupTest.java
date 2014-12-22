60866603c1f8219bc8f93161291b4ce2fb80bbf4
Jakub Flaska <jakub.flaska@nih.gov>
2014-12-22 17:16:42 -0500

    x

--- test/selenium/src/test/java/gov/nih/nlm/cde/test/WorkingGroupTest.java
+++ test/selenium/src/test/java/gov/nih/nlm/cde/test/WorkingGroupTest.java
@@ -140,6 +140,30 @@ public class WorkingGroupTest extends NlmCdeBaseTest {
         goToCdeSearch();        
         textPresent("NINDS-WG-1");
         textPresent("NINDS-WG-2");        
+        
+        //DeView Wg1 sees Wg2
+        mustBeLoggedInAs("nindsWg1User", "pass");
+        goToCdeByName("Urinary tract surgical procedure indicator");    
+        findElement(By.linkText("Classification")).click();
+        textPresent("NINDS-WG-2");
+        textPresent("WG2 Classif");
+        textPresent("WG2 Sub Classif");
+        
+        //DeView Ctep cannot see Wg2
+        mustBeLoggedInAs("ctepCurator", "pass"); 
+        goToCdeByName("Urinary tract surgical procedure indicator");    
+        findElement(By.linkText("Classification")).click();
+        textNotPresent("NINDS-WG-2");
+        textNotPresent("WG2 Classif");
+        textNotPresent("WG2 Sub Classif");   
+        
+        //DeView Anon cannot see Wg2
+        logout();
+        goToCdeByName("Urinary tract surgical procedure indicator");    
+        findElement(By.linkText("Classification")).click();
+        textNotPresent("NINDS-WG-2");
+        textNotPresent("WG2 Classif");
+        textNotPresent("WG2 Sub Classif");         
 
     }       
 }
