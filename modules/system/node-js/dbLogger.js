0320ea087fe94162a104963cb759f6d27257cd37
Jakub Flaska <jakub.flaska@nih.gov>
2015-01-20 11:39:37 -0500

    x

--- modules/system/node-js/dbLogger.js
+++ modules/system/node-js/dbLogger.js
@@ -59,17 +59,6 @@ exports.getLogs = function(inQuery, callback) {
     });
 };
 
-//exports.usageByDay = function(callback) {
-//    LogModel.aggregate(
-//        {$match: {date: {$exists: true}}},
-//        {$group : {_id: {ip: "$remoteAddr", daysAgo: {$subtract: [{$dayOfYear: new Date()}, {$dayOfYear: "$date"}]}}, number: {$sum: 1}}},
-//        {$sort: {"_id.daysAgo": 1}}
-//        , function (err, result) {
-//            callback(result);
-//        }
-//    );
-//};
-
 exports.usageByDay = function(callback) {
     LogModel.aggregate(
         {$match: {date: {$exists: true}}}
@@ -80,15 +69,4 @@ exports.usageByDay = function(callback) {
     );
 };
 
-//exports.usageByDay = function(callback) {
-//    LogModel.aggregate(
-//        {$match: {date: {$exists: true}}}
-//        , {$group : {_id: {ip: "$remoteAddr", daysAgo: {$subtract: [new Date(),"$date"]}}, number: {$sum: 1}}}
-//        , {$sort: {"_id.daysAgo": 1}}
-//        , function (err, result) {
-//            callback(result);
-//        }
-//    );
-//};
-    
 
