3e7a9cf8112a140f6b1ff8e912c5f283d2878962
Jakub Flaska <jakub.flaska@nih.gov>
2015-01-20 11:15:31 -0500

    x

--- modules/system/node-js/dbLogger.js
+++ modules/system/node-js/dbLogger.js
@@ -59,16 +59,36 @@ exports.getLogs = function(inQuery, callback) {
     });
 };
 
+//exports.usageByDay = function(callback) {
+//    LogModel.aggregate(
+//        {$match: {date: {$exists: true}}},
+//        {$group : {_id: {ip: "$remoteAddr", daysAgo: {$subtract: [{$dayOfYear: new Date()}, {$dayOfYear: "$date"}]}}, number: {$sum: 1}}},
+//        {$sort: {"_id.daysAgo": 1}}
+//        , function (err, result) {
+//            callback(result);
+//        }
+//    );
+//};
+
 exports.usageByDay = function(callback) {
     LogModel.aggregate(
-        {$match: {date: {$exists: true}}},
-        {$group : {_id: {ip: "$remoteAddr", daysAgo: {$subtract: [{$dayOfYear: new Date()}, {$dayOfYear: "$date"}]}}, number: {$sum: 1}}},
-        {$sort: {"_id.daysAgo": 1}}
+        {$match: {date: {$exists: true}}}
+        , {$group : {_id: {ip: "$remoteAddr", year: {$year: "$date"}, month: {$month: "$date"}, dayOfMonth: {$dayOfMonth: "$date"}}, number: {$sum: 1}}}
         , function (err, result) {
             callback(result);
         }
     );
 };
 
+//exports.usageByDay = function(callback) {
+//    LogModel.aggregate(
+//        {$match: {date: {$exists: true}}}
+//        , {$group : {_id: {ip: "$remoteAddr", daysAgo: {$subtract: [new Date(),"$date"]}}, number: {$sum: 1}}}
+//        , {$sort: {"_id.daysAgo": 1}}
+//        , function (err, result) {
+//            callback(result);
+//        }
+//    );
+//};
     
 
