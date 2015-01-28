15d1b5a42b9a46e048c634ff93f4112de9d50189
Jakub Flaska <jakub.flaska@nih.gov>
2015-01-28 10:56:06 -0500

    x

---
+++ modules/system/shared/authorizationShared.js
@@ -0,0 +1,14 @@
+if (typeof(exports)==="undefined") exports = {};
+
+exports.hasRole = function(user, role) {
+    if (!user) return false;
+    if (user.siteAdmin) return true;
+    if (user.roles && user.roles.indexOf(role) > -1) return true;
+};
+
+exports.isCuratorOf = function(user, orgName) {
+    if (!user) return false;
+    if (user.siteAdmin) return true;
+    return (req.user.orgAdmin && req.user.orgAdmin.indexOf(orgName) < 0)
+        || (req.user.orgCurator && req.user.orgCurator.indexOf(orgName) < 0);
+};
