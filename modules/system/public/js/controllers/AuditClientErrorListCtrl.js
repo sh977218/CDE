089e0dcba076f5ce9f8b7824b3de70372d355fe9
Jakub Flaska <jakub.flaska@nih.gov>
2015-02-04 16:08:31 -0500

    x

--- modules/system/public/js/controllers/AuditClientErrorListCtrl.js
+++ modules/system/public/js/controllers/AuditClientErrorListCtrl.js
@@ -1,5 +1,6 @@
 function AuditClientErrorListCtrl($scope, $controller){
     $scope.api = "/getClientErrors"; 
     $scope.errorType = "client";
+    $scope.fields = ["Date", "Name", "Message", "Stack"];
     $controller('AuditErrorListCtrl', {$scope: $scope}); 
 }
