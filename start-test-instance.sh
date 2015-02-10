721217190e798b7c9b8c7e35d3f013beaa73ae18
unknown <ludetc@NLM01889924OCCS.nlm.nih.gov>
2015-02-10 16:42:54 -0500

    fix build file

--- start-test-instance.sh
+++ start-test-instance.sh
@@ -3,7 +3,7 @@
 # Location of node. For dev testing use '.'  for prod testing use 'build'
 NODE_LOC='.'
 
-gradle -b test/selenium/build.gradle -PtestUrl=http://localhost:3001 -PforkNb=8 -Ptimeout=8 -Pbrowser=chrome clean compileTest &
+gradle -b test/selenium/build.gradle -PhubUrl=any -PtestUrl=any -PforkNb=any -Ptimeout=any -Pbrowser=any clean compileTest &
 
 mongo test deploy/dbInit.js
 
