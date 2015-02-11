a85e78af88e4c8d46dea617a2e14a025f2f89a26
Jakub Flaska <jakub.flaska@nih.gov>
2015-02-11 11:14:02 -0500

    x

--- app.js
+++ app.js
@@ -151,20 +151,6 @@ app.use(function(req, res, next) {
     expressLogger(req, res, next);    
 });
 
-//app.use(function(err, req, res, next){
-//    var meta = {
-//        stack: err.stack
-//        , origin: "app.express.error"
-//        , request: {username: req.user?req.user.username:null, method: req.method, url: req.url, params: req.params, body: req.body}
-//    }; 
-//    logging.errorLogger.error("Error: Express Default Error Handler", meta);
-//    if (err.status === 403) {
-//        res.status(403).send("Unauthorized");
-//    } else {
-//        res.status(500).send('Something broke!');
-//    }
-//});
-
 app.set('views', path.join(__dirname, './modules'));
 
 var originalRender = express.response.render;
