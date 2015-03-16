731b2b1ec2cee7b2d60213d7dc36725f701b786d
Jakub Flaska <jakub.flaska@nih.gov>
2015-03-16 11:10:47 -0400

    x

---
+++ modules/system/public/js/controllers/ApproveAttachmentCtrl.js
@@ -0,0 +1,23 @@
+angular.module('systemModule').controller('ApproveAttachmentCtrl', ['$scope', '$http', 'Mail', '$modal', function($scope, $http, Mail, $modal) {
+    $scope.approveAttachment = function(msg) {
+        $http.post('/attachment/approve', msg.typeAttachmentApproval).
+            success(function(data, status, headers, config) {
+                $scope.addAlert("success", data);
+                $scope.closeMessage(msg);
+            }).
+            error(function(data, status, headers, config) {
+                $scope.addAlert("danger", data);
+            });
+    };
+    $scope.declineAttachment = function(msg) {
+        $http.post('/file/remove', msg.typeAttachmentApproval).
+            success(function(data, status, headers, config) {
+                $scope.addAlert("success", data);
+                $scope.closeMessage(msg);
+            }).
+            error(function(data, status, headers, config) {
+                $scope.addAlert("danger", data);
+            });
+    };    
+        
+}]);
