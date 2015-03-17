913bd00fb6d2d66f908719cbe8352dee7c69e483
unknown <ludetc@NLM01889924OCCS.nlm.nih.gov>
2015-03-17 13:22:29 -0400

    test fixes

--- test/selenium/src/test/java/gov/nih/nlm/cde/test/NlmCdeBaseTest.java
+++ test/selenium/src/test/java/gov/nih/nlm/cde/test/NlmCdeBaseTest.java
@@ -266,7 +266,12 @@ public class NlmCdeBaseTest {
         wait.until(ExpectedConditions.visibilityOfElementLocated(by));
         return driver.findElement(by);
     }
-    
+
+    public void waitAndClick(By by) {
+        wait.until(ExpectedConditions.elementToBeClickable(by));
+        findElement(by).click();
+    }
+
     protected void clickElement(By by) {
         try {
             findElement(by).click();
@@ -317,8 +322,7 @@ public class NlmCdeBaseTest {
         hangon(1);
         findElement(By.name("version")).sendKeys(".1");
         textNotPresent("has already been used");
-        wait.until(ExpectedConditions.elementToBeClickable(By.id("confirmNewVersion")));
-        findElement(By.id("confirmNewVersion")).click();
+        waitAndClick(By.id("confirmNewVersion"));
         try{
             textPresent("Saved.");
         } catch(Exception e){
@@ -497,8 +501,7 @@ public class NlmCdeBaseTest {
         findElement(By.id("uname")).sendKeys(username);
         findElement(By.id("passwd")).clear();
         findElement(By.id("passwd")).sendKeys(password);
-        wait.until(ExpectedConditions.elementToBeClickable(By.id("login_button")));
-        findElement(By.id("login_button")).click();
+        waitAndClick(By.id("login_button"));
         try {
             textPresent(checkText, null, 12);
             // Assumption is that UMLS sometimes throws an error on login. With a socket hangup. login fails, we retry.
@@ -598,7 +601,7 @@ public class NlmCdeBaseTest {
                 wait.until(ExpectedConditions.textToBePresentInElementLocated(By.cssSelector("accordion"), cdeName));
                 break;
             } catch(Exception e){
-                findElement(By.id("older")).click();
+                findElement(By.linkText("Next")).click();
             }            
             
         }
