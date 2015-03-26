45c4a74aa1f2722a27fdc0d08a71bac5b786acad
Jakub Flaska <jakub.flaska@nih.gov>
2015-03-26 14:00:19 -0400

    x

--- modules/cde/public/js/controllers/QuickBoardCtrl.js
+++ modules/cde/public/js/controllers/QuickBoardCtrl.js
@@ -1,35 +1,9 @@
 angular.module('cdeModule').controller('QuickBoardCtrl', ['$scope', 'CdeList', function($scope, CdeList) {
-    
-    $scope.viewType = {
-        accordion : true
-        , grid : false
-        , sidebyside : false
-    };
 
     $scope.cdes = [];
     $scope.qbGridCdes = [];
-    
-    $scope.showAccordionView = function() {
-        $scope.viewType.accordion = true;
-        $scope.viewType.grid = false;
-        $scope.viewType.sidebyside = false;
-    };
 
-    $scope.showGridView = function() {
-        $scope.viewType.accordion = false;
-        $scope.viewType.grid = true;
-        $scope.viewType.sidebyside = false;
-    };
-    
-    $scope.showSideBySideView = function() {
-        if ($scope.cdes.length !== 2) {
-            $scope.addAlert("danger", "You may only compare 2 CDEs side by side.");
-        } else {
-            $scope.viewType.accordion = false;
-            $scope.viewType.grid = false;
-            $scope.viewType.sidebyside = true;      
-        }
-    };
+
     
     $scope.removeDE = function( index ) {
         $scope.cdes.splice(index, 1);
--- modules/system/public/js/controllers/SwitchListViewCtrl.js
+++ modules/system/public/js/controllers/SwitchListViewCtrl.js
@@ -6,8 +6,16 @@ angular.module('systemModule').controller('SwitchListViewCtrl', ['$scope', 'OrgH
     $scope.getUsedBy = OrgHelpers.getUsedBy;
 
     $scope.switchGridAccordionView = function() {
-        if ($scope.listViewType === 'accordion') $scope.listViewType = 'grid';
+        if ($scope.listViewType === 'accordion') $scope.listViewType = 'table';
         else $scope.listViewType = 'accordion';
+    };
+
+    $scope.showSideBySideView = function() {
+        if ($scope.cdes.length !== 2) {
+            $scope.addAlert("danger", "You may only compare 2 CDEs side by side.");
+        } else {
+            $scope.listViewType = "sideBySide";
+        }
     };
 
 }]);
