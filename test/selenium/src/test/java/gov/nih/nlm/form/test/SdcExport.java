0b3643b180d8625f4849cfb49b60e37b4795821b
unknown <ludetc@NLM01960809MLB.nlm.nih.gov>
2015-03-31 15:50:58 -0400

    add test

---
+++ test/selenium/src/test/java/gov/nih/nlm/form/test/SdcExport.java
@@ -0,0 +1,30 @@
+package gov.nih.nlm.form.test;
+
+import gov.nih.nlm.cde.test.NlmCdeBaseTest;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static com.jayway.restassured.RestAssured.get;
+
+public class SdcExport extends NlmCdeBaseTest {
+
+    @Test
+    public void sdcExport() {
+        goToFormByName("Apathy Scale (AS)");
+
+        String url = findElement(By.id("sdcExport")).getAttribute("href");
+
+        System.out.println(url);
+
+        String response = get(url).asString();
+
+        System.out.println(response);
+
+        Assert.assertTrue(response.contains("<sdc:form_package xmlns:sdc=\"http://nlm.nih.gov/sdc/for\" xmlns:mfi13=\"http://www.iso.org/19763/13/2013\"><sdc:form_design><sdc:designation><sdc:Context>SDC Pilot Project</sdc:Context><sdc:sign acceptability=\"preferred\">Apathy Scale (AS)</sdc:sign>"));
+
+
+    }
+
+
+}
