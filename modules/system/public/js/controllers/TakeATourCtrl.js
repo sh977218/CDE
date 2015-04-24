2a61154b9936d04860db06edd469e58a2ba28971
unknown <huangs8@NLM01961050MLB.nlm.nih.gov>
2015-04-24 14:47:41 -0400

    fix 508 tour

--- modules/system/public/js/controllers/TakeATourCtrl.js
+++ modules/system/public/js/controllers/TakeATourCtrl.js
@@ -1,20 +1,20 @@
-angular.module('systemModule').controller('TakeATourCtrl', ['$scope', 'TourContent', function($scope, TourContent) {  
-    
-    TourContent.stop = function() {
+angular.module('systemModule').controller('TakeATourCtrl', ['$scope', 'TourContent', function ($scope, TourContent) {
+
+    TourContent.stop = function () {
         if ($scope.tour)
             $scope.tour.end();
     };
 
-    $scope.setTour = function () {       
-        $scope.tour = new Tour({steps: TourContent.steps});
+    $scope.setTour = function () {
+        $scope.tour = new Tour({template: TourContent.template, steps: TourContent.steps});
         $scope.tour.init();
-        $scope.tour.restart();    
+        $scope.tour.restart();
     };
 
-    $scope.restartTour = function() {
+    $scope.restartTour = function () {
         $scope.setTour();
     };
-    
+
 
 }
 ]);
