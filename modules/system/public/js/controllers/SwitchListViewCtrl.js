2ebe3825c72cf271897a8199a32322dcda93a9be
Jakub Flaska <jakub.flaska@nih.gov>
2015-04-29 15:08:14 -0400

    x

--- modules/system/public/js/controllers/SwitchListViewCtrl.js
+++ modules/system/public/js/controllers/SwitchListViewCtrl.js
@@ -2,7 +2,8 @@ angular.module('systemModule').controller('SwitchListViewCtrl', ['$scope', 'OrgH
 
     $scope.maxLines = 5;
     $scope.lineLength = 50;
-    if ($scope.cache.get("listViewType")) $scope.listViewType = $scope.cache.get("listViewType");
+    var listViewCacheName = $scope.module + "listViewType";
+    if ($scope.cache.get(listViewCacheName)) $scope.listViewType = $scope.cache.get(listViewCacheName);
     else if (SearchSettings.getDefaultSearchView()) $scope.listViewType = SearchSettings.getDefaultSearchView();
     else $scope.listViewType = "accordion";
 
@@ -11,7 +12,7 @@ angular.module('systemModule').controller('SwitchListViewCtrl', ['$scope', 'OrgH
     $scope.switchGridAccordionView = function() {
         if ($scope.listViewType === 'accordion') $scope.listViewType = 'table';
         else $scope.listViewType = 'accordion';
-        $scope.cache.put("listViewType", $scope.listViewType);
+        $scope.cache.put(listViewCacheName, $scope.listViewType);
     };
 
     $scope.showSideBySideView = function() {
