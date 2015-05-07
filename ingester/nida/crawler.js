4c0197e24ee5c7c2b6b4d06fac8d469424aadfb3
Jakub Flaska <jakub.flaska@nih.gov>
2015-05-07 15:22:43 -0400

    x

--- ingester/nida/crawler.js
+++ ingester/nida/crawler.js
@@ -1,5 +1,7 @@
-var page = require('webpage').create()
+var webpage = require('webpage')
     , system = require('system');
+
+var page = webpage.create();
 
 page.onConsoleMessage = function(msg) {
     system.stderr.writeLine( 'console: ' + msg );
@@ -11,20 +13,49 @@ page.open('http://cde.drugabuse.gov/instruments', function(status) {
         phantom.exit();
     }
 
-    var findChildrenLinks = function(linksSelector) {
-        var sel = linksSelector;
-        return function () {
-            var results = [];
-            var links = document.querySelectorAll(sel);
-            for (var i = 0; i < links.length; i++) {
-                results.push({name: links[i].innerText, url: links[i].href});
-            }
-            return results;
+    var findChildrenLinks = function (linksSelector) {
+        var results = [];
+        var links = document.querySelectorAll(linksSelector);
+        for (var i = 0; i < links.length; i++) {
+            results.push({name: links[i].innerText, url: links[i].href});
         }
+        return results;
     };
 
-    var groupsL2 = page.evaluate(findChildrenLinks("fieldset:nth-of-type(1) a"));
-    console.log(groupsL2.map(function(e) {return e.name+":"+e.url}).join("\n"));
+    var instruments = page.evaluate(findChildrenLinks, "fieldset:nth-of-type(1) a");
+    instruments.forEach(function(instrument) {
+        var subPage = webpage.create();
+        subPage.open(instrument.url, function(status) {
+            var modules = subPage.evaluate(findChildrenLinks, "div.content > table.tableheader-processed td a");
+            modules.forEach(function(module){
+                var modulePage = webpage.create();
+                modulePage.open(module.url, function(status){
+                    var cdes = modulePage.evaluate(findChildrenLinks, "td:nth-child(1) a");
+                    var ids = modulePage.evaluate(findChildrenLinks, "td:nth-child(2) a");
+                    cdes.forEach(function(cde, i){
+                        console.log(instrument.name+" > "+module.name + " > " + cde.name + " : " + ids[i].name +"\n\n");
+                    });
 
-    phantom.exit();
+                    //ids.forEach(function(cde, i){
+                    //    console.log("id id id");
+                    //    console.log("id: "+instrument.name+" > "+module.name + " > " + cde.name + " > " /*+ ids[i].name*/ +"\n\n");
+                    //});
+
+                    //var rows = modulePage.evaluate(function(){
+                    //    var rows = document.querySelectorAll("tr");
+                    //    var result = [];
+                    //    for (var i = 0; i < rows.length; i++) result.push({
+                    //        name: jQuery(rows[i]).find("td:nth-child(1) a").text()
+                    //        , id: jQuery(rows[i]).find("td:nth-child(2) a").text()//jQuery(rows[i]).html()
+                    //    });
+                    //    return result;
+                    //});
+                    //for (var i = 0; i < rows.length; i ++) {
+                    //    console.log(instrument.name+" > "+module.name + " > " + rows[i].name + " : " + rows[i].id);
+                    //    //console.log(rows[i].name);
+                    //}
+                });
+            });
+        });
+    });
 });
