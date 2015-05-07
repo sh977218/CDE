da062b536740b48246b78edbb9e45343b0565074
Jakub Flaska <jakub.flaska@nih.gov>
2015-05-07 13:32:02 -0400

    x

--- ingester/nida/crawler.js
+++ ingester/nida/crawler.js
@@ -1,26 +1,25 @@
-var page = require('webpage').create();
+var page = require('webpage').create()
+    , system = require('system');
 
-page.open('http://cde.drugabuse.gov/', function(status) {
+page.onConsoleMessage = function(msg) {
+    system.stderr.writeLine( 'console: ' + msg );
+};
+
+page.open('http://cde.drugabuse.gov/instruments', function(status) {
     if(status !== "success") {
         console.log("Cannot connect");
         phantom.exit();
     }
-    //page.evaluate(function() {
-    //    jQuery(".fieldset-wrapper a");
-    //});
 
     var groupsL2 = page.evaluate(function() {
-        var links = document.querySelectorAll('.fieldset-wrapper:first a');
-        //var result;
-        //for (i = 0; i < list.length; i++) {
-        //    pizza.push(list[i].innerText);
-        //}
-        //return pizza;
-        return links.map(function(link) {
-            return link.innerText;
-        });
+        var results = [];
+        var links = document.querySelectorAll(".fieldset-wrapper a");
+        for (var i = 0; i<links.length; i++) {
+            results.push(links[i].innerText);
+        }
+        return results;
     });
-    console.log(groupsL2);
+    console.log(groupsL2.join("\n"));
 
     phantom.exit();
 });
