403e0639032ea63e09d9eaef497189d647204a19
unknown <ludetc@NLM01960809MLB.nlm.nih.gov>
2015-05-08 14:29:07 -0400

    reset was causing issue

--- test/selenium/src/test/java/gov/nih/nlm/cde/test/search/ResetSearchTest.java
+++ test/selenium/src/test/java/gov/nih/nlm/cde/test/search/ResetSearchTest.java
@@ -21,8 +21,15 @@ public class ResetSearchTest extends NlmCdeBaseTest {
         List<WebElement> linkList = driver.findElements(By.cssSelector("div.panel-default"));
         Assert.assertEquals(linkList.size(), 2);
         findElement(By.id("resetSearch")).click();
-        Assert.assertTrue(textPresent("10"));
+        textPresent("10");
         linkList = driver.findElements(By.cssSelector("div.panel-default"));
         Assert.assertTrue(linkList.size() > 10);
+
+        findElement(By.id("li-blank-caCORE")).click();
+        textPresent("caCORE 3.2");
+        findElement(By.id("li-blank-caCORE")).click();
+        findElement(By.id("li-blank-CSM")).click();
+        textPresent("2 results");
+
     }
 }
