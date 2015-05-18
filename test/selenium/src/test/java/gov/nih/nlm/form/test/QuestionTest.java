9c35ac3100ca4753085c7f389ac77c5a3ba9cc8f
unknown <huangs8@NLM01961050MLB.nlm.nih.gov>
2015-05-18 11:34:34 -0400

    remove FormEditTest test

--- test/selenium/src/test/java/gov/nih/nlm/form/test/QuestionTest.java
+++ test/selenium/src/test/java/gov/nih/nlm/form/test/QuestionTest.java
@@ -12,33 +12,33 @@ import org.testng.annotations.Test;
 import java.util.List;
 
 public class QuestionTest extends BaseFormTest {
-    
+
     public void addQuestionToSection(String cdeName, int sectionNumber) {
         findElement(By.id("resetSearch")).click();
         hangon(1);
         findElement(By.name("ftsearch")).sendKeys("\"" + cdeName + "\"");
         findElement(By.id("search.submit")).click();
-        textPresent("1 results");        
+        textPresent("1 results");
         textPresent(cdeName, By.id("acc_link_0"));
-        
+
         WebElement sourceElt = findElement(By.cssSelector("#accordionList .question-move-handle"));
         WebElement targetElt = findElement(By.id("section_drop_area_" + sectionNumber));
-        
+
         Assert.assertTrue(sourceElt.isDisplayed());
         //Assert.assertTrue(targetElt.isDisplayed());
 
-        String jsScroll = "var y = $(\"#section_drop_area_"+sectionNumber+"\").position().top;\n" +
+        String jsScroll = "var y = $(\"#section_drop_area_" + sectionNumber + "\").position().top;\n" +
                 "$(window).scrollTop(y);";
         ((JavascriptExecutor) driver).executeScript(jsScroll, "");
 
         scrollTo(targetElt.getLocation().getY());
-        
+
         (new Actions(driver)).dragAndDrop(sourceElt, targetElt).perform();
 
         textPresent(cdeName, By.id("section_drop_area_" + sectionNumber));
 
-    }    
-    
+    }
+
     public void addQuestionToSectionSafe(String cdeName, int sectionNumber) {
         findElement(By.id("resetSearch")).click();
         hangon(1);
@@ -46,21 +46,21 @@ public class QuestionTest extends BaseFormTest {
         findElement(By.id("search.submit")).click();
         textPresent("1 results");
         textPresent(cdeName, By.id("accordionList"));
-        
+
         WebElement sourceElt = findElement(By.xpath("//div[@id='accordionList']//i[contains(@class, 'question-move-handle')]"));
         WebElement targetElt = findElement(By.xpath("//*[text()=\"Patient Gender Category\"]"));
-        
+
         Assert.assertTrue(sourceElt.isDisplayed());
         Assert.assertTrue(targetElt.isDisplayed());
-        
+
         (new Actions(driver)).dragAndDrop(sourceElt, targetElt).perform();
-    }       
-    
-    @Test
+    }
+
+//    @Test
     public void questions() {
         Dimension currentWindowSize = getWindowSize();
         resizeWindow(1524, 1150);
-        
+
         mustBeLoggedInAs(ctepCurator_username, password);
 
         String formName = "Questions Form Test";
@@ -68,11 +68,11 @@ public class QuestionTest extends BaseFormTest {
         String formV = "0.1alpha";
 
         createForm(formName, formDef, formV, "CTEP");
-        
+
         findElement(By.linkText("Form Description")).click();
-        
+
         new CreateEditSectionTest().addSection("Patient Information", null);
-        
+
         startAddingQuestions();
 
         // Check status facet.
@@ -87,41 +87,41 @@ public class QuestionTest extends BaseFormTest {
         findElement(By.id("classifications-text-AECC")).click();
         hangon(1);
         Assert.assertEquals(driver.findElements(By.xpath("//ul[@items-per-page='20']/li")).size(), 3);
-        
+
         addQuestionToSection("Person Birth Date", 0);
 
         Assert.assertEquals("Person Birth Date", findElement(By.id("question_accordion_0_0")).getText());
         findElement(By.id("question_accordion_0_0")).click();
-        
+
         findElement(By.xpath("//dd[@id='dd_question_title_0']//i")).click();
         findElement(By.xpath("//div[@id='q_select_name_3']//button")).click();
         hangon(1);
         Assert.assertEquals("Date of Birth", findElement(By.id("dd_question_title_0")).getText());
- 
+
         String instXPath = "//dd[@id='dd_question_instructions_0']";
 
         Assert.assertEquals("N/A", findElement(By.id("dd_question_instructions_0")).getText());
-        findElement(By.xpath(instXPath +  "//i")).click();
+        findElement(By.xpath(instXPath + "//i")).click();
         findElement(By.xpath(instXPath + "//input")).sendKeys("Instructions for PBD");
         findElement(By.xpath(instXPath + "//button[text() = ' Confirm']")).click();
-        
+
         Assert.assertFalse(findElement(By.xpath("//dd[@id='dd_question_required_0']/input")).isSelected());
         findElement(By.xpath("//dd[@id='dd_question_required_0']//input")).click();
-        
+
         String cardXPath = "//dd[@id='dd_q_card_0']";
         Assert.assertEquals("Exactly 1", findElement(By.xpath(cardXPath)).getText().trim());
         findElement(By.xpath(cardXPath + "//i")).click();
         new Select(findElement(By.xpath(cardXPath + "//select"))).selectByVisibleText("1 or more");
         findElement(By.xpath(cardXPath + "//button[text() = ' Confirm']")).click();
-        
+
         String uomXPath = "//dd[@id='dd_q_uoms_0']";
         Assert.assertEquals("N/A", findElement(By.xpath(uomXPath + "/div")).getText());
         findElement(By.xpath(uomXPath + "//button")).click();
-        
+
         findElement(By.xpath(uomXPath + "//div[@id='q_uom_list_0']//i")).click();
         findElement(By.xpath(uomXPath + "//div[@id='q_uom_list_0']//input")).clear();
         findElement(By.xpath(uomXPath + "//div[@id='q_uom_list_0']//button[text() = ' Confirm']")).click();
-        
+
         List<WebElement> uoms = driver.findElements(By.xpath(uomXPath + "//div[contains(@id, 'q_uom_list_')]"));
         Assert.assertEquals(0, uoms.size());
 
@@ -137,9 +137,9 @@ public class QuestionTest extends BaseFormTest {
         findElement(By.xpath(uomXPath + "//div[@id='q_uom_list_1']//input")).clear();
         findElement(By.xpath(uomXPath + "//div[@id='q_uom_list_1']//input")).sendKeys("PBD UOM 2");
         findElement(By.xpath(uomXPath + "//div[@id='q_uom_list_1']//button[text() = ' Confirm']")).click();
-        
+
         String oldFormUrl = driver.getCurrentUrl();
-        
+
         textPresent("This element is not saved.");
         saveForm();
         String newFormUrl = driver.getCurrentUrl();
@@ -156,10 +156,10 @@ public class QuestionTest extends BaseFormTest {
         Assert.assertEquals("DATE", findElement(By.id("dd_datatype_0")).getText().trim());
 
         Assert.assertEquals("PBD UOM 1", findElement(By.xpath(uomXPath + "//div[@id='q_uom_list_0']")).getText());
-        
+
         driver.get(newFormUrl);
         findElement(By.linkText("Form Description")).click();
-        startAddingQuestions();    
+        startAddingQuestions();
 
         // Add 2nd Section
         new CreateEditSectionTest().addSection("Smoking Status", null);
