82c88c3ec4d824b6f1b40bdced9624d135e980df
unknown <huangs8@NLM01961050MLB.nlm.nih.gov>
2015-06-01 15:16:02 -0400

    test case

--- test/selenium/src/test/java/gov/nih/nlm/cde/common/test/AttachmentsTest.java
+++ test/selenium/src/test/java/gov/nih/nlm/cde/common/test/AttachmentsTest.java
@@ -1,19 +1,22 @@
 package gov.nih.nlm.cde.common.test;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
+import org.testng.Assert;
+import java.util.List;
 
 public class AttachmentsTest extends BaseAttachmentTest {
 
     @Test
     public void cdeAttachment() {
         String cdeName = "Alcohol use frequency";
-        
+
         mustBeLoggedInAs(ctepCurator_username, password);
         goToCdeByName(cdeName);
         findElement(By.linkText("Attachments")).click();
         textNotPresent("Upload more files");
-        
+
         mustBeLoggedInAs(ninds_username, password);
         goToCdeByName(cdeName);
 
@@ -29,11 +32,11 @@ public class AttachmentsTest extends BaseAttachmentTest {
 
         openCdeInList(cdeName);
         findElement(By.cssSelector("img.cdeAttachmentThumbnail"));
-        findElement(By.xpath("//a[@id='openEltInCurrentTab_0']")).click();    
+        findElement(By.xpath("//a[@id='openEltInCurrentTab_0']")).click();
 
         goToCdeByName(cdeName);
         checkAttachmentReviewed("glass.jpg");
-        
+
         mustBeLoggedInAs(ninds_username, password);
         goToCdeByName(cdeName);
         removeAttachment();
@@ -41,11 +44,11 @@ public class AttachmentsTest extends BaseAttachmentTest {
 
     @Test
     public void declineCdeAttachment() {
-        String cdeName = "Alcohol Smoking and Substance Use Involvement Screening Test (ASSIST) - Sedative sleep pill frequency";        
+        String cdeName = "Alcohol Smoking and Substance Use Involvement Screening Test (ASSIST) - Sedative sleep pill frequency";
         mustBeLoggedInAs(ninds_username, password);
         goToCdeByName(cdeName);
         findElement(By.linkText("Attachments")).click();
-        
+
         mustBeLoggedInAs(ninds_username, password);
         goToCdeByName("Alcohol use frequency");
 
@@ -56,6 +59,32 @@ public class AttachmentsTest extends BaseAttachmentTest {
         goToCdeByName(cdeName);
         findElement(By.linkText("Attachments")).click();
         textNotPresent("glass.jpg");
-    }   
+    }
 
+    @Test
+    public void checkOnlyShowingDefaultAttachment() {
+        String cdeName = "Geriatric Depression Scale (GDS) - life satisfaction indicator";
+        mustBeLoggedInAs(ninds_username, password);
+        goToCdeByName(cdeName);
+
+        addAttachment("default.jpg");
+        reviewAttachment("default.jpg");
+
+
+        mustBeLoggedInAs(ninds_username, password);
+        goToCdeByName(cdeName);
+        setAttachmentDefault();
+
+        goToCdeByName(cdeName);
+        addAttachment("nonDefault.jpg");
+
+        openEltInList(cdeName, null, null);
+
+        List<WebElement> l = driver.findElements(By.cssSelector("cdeAttachmentThumbnail"));
+        for (WebElement we : l){
+            String src = we.getAttribute("src");
+            Assert.assertTrue(src.contains("556ca45669b04bf418b7aeb8"));
+            Assert.assertFalse(src.contains("556ca46a69b04bf418b7aef8"));
+        }
+    }
 }
