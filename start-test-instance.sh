312e60a3beee3837a9a5bf11e7283b77753a120c
unknown <huangs8@NLM01961050MLB.nlm.nih.gov>
2015-06-02 14:16:20 -0400

    make username and password configurable

--- start-test-instance.sh
+++ start-test-instance.sh
@@ -3,12 +3,17 @@
 # Location of node. For dev testing use '.'  for prod testing use 'build'
 NODE_LOC='.'
 
-gradle -b test/selenium/build.gradle -PhubUrl=any -PtestUrl=any -PforkNb=8 -Ptimeout=8 -Pbrowser=any clean compileTest &
-
-mongo test deploy/dbInit.js -u siteRootAdmin -p password -authenticationDatabase admin
+db_user='siteRootAdmin'
+db_password='password'
 
 target='{"count":0,"_shards":{"total":1,"successful":1,"failed":0}}'
+
+gradle -b test/selenium/build.gradle -PhubUrl=any -PtestUrl=any -PforkNb=8 -Ptimeout=8 -Pbrowser=any clean compileTest &
+
+mongo test deploy/dbInit.js -u $db_user -p $db_password -authenticationDatabase admin
+
 #wait for empty
+
 COUNTER=0
 while [ $COUNTER -lt 60 ]; do
     curl_res=$(curl http://localhost:9200/cdetest/_count)
@@ -31,16 +36,16 @@ else
 fi
 
 
-mongo test test/data/testForms.js -u siteRootAdmin -p password -authenticationDatabase admin
-mongo cde-logs-test deploy/logInit.js -u siteRootAdmin -p password -authenticationDatabase admin
+mongo test test/data/testForms.js -u $db_user -p $db_password -authenticationDatabase admin
+mongo cde-logs-test deploy/logInit.js -u $db_user -p $db_password -authenticationDatabase admin
 
-mongorestore -d test -c dataelements test/data/cdedump/dataelements.bson -u siteRootAdmin -p password -authenticationDatabase admin
-mongorestore -d test -c forms test/data/nindsDump/test/forms.bson -u siteRootAdmin -p password -authenticationDatabase admin
-mongoimport --drop -d test -c orgs test/data/cdedump/orgs.bson -u siteRootAdmin -p password -authenticationDatabase admin
+mongorestore -d test -c dataelements test/data/cdedump/dataelements.bson -u $db_user -p $db_password -authenticationDatabase admin
+mongorestore -d test -c forms test/data/nindsDump/test/forms.bson -u $db_user -p $db_password -authenticationDatabase admin
+mongoimport --drop -d test -c orgs test/data/cdedump/orgs.bson -u $db_user -p $db_password -authenticationDatabase admin
 
-mongo test test/createLargeBoard.js -u siteRootAdmin -p password -authenticationDatabase admin
-mongo test test/createManyBoards.js -u siteRootAdmin -p password -authenticationDatabase admin
-mongo test test/initOrgs.js -u siteRootAdmin -p password -authenticationDatabase admin
+mongo test test/createLargeBoard.js -u $db_user -p $db_password -authenticationDatabase admin
+mongo test test/createManyBoards.js -u $db_user -p $db_password -authenticationDatabase admin
+mongo test test/initOrgs.js -u $db_user -p $db_password -authenticationDatabase admin
 
 target='{"count":9575,"_shards":{"total":1,"successful":1,"failed":0}}'
 #wait for full
