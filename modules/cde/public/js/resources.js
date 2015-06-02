22193c873d9fbdf0d4cbd7a9aa86cc191cff3b0e
Jakub Flaska <jakub.flaska@nih.gov>
2015-06-02 11:03:02 -0400

    x

--- modules/cde/public/js/resources.js
+++ modules/cde/public/js/resources.js
@@ -1,18 +1,3 @@
-angular.module('cdeModule')
-.config(function($routeProvider) {
-    $routeProvider.
-    when('/cde/search', {controller: 'DEListCtrl', templateUrl: 'template/system/list'}).
-    when('/quickBoard', {controller: 'QuickBoardCtrl', templateUrl: '/quickBoard'}).
-    when('/sdcview', {controller: 'SDCViewCtrl', templateUrl: '/sdcView'}).
-    when('/boardExport/:boardId', {controller: 'ExportCtrl', templateUrl: '/cde/public/html/boardExport.html'}).
-    when('/cdeSearchExport', {controller: 'DEListCtrl', templateUrl: '/exportCdeSearch'}).
-    when('/myboards', {controller: 'MyBoardsCtrl', templateUrl: '/myboards'}).
-    when('/board/:boardId', {controller: 'BoardViewCtrl', templateUrl: '/board'}).
-    when('/boardList', {controller: 'BoardListCtrl', templateUrl: '/boardList'}).   
-    when('/createCde', {controller: 'CreateCdeCtrl', templateUrl:'/createcde'}).
-    when('/deview', {controller: 'DEViewCtrl', templateUrl: '/deview'});
-});
-
 angular.module('resourcesCde', ['ngResource'])
 .factory('BoardSearch', function($resource) {
     return $resource('/listboards');
