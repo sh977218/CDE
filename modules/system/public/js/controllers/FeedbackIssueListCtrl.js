755d87c712dbe412c8488c9e89087338e17e40c7
Jakub Flaska <jakub.flaska@nih.gov>
2015-06-10 15:05:32 -0400

    x

--- modules/system/public/js/controllers/FeedbackIssueListCtrl.js
+++ modules/system/public/js/controllers/FeedbackIssueListCtrl.js
@@ -1,9 +1,16 @@
-angular.module('systemModule').controller('FeedbackIssueListCtrl', ['$scope', '$controller', function($scope, $controller) {
+angular.module('systemModule').controller('FeedbackIssueListCtrl', ['$scope', '$controller', '$modal', function($scope, $controller, $modal) {
     $scope.api = "/getFeedbackIssues";
     $scope.errorType = "feedback";
-    $scope.fields = ["Date", "Message", "Screenshot", "HTML"];
+    $scope.fields = ["Date", "User", "Message", "Screenshot", "HTML"];
     $controller('AuditErrorListCtrl', {$scope: $scope});
     $scope.renderImage = function(src){
         return "<img src=\"" + src + "\">";
+    };
+
+    $scope.showScreenshot = function(content){
+        var modalInstance = $modal.open({
+            template: '<img src="'+content+'">'
+            , size: "lg"
+        });
     };
 }]);
