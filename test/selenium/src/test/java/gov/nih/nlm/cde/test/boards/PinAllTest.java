f15f138ee51d041a3ccf94b092b3e575f1027ac0
unknown <huangs8@NLM01961050MLB.nlm.nih.gov>
2015-06-11 17:47:39 -0400

    modify pin all test

--- test/selenium/src/test/java/gov/nih/nlm/cde/test/boards/PinAllTest.java
+++ test/selenium/src/test/java/gov/nih/nlm/cde/test/boards/PinAllTest.java
@@ -8,29 +8,35 @@ public class PinAllTest extends BoardTest {
 
     @Test
     public void pinAll() {
+        String board_name = "Cerebral Palsy > Public Review";
+        String board_description = "CDEs to be use for Cerebral Palsy";
         mustBeLoggedInAs(ninds_username, password);
+        createBoard(board_name, board_description);
+        String num_cde_before_pinAll_string = goToBoardOf();
+        int num_cde_before_pinAll_int = Integer.parseInt(num_cde_before_pinAll_string);
         goToCdeSearch();
-        createBoard("Cerebral Palsy > Public Review", "CDEs to be use for Cerebral Palsy");
-        goToCdeSearch();
-        findElement(By.id("classifications-text-NINDS")).click();
-        findElement(By.id("li-blank-Disease")).click();
-        findElement(By.id("li-blank-General (For all diseases)")).click();
-        findElement(By.id("li-blank-Classification")).click();
-        findElement(By.id("li-blank-Core")).click();
-        hangon(2);
-        int expectedSize = driver.findElements(By.cssSelector(".accordion-toggle")).size();
+        hangon(1);
+        findElement(By.id("resetSearch")).click();
+        hangon(1);
         findElement(By.id("pinAll")).click();
+        hangon(1);
         findElement(By.linkText("Cerebral Palsy > Public Review")).click();
-        modalGone();
-        gotoMyBoards();
-        findElement(By.xpath("//a[../dl/dd/div/div/span[contains(text(),'CDEs to be use for Cerebral Palsy')]]")).click();
-        Assert.assertEquals(driver.findElements(By.xpath("//h4[@class=\"panel-title\"]")).size(), expectedSize);
-        textPresent("Birth date");
-        textPresent("Race USA category");
-        textPresent("Ethnicity USA category");
-        textPresent("Medical history condition text");
-        textPresent("Medical history condition SNOMED CT code");
-        textPresent("Gender Type");
+        hangon(1);
+        String num_cde_after_pinAll_string = goToBoardOf();
+        int num_cde_after_pinAll_int = Integer.parseInt(num_cde_after_pinAll_string);
+        System.out.println("num_cde_before_pinAll_int:" + num_cde_before_pinAll_int);
+        System.out.println("num_cde_after_pinAll_int:" + num_cde_after_pinAll_int);
+        Assert.assertEquals(num_cde_before_pinAll_int + 20, num_cde_after_pinAll_int);
     }
 
+    private String goToBoardOf() {
+        findElement(By.id("boardsMenu")).click();
+        hangon(1);
+        findElement(By.linkText("My Boards")).click();
+        hangon(1);
+        // this could be better if find the number based on the board name
+        // if there are more than 1 board in My Boards, there might be problem
+        String num_Cdes_String = findElement(By.id("dd_numb_0")).getText();
+        return num_Cdes_String;
+    }
 }
