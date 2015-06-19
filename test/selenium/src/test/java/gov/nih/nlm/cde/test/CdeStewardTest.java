ccf7fbdde15bfd68b4eb018e1554b090476aa33f
unknown <ludetc@NLM01960809MLB.nlm.nih.gov>
2015-06-19 13:24:50 -0400

    fix checkOrgDetails

--- test/selenium/src/test/java/gov/nih/nlm/cde/test/CdeStewardTest.java
+++ test/selenium/src/test/java/gov/nih/nlm/cde/test/CdeStewardTest.java
@@ -2,6 +2,7 @@ package gov.nih.nlm.cde.test;
 
 import gov.nih.nlm.system.NlmCdeBaseTest;
 import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -37,12 +38,12 @@ public class CdeStewardTest extends NlmCdeBaseTest {
         goToCdeByName("MFLIE Hardship 24 Hour Nausea Personal Affect 7 Point Likert Scale", "Candidate");
         hangon(1);
         hoverOverElement(findElement(By.linkText("caBIG")));
-        textPresent("Organization Details");
-        textPresent("Cancer Biomedical Informatics Grid");
-        textPresent("123 Somewhere On Earth, Abc, Def, 20001");
-        textPresent("caBig@nih.gov");
-        textPresent("111-222-3333");
-        textPresent("https://cabig.nci.nih.gov/");
+        checkTooltipText(By.linkText("caBIG"), "Organization Details");
+        checkTooltipText(By.linkText("caBIG"), "Cancer Biomedical Informatics Grid");
+        checkTooltipText(By.linkText("caBIG"), "123 Somewhere On Earth, Abc, Def, 20001");
+        checkTooltipText(By.linkText("caBIG"), "caBig@nih.gov");
+        checkTooltipText(By.linkText("caBIG"), "111-222-3333");
+        checkTooltipText(By.linkText("caBIG"), "https://cabig.nci.nih.gov/");
         
         // Changes Steward and save
         findElement(By.id("dd_edit_steward")).click();
@@ -57,11 +58,21 @@ public class CdeStewardTest extends NlmCdeBaseTest {
         hangon(1);
         
         hoverOverElement(findElement(By.linkText("CTEP")));
-        textPresent("Organization Details");
-        textPresent("Cancer Therapy Evaluation Program");
-        textNotPresent("Cancer Biomedical Informatics Grid");
-        textPresent("75 Sunshine Street, Blah, Doh 12345");
-        textPresent("https://cabig.nci.nih.gov/");
+        checkTooltipText(By.linkText("CTEP"), "Organization Details");
+        checkTooltipText(By.linkText("CTEP"), "Cancer Therapy Evaluation Program");
+        checkTooltipText(By.linkText("CTEP"), "Cancer Biomedical Informatics Grid");
+        checkTooltipText(By.linkText("CTEP"), "75 Sunshine Street, Blah, Doh 12345");
+        checkTooltipText(By.linkText("CTEP"), "https://cabig.nci.nih.gov/");
     }
-    
+
+    private void checkTooltipText(By by, String text) {
+        try {
+            textPresent(text);
+        } catch (TimeoutException e) {
+            hoverOverElement(findElement(By.id("searchSettings")));
+            hoverOverElement(findElement(by));
+            textPresent(text);
+        }
+    }
+
 }
