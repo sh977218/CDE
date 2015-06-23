f038d0448551ed49d1b1e13cb9b668d68ef949e6
unknown <huangs8@NLM01961050MLB.nlm.nih.gov>
2015-06-23 16:02:23 -0400

    fix comment

--- test/selenium/src/test/java/gov/nih/nlm/cde/test/boards/PinAllTest.java
+++ test/selenium/src/test/java/gov/nih/nlm/cde/test/boards/PinAllTest.java
@@ -25,9 +25,8 @@ public class PinAllTest extends BoardTest {
         hangon(1);
         String searchResultNum_string = findElement(By.id("searchResultNum")).getText().trim();
         int searchResultNum_int = Integer.parseInt(searchResultNum_string);
-        hangon(1);
         findElement(By.id("pinAll")).click();
-        hangon(1);
+        textPresent(board_name);
         findElement(By.linkText(board_name)).click();
         hangon(1);
         gotoMyBoards();
@@ -42,25 +41,20 @@ public class PinAllTest extends BoardTest {
         String board_description = "This board is only for pin all more than 20 test.";
         mustBeLoggedInAs(pinAllBoardUser_username, password);
         createBoard(board_name, board_description);
-        int num_cde_before_pinAll_int = 0;
         goToCdeSearch();
-        hangon(1);
-        findElement(By.id("resetSearch")).click();
         pickClassification();
         hangon(1);
         pickRegistrationStatus();
         hangon(1);
         String searchResultNum_string = findElement(By.id("searchResultNum")).getText().trim();
         int searchResultNum_int = Integer.parseInt(searchResultNum_string);
-        hangon(1);
         findElement(By.id("pinAll")).click();
-        hangon(1);
         findElement(By.linkText(board_name)).click();
         hangon(1);
         gotoMyBoards();
         String num_cde_after_pinAll_string = findElement(By.id("dd_numb_1")).getText();
         int num_cde_after_pinAll_int = Integer.parseInt(num_cde_after_pinAll_string);
-        Assert.assertEquals(num_cde_before_pinAll_int + searchResultNum_int, num_cde_after_pinAll_int);
+        Assert.assertEquals(searchResultNum_int, num_cde_after_pinAll_int);
     }
 
     private void pickClassification() {
