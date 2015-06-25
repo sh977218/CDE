db116bb5124e86e806d2263fb5def3fc802382ab
unknown <ludetc@NLM01960809MLB.nlm.nih.gov>
2015-06-25 09:10:01 -0400

    temp

--- modules/system/public/js/controllers/ListCtrl.js
+++ modules/system/public/js/controllers/ListCtrl.js
@@ -6,7 +6,7 @@ angular.module('systemModule').controller('ListCtrl',
     $scope.focusClassification = function(){
         //any good angular way to do this?
         $('#classif_filter_title').focus();
-    }
+    };
 
     $timeout(function(){
         if($scope.isScreenSizeXsSm) {
@@ -230,6 +230,14 @@ angular.module('systemModule').controller('ListCtrl',
     };
 
     $scope.reload = function() {
+        var e = new Error('dummy');
+        var stack = e.stack.replace(/^[^\(]+?[\n$]/gm, '')
+            .replace(/^\s+at\s+/gm, '')
+            .replace(/^Object.<anonymous>\s*\(/gm, '{anonymous}()@')
+            .split('\n');
+        console.log(stack);
+
+
         var timestamp = new Date().getTime();
         if (!userResource.user) return;
         $scope.lastQueryTimeStamp = timestamp;
