859d971adbdea541d227ada15b76740fd27966c7
Jakub Flaska <jakub.flaska@nih.gov>
2015-06-29 10:16:59 -0400

    x

--- test/selenium/src/test/java/gov/nih/nlm/cde/test/BaseClassificationTest.java
+++ test/selenium/src/test/java/gov/nih/nlm/cde/test/BaseClassificationTest.java
@@ -7,7 +7,7 @@ import org.testng.Assert;
 
 public class BaseClassificationTest extends NlmCdeBaseTest {
    public void addClassificationMethod(String[] categories) {     
-        findElement(By.id("addClassification")).click();
+        findElement(By.id("[id^=addClassification]")).click();
        addClassificationMethodDo(categories);
     }
 
