7c7758ba37f9754d9c31a65fa74013daba24027d
unknown <ludetc@NLM01960809MLB.nlm.nih.gov>
2015-07-02 14:53:55 -0400

    test fixes

--- test/selenium/src/test/java/gov/nih/nlm/cde/test/boards/PinAllTest.java
+++ test/selenium/src/test/java/gov/nih/nlm/cde/test/boards/PinAllTest.java
@@ -42,8 +42,11 @@ public class PinAllTest extends BoardTest {
         mustBeLoggedInAs(pinAllBoardUser_username, password);
         createBoard(board_name, board_description);
         goToCdeSearch();
-        findElement(By.id("browseOrg-CTEP")).click();
-        findElement(By.id("li-blank-Qualified")).click();
+        findElement(By.id("browseOrg-NINDS")).click();
+        findElement(By.id("li-blank-Disease")).click();
+        findElement(By.id("li-blank-Amyotrophic Lateral Sclerosis")).click();
+        findElement(By.id("li-blank-Classification")).click();
+        findElement(By.id("li-blank-Core")).click();
         hangon(3);
         int searchResultNum_int = Integer.parseInt(findElement(By.id("searchResultNum")).getText().trim());
         Assert.assertTrue(searchResultNum_int > 20);
