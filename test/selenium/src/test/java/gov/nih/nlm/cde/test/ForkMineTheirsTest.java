b4afa003bc66a93ea118926d0caab2e451eb13ac
Jakub Flaska <jakub.flaska@nih.gov>
2015-07-29 11:24:08 -0400

    x

--- test/selenium/src/test/java/gov/nih/nlm/cde/test/ForkMineTheirsTest.java
+++ test/selenium/src/test/java/gov/nih/nlm/cde/test/ForkMineTheirsTest.java
@@ -35,11 +35,8 @@ public class ForkMineTheirsTest extends ForkTest {
 
         Assert.assertEquals("Adverse Event Ongoing Event Indicator - ST FORKED", findElement(By.id("dd_general_name")).getText());
         Assert.assertEquals("Standard", findElement(By.id("dd_status")).getText());
-
         waitForESUpdate();
-
         goToCdeSearch();
-        waitForESUpdate();
         findElement(By.id("ftsearch-input")).sendKeys("\"Adverse Event Ongoing Event Indicator\"");
         findElement(By.cssSelector("i.fa-search")).click();
         textPresent("1 results for");
