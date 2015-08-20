61582b0b021a83512bd255a29b774c54d717edc5
unknown <ludetc@NLM01960809MLB.nlm.nih.gov>
2015-08-20 15:30:02 -0400

    add Test

---
+++ test/selenium/src/test/java/gov/nih/nlm/cde/test/search/CuratorLowStatusSearch.java
@@ -0,0 +1,31 @@
+package gov.nih.nlm.cde.test.search;
+
+import gov.nih.nlm.system.NlmCdeBaseTest;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class CuratorLowStatusSearch extends NlmCdeBaseTest {
+
+    @Test
+    public void curatorLowStatusSearch() {
+        mustBeLoggedOut();
+        setVisibleStatus("minStatus-Qualified");
+        goToCdeSearch();
+        findElement(By.id("ftsearch-input")).clear();
+        findElement(By.id("ftsearch-input")).sendKeys("java");
+        textNotPresent("Candidate (");
+        setVisibleStatus("minStatus-Candidate");
+        findElement(By.id("li-blank-Candidate"));
+        textPresent("caCORE (");
+
+        mustBeLoggedInAs(cabigAdmin_username, password);
+        setVisibleStatus("minStatus-Qualified");
+        goToCdeSearch();
+        findElement(By.id("ftsearch-input")).clear();
+        findElement(By.id("ftsearch-input")).sendKeys("java");
+        textPresent("caCORE (");
+        findElement(By.id("li-blank-Candidate")).click();
+        textNotPresent("caCORE (");
+    }
+
+}
