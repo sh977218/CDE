a6eb8453fa2bfc91e6e81b53b8f4c91d9f3d9296
unknown <ludetc@NLM01960809MLB.nlm.nih.gov>
2015-08-25 13:54:37 -0400

    done

--- test/initOrgs.js
+++ test/initOrgs.js
@@ -1,2 +1,5 @@
 db.orgs.insert({name: "SeleniumOrg", classifications: [{name: "Test Classif", elements: [{name: "Sub Classif"}]}]});
+db.orgs.insert({"name": "org / or Org", "classifications": []});
+
+
 
