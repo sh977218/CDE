a328dbee3bb855d9948a68b339e18dd0132e091f
unknown <ludetc@NLM01960809MLB.nlm.nih.gov>
2015-08-25 15:23:58 -0400

    test added

---
+++ test/selenium/src/test/java/gov/nih/nlm/form/test/FormNamingTest.java
@@ -0,0 +1,49 @@
+package gov.nih.nlm.form.test;
+
+import gov.nih.nlm.system.NlmCdeBaseTest;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class FormNamingTest extends BaseFormTest {
+
+    @Test
+    public void formNaming() {
+        mustBeLoggedInAs(ninds_username, password);
+        goToFormByName("Study Drug Compliance");
+        findElement(By.linkText("Naming")).click();
+        findElement(By.id("addNamePair")).click();
+        findElement(By.name("designation")).sendKeys("This new form Name");
+        findElement(By.name("definition")).sendKeys("A lazy definition");
+        findElement(By.name("context")).clear();
+        findElement(By.name("context")).sendKeys("Great CTX");
+        findElement(By.id("createNamePair")).click();
+        modalGone();
+        saveForm();
+
+        goToFormByName("Study Drug Compliance");
+        findElement(By.linkText("Naming")).click();
+        textPresent("This new form Name");
+        textPresent("A lazy definition");
+        textPresent("Great CTX");
+
+        findElement(By.id("removeNaming-1")).click();
+        saveForm();
+
+        goToFormByName("Study Drug Compliance");
+        findElement(By.linkText("Naming")).click();
+        textNotPresent("\"A lazy definition\"");
+
+        mustBeLoggedOut();
+        goToFormByName("Study Drug Compliance");
+        findElement(By.linkText("Naming")).click();
+        for (WebElement elt : driver.findElements(By.cssSelector(".fa-trash-o"))) {
+            Assert.assertFalse(elt.isDisplayed());
+        }
+
+
+    }
+
+}
