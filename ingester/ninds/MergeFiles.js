e5a800b572b9ae8cba03e29e106d1c3f56927dbe
unknown <huangs8@NLM01961050MLB.nlm.nih.gov>
2015-09-04 14:09:06 -0400

    x

--- ingester/ninds/MergeFiles.js
+++ ingester/ninds/MergeFiles.js
@@ -9,8 +9,7 @@ var excluString = ['See "CRF Search" to find all Imaging forms under Subdomain o
     'Note: The General CDE Standards contain additional useful CRF Modules and CDEs for this category of data.'];
 for (var i = 1; i < 27; i++) {
     var task = "nindsFormsChrist" + i + ".json";
-    if (i != 15)
-        tasks.push(task);
+    tasks.push(task);
 }
 
 function compare(a, b) {
