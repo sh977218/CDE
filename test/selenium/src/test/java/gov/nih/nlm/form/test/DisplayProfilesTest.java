baf9cc83899ed156784856b13bdf458afedbca00
unknown <ludetc@NLM01960809MLB.nlm.nih.gov>
2015-09-04 10:24:32 -0400

    tests fixed

--- test/selenium/src/test/java/gov/nih/nlm/form/test/DisplayProfilesTest.java
+++ test/selenium/src/test/java/gov/nih/nlm/form/test/DisplayProfilesTest.java
@@ -2,6 +2,8 @@ package gov.nih.nlm.form.test;
 
 import gov.nih.nlm.system.NlmCdeBaseTest;
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class DisplayProfilesTest extends BaseFormTest {
@@ -12,8 +14,8 @@ public class DisplayProfilesTest extends BaseFormTest {
         findElement(By.xpath("//div[@id='profileNameEdit_" + index + "']//input[@type='text']")).clear();
         findElement(By.xpath("//div[@id='profileNameEdit_" + index + "']//input[@type='text']")).sendKeys(name);
         findElement(By.xpath("//div[@id='profileNameEdit_" + index + "']//button[contains(@class, 'fa-check')]")).click();
-        if (matrix) findElement(By.id("displayAsMatrix_0")).click();
-        if (displayValues) findElement(By.id("displayValues_0")).click();
+        if (matrix) findElement(By.id("displayAsMatrix_" + index)).click();
+        if (displayValues) findElement(By.id("displayValues_" + index)).click();
     }
 
     @Test
@@ -34,7 +36,31 @@ public class DisplayProfilesTest extends BaseFormTest {
         textPresent("In the past 7 days");
         textPresent("Display Profile:");
 
-        
+        Assert.assertEquals(driver.findElements(By.xpath("//table//input[@type='radio']")).size(), 25);
+        Assert.assertEquals(driver.findElements(By.xpath("//select[@ng-model='question.question.answer']")).size(), 0);
+        textPresent("1");textPresent("2");textPresent("3");textPresent("4");textPresent("5");
+
+        new Select(driver.findElement(By.id("select_display_profile"))).selectByVisibleText("Matrix No Values");
+        hangon(1);
+        Assert.assertEquals(driver.findElements(By.xpath("//table//input[@type='radio']")).size(), 25);
+        Assert.assertEquals(driver.findElements(By.xpath("//select[@ng-model='question.question.answer']")).size(), 0);
+
+        new Select(driver.findElement(By.id("select_display_profile"))).selectByVisibleText("No Matrix No Values");
+        hangon(1);
+        Assert.assertEquals(driver.findElements(By.xpath("//table//input[@type='radio']")).size(), 0);
+        Assert.assertEquals(driver.findElements(By.xpath("//select[@ng-model='question.question.answer']")).size(), 5);
+
+        findElement(By.linkText("Display Profiles")).click();
+
+        for (int i =0; i < 3; i++) {
+            findElement(By.id("removeDisplayProfile-0")).click();
+            findElement(By.id("confirmRemoveDisplayProfile-0")).click();
+        }
+
+        saveForm();
+        findElement(By.linkText("General Details")).click();
+        textNotPresent("Display Profile:");
+
     }
 
 }
