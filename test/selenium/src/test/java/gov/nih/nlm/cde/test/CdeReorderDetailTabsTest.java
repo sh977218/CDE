3d489de215120b0120db8bb4cd19ba863e112668
unknown <huangs8@NLM01961050MLB.nlm.nih.gov>
2015-09-10 15:04:32 -0400

    fix test

--- test/selenium/src/test/java/gov/nih/nlm/cde/test/CdeReorderDetailTabsTest.java
+++ test/selenium/src/test/java/gov/nih/nlm/cde/test/CdeReorderDetailTabsTest.java
@@ -19,90 +19,106 @@ public class CdeReorderDetailTabsTest extends NlmCdeBaseTest {
     }
 
     private void testIcon(String tabName) {
+        String prefix = "//div[@id='" + tabName + "']//div//*[@id='";
+        String postfix = "']";
+        Assert.assertEquals(driver.findElements(By.xpath(prefix + "moveDown-0" + postfix)).size(), 1);
+        Assert.assertEquals(driver.findElements(By.xpath(prefix + "moveUp-0" + postfix)).size(), 0);
+        Assert.assertEquals(driver.findElements(By.xpath(prefix + "moveBottom-0" + postfix)).size(), 1);
+        Assert.assertEquals(driver.findElements(By.xpath(prefix + "moveTop-0" + postfix)).size(), 0);
 
-        Assert.assertEquals(driver.findElements(By.xpath("//div[@id='" + tabName + "']//div//*[@id='moveDown-0']")).size(), 0);
-        Assert.assertEquals(driver.findElements(By.xpath("//div[@id='" + tabName + "']//div//*[@id='moveTop-0']")).size(), 0);
-        Assert.assertEquals(driver.findElements(By.xpath("//div[@id='" + tabName + "']//div//*[@id='moveDown-0']")).size(), 1);
-        Assert.assertEquals(driver.findElements(By.xpath("//div[@id='" + tabName + "']//div//*[@id='moveBottom-0']")).size(), 1);
+        Assert.assertEquals(driver.findElements(By.xpath(prefix + "moveDown-1" + postfix)).size(), 1);
+        Assert.assertEquals(driver.findElements(By.xpath(prefix + "moveUp-1" + postfix)).size(), 1);
+        Assert.assertEquals(driver.findElements(By.xpath(prefix + "moveBottom-1" + postfix)).size(), 1);
+        Assert.assertEquals(driver.findElements(By.xpath(prefix + "moveTop-1" + postfix)).size(), 1);
 
-        Assert.assertEquals(driver.findElements(By.xpath("//div[@id='" + tabName + "']//div//*[@id='moveUp-1']")).size(), 1);
-        Assert.assertEquals(driver.findElements(By.xpath("//div[@id='" + tabName + "']//div//*[@id='moveTop-1']")).size(), 1);
-        Assert.assertEquals(driver.findElements(By.xpath("//div[@id='" + tabName + "']//div//*[@id='moveDown-1']")).size(), 1);
-        Assert.assertEquals(driver.findElements(By.xpath("//div[@id='" + tabName + "']//div//*[@id='moveBottom-1']")).size(), 1);
-
-        Assert.assertEquals(driver.findElements(By.xpath("//div[@id='" + tabName + "']//div//*[@id='moveUp-2']")).size(), 1);
-        Assert.assertEquals(driver.findElements(By.xpath("//div[@id='" + tabName + "']//div//*[@id='moveTop-2']")).size(), 1);
-        Assert.assertEquals(driver.findElements(By.xpath("//div[@id='" + tabName + "']//div//*[@id='moveDown-2']")).size(), 0);
-        Assert.assertEquals(driver.findElements(By.xpath("//div[@id='" + tabName + "']//div//*[@id='moveBottom-2']")).size(), 0);
+        Assert.assertEquals(driver.findElements(By.xpath(prefix + "moveDown-2" + postfix)).size(), 0);
+        Assert.assertEquals(driver.findElements(By.xpath(prefix + "moveUp-2" + postfix)).size(), 1);
+        Assert.assertEquals(driver.findElements(By.xpath(prefix + "moveBottom-2" + postfix)).size(), 0);
+        Assert.assertEquals(driver.findElements(By.xpath(prefix + "moveTop-2" + postfix)).size(), 1);
     }
 
     private void testPermissible() {
+        String tabName = "permissibleValueDiv";
+        String prefix = "//div[@id='" + tabName + "']//div//*[@id='";
+        String postfix = "']";
         findElement(By.linkText("Permissible Values")).click();
         textPresent("cs3");
-        testIcon("permissibleValueDiv");
-        findElement(By.id("moveDown-0")).click();
-        Assert.assertTrue(findElement(By.id("pv-1")).getText().contains("pv1 Confirm Discard"));
-        findElement(By.id("moveBottom-0")).click();
-        Assert.assertTrue(findElement(By.id("pv-2")).getText().contains("pv2 Confirm Discard"));
-        findElement(By.id("moveUp-2")).click();
-        Assert.assertTrue(findElement(By.id("pv-1")).getText().contains("pv2 Confirm Discard"));
-        findElement(By.id("moveTop-2")).click();
-        Assert.assertTrue(findElement(By.id("pv-0")).getText().contains("pv3 Confirm Discard"));
+        testIcon(tabName);
+        findElement(By.xpath(prefix + "moveDown-0" + postfix)).click();
+        Assert.assertTrue(findElement(By.xpath(prefix + "pv-1" + postfix)).getText().contains("pv1"));
+        findElement(By.xpath(prefix + "moveBottom-0" + postfix)).click();
+        Assert.assertTrue(findElement(By.xpath(prefix + "pv-2" + postfix)).getText().contains("pv2"));
+        findElement(By.xpath(prefix + "moveUp-2" + postfix)).click();
+        Assert.assertTrue(findElement(By.xpath(prefix + "pv-1" + postfix)).getText().contains("pv2"));
+        findElement(By.xpath(prefix + "moveTop-2" + postfix)).click();
+        Assert.assertTrue(findElement(By.xpath(prefix + "pv-0" + postfix)).getText().contains("pv3"));
     }
 
     private void testNaming() {
+        String tabName = "namingDiv";
+        String prefix = "//div[@id='" + tabName + "']//div//*[@id='";
+        String postfix = "']";
         findElement(By.linkText("Naming")).click();
         textPresent("Definition:");
-        testIcon("namingDiv");
-        findElement(By.id("moveDown-0")).click();
-        Assert.assertTrue(findElement(By.id("dd_name_1")).getText().contains("cde for test cde reorder detail tabs Confirm Discard"));
-        findElement(By.id("moveBottom-0")).click();
-        Assert.assertTrue(findElement(By.id("dd_name-2")).getText().contains("cde for test cde reorder detail tabs 2 Confirm Discard"));
-        findElement(By.id("moveUp-2")).click();
-        Assert.assertTrue(findElement(By.id("dd_name-1")).getText().contains("cde for test cde reorder detail tabs 2 Confirm Discard"));
-        findElement(By.id("moveTop-2")).click();
-        Assert.assertTrue(findElement(By.id("dd_name-0")).getText().contains("cde for test cde reorder detail tabs 3 Confirm Discard"));
+        testIcon(tabName);
+        findElement(By.xpath(prefix + "moveDown-0" + postfix)).click();
+        Assert.assertTrue(findElement(By.xpath(prefix + "dd_name_1" + postfix)).getText().contains("cde for test cde reorder detail tabs"));
+        findElement(By.xpath(prefix + "moveBottom-0" + postfix)).click();
+        Assert.assertTrue(findElement(By.xpath(prefix + "dd_name_2" + postfix)).getText().contains("cde for test cde reorder detail tabs 1"));
+        findElement(By.xpath(prefix + "moveUp-2" + postfix)).click();
+        Assert.assertTrue(findElement(By.xpath(prefix + "dd_name_1" + postfix)).getText().contains("cde for test cde reorder detail tabs 1"));
+        findElement(By.xpath(prefix + "moveTop-2" + postfix)).click();
+        Assert.assertTrue(findElement(By.xpath(prefix + "dd_name_0" + postfix)).getText().contains("cde for test cde reorder detail tabs 2"));
     }
 
     private void testConcepts() {
+        String tabName = "conceptsDiv";
+        String prefix = "//div[@id='" + tabName + "']//div//*[@id='";
+        String postfix = "']";
         findElement(By.linkText("Concepts")).click();
         textPresent("Data Element");
-        testIcon("conceptsDiv");
-        findElement(By.id("moveDown-0")).click();
-        Assert.assertTrue(findElement(By.id("concept_cde_name_1")).getText().contains("cb1"));
-        findElement(By.id("moveBottom-0")).click();
-        Assert.assertTrue(findElement(By.id("concept_cde_name-2")).getText().contains("cb2"));
-        findElement(By.id("moveUp-2")).click();
-        Assert.assertTrue(findElement(By.id("concept_cde_name-1")).getText().contains("cb2"));
-        findElement(By.id("moveTop-2")).click();
-        Assert.assertTrue(findElement(By.id("concept_cde_name-0")).getText().contains("cb3"));
+        testIcon(tabName);
+        findElement(By.xpath(prefix + "moveDown-0" + postfix)).click();
+        Assert.assertTrue(findElement(By.xpath(prefix + "concept_cde_name_1" + postfix)).getText().contains("cb1"));
+        findElement(By.xpath(prefix + "moveBottom-0" + postfix)).click();
+        Assert.assertTrue(findElement(By.xpath(prefix + "concept_cde_name_2" + postfix)).getText().contains("cn2"));
+        findElement(By.xpath(prefix + "moveUp-2" + postfix)).click();
+        Assert.assertTrue(findElement(By.xpath(prefix + "concept_cde_name_1" + postfix)).getText().contains("cn2"));
+        findElement(By.xpath(prefix + "moveTop-2" + postfix)).click();
+        Assert.assertTrue(findElement(By.xpath(prefix + "concept_cde_name_0" + postfix)).getText().contains("cn3"));
     }
 
     private void testReferrenceDocuments() {
+        String tabName = "referrenceDocumentsDiv";
+        String prefix = "//div[@id='" + tabName + "']//div//*[@id='";
+        String postfix = "']";
         findElement(By.linkText("Reference Documents")).click();
         textPresent("Language Code:");
-        testIcon("referrenceDocumentsDiv");
-        findElement(By.id("moveDown-0")).click();
-        Assert.assertTrue(findElement(By.id("rd_id_1")).getText().contains("rd1"));
-        findElement(By.id("moveBottom-0")).click();
-        Assert.assertTrue(findElement(By.id("rd_id-2")).getText().contains("rd2"));
-        findElement(By.id("moveUp-2")).click();
-        Assert.assertTrue(findElement(By.id("rd_id-1")).getText().contains("rd2"));
-        findElement(By.id("moveTop-2")).click();
-        Assert.assertTrue(findElement(By.id("rd_id-0")).getText().contains("rd3"));
+        testIcon(tabName);
+        findElement(By.xpath(prefix + "moveDown-0" + postfix)).click();
+        Assert.assertTrue(findElement(By.xpath(prefix + "rd_id_1" + postfix)).getText().contains("rd1"));
+        findElement(By.xpath(prefix + "moveBottom-0" + postfix)).click();
+        Assert.assertTrue(findElement(By.xpath(prefix + "rd_id_2" + postfix)).getText().contains("rd2"));
+        findElement(By.xpath(prefix + "moveUp-2" + postfix)).click();
+        Assert.assertTrue(findElement(By.xpath(prefix + "rd_id_1" + postfix)).getText().contains("rd2"));
+        findElement(By.xpath(prefix + "moveTop-2" + postfix)).click();
+        Assert.assertTrue(findElement(By.xpath(prefix + "rd_id_0" + postfix)).getText().contains("rd3"));
     }
 
     private void testProperties() {
+        String tabName = "propertiesDiv";
+        String prefix = "//div[@id='" + tabName + "']//div//*[@id='";
+        String postfix = "']";
         findElement(By.linkText("Properties")).click();
         textPresent("Add Property");
-        testIcon("propertiesDiv");
-        findElement(By.id("moveDown-0")).click();
-        Assert.assertTrue(findElement(By.id("dd_name_1")).getText().contains("pk1"));
-        findElement(By.id("moveBottom-0")).click();
-        Assert.assertTrue(findElement(By.id("dd_name-2")).getText().contains("pk2"));
-        findElement(By.id("moveUp-2")).click();
-        Assert.assertTrue(findElement(By.id("dd_name-1")).getText().contains("pk2"));
-        findElement(By.id("moveTop-2")).click();
-        Assert.assertTrue(findElement(By.id("dd_name-0")).getText().contains("pk3"));
+        testIcon(tabName);
+        findElement(By.xpath(prefix + "moveDown-0" + postfix)).click();
+        Assert.assertTrue(findElement(By.xpath(prefix + "dd_name_1" + postfix)).getText().contains("pk1"));
+        findElement(By.xpath(prefix + "moveBottom-0" + postfix)).click();
+        Assert.assertTrue(findElement(By.xpath(prefix + "dd_name_2" + postfix)).getText().contains("pk2"));
+        findElement(By.xpath(prefix + "moveUp-2" + postfix)).click();
+        Assert.assertTrue(findElement(By.xpath(prefix + "dd_name_1" + postfix)).getText().contains("pk2"));
+        findElement(By.xpath(prefix + "moveTop-2" + postfix)).click();
+        Assert.assertTrue(findElement(By.xpath(prefix + "dd_name_0" + postfix)).getText().contains("pk3"));
     }
 }
