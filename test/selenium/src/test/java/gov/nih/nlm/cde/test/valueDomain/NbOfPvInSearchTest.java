4998059cf6122e9c4c82d69bb3e95d22bb6501b7
unknown <ludetc@NLM01960809MLB.nlm.nih.gov>
2015-09-28 09:53:23 -0400

    added test

---
+++ test/selenium/src/test/java/gov/nih/nlm/cde/test/valueDomain/NbOfPvInSearchTest.java
@@ -0,0 +1,26 @@
+package gov.nih.nlm.cde.test.valueDomain;
+
+import gov.nih.nlm.system.NlmCdeBaseTest;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class NbOfPvInSearchTest extends NlmCdeBaseTest{
+
+    @Test
+    public void nbOfPVsInSearch() {
+        openCdeInList("country iso");
+        textPresent("(249 total)");
+        clickElement(By.id("gridView"));
+        textPresent("Nb of PVs");
+        Assert.assertEquals(findElement(By.cssSelector("td.nbOfPVs")).getText(), "249");
+        findElement(By.id("searchSettings")).click();
+        textPresent("Number of Permissible Values (PVs)");
+        findElement(By.id("nbOfPVs")).click();
+        Assert.assertFalse(findElement(By.id("nbOfPVs")).isSelected());
+        findElement(By.id("saveSettings")).click();
+        textPresent("Settings saved!");
+        textNotPresent("249");
+    }
+
+}
