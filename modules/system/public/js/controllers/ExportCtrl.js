8477ea0f5b299f76b86d2749e3e2d20f3254c021
unknown <huangs8@NLM01961050MLB.nlm.nih.gov>
2015-09-28 15:19:35 -0400

    add export all button

--- modules/system/public/js/controllers/ExportCtrl.js
+++ modules/system/public/js/controllers/ExportCtrl.js
@@ -1,5 +1,4 @@
-angular.module('systemModule').controller('ExportCtrl', ['$scope', 'Elastic', function ($scope, Elastic)
-{
+angular.module('systemModule').controller('ExportCtrl', ['$scope', 'Elastic', function ($scope, Elastic) {
 
     $scope.feedbackClass = ["fa-download"];
     $scope.csvDownloadState = "none";
@@ -18,13 +17,41 @@ angular.module('systemModule').controller('ExportCtrl', ['$scope', 'Elastic', fu
                     type: "text/csv"
                 });
                 saveAs(blob, 'SearchExport' + '.csv');
-                $scope.addAlert("success", "Export downloaded.")
+                $scope.addAlert("success", "Export downloaded.");
                 $scope.feedbackClass = ["fa-download"];
             } else {
                 $scope.addAlert("danger", "The server is busy processing similar request, please try again in a minute.");
             }
         });
     };
+
+    $scope.exportAll = function () {
+        try {
+            var isFileSaverSupported = !!new Blob;
+        } catch (e) {
+            return $scope.addAlert("danger", "Export feature is not supported in this browser. Please try Google Chrome or Mozilla FireFox.");
+        }
+        $scope.feedbackClass = ['fa-spinner', 'fa-pulse'];
+        $scope.addAlert("warning", "Your export is being generated, please wait.");
+        var allSearchSetting = {
+            includeAggregations: false,
+            selectedElements: [],
+            selectedElementsAlt: [],
+            selectedStatuses: ["Preferred Standard", "Standard", "Qualified", "Recorded", "Candidate", "Incomplete"]
+        };
+        Elastic.getExport(Elastic.buildElasticQuerySettings(allSearchSetting), $scope.module, function (result) {
+            if (result) {
+                var blob = new Blob([result], {
+                    type: "text/csv"
+                });
+                saveAs(blob, 'SearchExport' + '.csv');
+                $scope.addAlert("success", "Export downloaded.");
+                $scope.feedbackClass = ["fa-download"];
+            } else {
+                $scope.addAlert("danger", "The server is busy processing similar request, please try again in a minute.");
+            }
+        });
+    }
 
 }])
 ;
