f8c984e2567df1abc4ce66b2a0bcc7c7cd041241
unknown <huangs8@NLM01961050MLB.nlm.nih.gov>
2015-10-01 17:13:58 -0400

    continue

--- ingester/phenx/PhenxLoader.js
+++ ingester/phenx/PhenxLoader.js
@@ -14,12 +14,35 @@ var webdriver = require('selenium-webdriver'),
 var baseUrl = "https://www.phenxtoolkit.org/index.php?pageLink=browse.measures&tree=off";
 var user = {username: "batchloader"};
 var driver = new webdriver.Builder().forBrowser('firefox').build();
-
+var allMeasures = [];
 driver.get(baseUrl);
-var td1 = driver.findElements(By.xpath("//*[@id='phenxTooltip']//following-sibling::table/tbody/tr/td[1]/div/div")).length;
-var td2 = driver.findElements(By.xpath("//*[@id='phenxTooltip']//following-sibling::table/tbody/tr/td[2]/div/div")).length;
-
-console.log('ho');
+var xpaths = ["//*[@id='phenxTooltip']//following-sibling::table/tbody/tr/td[1]/div/div", "//*[@id='phenxTooltip']//following-sibling::table/tbody/tr/td[2]/div/div"];
+var counter = 0;
+async.eachSeries(xpaths, function (xpath, doneAllXpaths) {
+        driver.findElements(webdriver.By.xpath(xpath)).then(function (links) {
+            var measures = {};
+            links.forEach(function (link) {
+                link.findElement(webdriver.By.tagName('a')).then(function (hrefs) {
+                    hrefs[0].getText().then(function (text) {
+                        measures['addText'] = text;
+                    });
+                    hrefs[0].getAttrib('href').then(function (text) {
+                        measures['addLink'] = text;
+                    });
+                    hrefs[1].getText().then(function (text) {
+                        measures['text'] = text;
+                    });
+                    hrefs[1].getAttrib('href').then(function (text) {
+                        measures['link'] = text;
+                    });
+                    console.log('ha');
+                })
+            });
+        });
+    }, function doneAllXpaths() {
+        console.log('finished all measures');
+    }
+);
 /*
  mongo_form.byId(form, function (err, existingForm) {
  if (err) throw err;
