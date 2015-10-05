b2e01d9e05267c53168921d714ece80af11e208f
unknown <huangs8@NLM01961050MLB.nlm.nih.gov>
2015-10-05 12:07:37 -0400

    x

--- ingester/phenx/PhenxLoader.js
+++ ingester/phenx/PhenxLoader.js
@@ -7,17 +7,19 @@ var webdriver = require('selenium-webdriver'),
     async = require('async');
 
 // global variables
+var baseUrl = "https://www.phenxtoolkit.org/index.php?pageLink=browse.measures&tree=off";
 var mongoUrl = config.mongoUri;
+var conn = mongoose.createConnection(mongoUrl, {auth: {authdb: "admin"}});
+var cacheSchema = mongoose.Schema({}, {strict: false});
+var Cache = conn.model('cache', cacheSchema);
+var driver = new webdriver.Builder().forBrowser('firefox').build();
+var user = {username: "batchloader"};
+
 var allTasks = [];
-var init = function () {
-    var driver = new webdriver.Builder().forBrowser('firefox').build();
-    var baseUrl = "https://www.phenxtoolkit.org/index.php?pageLink=browse.measures&tree=off";
-    var user = {username: "batchloader"};
+var step1 = function () {
     var xpaths = ["//*[@id='phenxTooltip']//following-sibling::table/tbody/tr/td[1]/div/div", "//*[@id='phenxTooltip']//following-sibling::table/tbody/tr/td[2]/div/div"];
     var counter = 0;
     driver.get(baseUrl);
-    var cacheSchema = mongoose.Schema({}, {strict: false});
-    var Cache = conn.model('cache', cacheSchema);
     async.eachSeries(xpaths, function (xpath, doneOneXpath) {
             driver.findElements(webdriver.By.xpath(xpath)).then(function (links) {
                 async.eachSeries(links, function (link, doneOneLink) {
@@ -37,18 +39,13 @@ var init = function () {
             async.eachSeries(allTasks, function (task, doneOneTask) {
                 var obj = {};
                 obj['href1'] = task;
-                driver.get(task);
-                driver.findElement(webdriver.By.xpath('//*[@id="browse_measure_protocol_list"]/table/tbody/tr/td/div/div/a[2]')).then(function (link) {
-                    link.getAttribute('href').then(function (text) {
-                        obj['href2'] = text;
-                        obj['done'] = false;
-                    });
-                });
+                obj['done'] = false;
+                var cache = new Cache(obj);
+                cache.save();
+                doneOneTask();
             }),
                 function doneAllTasks() {
                     driver.quit();
-                    var cache = new Cache(obj);
-                    cache.save();
                     conn.close(function (err) {
                         if (err) throw err;
                     });
@@ -58,12 +55,40 @@ var init = function () {
     );
 };
 
-var conn = mongoose.createConnection(mongoUrl, {auth: {authdb: "admin"}});
+var step2 = function () {
+    Cache.find({}, function (err, caches) {
+        if (err) throw err;
+        async.eachSeries(caches, function (cache, doneOne) {
+            var href1 = cache.get('href1');
+            driver.get(href1);
+            var href2 = [];
+            driver.findElements(webdriver.By.xpath("//*[@id='browse_measure_protocol_list']/table/tbody/tr")).then(function (trs) {
+                async.eachSeries(trs, function (tr, doneOneTr) {
+                    tr.findElement(webdriver.By.css('a')).then(function (links) {
+                        links[1].getAttribute('href').then(function (text) {
+                            href2.push(text);
+                            doneOneTr();
+                        })
+                    });
+                }, function doneAllTrs() {
+                    cache['href2'] = href2;
+                    cache.save(function () {
+                        doneOne();
+                    });
+                });
+            });
+        }, function doneAll() {
+            console.log('finished all');
+        });
+    })
+};
 conn.on('error', function (err) {
     throw err;
 });
 conn.once('open', function callback() {
     console.log("connected to " + mongoUrl);
-    setTimeout(init(), 3000);
+
+//    setTimeout(step1(), 3000);
+    setTimeout(step2(), 3000);
 });
 
