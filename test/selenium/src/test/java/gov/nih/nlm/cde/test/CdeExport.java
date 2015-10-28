bbd92dc2f3fdf0a89317774342c67631892ec884
Jakub Flaska <jakub.flaska@nih.gov>
2015-10-28 14:29:59 -0400

    x

--- test/selenium/src/test/java/gov/nih/nlm/cde/test/CdeExport.java
+++ test/selenium/src/test/java/gov/nih/nlm/cde/test/CdeExport.java
@@ -14,9 +14,13 @@ public class CdeExport extends NlmCdeBaseTest {
         String cdeName = "Spinal column injury number";
         goToCdeByName(cdeName);
         findElement(By.id("export")).click();
-        String url = findElement(By.id("jsonExport")).getAttribute("href");
-        String response = get(url).asString();
+        findElement(By.id("jsonExport")).click();
+
+        switchTab(1);
+        String response = findElement(By.cssSelector("HTML")).getAttribute("innerHTML");
+        System.out.print(response);
         Assert.assertTrue(response.contains("derivationRules\":[],\"referenceDocuments\":[],\"attachments\":[],\"comments\":[],\"mappingSpecifications\":[],\"ids\":[{\"source\":\"NINDS\",\"id\":\"C06429\",\"version\":\"3\"},{\"source\":\"NINDS Variable Name\",\"id\":\"SpnlColmInjNum\"}],\"properties\":[{\"key\":\"NINDS Guidelines\",\"value\":\"SCI:\\nRecord the injury number if 2 or more injuries were experienced. Check against single/multiple variable to ensure only one record exists if Single was checked, and 2 or more records exist if Multiple was checked.\",\"valueFormat\":\"html\"},{\"key\":\"NINDS Notes\",\"value\":\"SCI:\\nSCI Variable: SPNINJNO\",\"valueFormat\":\"html\"},{\"key\":\"NINDS Suggested Question\",\"value\":\"SCI (F0831):\\nSpinal column injury number\",\"valueFormat\":\"html\"},{\"key\":\"NINDS References\",\"value\":\"SCI:\\nDvorak MF, Wing PC, Fehlings MG, Vaccaro AR, Itshayek E, Biering-Sorensen F, Noonan VK. International Spinal Cord Injury Spinal Column Injury Basic Data Set. Spinal Cord. 2012 Jun 5. [Epub ahead of print]\",\"valueFormat\":\"html\"}],\"classification\":[{\"elements\":[{\"name\":\"Population\",\"elements\":[{\"name\":\"Adult\"}]},{\"name\":\"Domain\",\"elements\":[{\"elements\":[{\"name\":\"The International SCI Data Sets\"}],\"name\":\"The International SCI Data Sets\"}]},{\"name\":\"Disease\",\"elements\":[{\"elements\":[{\"elements\":[{\"name\":\"Supplemental\"}],\"name\":\"Classification\"}],\"name\":\"Spinal Cord Injury\"}]}],\"stewardOrg\":{\"name\":\"NINDS\"}}],\"registrationState\":{\"registrationStatus\":\"Qualified\"},\"history\":[],\"valueDomain\":{\"datatype\":\"Integer\",\"permissibleValues\":[]},\"property\":{\"concepts\":[]},\"objectClass\":{\"concepts\":[]},\"dataElementConcept\":{\"concepts\":[]},\"stewardOrg\":{\"name\":\"NINDS\"},\"naming\":[{\"designation\":\"Spinal column injury number\",\"definition\":\"Number assigned to the spinal column injury. The spinal column injuries are assigned numbers starting with the most cephalic spinal column injury.\",\"languageCode\":\"EN-US\",\"context\":{\"contextName\":\"Health\",\"acceptability\":\"preferred\"}}]}"));
+        switchTabAndClose(0);
     }
 
     @Test
@@ -24,10 +28,12 @@ public class CdeExport extends NlmCdeBaseTest {
         String cdeName = "Spinal column injury number";
         goToCdeByName(cdeName);
         findElement(By.id("export")).click();
-        String url = findElement(By.id("xmlExport")).getAttribute("href");
-        String response = get(url).asString();
-        Assert.assertTrue(response.replaceAll("\\s+","").contains((
-                        "</ids>\n" +
+        findElement(By.id("xmlExport")).click();
+
+        switchTab(1);
+        String response = findElement(By.cssSelector("HTML")).getAttribute("innerHTML");
+        Assert.assertTrue(response.replaceAll("\\s+", "").contains((
+                "</ids>\n" +
                         "<ids>\n" +
                         "<source>NINDS Variable Name</source>\n" +
                         "<id>SpnlColmInjNum</id>\n" +
@@ -111,5 +117,6 @@ public class CdeExport extends NlmCdeBaseTest {
                         "<acceptability>preferred</acceptability>\n" +
                         "</context>\n" +
                         "</naming>\n").replaceAll("\\s+","")));
+        switchTabAndClose(0);
     }
 }
