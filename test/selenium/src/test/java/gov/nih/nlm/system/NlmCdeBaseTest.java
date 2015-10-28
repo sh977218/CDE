63c9b39be86853109d070c39cd8ea65834868d77
unknown <ludetc@NLM01960809MLB.nlm.nih.gov>
2015-10-28 15:01:47 -0400

    fix reorder name test

--- test/selenium/src/test/java/gov/nih/nlm/system/NlmCdeBaseTest.java
+++ test/selenium/src/test/java/gov/nih/nlm/system/NlmCdeBaseTest.java
@@ -667,16 +667,16 @@ public class NlmCdeBaseTest {
     protected void reorderIconTest(String tabName) {
         String prefix = "//div[@id='" + tabName + "']//div//*[@id='";
         String postfix = "']";
-        Assert.assertEquals(driver.findElements(By.xpath(prefix + "moveDown-0" + postfix)).size(), 1);
+        findElement(By.xpath(prefix + "moveDown-0" + postfix));
         Assert.assertEquals(driver.findElements(By.xpath(prefix + "moveUp-0" + postfix)).size(), 0);
         Assert.assertEquals(driver.findElements(By.xpath(prefix + "moveTop-0" + postfix)).size(), 0);
 
-        Assert.assertEquals(driver.findElements(By.xpath(prefix + "moveDown-1" + postfix)).size(), 1);
-        Assert.assertEquals(driver.findElements(By.xpath(prefix + "moveUp-1" + postfix)).size(), 1);
-        Assert.assertEquals(driver.findElements(By.xpath(prefix + "moveTop-1" + postfix)).size(), 1);
+        findElement(By.xpath(prefix + "moveDown-1" + postfix));
+        findElement(By.xpath(prefix + "moveUp-1" + postfix));
+        findElement(By.xpath(prefix + "moveTop-1" + postfix));
 
         Assert.assertEquals(driver.findElements(By.xpath(prefix + "moveDown-2" + postfix)).size(), 0);
-        Assert.assertEquals(driver.findElements(By.xpath(prefix + "moveUp-2" + postfix)).size(), 1);
-        Assert.assertEquals(driver.findElements(By.xpath(prefix + "moveTop-2" + postfix)).size(), 1);
+        driver.findElement(By.xpath(prefix + "moveUp-2" + postfix));
+        findElement(By.xpath(prefix + "moveTop-2" + postfix));
     }
 }
