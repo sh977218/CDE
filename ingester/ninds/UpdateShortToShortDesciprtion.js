c0f2838a11aeb1a800c1c9153e0e63029991ca9f
unknown <huangs8@NLM01961050MLB.nlm.nih.gov>
2015-11-03 14:15:06 -0500

    fix script with limit

--- ingester/ninds/UpdateShortToShortDesciprtion.js
+++ ingester/ninds/UpdateShortToShortDesciprtion.js
@@ -1,10 +1,15 @@
-db.getCollection('dataelements').update(
-{"naming.context.contextName": "Short", source: "NINDS"},
-{$set: {"naming.$.context.contextName": "Short Description"}},
-{multi: true})
+db.dataelements.find(
+    {"naming.context.contextName": "Short", source: "NINDS"})
+    .limit(1000).forEach(function (doc) {
+        doc.naming.forEach(function (name) {
+            if (name.context.contextName === 'Short')
+                name.context.contextName = 'Short Description';
+            db.dataelements.save(doc);
+        })
+    });
 
-
 db.getCollection('dataelements').update(
-{source: "NINDS"},
-{$rename: {"valueDomain.datatypeInteger": "valueDomain.datatypeNumber"}},
-{multi: true})
+    {source: "NINDS","valueDomain.datatypeInteger":{ $exists: true }},
+    {$rename: {"valueDomain.datatypeInteger": "valueDomain.datatypeNumber"}},
+    {multi: true});
+
