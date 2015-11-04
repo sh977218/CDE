a7544c6a3618caa436c43a5b1bc4e2b466847152
Jakub Flaska <jakub.flaska@nih.gov>
2015-11-04 13:23:21 -0500

    x

--- modules/system/node-js/elastic.js
+++ modules/system/node-js/elastic.js
@@ -404,7 +404,7 @@ exports.elasticSearchExport = function (res, query, type, exporter) {
                     else {
                         for (var i = 0; i < response.hits.hits.length; i++) {
                             var thisCde = response.hits.hits[i]._source;
-                            res.write(exporter.transformObject(cdesvc.hideProprietaryPvs([thisCde])));
+                            res.write(exporter.transformObject(cdesvc.hideProprietaryPvs(thisCde)));
                             sentElements++;
                             var isLast = sentElements === response.hits.total;
                             if (exporter.delimiter && !isLast) res.write(exporter.delimiter);
