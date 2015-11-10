30f25354bad3295c797144f7b68024c7e09d4e87
unknown <huangs8@NLM01961050MLB.nlm.nih.gov>
2015-11-10 17:31:42 -0500

    finished compare for non-question properties

--- modules/cde/public/js/ng-compare-side-by-side.js
+++ modules/cde/public/js/ng-compare-side-by-side.js
@@ -1,56 +1,54 @@
 (function () {
     'use strict';
     angular.module('ngCompareSideBySide', [])
-        .directive("ngCompareSideBySide", ["$compile", "Comparsion",
-            function ($compile, Comparsion) {
+        .directive("ngCompareSideBySide", ["$compile", "Comparison",
+            function ($compile, Comparison) {
                 return {
                     restrict: "AE",
                     scope: {
                         left: '=',
                         right: '='
                     },
-                    controller: function ($scope, $element, $attrs) {
+                    controller: function ($scope, $element) {
                     },
-                    link: function ($scope, $element, $attrs) {
-                        var elt1 = Comparsion.deepCopy($scope.left);
+                    link: function ($scope, $element) {
+                        var elt1 = Comparison.deepCopy($scope.left);
                         elt1.questions = [];
-                        Comparsion.flatFormQuestions(elt1, elt1.questions);
-                        Comparsion.sortPropertiesByName(elt1);
-                        Comparsion.wipeUseless(elt1);
+                        Comparison.flatFormQuestions(elt1, elt1.questions);
+                        Comparison.sortPropertiesByName(elt1);
+                        Comparison.wipeUseless(elt1);
 
-                        var elt2 = Comparsion.deepCopy($scope.right);
+                        var elt2 = Comparison.deepCopy($scope.right);
                         elt2.questions = [];
-                        Comparsion.flatFormQuestions(elt2, elt2.questions);
-                        Comparsion.sortPropertiesByName(elt2);
-                        Comparsion.wipeUseless(elt2);
+                        Comparison.flatFormQuestions(elt2, elt2.questions);
+                        Comparison.sortPropertiesByName(elt2);
+                        Comparison.wipeUseless(elt2);
 
-                        var result1 = {};
-                        var result2 = {};
-                        for (var property in elt1) {
+                        var result = {};
+                        Comparison.propertiesToBeCompared().forEach(function (property) {
                             var diff = DeepDiff(elt1[property], elt2[property]);
                             if (diff && diff.length > 0) {
-                                result1[property] = 'modified';
-                                result2[property] = 'modified';
-                                console.log('diff on ' + property);
-                                console.log(diff);
+                                result[property] = 'modified';
                             }
                             else {
-                                result1[property] = 'unmodified';
-                                result2[property] = 'unmodified';
-                                console.log('no diff on ' + property);
+                                result[property] = 'unmodified';
                             }
-                        }
-                        $scope.result1 = result1;
-                        $scope.result2 = result2;
-                        $scope.elt1 = elt1;
-                        $scope.elt2 = elt2;
+                        });
+                        $scope.result = result;
+                        $scope.elts = [];
+                        $scope.elts.push(elt1);
+                        $scope.elts.push(elt2);
 
-                        Comparsion.applyComparsion($scope, $element);
+                        Comparison.applyComparison($scope, $element);
                     }
                 };
             }])
-        .factory("Comparsion", ["$compile", function ($compile) {
+        .factory("Comparison", ["$compile", function ($compile) {
             return {
+                propertiesToBeCompared: function () {
+                    var p = ['naming', 'properties', 'dataElementConcept', 'referenceDocuments', 'stewardOrg', 'registrationState', 'formUsageCounter', 'views', 'created', 'updated', 'createdBy'];
+                    return p;
+                },
                 sortPropertiesByName: function (o) {
                     for (var p in o) {
                         if (p !== 'questions' && Array.isArray(p)) {
@@ -80,15 +78,11 @@
                     delete o.__v;
                     delete o.history;
                     delete o.imported;
-                    delete o.created;
-                    delete o.createdBy;
                     delete o.noRenderAllowed;
                     delete o.displayProfiles;
                     delete o.attachments;
-                    delete o.updated;
                     delete o.version;
                     delete o.comments;
-                    delete o.registrationState;
                     delete o.tinyId;
                     delete o.derivationRules;
                     delete o.usedBy;
@@ -100,39 +94,89 @@
                         delete q._id;
                     })
                 },
-                applyComparsion: function ($scope, $element) {
+                applyComparison: function ($scope, $element) {
                     var elStr = '' +
-                        '<div class="row">' +
-                        '   <div class="col-xs-6 col-lg-6 col-md-6 noLeftPadding">' +
-                        '       <div class="col-xs-12 col-md-12" ng-repeat="(attribute, value) in result1">' +
-                        '           <div class="col-lg-12 col-md-12 col-xs-12 noLeftRightPadding">' +
-                        '               <strong>{{attribute}}</strong>:' +
-                        '           </div>' +
-                        '           <div class="col-lg-12 col-md-12 col-xs-12 noLeftRightPadding" ng-if="value===' + "'modified'" + '"' +
-                        '           ng-class="{quickBoardCompareDifferent:value===' + "'modified'" + '}">{{elt1[attribute]}}' +
-                        '           </div>' +
-                        '           <div class="col-lg-12 col-md-12 col-xs-12 noLeftRightPadding" ng-if="value===' + "'unmodified'" + '"' +
-                        '           ng-class="{quickBoardCompareSame:value===' + "'unmodified'" + '}">{{elt1[attribute]}}' +
-                        '           </div>' +
-                        '           <div class="smallSpace"></div>' +
-                        '       </div>' +
+                        '<div class="row" ng-class="{quickBoardCompareDifferent:{{result.naming===\'modified\'}}}">' +
+                        '   <div class="col-xs-6 col-lg-6 col-md-6 noLeftPadding" ng-repeat="elt in elts">' +
+                        '       <div ng-include="\'/system/public/html/naming.html\'"></div>' +
                         '   </div>' +
+                        '</div>' +
+                        '<hr class="divider">';
 
-                        '   <div class="col-xs-6 col-lg-6 col-md-6 noRightPadding">' +
-                        '       <div class="col-xs-12 col-md-12" ng-repeat="(attribute, value) in result2">' +
-                        '       <div class="col-lg-12 col-md-12 col-xs-12 noLeftRightPadding">' +
-                        '           <strong>{{attribute}}</strong>:' +
-                        '       </div>' +
-                        '       <div class="col-lg-12 col-md-12 col-xs-12 noLeftRightPadding" ng-if="value===' + "'modified'" + '"' +
-                        '       ng-class="{quickBoardCompareDifferent:value===' + "'modified'" + '}">{{elt2[attribute]}}' +
-                        '       </div>' +
-                        '       <div class="col-lg-12 col-md-12 col-xs-12 noLeftRightPadding" ng-if="value===' + "'unmodified'" + '"' +
-                        '       ng-class="{quickBoardCompareSame:value===' + "'unmodified'" + '}">{{elt2[attribute]}}' +
-                        '       </div>' +
-                        '       <div class="smallSpace"></div>' +
-                        '   </div>' +
+                    elStr = elStr +
+                    '<div class="row" ng-class="{quickBoardCompareDifferent:{{result.stewardOrg===\'modified\'}}}">' +
+                    '   <div ng-repeat="elt in elts" class="col-md-6">' +
+                    '       <div tabindex="0" class="col-lg-2 col-md-2"><strong>Steward:</strong></div>' +
+                    '       <div tabindex="0" class="col-lg-10 col-md-10">{{elt.stewardOrg.name}}</div>' +
+                    '   </div>' +
+                    '</div>';
+                    elStr = elStr +
+                    '<div class="row" ng-class="{quickBoardCompareDifferent:{{result.registrationState===\'modified\'}}}">' +
+                    '   <div ng-repeat="elt in elts" class="col-md-6">' +
+                    '       <div tabindex="0" class="col-lg-2 col-md-2"><strong>Status:</strong></div>' +
+                    '       <div tabindex="0" class="col-lg-10 col-md-10">{{elt.registrationState.registrationStatus}}</div>' +
+                    '   </div>' +
+                    '</div>';
+                    elStr = elStr +
+                    '<div class="row" ng-class="{quickBoardCompareDifferent:{{result.formUsageCounter===\'modified\'}}}">' +
+                    '   <div ng-repeat="elt in elts" class="col-md-6">' +
+                    '       <div tabindex="0" class="col-lg-2 col-md-2" ng-if="elt.formUsageCounter"><strong>Form Usage:</strong></div>' +
+                    '       <div tabindex="0" class="col-lg-10 col-md-10" ng-if="elt.formUsageCounter">{{elt.formUsageCounter}}</div>' +
+                    '   </div>' +
+                    '</div>';
+                    elStr = elStr +
+                    '<div class="row" ng-class="{quickBoardCompareDifferent:{{result.views===\'modified\'}}}">' +
+                    '   <div ng-repeat="elt in elts" class="col-md-6">' +
+                    '       <div tabindex="0" class="col-lg-2 col-md-2" ng-if="elt.views"><strong>Views:</strong></div>' +
+                    '       <div tabindex="0" class="col-lg-10 col-md-10" ng-if="elt.views">{{elt.views}}</div>' +
+                    '   </div>' +
+                    '</div>';
+                    elStr = elStr +
+                    '<div class="row" ng-class="{quickBoardCompareDifferent:{{result.created===\'modified\'}}}">' +
+                    '   <div ng-repeat="elt in elts" class="col-md-6">' +
+                    '       <div tabindex="0" class="col-lg-2 col-md-2"><strong>Added to NLM:</strong></div>' +
+                    '       <div tabindex="0" class="col-lg-10 col-md-10">{{elt.created | date: \'MM/dd/yyyy @ h:mma\'}}</div>' +
+                    '   </div>' +
+                    '</div>';
+                    elStr = elStr +
+                    '<div class="row" ng-class="{quickBoardCompareDifferent:{{result.updated===\'modified\'}}}">' +
+                    '   <div ng-repeat="elt in elts" class="col-md-6">' +
+                    '       <div tabindex="0" class="col-lg-2 col-md-2" ng-if="elt.updated"><strong>Updated in NLM:</strong></div>' +
+                    '       <div tabindex="0" class="col-lg-10 col-md-10" ng-if="elt.updated">{{elt.updated | date: \'MM/dd/yyyy @ h:mma\'}}</div>' +
+                    '   </div>' +
+                    '</div>';
+                    elStr = elStr +
+                    '<div class="row" ng-class="{quickBoardCompareDifferent:{{result.createdBy===\'modified\'}}}">' +
+                    '   <div ng-repeat="elt in elts" class="col-md-6">' +
+                    '       <div tabindex="0" class="col-lg-2 col-md-2" ng-if="elt.createdBy"><strong>Created By:</strong></div>' +
+                    '       <div tabindex="0" class="col-lg-10 col-md-10" ng-if="elt.createdBy">{{elt.createdBy.username}}</div>' +
+                    '   </div>' +
+                    '</div>';
 
-                        '</div>';
+                    elStr = elStr +
+                    '<div class="row" ng-class="{quickBoardCompareDifferent:{{result.dataElementConcept===\'modified\'}}}">' +
+                    '   <div class="col-xs-6 col-lg-6 col-md-6 noLeftPadding" ng-repeat="elt in elts">' +
+                    '       <div ng-controller="ConceptsCtrl" ng-include="\'/cde/public/html/deConcepts.html\'"></div>' +
+                    '   </div>' +
+                    '</div>' +
+                    '<hr class="divider">';
+
+                    elStr = elStr +
+                    '<div class="row" ng-class="{quickBoardCompareDifferent:{{result.referenceDocuments===\'modified\'}}}">' +
+                    '   <div class="col-xs-6 col-lg-6 col-md-6 noLeftPadding" ng-repeat="elt in elts">' +
+                    '       <div ng-include="\'/system/public/html/referenceDocument.html\'"></div>' +
+                    '   </div>' +
+                    '</div>' +
+                    '<hr class="divider">';
+
+                    elStr = elStr +
+                    '<div class="row" ng-class="{quickBoardCompareDifferent:{{result.properties===\'modified\'}}}">' +
+                    '   <div class="col-xs-6 col-lg-6 col-md-6 noLeftPadding" ng-repeat="elt in elts">' +
+                    '       <div ng-include="\'/system/public/html/properties.html\'"></div>' +
+                    '   </div>' +
+                    '</div>' +
+                    '<hr class="divider">';
+
                     var el = angular.element(elStr);
                     $compile(el)($scope);
                     $element.append(el);
