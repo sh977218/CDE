4a712945f92ebf2510aa7839492f031b680cab26
unknown <huangs8@NLM01961050MLB.nlm.nih.gov>
2015-11-23 16:22:22 -0500

    fix css

--- modules/system/public/js/ng-display-object.js
+++ modules/system/public/js/ng-display-object.js
@@ -67,7 +67,7 @@
                             return "<p>" + o[$scope.properties.displayAs] + "</p>";
                         }).join("");
                     var objectHtml = '' +
-                        '<div class="row" ng-class="{quickBoardContentCompareDiff:$scope.showWarning && properties.match===false}">' +
+                        '<div class="row" ng-class="{quickBoardContentCompareDiff:showwarningicon && properties.match===false}">' +
                         '   <div class="col-xs-4">{{properties.label}}:</div>' +
                         '   <div ng-if="properties.link" class="col-xs-7"><a ng-href="{{properties.url}}' + _this.getValueByNestedProperty($scope.obj, $scope.properties.property) + '">' + _this.getValueByNestedProperty($scope.obj, $scope.properties.property) + '</a></div>' +
                         '   <div ng-if="!properties.link" class="col-xs-7" ng-bind-html="value" ng-text-truncate="value" ng-tt-threshold="500"></div>';
