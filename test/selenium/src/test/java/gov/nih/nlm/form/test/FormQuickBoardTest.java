fde0f0b89d3edf18d2433f15ab37ece5de106c40
unknown <huangs8@NLM01961050MLB.nlm.nih.gov>
2015-11-27 16:31:31 -0500

    add 2 form in bson

---
+++ test/selenium/src/test/java/gov/nih/nlm/form/test/FormQuickBoardTest.java
@@ -0,0 +1,19 @@
+package gov.nih.nlm.form.test;
+
+import gov.nih.nlm.system.NlmCdeBaseTest;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class FormQuickBoardTest extends NlmCdeBaseTest {
+    @Test
+    public void formSideBySideCompare() {
+        addFormToQuickBoard("compareForm1");
+        addFormToQuickBoard("compareForm2");
+        textPresent("Quick Board (2)");
+        clickElement(By.id("menu_qb_link"));
+        clickElement(By.xpath("//*[@id=\"qb_form_tab\"]/a"));
+        clickElement(By.id("qb_cde_compare_0"));
+        clickElement(By.id("qb_cde_compare_1"));
+        clickElement(By.id("qb_cde_compare"));
+    }
+}
