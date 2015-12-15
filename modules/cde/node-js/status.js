32a824d73bbd12e1a195aa0cd3e79918fe39c3d1
Jakub Flaska <jakub.flaska@nih.gov>
2015-12-15 14:24:24 -0500

    x

--- modules/cde/node-js/status.js
+++ modules/cde/node-js/status.js
@@ -124,8 +124,8 @@ status.checkElasticUpdating = function(body, statusReport, elasticUrl, mongoColl
     var fakeCde = {
         stewardOrg: {name: "FAKE"}
         , naming: [{
-            designation: "NLM_APP_Status_Report_" + config.hostname
-            , definition: "NLM_APP_Status_Report_" + config.hostname
+            designation: "NLM_APP_Status_Report_" + config.hostname.replace(/[^A-z|0-9]/g,"")
+            , definition: "NLM_APP_Status_Report_" + config.hostname.replace(/[^A-z|0-9]/g,"")
         }]
         , registrationState: {registrationStatus: "Retired"}
         , version: stamp
@@ -133,7 +133,7 @@ status.checkElasticUpdating = function(body, statusReport, elasticUrl, mongoColl
 
     mongoCollection.upsertStatusCde(fakeCde, function(err, mongoCde) {
         setTimeout(function() {
-            request.get(elasticUrl + "_search?q=" + "NLM_APP_Status_Report_"+config.hostname, function (error, response, bodyStr) {
+            request.get(elasticUrl + "_search?q=" + "NLM_APP_Status_Report_"+config.hostname.replace(/[^A-z|0-9]/g,""), function (error, response, bodyStr) {
                 var errorToLog = {
                     origin: "cde.status.checkElasticUpdating"
                     , stack: new Error().stack
