3246fa5d973aa49cda6127cbef91d303903c2194
unknown <ludetc@NLM01960809MLB.nlm.nih.gov>
2015-12-29 09:52:17 -0500

    catch exceptionin screenshot listener

--- test/selenium/src/test/java/gov/nih/nlm/system/ScreenShotListener.java
+++ test/selenium/src/test/java/gov/nih/nlm/system/ScreenShotListener.java
@@ -11,8 +11,6 @@ import org.testng.TestListenerAdapter;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
@@ -26,11 +24,11 @@ public class ScreenShotListener extends TestListenerAdapter {
     public void onTestFailure(ITestResult itr) {
         String methodName = itr.getName();
         if (!itr.isSuccess()) {
-            File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
             try {
+                File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
                 FileUtils.copyFile(scrFile, new File("build/screenshots/" + methodName + "_" + formater.format(calendar.getTime()) + ".png"));
-            } catch (IOException e1) {
-                e1.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         }
         saveLogs(methodName);
@@ -46,14 +44,18 @@ public class ScreenShotListener extends TestListenerAdapter {
         LogEntries logEntries = driver.manage().logs().get(LogType.BROWSER);
         StringBuilder sb = new StringBuilder();
         for (LogEntry entry : logEntries) {
-            if (entry.getMessage().indexOf("Range.detach") < 0)
-                sb.append(new Date(entry.getTimestamp()) + " " + entry.getLevel() + " " + entry.getMessage() + "\n");
+            if (!entry.getMessage().contains("Range.detach"))
+                sb.append(new Date(entry.getTimestamp()));
+                sb.append(" ");
+                sb.append(entry.getLevel());
+                sb.append(" ");
+                sb.append(entry.getMessage());
+                sb.append("\n");
         }
         if (sb.length() > 0) {
             try {
                 FileUtils.writeStringToFile(new File("build/consolelogs/" + methodName + "_" + formater.format(calendar.getTime()) + ".txt"), sb.toString());
             } catch (IOException e1) {
-                System.out.println(e1);
                 e1.printStackTrace();
             }
         }        
