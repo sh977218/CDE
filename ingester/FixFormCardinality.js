8c5fec6e6b3533765ac7e7823f432d69e01948a3
unknown <huangs8@NLM01961050MLB.nlm.nih.gov>
2016-01-11 16:54:51 -0500

    add script

---
+++ ingester/FixFormCardinality.js
@@ -0,0 +1,49 @@
+var mongoose = require('mongoose'),
+    config = require('../modules/system/node-js/parseConfig'),
+    form_schemas = require('../modules/form/node-js/schemas'),
+    form_mongo = require('../modules/form/node-js/mongo-form'),
+    async = require('async');
+
+var mongoUrl = config.mongoUri;
+var conn = mongoose.createConnection(mongoUrl);
+var Form = conn.model('Form', form_schemas.formSchema);
+var counter = 0;
+var user = {username: "peter"};
+
+function loopFormElements(fe) {
+    fe.formElements.forEach(function (e) {
+        if (e.cardinality !== undefined) {
+            e.cardinality = {
+                "0.1": {
+                    min: 0, max: 1
+                },
+                "*": {
+                    min: 0, max: -1
+                },
+                "+": {
+                    min: 1, max: -1
+                },
+                "1": {
+                    min: 1, max: 1
+                }
+            }[e.cardinality];
+        }
+        loopFormElements(e);
+    });
+    return;
+};
+
+Form.find({}).exec(function (err, forms) {
+    if (err) throw err;
+    async.eachSeries(forms, function (form, doneOneForm) {
+        var formObj = form.toObject();
+        loopFormElements(formObj);
+        form_mongo.update(formObj, user, function () {
+            counter++;
+            console.log('form' + counter + ' form id: ' + form.tinyId);
+            doneOneForm();
+        });
+    }, function doneAllForms() {
+        console.log('finished all forms: ' + counter);
+    });
+});
--- modules/form/public/js/controllers/SectionCtrl.js
+++ modules/form/public/js/controllers/SectionCtrl.js
@@ -10,19 +10,16 @@ angular.module('formModule').controller('SectionCtrl', ['$scope', '$uibModal', '
                 {label: "0 or 1", value: {min: 0, max: 1}}
             ];
         $scope.getCardinalityLabel = function (cardinality) {
-            if (JSON.stringify(cardinality) === JSON.stringify({min: 1, max: 1}))
-                return "Exactly 1";
-            else if (JSON.stringify(cardinality) === JSON.stringify({min: 1, max: -1}))
-                return "1 or more";
-            else if (JSON.stringify(cardinality) === JSON.stringify({min: 0, max: -1}))
-                return "0 or more";
-            else if (JSON.stringify(cardinality) === JSON.stringify({min: 0, max: 1}))
-                return "0 or 1";
-            else return "";
-        };
-
-        $scope.saveCardinality = function (o, cardinality) {
-            o.cardinality = cardinality.value;
+            return {
+                "0": {
+                    "1": "0 or 1"
+                    , "-1": "0 or more"
+                },
+                "1": {
+                    "1": "Exactly 1"
+                    , "-1": "1 or more"
+                }
+            }[cardinality.min][cardinality.max];
         };
 
         $scope.addSection = function () {
