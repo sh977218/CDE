042b6f27c8bb421cf46f82e2b5745565031f9ef9
unknown <ludetc@NLM01960809MLB.nlm.nih.gov>
2016-01-28 10:59:17 -0500

    fix required test

--- test/selenium/src/test/java/gov/nih/nlm/form/test/RenderRequiredFieldsTest.java
+++ test/selenium/src/test/java/gov/nih/nlm/form/test/RenderRequiredFieldsTest.java
@@ -16,14 +16,20 @@ public class RenderRequiredFieldsTest extends NlmCdeBaseTest {
         clickElement(By.linkText("native"));
         List<WebElement> inputs = findElements(By.xpath("//div[@id='formRenderSection_New Section']//input"));
 
-        Assert.assertFalse(isAttributePresent(inputs.get(0), "required"));
-        Assert.assertTrue(isAttributePresent(inputs.get(1), "required"));
+        Assert.assertFalse(isAttributePresent(
+                findElement(By.xpath("(//div[@id='formRenderSection_New Section']//input)[1]")), "required"));
+        Assert.assertTrue(isAttributePresent(
+                findElement(By.xpath("(//div[@id='formRenderSection_New Section']//input)[2]")), "required"));
 
-        Assert.assertFalse(isAttributePresent(inputs.get(2), "required"));
-        Assert.assertTrue(isAttributePresent(inputs.get(3), "required"));
+        Assert.assertFalse(isAttributePresent(
+                findElement(By.xpath("(//div[@id='formRenderSection_New Section']//input)[3]")), "required"));
+        Assert.assertTrue(isAttributePresent(
+                findElement(By.xpath("(//div[@id='formRenderSection_New Section']//input)[4]")), "required"));
 
-        Assert.assertFalse(isAttributePresent(inputs.get(4), "required"));
-        Assert.assertTrue(isAttributePresent(inputs.get(5), "required"));
+        Assert.assertFalse(isAttributePresent(
+                findElement(By.xpath("(//div[@id='formRenderSection_New Section']//input)[5]")), "required"));
+        Assert.assertTrue(isAttributePresent(
+                findElement(By.xpath("(//div[@id='formRenderSection_New Section']//input)[6]")), "required"));
 
     }
 
