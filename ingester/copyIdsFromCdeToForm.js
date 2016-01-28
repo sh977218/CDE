5794494126b94705249e334dcec4e6bab3a59ff4
unknown <huangs8@NLM01961050MLB.nlm.nih.gov>
2016-01-28 18:00:29 -0500

    revert back

--- ingester/copyIdsFromCdeToForm.js
+++ ingester/copyIdsFromCdeToForm.js
@@ -13,58 +13,62 @@ var Form = conn.model('Form', form_schemas.formSchema);
 
 var formCounter = 0;
 
-var stream = Form.find({archived: null}).stream();
-stream.on('data', function (form) {
-    formCounter++;
-    console.log("begin " + formCounter + " form id: " + form.tinyId);
-    var questionCount = 0;
-    var formElements = form.formElements;
-    var areYouDone = function () {
-        console.log(questionCount);
-        if (questionCount === 0) {
-            form.save(function (err) {
-                if (err)
-                    process.exit(1);
-                else {
-                    console.log('saved form id: ' + form.tinyId);
-                }
-            });
-        }
-    };
-    var getQuestions = function (formElements) {
-        formElements.forEach(function (fe) {
-            if (fe.elementType === 'question') {
-                questionCount++;
-                var cdeTinyId = fe.question.cde.tinyId;
-                var version = fe.question.cde.version;
-                DataElement.findOne({tinyId: cdeTinyId, version: version}).exec(function (err, cde) {
-                    questionCount--;
-                    if (err) {
-                        console.log(err);
-                        process.exit(0);
-                    }
-                    console.log('found cde id: ' + cdeTinyId + ' version: ' + version);
-                    if (cde) fe.question.cde.ids = cde.ids;
+Form.find({
+    archived: null
+}).exec(function (err, forms) {
+    if (err) {
+        console.log(err);
+        process.exit(0);
+    }
+    async.eachSeries(forms, function (form, doneOneForm) {
+        console.log("begin " + formCounter + "form id: " + form.tinyId);
+        var questionCount = 0;
+        var areYouDone = function () {
+            console.log(questionCount);
+            if (questionCount === 0) {
+                form.save(function (err) {
+                    if (err)
+                        process.exit(1);
                     else {
-                        console.log("no CDE with id: " + cdeTinyId)
+                        console.log('saved form id: ' + form.tinyId);
+                        formCounter++;
+                        doneOneForm();
                     }
-                    areYouDone();
                 });
             }
-            else {
-                getQuestions(fe.formElements);
-            }
-        });
-    };
-    getQuestions(formElements);
-    form.markModified('formElements');
-    areYouDone();
-}).on('error', function (err) {
-    console.log(err);
-    process.exit(1);
-}).on('close', function () {
-    console.log('finished all forms, # form: ' + formCounter);
-    console.log('stream closed');
-    if (formCounter === 807)
+        };
+        var formElements = form.formElements;
+        var getQuestions = function (formElements) {
+            formElements.forEach(function (fe) {
+                if (fe.elementType === 'question') {
+                    questionCount++;
+                    var cdeTinyId = fe.question.cde.tinyId;
+                    var version = fe.question.cde.version;
+                    DataElement.findOne({tinyId: cdeTinyId, version: version}).exec(function (err, cde) {
+                        questionCount--;
+                        if (err) {
+                            console.log(err);
+                            process.exit(0);
+                        }
+                        console.log('found cde id: ' + cdeTinyId + ' version: ' + version);
+                        if (cde && cde.ids) fe.question.cde.ids = cde.ids;
+                        else {
+                            console.log("no CDE with id: " + cdeTinyId)
+                        }
+                        areYouDone();
+                    });
+                }
+                else {
+                    getQuestions(fe.formElements);
+                }
+            });
+        };
+        getQuestions(formElements);
+        areYouDone();
+        form.markModified('formElements');
+    }, function doneAllForms() {
+        console.log('finished all forms, # form: ' + formCounter);
         process.exit(0);
+    });
 });
+
