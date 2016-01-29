4241ad72befc91014ca2e64c8d596d76c10f9621
unknown <huangs8@NLM01961050MLB.nlm.nih.gov>
2016-01-29 14:45:20 -0500

    remove @test

--- test/selenium/src/test/java/gov/nih/nlm/common/test/ViewTabTest.java
+++ test/selenium/src/test/java/gov/nih/nlm/common/test/ViewTabTest.java
@@ -1,11 +1,7 @@
 package gov.nih.nlm.common.test;
 
-
-import org.testng.annotations.Test;
-
 public abstract class ViewTabTest extends CommonTest {
 
-    @Test
     public void showHideAllTabTest(String eltName) {
         goToEltByName(eltName);
         showAllTabs();
