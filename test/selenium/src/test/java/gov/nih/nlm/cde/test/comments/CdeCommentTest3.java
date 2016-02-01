c9f5cb5f1518c1b3dafcc2da2410c8ebfcc7dfb0
Jakub Flaska <jakub.flaska@nih.gov>
2016-02-01 16:50:00 -0500

    x

--- test/selenium/src/test/java/gov/nih/nlm/cde/test/comments/CdeCommentTest3.java
+++ test/selenium/src/test/java/gov/nih/nlm/cde/test/comments/CdeCommentTest3.java
@@ -1,12 +1,22 @@
 package gov.nih.nlm.cde.test.comments;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import static com.jayway.restassured.RestAssured.get;
 
 public class CdeCommentTest3 extends CdeCommentTest {
         
     @Test
     public void cdeComments() {
         comments("Hospital Confidential Institution Referred From");
+    }
+
+    @Test(dependsOnMethods = {"cdeComments"})
+    public void modifiedSinceAPI() {
+        String response = get(baseUrl + "/api/cde/modifiedElements?from=2016-01-01").asString();
+        Assert.assertFalse(response.contains("Invalid"));
+        Assert.assertTrue(response.contains("Hemf6zdy6o4"));
     }
 
     @Test
