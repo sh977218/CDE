d0ca2818a3d7bcff5a45b54b3e092edfddab7438
unknown <huangs8@NLM01961050MLB.nlm.nih.gov>
2016-02-02 14:46:19 -0500

    add 2 cde 2 form form OrgAdmin2Test test

--- test/selenium/src/test/java/gov/nih/nlm/cde/test/OrgAdmin2Test.java
+++ test/selenium/src/test/java/gov/nih/nlm/cde/test/OrgAdmin2Test.java
@@ -1,6 +1,5 @@
 package gov.nih.nlm.cde.test;
 
-import gov.nih.nlm.form.test.BaseFormTest;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.support.ui.Select;
@@ -12,8 +11,8 @@ public class OrgAdmin2Test extends BaseClassificationTest {
     @Test
     public void adminProfile() {
         mustBeLoggedInAs(cabigAdmin_username, password);
-        findElement(By.id("username_link")).click();
-        findElement(By.linkText("Profile")).click();
+        clickElement(By.id("username_link"));
+        clickElement(By.linkText("Profile"));
         Assert.assertEquals("cabigAdmin", findElement(By.id("username")).getText());
         Assert.assertEquals("1,024.00 MB", findElement(By.id("quota")).getText());
         Assert.assertEquals("", findElement(By.id("curatorFor")).getText());
@@ -26,46 +25,29 @@ public class OrgAdmin2Test extends BaseClassificationTest {
         resizeWindow(1524, 1150);
         mustBeLoggedInAs(transferStewardUser_username, password);
 
-        // create 2 cdes
-        String cde1 = "Transfer Steward Test CDE 1";
-        String definition1 = "Definition for Transfer Steward Test CDE 1";
         String org1 = "PS&CC";
-        String class1 = "caSEER";
-        String subclass1 = "com.imsweb.caseer";
-        fillOutBasicCreateFields(cde1, definition1, org1, class1, subclass1);
-        textPresent(class1);
-        textPresent(subclass1);
-        findElement(By.id("submit")).click();
-        modalGone();
 
-        String cde2 = "Transfer Steward Test CDE 2";
-        String definition2 = "Definition for Transfer Steward Test CDE 2";
-        fillOutBasicCreateFields(cde2, definition2, org1, class1, subclass1);
-        textPresent(class1);
-        textPresent(subclass1);
-        findElement(By.id("submit")).click();
-        modalGone();
-
-        findElement(By.id("username_link")).click();
-        findElement(By.linkText("Account Management")).click();
-        findElement(By.linkText("CDE & Form Management")).click();
+        //
+        clickElement(By.id("username_link"));
+        clickElement(By.linkText("Account Management"));
+        clickElement(By.linkText("CDE & Form Management"));
         scrollToTop();
 
         new Select(findElement(By.name("transferSteward_from"))).selectByVisibleText(org1);
         new Select(findElement(By.name("transferSteward_to"))).selectByVisibleText("LCC");
-        findElement(By.id("transferStewardButton")).click();
+        clickElement(By.id("transferStewardButton"));
         textPresent("8 CDEs transferred.");
         textPresent("2 forms transferred.");
 
         new Select(findElement(By.name("transferSteward_from"))).selectByVisibleText(org1);
         new Select(findElement(By.name("transferSteward_to"))).selectByVisibleText("LCC");
-        findElement(By.id("transferStewardButton")).click();
+        clickElement(By.id("transferStewardButton"));
         textPresent("There are no CDEs to transfer.");
         textPresent("There are no forms to transfer.");
 
         new Select(findElement(By.name("transferSteward_from"))).selectByVisibleText("LCC");
         new Select(findElement(By.name("transferSteward_to"))).selectByVisibleText(org1);
-        findElement(By.id("transferStewardButton")).click();
+        clickElement(By.id("transferStewardButton"));
         textPresent("8 CDEs transferred.");
         textPresent("2 forms transferred.");
         resizeWindow(currentWindowSize.getWidth(), currentWindowSize.getHeight());
@@ -76,7 +58,7 @@ public class OrgAdmin2Test extends BaseClassificationTest {
         mustBeLoggedInAs(cabigAdmin_username, password);
 
         clickElement(By.id("username_link"));
-        findElement(By.linkText("Account Management")).click();
+        clickElement(By.linkText("Account Management"));
 
         textPresent("Organizations Admins");
         textNotPresent("CDE & Form Management");
