37b028b53d83fb07d7ab4b966830509c0882c126
Jakub Flaska <jakub.flaska@nih.gov>
2016-02-03 15:05:33 -0500

    x

--- modules/cde/node-js/mongo-cde.js
+++ modules/cde/node-js/mongo-cde.js
@@ -497,5 +497,5 @@ exports.upsertStatusCde = function(cde, cb){
 };
 
 exports.findModifiedElementsSince = function(date, cb){
-    DataElement.find({updated: {$gte: date}}, {tinyId: 1, _id: 0}).limit(2000).exec(cb);
+    DataElement.find({updated: {$gte: date}}, {tinyId: 1, _id: 0}).sort({updated:-1}).limit(2000).exec(cb);
 };
