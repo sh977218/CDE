6f437dc35d2870ea60a07aae1fdc77ec3b03d9d5
unknown <ludetc@NLM01960809MLB.nlm.nih.gov>
2016-02-10 13:57:56 -0500

    done

--- test/selenium/src/test/java/gov/nih/nlm/cde/test/boards/BoardExportTest.java
+++ test/selenium/src/test/java/gov/nih/nlm/cde/test/boards/BoardExportTest.java
@@ -1,7 +1,6 @@
 package gov.nih.nlm.cde.test.boards;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.TimeoutException;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -16,8 +15,6 @@ public class BoardExportTest extends BoardTest {
     @Test
     public void boardExport() {
         String board_name = "Board Export Test";
-        String board_description = "Test for board export";
-        mustBeLoggedInAs("boardexport", password);
 
         goToBoard(board_name);
         textPresent("Export Board");
@@ -26,25 +23,24 @@ public class BoardExportTest extends BoardTest {
         closeAlert();
 
         String[] expected = {
-                "Name, Other Names, Value Domain, Permissible Values, Identifiers, Steward, Registration Status, Administrative Status, Used By",
-                "\"Scale for the Assessment of Positive Symptoms (SAPS) - voice conversing  scale\",\"Like voices commenting, voices conversing are cons",
-                "\"Intravesical Protocol Agent Administered Specify\",\"No explain\",\"CHARACTER\",\"\",\"caDSR: 2399243 v1;",
-                "\"User Login Name java.lang.String\",\"\",\"java.lang.String\",\"\",\"caDSR: 2223533 v3\",\"caCORE\",\"Qualified\",\"\",\"caBIG; caCORE\","
+            "Name, Other Names, Value Domain, Permissible Values, Identifiers, Steward, Registration Status, Administrative Status, Used By",
+            "PTSD Checklist Military (PCLM) - Happening again indicator","Suddenly acting or feeling as if the stressful experience were happening again (as if you were reliving it)?","Value List","1; 2; 3; 4; 5","NINDS: C07394 v3; NINDS Variable Name: PCLMHappeningAgainInd","NINDS","Qualified","","NINDS",
+            "Parkinson's Disease Quality of Life (PDQUALIF) - away from social scale","My Parkinson�s symptoms cause me to stay away from social gatherings","Value List","Strongly Agree; Somewhat Agree; Agree; Somewhat disagree; Strongly Disagree","NINDS: C17382 v3; NINDS Variable Name: PDQUALIFAwyFrmSocScale","NINDS","Qualified","","NINDS"
         };
 
         try {
-            String actual = new String(Files.readAllBytes(Paths.get(downloadFolder + "/QuickBoardExport.csv")));
+            String actual = new String(Files.readAllBytes(Paths.get(downloadFolder + "/BoardExport.csv")));
             for (String s : expected) {
                 if (!actual.contains(s)) {
                     Files.copy(
-                            Paths.get(downloadFolder + "/QuickBoardExport.csv"),
-                            Paths.get(tempFolder + "/ExportTest-quickBoardExport.csv"), REPLACE_EXISTING);
+                            Paths.get(downloadFolder + "/BoardExport.csv"),
+                            Paths.get(tempFolder + "/ExportTest-boardExport.csv"), REPLACE_EXISTING);
                     Assert.fail("missing line in export : " + s);
                 }
             }
         } catch (IOException e) {
             e.printStackTrace();
-            Assert.fail("Exception reading QuickBoardExport.csv " + e);
+            Assert.fail("Exception reading boardExport.csv " + e);
         }
 
 
@@ -52,8 +48,14 @@ public class BoardExportTest extends BoardTest {
         String bid = url.substring(url.lastIndexOf("/") + 1);
         String url_string = baseUrl + "/board/" + bid + "/0/500";
         String response = given().when().get(url_string).asString();
-        String result = "\"name\":\"Export my board test\",\"description\":\"This test tests export board.\",\"shareStatus\":\"Public\",";
-        Assert.assertTrue(response.contains(result));
+        String[] expected2 = {
+                "\"name\":\"Board Export Test\",\"description\":\"Test for board export\",\"shareStatus\":\"Public\"",
+                "\"name\":\"Acute Hospitalized\"},{\"elements\":[{\"elements\":[{\"elements\":[],\"name\":\"Psychiatric and Psychological"
+        };
+
+        for (String s : expected2) {
+            Assert.assertTrue(response.contains(s));
+        }
     }
 
 }
