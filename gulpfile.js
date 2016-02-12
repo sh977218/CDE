d718ff76c55d85bde99361d67ad78ab2bfbecc50
unknown <ludetc@NLM01960809MLB.nlm.nih.gov>
2016-02-12 11:55:22 -0500

    wait to create river

--- gulpfile.js
+++ gulpfile.js
@@ -124,15 +124,23 @@ gulp.task('es', function() {
 
     [
         {uri: config.elasticUri, data: elastic.createIndexJson},
-        {uri: config.elasticRiverUri + "/_meta", data: elastic.createRiverJson},
         {uri: config.elasticFormUri, data: elastic.createFormIndexJson},
-        {uri: config.elasticFormRiverUri + "/_meta", data: elastic.createFormRiverJson},
         {uri: config.elasticBoardIndexUri, data: elastic.createBoardIndexJson},
-        {uri: config.elasticBoardRiverUri + "/_meta", data: elastic.createBoardRiverJson},
         {uri: config.elasticStoredQueryUri, data: elastic.createStoredQueryIndexJson}
     ].forEach(function (item) {
         request.post(item.uri, {json: true, body: item.data});
     });
+
+    setTimeout(function() {
+        [
+            {uri: config.elasticRiverUri + "/_meta", data: elastic.createRiverJson},
+            {uri: config.elasticFormRiverUri + "/_meta", data: elastic.createFormRiverJson},
+            {uri: config.elasticBoardRiverUri + "/_meta", data: elastic.createBoardRiverJson},
+        ].forEach(function (item) {
+            request.post(item.uri, {json: true, body: item.data});
+        });
+    }, 2000)
+
 });
 
 gulp.task('tarCode', function () {
