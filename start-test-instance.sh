33d1fda9624e84de7264ae091e90a491ad535411
unknown <ludetc@NLM01960809MLB.nlm.nih.gov>
2016-02-22 15:04:25 -0500

    test fixes

--- start-test-instance.sh
+++ start-test-instance.sh
@@ -50,6 +50,6 @@ mongo test test/initOrgs.js -u $db_user -p $db_password
 
 #gradle -b test/selenium/build.gradle -PhubUrl=$HUB_URL -PtestUrl=$TEST_URL -PforkNb=$NB_OF_FORKS -Ptimeout=8 -Pbrowser=chrome -PdownloadFolder=/usr/nlm/selenium/cde/downloads/ test &
 #gradle -b test/selenium/build.gradle -PhubUrl=$HUB_URL -PtestUrl=$TEST_URL -PforkNb=6 -Ptimeout=8 -Pbrowser=chrome -PdownloadFolder=S://data test &
-gradle -b test/selenium/build.gradle -PhubUrl=$HUB_URL -PtestUrl=$TEST_URL -Pbrowser=chrome -PforkNb=2 -Ptimeout=8 test --tests *CdeQui* &
+gradle -b test/selenium/build.gradle -PhubUrl=$HUB_URL -PtestUrl=$TEST_URL -Pbrowser=chrome -PforkNb=2 -Ptimeout=8 test --tests *Board* &
 export NODE_ENV=test
 node $NODE_LOC/app > test-console.o
