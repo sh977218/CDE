850655fcbe934aeee5d66ecd670cbbf01d8504af
Jakub Flaska <jakub.flaska@nih.gov>
2016-02-24 14:56:30 -0500

    new file

--- ingester/loinc/loadLoincFields.js
+++ ingester/loinc/loadLoincFields.js
@@ -1,5 +1,6 @@
 var csv = require('csv')
     , fs = require('fs')
+    , byline = require('byline')
 ;
 
 var loincXmlMap = {
@@ -14,8 +15,38 @@ var loincXmlMap = {
     , 23: 'SHORTNAME'
 };
 
-//fs.readFile("loinc.csv", "utf-8", function(err, data) {
-//    csv.parse(data, function(err, data){
-//        console.log(data);
-//    });
-//});
+var loincData = function(){
+    var ld = this;
+    this.data = {};
+    this.add = function(line){
+        var ar = line.split(",");
+        var loincId = ar[0].replace(/"/g,"");
+        ld.data[loincId] = {};
+        Object.keys(loincXmlMap).forEach(function(k){
+            ld.data[loincId][loincXmlMap[k]] = ar[k].replace(/"/g,"");
+        });
+    };
+    this.getPropertiesById = function(id){
+        var obj = ld.data[id];
+        var output = "LOINC_NUM: " + id + ", ";
+        Object.keys(obj).forEach(function(k){
+            output += k + ": " + obj[k] + ", ";
+        });
+        output = output.substr(0, output.length-1);
+        return output;
+    };
+};
+
+var loinc = new loincData();
+
+var fileStream = fs.createReadStream('loinc.csv', {encoding: 'utf8'});
+bylineStream = byline.createStream(fileStream);
+
+bylineStream.on('data', function(line) {
+    if (line.indexOf('LOINC_NUM')>-1) return;
+    loinc.add(line);
+});
+
+bylineStream.on('end', function(){
+    console.log(loinc.getPropertiesById('10137-8'));
+});
