83c1c17f3cbfa1f46815fde95c8ae6153700250d
unknown <ludetc@NLM01960809MLB.nlm.nih.gov>
2016-02-29 10:31:07 -0500

    added test

--- test/selenium/src/test/java/gov/nih/nlm/cde/test/boards/PinWhenNotLoggedIn.java
+++ test/selenium/src/test/java/gov/nih/nlm/cde/test/boards/PinWhenNotLoggedIn.java
@@ -1,6 +1,7 @@
 package gov.nih.nlm.cde.test.boards;
 
 import gov.nih.nlm.system.NlmCdeBaseTest;
+import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
 public class PinWhenNotLoggedIn extends NlmCdeBaseTest {
@@ -8,7 +9,24 @@ public class PinWhenNotLoggedIn extends NlmCdeBaseTest {
     @Test
     public void pinWhenNotLoggedIn() {
         mustBeLoggedOut();
-        
+        goToCdeSearch();
+        clickElement(By.id("browseOrg-NINDS"));
+        clickElement(By.id("pinAll"));
+        textPresent("Create Boards and attach CDEs to them");
+        clickElement(By.id("signUpLink"));
+        textPresent("Log in with your UMLS");
+        driver.navigate().back();
+
+        clickElement(By.id("pinToBoard_0"));
+        textPresent("Create Boards and attach CDEs to them");
+        clickElement(By.id("cancelSelect"));
+        modalGone();
+
+        clickElement(By.id("linkToElt_0"));
+        clickElement(By.id("addToBoard"));
+        textPresent("Create Boards and attach CDEs to them");
+        clickElement(By.id("cancelSelect"));
+        modalGone();
     }
 
 }
