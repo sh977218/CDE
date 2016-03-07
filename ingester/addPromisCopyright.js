72f0ce72652798c7750fba0aba8cdc44e2f9cf92
Jakub Flaska <jakub.flaska@nih.gov>
2016-03-07 14:19:09 -0500

    x

--- ingester/addPromisCopyright.js
+++ ingester/addPromisCopyright.js
@@ -1,5 +1,2 @@
-// Neuro-QOL Measures: © 2008-201 David Cella and the PROMIS Health Organization on behalf of the National Institute for Neurological Disorders and Stroke (NINDS).
-// PROMIS Instruments: © 2008-2015 PROMIS Health Organization and PROMIS Cooperative Group
-
 db.forms.update({"classification.elements.name": "Neuro-QOL Measures"}, {$set: {"copyright.text": "2008-2015 David Cella and the PROMIS Health Organization on behalf of the National Institute for Neurological Disorders and Stroke (NINDS)"}}, {multi: true});
 db.forms.update({"classification.elements.name": "PROMIS Instruments"}, {$set: {"copyright.text": "2008-2015 PROMIS Health Organization and PROMIS Cooperative Group"}}, {multi: true});
