2f6f74b4f4a047513225cc7e6ed6cbf9be487f62
Jakub Flaska <jakub.flaska@nih.gov>
2016-03-15 14:57:51 -0400

    x

---
+++ test/selenium/src/test/java/gov/nih/nlm/cde/test/StripHtmlFromDefTest.java
@@ -0,0 +1,18 @@
+package gov.nih.nlm.cde.test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.Test;
+
+public class StripHtmlFromDefTest extends BaseClassificationTest {
+
+    @Test
+    public void stripHtmlFromDef() {
+        goToFormSearch();
+        findElement(By.id("browseOrg-NIDA")).click();
+        textPresent("Single-Question Screening Test - Self-Administered");
+        textPresent("<b>");
+        textNotPresent("<p>");
+    }
+
+}
