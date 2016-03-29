995687e388a15b4e8be0dd211728696d5c6f1428
Jakub Flaska <jakub.flaska@nih.gov>
2016-03-29 16:47:35 -0400

    x

--- test/selenium/src/test/java/gov/nih/nlm/form/test/FormTableView.java
+++ test/selenium/src/test/java/gov/nih/nlm/form/test/FormTableView.java
@@ -8,7 +8,11 @@ public class FormTableView extends BaseFormTest {
 
     @Test
     public void goToEltByName() {
+        findElement(By.id("cde_gridView")).click();
         goToFormSearch();
         findElement(By.id("browseOrg-NINDS")).click();
+        findElement(By.id("source")).click();
+        findElement(By.id("saveSettings")).click();
+        findElement(By.id("form_gridView")).click();
     }
 }
