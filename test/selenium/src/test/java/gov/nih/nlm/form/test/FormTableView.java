baf082d19cc21a8de46eba6e3caf41ad718f0ff2
Jakub Flaska <jakub.flaska@nih.gov>
2016-04-05 14:20:31 -0400

    x

--- test/selenium/src/test/java/gov/nih/nlm/form/test/FormTableView.java
+++ test/selenium/src/test/java/gov/nih/nlm/form/test/FormTableView.java
@@ -6,14 +6,43 @@ import org.testng.annotations.Test;
 
 public class FormTableView extends BaseFormTest {
 
+    private void seeTableForms(){
+        findElement(By.id("menu_forms_link")).click();
+        findElement(By.id("browseOrg-TEST")).click();
+        findElement(By.id("form_gridView")).click();
+    }
+
     @Test
     public void seeFormSource() {
+        mustBeLoggedOut();
+        seeTableForms();
+        textPresent("Other Names");
+        textPresent("Steward");
+        textPresent("Used By");
+        textPresent("Registration Status");
+        textPresent("Identifiers");
+        textPresent("Questions");
+
         findElement(By.id("searchSettings")).click();
+        findElement(By.id("naming")).click();
+        findElement(By.id("stewardOrg")).click();
+        findElement(By.id("usedBy")).click();
+        findElement(By.id("registrationStatus")).click();
+        findElement(By.id("administrativeStatus")).click();
+        findElement(By.id("ids")).click();
         findElement(By.id("source")).click();
+        findElement(By.id("updated")).click();
+        findElement(By.id("numQuestions")).click();
         findElement(By.id("saveSettings")).click();
-        findElement(By.id("menu_forms_link")).click();
-        findElement(By.id("browseOrg-NINDS")).click();
-        findElement(By.id("form_gridView")).click();
+        seeTableForms();
+
+        textNotPresent("Other Names");
+        textPresent("Steward");
+        textPresent("Used By");
+        textPresent("Registration Status");
+        textPresent("Identifiers");
+        textPresent("Questions");
+
         textPresent("Source");
     }
 }
