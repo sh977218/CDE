40dc026b73fe237e9656aad0894755a6eab77734
unknown <huangs8@NLM01961050MLB.nlm.nih.gov>
2016-04-20 16:56:54 -0400

    done

---
+++ ingester/ninds/FixNindsFormQuestionsNoLabel.js
@@ -0,0 +1,45 @@
+var async = require('async'),
+    FormModel = require('./../createConnection').FormModel
+    ;
+
+var totalForm = 0;
+var modifiedForm = 0;
+var sameForm = 0;
+FormModel.find({'stewardOrg.name': 'NINDS', archived: null}).exec(function (err, allForms) {
+    if (err) throw err;
+    async.each(allForms, function (form, cb) {
+        doForm(form, cb);
+    }, function () {
+        console.log('-----------------------------------------');
+        console.log('total CDE: ' + totalForm);
+        console.log('modified CDE: ' + modifiedCDE);
+        console.log('same CDE: ' + sameCDE);
+        //noinspection JSUnresolvedVariable
+        process.exit(0);
+    });
+});
+
+function doForm(form, cb) {
+    totalForm++;
+    var formElements = form.get('formElements');
+    if (formElements && formElements.length === 0) {
+        sameForm++;
+        cb();
+    } else if (formElements && formElements.length > 0) {
+        modifiedForm++;
+        var questions = formElements[0].formElements;
+        questions.forEach(function (q) {
+            if (q.label.length === 0) {
+                q.hideLabel = true;
+            }
+        });
+        form.save(function (err) {
+            if (err) throw err;
+            cb();
+        });
+    } else {
+        console.log("something wrong with form.");
+        console.log("form Id: " + form.get('tinyId'));
+        process.exit(0);
+    }
+}
