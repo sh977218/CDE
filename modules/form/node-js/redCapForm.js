3dba1a0b9de744ce9cf8d6a4c009f43bf6c2f280
unknown <huangs8@NLM01961050MLB.nlm.nih.gov>
2016-04-27 15:46:40 -0400

    x

--- modules/form/node-js/redCapForm.js
+++ modules/form/node-js/redCapForm.js
@@ -100,7 +100,6 @@ function getRedCap(form) {
 
 exports.formToRedCap = function (form) {
     if (form.toObject()) form = form.toObject();
-    var redCapHeader = 'Variable / Field Name,Form Name,Section Header,Field Type,Field Label,Choices Calculations OR Slider Labels,Field Note,Text Validation Type OR Show Slider Number,Text Validation Min,Text Validation Max,Identifier?,Branching Logic (Show field only if...),Required Field?,Custom Alignment,Question Number (surveys only),Matrix Group Name,Matrix Ranking?\n';
     var instrumentResult = getRedCap(form);
-    return redCapHeader + instrumentResult;
+    return exportShared.redCapHeader + instrumentResult;
 };
--- modules/system/shared/exportShared.js
+++ modules/system/shared/exportShared.js
@@ -121,22 +121,18 @@ exports.convertToCsv = function (ele) {
     var sanitize = function (v) {
         return v.trim ? v.trim().replace(/\"/g, "\"\"") : v;
     };
-    var length = Object.keys(ele).length;
     var row = "";
-    Object.keys(ele).forEach(function (key, i) {
+    Object.keys(ele).forEach(function (key) {
+        row += "\"";
         var value = ele[key];
-        var type = typeof value;
         if (Array.isArray(value)) {
             row += value.map(function (value) {
                 return sanitize(value);
             }).join("; ");
-        } else if (type === 'string') {
-            row += "\"" + sanitize(value) + "\"";
-        } else if (type === 'number') {
-            row += value + ",";
-        } else row += "\"" + sanitize(value) + "\"";
-
-        if (i !== length)row += ",";
+        } else if (value) {
+            row += sanitize(value);
+        }
+        row += "\",";
     });
     return row + "\n";
 };
