df2b063204d9fcdc592304aceff8d30dabd09ea3
unknown <ludetc@NLM01960809MLB.nlm.nih.gov>
2016-04-28 13:27:08 -0400

    exclude server errors

--- modules/system/public/js/controllers/AuditErrorListCtrl.js
+++ modules/system/public/js/controllers/AuditErrorListCtrl.js
@@ -1,18 +1,29 @@
-angular.module('systemModule').controller('AuditErrorListCtrl', ['$scope', '$http', '$q', function($scope, $http, $q) {
-    $scope.records = [];
-    var itemsPerPage = 10;    
+angular.module('systemModule').controller('AuditErrorListCtrl', ['$scope', '$http', // jshint ignore:line
+    function ($scope, $http) {
 
-    $scope.fetchErrors = function(skip, limit, cb){
-        $http.post($scope.api, {skip: skip, limit: limit}).success(function(result){
+    $scope.records = [];
+
+    var itemsPerPage = 10;
+
+    function fetchErrors(skip, limit, cb) {
+        $http.post($scope.api, {skip: skip, limit: limit, exclude: []}).success(function (result) {
             $scope.records = result;
             if (cb) cb();
         });
+    }
+
+    $scope.gotoPage = function (page, cb) {
+        $scope.fetchErrors(itemsPerPage * (page - 1), itemsPerPage, cb);
     };
 
-    $scope.gotoPage = function(page, cb){
-        $scope.fetchErrors(itemsPerPage * (page-1), itemsPerPage, cb);
-    };    
+    $scope.excludeFilters = [];
+    $scope.addExcludeFilter = function (toAdd) {
+        if (toAdd.length > 0) {
+            $scope.excludeFilters.push(toAdd);
+            $scope.f
+        }
+    };
 
     $scope.currentPage = 1;
-    
+
 }]);
