f337fef179e9422149702a4e76c1325a4afddb93
Jakub Flaska <jakub.flaska@nih.gov>
2016-04-28 17:24:04 -0400

    x

--- test/selenium/src/test/java/gov/nih/nlm/cde/test/LinkedFormsTest.java
+++ test/selenium/src/test/java/gov/nih/nlm/cde/test/LinkedFormsTest.java
@@ -11,13 +11,13 @@ public class LinkedFormsTest extends NlmCdeBaseTest {
     public void linkedForms() {
         goToCdeByName("Psychiatric history clinical depression past year indicator");
         showAllTabs();
-        findElement(By.id("forms_tab")).click();
+        clickElement(By.id("forms_tab"));
         textPresent("Medical History");
         textPresent("There is 1 form that uses this CDE.");
         textPresent("Contains data elements related to the study participant's/ subject's health history. (Examples of CDEs included: review of symptoms and conditions in specific body systems (pulmonary, endocrine, muscu...");
         textPresent("47");
         textPresent("Quick Board (0)");
-        findElement(By.id("addToCompare_0")).click();
+        clickElement(By.id("addToCompare_0"));
         textPresent("Quick Board (1)");
     }
 
