adb84d8d5391f41bc8b78b49d369c5dd1ff22e65
Simon Reiffen <simon.reiffen@nih.gov>
2016-04-29 17:24:19 -0400

    I think I added the captcha test correctly

---
+++ test/selenium/src/test/java/gov/nih/nlm/system/captchaTest.java
@@ -0,0 +1,33 @@
+package gov.nih.nlm.system;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class captchaTest extends NlmCdeBaseTest {
+
+    @Test
+    public void repeatFailure() {
+        goHome();
+		int i = 0;
+		try {
+            findElement(By.linkText("Log In")).click();
+        } catch (TimeoutException e) {
+            logout();
+            findElement(By.linkText("Log In")).click();
+        }
+		while (i <3){
+        enterUsernamePasswordSubmit("bad-username", "bad-password", "Failed to log in.");
+		i++;
+		}
+        enterUsernamePasswordSubmit("bad-username", "bad-password", "Please fill out the Captcha before login in.");
+
+
+		
+
+		
+    }
+
+}
