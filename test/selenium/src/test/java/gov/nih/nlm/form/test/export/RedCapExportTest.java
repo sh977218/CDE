ac913682f355a802d000d181d39374c578e98a45
unknown <huangs8@NLM01961050MLB.nlm.nih.gov>
2016-05-02 16:05:26 -0400

    pre create forms in bson

--- test/selenium/src/test/java/gov/nih/nlm/form/test/export/RedCapExportTest.java
+++ test/selenium/src/test/java/gov/nih/nlm/form/test/export/RedCapExportTest.java
@@ -1,11 +1,10 @@
 package gov.nih.nlm.form.test.export;
 
 import gov.nih.nlm.form.test.BaseFormTest;
-import gov.nih.nlm.system.RecordVideo;
 import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
-public class RedCapExport extends BaseFormTest {
+public class RedCapExportTest extends BaseFormTest {
 
     @Test
     public void promisCanNotExportRedCap() {
@@ -18,17 +17,4 @@ public class RedCapExport extends BaseFormTest {
         textPresent("You can download PROMIS / Neuro-QOL REDCap from here.");
         switchTabAndClose(0);
     }
-
-    @Test
-    public void phenxCanNotExportRedCap() {
-        mustBeLoggedInAs(testAdmin_username, password);
-        goToFormByName("PhenX Form RedCAP Export");
-        enableBetaFeature();
-        clickElement(By.id("export"));
-        clickElement(By.id("nihRedCap"));
-        switchTab(1);
-        textPresent("You can download PhenX REDCap from here.");
-        switchTabAndClose(0);
-    }
-
 }
