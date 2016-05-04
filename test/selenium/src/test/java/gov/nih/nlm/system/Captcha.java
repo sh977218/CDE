8c713aa7c9f90224371b450075efaaf6f564fc6d
Simon Reiffen <simon.reiffen@nih.gov>
2016-05-04 11:34:32 -0400

    I think I reset the bad commits

---
+++ test/selenium/src/test/java/gov/nih/nlm/system/Captcha.java
@@ -0,0 +1,18 @@
+package gov.nih.nlm.system;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class Captcha extends NlmCdeBaseTest {
+
+    @Test
+    public void loginFailureCaptcha() {
+        mustBeLoggedOut();
+            clickElement(By.linkText("Log In"));
+
+        for (int i = 0; i < 3; i++) {
+            enterUsernamePasswordSubmit("bad-username", "bad-password", "Failed to log in.");
+        }
+        enterUsernamePasswordSubmit("bad-username", "bad-password", "Please fill out the Captcha before login in.");
+    }
+}
