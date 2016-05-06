5b6a9bcf5fbc4052af2491a3d1d447eaf49e8ffb
unknown <ludetc@NLM01960809MLB.nlm.nih.gov>
2016-05-06 10:26:07 -0400

    add goHome to test

--- test/selenium/src/test/java/gov/nih/nlm/system/BetaEnableTest.java
+++ test/selenium/src/test/java/gov/nih/nlm/system/BetaEnableTest.java
@@ -3,8 +3,10 @@ package gov.nih.nlm.system;
 import org.testng.annotations.Test;
 
 public class BetaEnableTest extends NlmCdeBaseTest {
+
     @Test
     public void enableDisableBetaFeatureTest() {
+        goHome();
         enableBetaFeature();
     }
 
