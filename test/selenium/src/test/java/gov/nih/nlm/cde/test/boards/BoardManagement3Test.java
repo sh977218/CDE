c31eaf587bf454f212d701e48f51eef12a8a8f8c
Peter Huang <shi.huang@nih.gov>
2016-06-02 10:42:58 -0400

    pre create board for BoardManagement3Test

--- test/selenium/src/test/java/gov/nih/nlm/cde/test/boards/BoardManagement3Test.java
+++ test/selenium/src/test/java/gov/nih/nlm/cde/test/boards/BoardManagement3Test.java
@@ -6,10 +6,9 @@ import org.testng.annotations.Test;
 
 public class BoardManagement3Test extends BoardTest {
 
-    @Test(priority = 3)
+    @Test
     public void editBoard() {
         mustBeLoggedInAs(boarduserEdit_username, password);
-        createBoard("Edit Board", "Test");
         gotoMyBoards();
         String modified = findElement(By.id("board_mod_0")).getText();
         clickElement(By.id("edit_board_0"));
@@ -25,39 +24,22 @@ public class BoardManagement3Test extends BoardTest {
         textPresent("-- Desc Edited");
 
         Assert.assertNotEquals(modified + " --- " + findElement(By.id("dd_mod")).getText(), modified, findElement(By.id("dd_mod")).getText());
-
-        removeBoard("Edit Board -- Name Edited");
     }
 
     @Test
     public void searchBoard() {
         mustBeLoggedInAs(boardSearchUser_username, password);
         String pubBlood = "Public Blood Board";
-        String privBlood = "Private Blood Board";
-        String pubSmoking = "Public Smoking Board";
 
-        createBoard(pubBlood, "");
-        createBoard(privBlood, "");
-        createBoard(pubSmoking, "");
-
-        makePublic(pubBlood);
-        makePublic(pubSmoking);
-
-        modalGone();
         gotoPublicBoards();
 
         findElement(By.name("search")).sendKeys("Blood");
-        findElement(By.id("search.submit")).click();
+        clickElement(By.id("search.submit"));
 
         textPresent(pubBlood);
 
         textNotPresent("Smoking");
         textNotPresent("Private");
-
-        removeBoard(pubBlood);
-        removeBoard(privBlood);
-        removeBoard(pubSmoking);
-
     }
 
 }
