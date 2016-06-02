675d9d072c542b5a8b12700bcb10721718a85861
Peter Huang <shi.huang@nih.gov>
2016-06-02 11:45:10 -0400

    fix pinAllCdesInForm

--- test/selenium/src/test/java/gov/nih/nlm/form/test/PinAllCdesInForm.java
+++ test/selenium/src/test/java/gov/nih/nlm/form/test/PinAllCdesInForm.java
@@ -1,14 +1,11 @@
 package gov.nih.nlm.form.test;
 
 import gov.nih.nlm.cde.test.boards.BoardTest;
-import gov.nih.nlm.system.NlmCdeBaseTest;
 import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class PinAllCdesInForm extends NlmCdeBaseTest {
-
-    private BoardTest boardTest = new BoardTest();
+public class PinAllCdesInForm extends BoardTest {
 
     @Test
     public void pinAllCdesInForm() {
@@ -19,12 +16,11 @@ public class PinAllCdesInForm extends NlmCdeBaseTest {
         clickElement(By.linkText(boardName));
         textPresent("All elements pinned");
         closeAlert();
-        boardTest.gotoMyBoards();
-        Assert.assertEquals(findElement(By.xpath("//div[@data-id=\"boardDiv_" + boardName +
-                "\"]//dd[contains(@id,'dd_numb_')]")).getText(), "7");
-        clickElement(By.xpath("//div[@data-id=\"boardDiv_" + boardName + "\"]//a"));
+        gotoMyBoards();
+
+        Assert.assertEquals(findElement(By.xpath("//div[@data-id='boardDiv_" + boardName + "']//span[contains(@id,'board_num_cdes_')]")).getText(), "7");
+        clickElement(By.xpath("//div[@data-id='boardDiv_" + boardName + "']//a"));
         textPresent("Optical coherence");
-        boardTest.removeBoard(boardName);
     }
 
 }
