d4525d3b21fbe7371512079610f7653ed1c6278f
Peter Huang <shi.huang@nih.gov>
2016-06-02 14:29:34 -0400

    fix score test

--- test/selenium/src/test/java/gov/nih/nlm/form/test/QuestionTest.java
+++ test/selenium/src/test/java/gov/nih/nlm/form/test/QuestionTest.java
@@ -18,7 +18,7 @@ public class QuestionTest extends BaseFormTest {
         textPresent("1 results");
         textPresent(cdeName, By.id("acc_link_0"));
 
-        WebElement sourceElt = findElement(By.cssSelector("#accordionList .question-move-handle"));
+        WebElement sourceElt = findElement(By.xpath("//*[@id='accordionList']//*[contains(@class,'question-move-handle')]"));
         WebElement targetElt = findElement(By.id("section_drop_area_" + sectionNumber));
 
         Assert.assertTrue(sourceElt.isDisplayed());
