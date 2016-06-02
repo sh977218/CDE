71a7c0eb983652fd619fb06ebf80b120f3a82677
Jakub Flaska <jakub.flaska@nih.gov>
2016-06-02 14:38:52 -0400

    arrays working

--- modules/cde/public/js/controllers/RegStatusValidationCtrl.js
+++ modules/cde/public/js/controllers/RegStatusValidationCtrl.js
@@ -22,6 +22,16 @@ angular.module('cdeModule').controller('RegStatusValidationCtrl', ['$scope', 'Or
                 if (!object[key]) return false;
                 if (level === rule.field.split(".").length-1) return checkRe(object[key], rule);
                 if (!Array.isArray(object[key])) return checkSubTree(object[key], rule, level+1);
+                if (Array.isArray(object[key])) {
+                    if (rule.occurence === "atLeastOne") {
+                        var result = false;
+                        object[key].forEach(function(subTree){
+                            result = result || checkSubTree(subTree, rule, level+1);
+                        });
+                        return result;
+                    }
+
+                }
             }
             return checkSubTree(cde, rule, 0);
         };
