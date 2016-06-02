c89ca75548e755a005de0265a51b71b039b89f6f
Jakub Flaska <jakub.flaska@nih.gov>
2016-06-02 14:13:45 -0400

    rewritten recurcive

--- modules/cde/public/js/controllers/RegStatusValidationCtrl.js
+++ modules/cde/public/js/controllers/RegStatusValidationCtrl.js
@@ -13,6 +13,51 @@ angular.module('cdeModule').controller('RegStatusValidationCtrl', ['$scope', 'Or
         $scope.cdeOrgRules = $scope.getOrgRulesForCde($scope.elt);
 
         $scope.cdePassingRule = function(cde, rule){
-            return false;
+            function checkRe(){
+                return true;
+            }
+            function checkSubTree(object, rule, level){
+                var key = rule.field.split(".")[level];
+                if (!object[key]) return false;
+                if (level === rule.field.split(".").length-1) return checkRe(object[key], rule);
+                if (!Array.isArray(object[key])) return checkSubTree(object[key], rule, level+1);
+            }
+            return checkSubTree(cde, rule, 0);
         };
+
+        //$scope.cdePassingRule = function(object, rule){
+        //    var fields = rule.field.split(".");
+        //
+        //    var obj = object;
+        //    var failed = false;
+        //    fields.forEach(function(f){
+        //        if (failed) return;
+        //        if (!obj[f]) {
+        //            failed = true;
+        //            return;
+        //        } else {
+        //            obj = obj[f]
+        //        }
+        //    });
+        //    if (!Array.isArray(obj)) {
+        //        return !failed;
+        //    } else {
+        //        if (rule.occurence === "all") {
+        //            var result = true;
+        //            obj.forEach(function (subTree) {
+        //                result = result && $scope.cdePassingRule(subTree, rule);
+        //            });
+        //            return result;
+        //        }
+        //
+        //        if (rule.occurence === "atLeastOne") {
+        //            var result = false;
+        //            obj.forEach(function (subTree) {
+        //                result = result || $scope.cdePassingRule(subTree, rule);
+        //            });
+        //            return result;
+        //        }
+        //    }
+        //
+        //};
     }]);
