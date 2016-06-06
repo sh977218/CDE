d0ee75e023cb56ed3a0639680085656b36fe09a8
Simon Reiffen <simon.reiffen@nih.gov>
2016-06-06 11:37:23 -0400

    I think I implemented the new board test system

--- test/selenium/src/test/java/gov/nih/nlm/cde/test/boards/PinReorderingTest.java
+++ test/selenium/src/test/java/gov/nih/nlm/cde/test/boards/PinReorderingTest.java
@@ -1,7 +1,6 @@
 package gov.nih.nlm.cde.test.boards;
 
 import org.openqa.selenium.By;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class PinReorderingTest extends BoardTest {
@@ -10,9 +9,13 @@ public class PinReorderingTest extends BoardTest {
     public void pin() {
         //Essentially, what this code does is that it ensures that we are able to move down a CDE, and check that we have done so successfully.
         //We're probably going to want to expand on this though, as it's really bare bones
+        mustBeLoggedInAs(nlm_username,  nlm_password);
+        goToBoard("Classify Board");
         findElement(By.id("linkToElt_0")).getText().equals("Medication affecting cardiovascular function type exam day indicator");
         findElement(By.id("moveDown-0"));
         findElement(By.id("linkToElt_0")).getText().equals("Ethnicity USA maternal category");
 
     }
 }
+
+
