88722dce4efe6980fe4e8bbd8e9ed24819cb27ca
Simon Reiffen <simon.reiffen@nih.gov>
2016-06-07 12:39:53 -0400

    fixed bad ideas I added in

--- modules/system/node-js/app.js
+++ modules/system/node-js/app.js
@@ -467,6 +467,7 @@ exports.init = function (app) {
         res.render('orgAccountManagement', "system");
     });
 
+
     app.get('/data/:imgtag', function (req, res) {
         mongo_data_system.getFile(req.user, req.params.imgtag, res);
     });
