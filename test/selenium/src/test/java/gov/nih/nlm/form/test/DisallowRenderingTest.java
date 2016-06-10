6174d212931616c7ff1c3eccea9f44e960974b90
ludetc <ludetc@mail.nih.gov>
2016-06-10 14:18:05 -0400

    done

--- test/selenium/src/test/java/gov/nih/nlm/form/test/DisallowRenderingTest.java
+++ test/selenium/src/test/java/gov/nih/nlm/form/test/DisallowRenderingTest.java
@@ -10,12 +10,23 @@ public class DisallowRenderingTest extends BaseFormTest {
         mustBeLoggedInAs(nlm_username, nlm_password);
         goToFormByName("Short Form 36-Item Health Survey (SF-36)");
         textNotPresent("In general, would you say");
+        clickElement(By.id("description_tab"));
+        textPresent("Rendering is disabled for this form");
+        clickElement(By.id("more_tab"));
+        clickElement(By.id("cdeList_tab"));
+        textPresent("Rendering is disabled for this form");
+        clickElement(By.id("general_tab"));
         findElement(By.id("disallowRendering")).click();
         saveForm();
         goToFormByName("Short Form 36-Item Health Survey (SF-36)");
         textPresent("This form is large");
         clickElement(By.id("renderPreviewButton"));
         textPresent("In general, would you say");
+        clickElement(By.id("description_tab"));
+        textPresent("In general, would you say");
+        clickElement(By.id("more_tab"));
+        clickElement(By.id("cdeList_tab"));
+        textPresent("36-item Short Form Health Survey (SF-36) - Bodily pain score");
     }
 
 }
