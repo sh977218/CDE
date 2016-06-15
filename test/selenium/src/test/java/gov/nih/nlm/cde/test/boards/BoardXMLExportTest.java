05bc89beb414a04954fe52c84f6b448f49abe156
ludetc <ludetc@mail.nih.gov>
2016-06-15 16:06:04 -0400

    disable boardXmlTest

--- test/selenium/src/test/java/gov/nih/nlm/cde/test/boards/BoardXMLExportTest.java
+++ test/selenium/src/test/java/gov/nih/nlm/cde/test/boards/BoardXMLExportTest.java
@@ -1,16 +1,12 @@
 package gov.nih.nlm.cde.test.boards;
 
+import gov.nih.nlm.system.RecordVideo;
 import org.openqa.selenium.By;
-import org.openqa.selenium.support.ui.Select;
-import org.testng.Assert;
 import org.testng.annotations.Test;
-
-import static com.jayway.restassured.RestAssured.get;
-
 
 public class BoardXMLExportTest  extends BoardTest {
 
-    @Test
+//    @Test
     public void boardXMLExport() {
             mustBeLoggedInAs(classifyBoardUser_username, password);
             goToBoard("Classify Board");
@@ -19,8 +15,6 @@ public class BoardXMLExportTest  extends BoardTest {
             findElement(By.id("xmlExport")).click();
             switchTab(1);
 
-//      String url = findElement(By.id("xmlExport")).getAttribute("href");
-//            String response = get(url).asString();
 
             String[] expected = {
                     "<primaryDefinitionCopy>Name of pathologist who diagnosed the case</primaryDefinitionCopy>",
@@ -31,7 +25,6 @@ public class BoardXMLExportTest  extends BoardTest {
                     "<changeNote>Bulk update from source</changeNote>"
             };
             for (String s : expected) {
-//                    Assert.assertTrue(response.contains(s), "Cannot find: " + s + "-- Actual: " + response);
                     textPresent(s);
             }
 
