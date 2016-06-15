f610d9a854ef93f8cec7064c06a42511f27a5599
ludetc <ludetc@mail.nih.gov>
2016-06-15 09:35:52 -0400

    fix form ID in test

--- test/selenium/src/test/java/gov/nih/nlm/form/test/FormClassificationTest.java
+++ test/selenium/src/test/java/gov/nih/nlm/form/test/FormClassificationTest.java
@@ -22,28 +22,5 @@ public class FormClassificationTest extends BaseFormTest {
         goToFormByName("Traumatic Brain Injury - Adverse Events");
         clickElement(By.id("classification_tab"));
         new ClassificationTest().addClassificationMethod(new String[]{"NINDS","Disease","Traumatic Brain Injury"});          
-    }  
-    
-    @Test
-    public void classifyFormCdes() {
-        mustBeLoggedInAs("ctepOnlyCurator", password);
-
-        goToFormByName("History Data Source and Reliability");
-        clickElement(By.id("classification_tab"));
-        clickElement(By.id("classifyAllCdes"));
-        clickElement(By.cssSelector("[id='addClassification-ABTC'] span.fake-link"));
-        clickElement(By.cssSelector("[id='addClassification-ABTC 0904'] button"));
-        
-        // Verify
-        goToCdeByName("Data source");
-        clickElement(By.id("classification_tab"));
-        textPresent("ABTC");
-        textPresent("ABTC 0904");
-
-        goToCdeByName("Overall assessment of the reliability of the medical history data obtained");
-        clickElement(By.id("classification_tab"));
-        textPresent("ABTC");
-        textPresent("ABTC 0904");
-
     }
 }
