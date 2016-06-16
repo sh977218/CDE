1b1467ded46ab224605c7bce19a802594b883150
Peter Huang <shi.huang@nih.gov>
2016-06-16 16:46:35 -0400

    fix test

--- test/selenium/src/test/java/gov/nih/nlm/cde/test/score/CdeScoreValidationErrorTest.java
+++ test/selenium/src/test/java/gov/nih/nlm/cde/test/score/CdeScoreValidationErrorTest.java
@@ -1,18 +1,15 @@
 package gov.nih.nlm.cde.test.score;
 
-import gov.nih.nlm.cde.test.quickboard.CdeQuickBoardTest1;
 import gov.nih.nlm.form.test.BaseFormTest;
 import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class CdeScoreValidationErrorTest extends BaseFormTest {
-    private CdeQuickBoardTest1 qbTest = new CdeQuickBoardTest1();
-
     @Test
     public void cdeScoreValidationError() {
         mustBeLoggedInAs(ninds_username, password);
-        qbTest.emptyQuickBoardByModule("cde");
+        emptyQuickBoardByModule("cde");
         goToCdeByName("DRS Total Score");
         showAllTabs();
         clickElement(By.id("derivationRules_tab"));
