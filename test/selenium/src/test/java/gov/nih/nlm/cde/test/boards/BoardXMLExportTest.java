63114db7f4e2473d54d7ec4d64a0554412fa813b
Simon Reiffen <simon.reiffen@nih.gov>
2016-06-21 16:17:32 -0400

    fixed some tests, some cannot be fixed

--- test/selenium/src/test/java/gov/nih/nlm/cde/test/boards/BoardXMLExportTest.java
+++ test/selenium/src/test/java/gov/nih/nlm/cde/test/boards/BoardXMLExportTest.java
@@ -12,27 +12,26 @@ public class BoardXMLExportTest  extends BoardTest {
 
     @Test
     public void boardXMLExport() {
-            mustBeLoggedInAs(classifyBoardUser_username, password);
-            goToBoard("Classify Board");
-            textPresent("Export Board");
-            clickElement(By.id(("export")));
-            //findElement(By.id("xmlExport")).click();
-          //  switchTab(1);
-            String url = findElement(By.id("xmlExport")).getAttribute("href");
-            String response = get(url).asString();
-            System.out.println("response was");
-            System.out.println(response);
-            Assert.assertTrue(true);
-        /*
-           Assert.assertTrue(response.replaceAll("\\s+","").contains(( "<primaryDefinitionCopy>Name of pathologist who diagnosed the case</primaryDefinitionCopy>" +
-                    "</elements>\n<name>Domain</name>"+
-                    "<primaryDefinitionCopy>\nDate (and time, if applicable and known) the Manual Muscle Testing (MMT) was performed\n</primaryDefinitionCopy>" +
-                "Contains data elements collected when an imaging study is performed to measure parenchyma; data recorded attempt to divide the strokes into ischemic or hemorrhagic subtypes, as distinction of hemorrhage versus infarction is the initial critical branch point in acute stroke triage. (Examples of CDEs included: Acute infarcts present; Planimetic acute ischemic lesion volume; and Acute hematoma present)\n" +
-                    "<source>NINDS Variable Name</source>" +
-                            "<primaryDefinitionCopy>\nIndicator how often the subject feels irritable or has angry outbursts as part of PTSD Checklist Military (PCLM).\n</primaryDefinitionCopy>" +
-                "<changeNote>Bulk update from source</changeNote>").replaceAll("\\s+", "")));
+        mustBeLoggedInAs(classifyBoardUser_username, password);
+        goToBoard("Classify Board");
+        textPresent("Export Board");
+        clickElement(By.id(("export")));
+        String url = findElement(By.id("xmlExport")).getAttribute("href");
+        String response = get(url).asString();
 
-         */
-        }
+        System.out.println("URL: " + url);
+        System.out.println("REsponse: " + response);
+//        findElement(By.id("xmlExport")).click();
+//        switchTab(1);
+//        textPresent( "<primaryDefinitionCopy>Name of pathologist who diagnosed the case</primaryDefinitionCopy>" );
+
+
+//        System.out.println("parent source" + driver.getPageSource());
+
+    //    Assert.assertTrue(driver.getPageSource().contains( "<primaryDefinitionCopy>Name of pathologist who diagnosed the case</primaryDefinitionCopy>" ));
+
+//        switchTabAndClose(0);
+
+    }
 
 }
