760a74f8a42baaef17fdec50de58482beee748d5
Simon Reiffen <simon.reiffen@nih.gov>
2016-06-22 11:00:24 -0400

    Added one of the tests we need

--- test/selenium/src/test/java/gov/nih/nlm/cde/test/admin/AdminAddsPropertyKey.java
+++ test/selenium/src/test/java/gov/nih/nlm/cde/test/admin/AdminAddsPropertyKey.java
@@ -23,16 +23,30 @@ public class AdminAddsPropertyKey extends NlmCdeBaseTest{
         driver.navigate().refresh(); //it takes a while for the new element to pop up. Might even include this in a loop, up to X times
         clickElement(By.linkText("More..."));
         clickElement(By.linkText("Properties"));
-        clickElement(By.id("newProperty"));
+        clickElement(By.id("addProperty"));
         clickElement(By.id("newPropertyKey"));
         textPresent("doYouSeeThis");
+        clickElement(By.id("cancelCreate"));
+
+        ///////////
+        clickElement(By.id("username_link"));
+        clickElement(By.linkText("Org Management"));
+        clickElement(By.linkText("List Management"));
+        clickElement(By.id("edit_org_props_TEST"));
+        findElement(By.xpath("//div[@id='text_entry_box_TEST']//input")).sendKeys(Keys.BACK_SPACE);
+        findElement(By.xpath("//div[@id='text_entry_box_TEST']//input")).sendKeys(Keys.BACK_SPACE);
+
+        clickElement(By.id("confirmEdit_TEST"));
+        goToCdeByName("Distance from Closest Margin Value");
+        driver.navigate().refresh(); //it takes a while for the new element to pop up. Might even include this in a loop, up to X times
+        clickElement(By.linkText("More..."));
+        clickElement(By.linkText("Properties"));
+        clickElement(By.id("addProperty"));
+        clickElement(By.id("newPropertyKey"));
+        textNotPresent("doYouSeeThis");
 
 
 
-    }
-    @Test
-    public void doNotForgetToFinishThisTest(){
-        clickElement(By.id("AUTOMATIC FAILURE"));
 
     }
 
