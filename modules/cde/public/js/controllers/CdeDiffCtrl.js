34e2700b758f221232d20e11e11baa22b663eb3a
Peter Huang <shi.huang@nih.gov>
2016-06-23 16:32:48 -0400

    almost finished

--- modules/cde/public/js/controllers/CdeDiffCtrl.js
+++ modules/cde/public/js/controllers/CdeDiffCtrl.js
@@ -45,15 +45,46 @@ angular.module('cdeModule').controller('CdeDiffCtrl', ['$scope', '$http', '$uibM
 
 }]);
 
-angular.module('systemModule').controller('CdeDiffModalCtrl', ['$scope', '$http', '$uibModalInstance', 'eltId', 'priorCdeId', function ($scope, $http, $modal, eltId, priorCdeId) {
-    $scope.option = {
-        properties: [{label: 'version', property: 'version'}]
-    };
+angular.module('systemModule').controller('CdeDiffModalCtrl', ['$scope', '$http', '$timeout', '$uibModalInstance', 'eltId', 'priorCdeId', function ($scope, $http, $timeout, $modal, eltId, priorCdeId) {
     $http.get('/cdeById/' + eltId).then(function (result) {
         $scope.elt = result.data;
-    });
-    $http.get('/cdeById/' + priorCdeId).then(function (result) {
-        $scope.priorCde = result.data;
+        $http.get('/cdeById/' + priorCdeId).then(function (result) {
+            $scope.priorCde = result.data;
+            $scope.optionArray = [{
+                properties: [{label: 'Version', property: 'version'}],
+                left: $scope.elt.version,
+                right: $scope.priorCde.version
+            }, {
+                properties: [
+                    {label: 'Name', property: 'designation'}, {
+                        label: 'Definition',
+                        property: 'definition'
+                    }, {label: 'Context', property: 'context.contextName'}
+                ],
+                left: $scope.elt.naming,
+                right: $scope.priorCde.naming
+            }, {
+                properties: [
+                    {label: 'Title', property: 'title'},
+                    {label: 'URI', property: 'uri'},
+                    {
+                        label: 'Provider Org',
+                        property: 'providerOrg'
+                    },
+                    {label: 'Language Code', property: 'languageCode'},
+                    {label: 'Document', property: 'document'}
+                ],
+                left: $scope.elt.referenceDocuments,
+                right: $scope.priorCde.referenceDocuments
+            }, {
+                properties: [
+                    {label: 'Key', property: 'key'},
+                    {label: 'Value', property: 'value'}
+                ],
+                left: $scope.elt.properties,
+                right: $scope.priorCde.properties
+            }];
+        });
     });
 
 }]);
