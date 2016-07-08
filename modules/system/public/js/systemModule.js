1c73d6bfc16164f1eacf1828cbd4291562ba39d9
ludetc <ludetc@mail.nih.gov>
2016-07-08 10:24:02 -0400

    fix random error in uploadMalicious

--- modules/system/public/js/systemModule.js
+++ modules/system/public/js/systemModule.js
@@ -331,7 +331,7 @@ angular.module('systemModule').config(function ($provide) {
                         url: window.location.pathname
                     });
                 } catch (e) {
-
+    
                 }
             };
         }]);
