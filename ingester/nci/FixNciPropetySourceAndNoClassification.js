c9994fa27aaf37f413906ffabb5214f49ec87455
Peter Huang <shi.huang@nih.gov>
2016-07-12 11:23:10 -0400

    fix script

--- ingester/nci/FixNciPropetySourceAndNoClassification.js
+++ ingester/nci/FixNciPropetySourceAndNoClassification.js
@@ -3,18 +3,17 @@ var async = require('async'),
     ;
 
 var today = new Date();
-var yesterday = new Date();
-yesterday.setDate(today.getDate() - 1);
 var user = {username: 'BatchLoader'};
 var deCounter = 0;
 DataElementModel.find({
     'stewardOrg.name': 'NCI',
     'archived': null,
     'registrationState.registrationStatus': {$ne: "Retired"},
-    'updated': {$lt: yesterday}
-}).limit(5000).exec(function (err, DEs) {
+    'updated': {$exists: false}
+//    'updated': {$ne:today}
+}).limit(10000).exec(function (err, DEs) {
     if (err) throw err;
-    async.forEach(DEs, function (de, doneOneDe) {
+    async.forEachSeries(DEs, function (de, doneOneDe) {
         de.source = 'caDSR';
         de.updatedBy = user;
         de.updated = today.toJSON();
