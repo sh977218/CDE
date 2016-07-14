40f6f9b0639c773095471ee608bcc048600b7ad3
ludetc <ludetc@mail.nih.gov>
2016-07-14 11:41:51 -0400

    fix test

--- test/selenium/src/test/java/gov/nih/nlm/cde/test/boards/BoardXMLExportTest.java
+++ test/selenium/src/test/java/gov/nih/nlm/cde/test/boards/BoardXMLExportTest.java
@@ -1,14 +1,8 @@
 package gov.nih.nlm.cde.test.boards;
 
 import org.openqa.selenium.By;
-import gov.nih.nlm.system.NlmCdeBaseTest;
-
-import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
-import static com.jayway.restassured.RestAssured.get;
-
 
 public class BoardXMLExportTest  extends BoardTest {
 
@@ -18,33 +12,12 @@ public class BoardXMLExportTest  extends BoardTest {
         goToBoard("Classify Board");
         textPresent("Export Board");
         clickElement(By.id(("export")));
-        String url = findElement(By.id("xmlExport")).getAttribute("href");
-        String response = get(url).asString();
+        clickElement(By.id("xmlExport"));
 
-        System.out.println("URL: " + url);
-        System.out.println("REsponse: " + response);
+        switchTab(1);
+        Assert.assertTrue(driver.getPageSource().contains("<primaryDefinitionCopy>Name of pathologist who diagnosed the case</primaryDefinitionCopy>"));
 
-  /*      mustBeLoggedInAs(reguser_username, password);
-        String form = "Parenchymal Imaging";
-        goToFormByName(form);
-
-        findElement(By.id("export")).click();
-        String url = findElement(By.id("nihXml")).getAttribute("href");
-        String response = get(url).asString();
-*/
-
-        Assert.assertTrue((response.length() > 0));
-//        findElement(By.id("xmlExport")).click();
-//        switchTab(1);
-//        textPresent( "<primaryDefinitionCopy>Name of pathologist who diagnosed the case</primaryDefinitionCopy>" );
-
-
-//        System.out.println("parent source" + driver.getPageSource());
-
-    //    Assert.assertTrue(driver.getPageSource().contains( "<primaryDefinitionCopy>Name of pathologist who diagnosed the case</primaryDefinitionCopy>" ));
-
-//        switchTabAndClose(0);
-
+        switchTabAndClose(0);
     }
 
 }
