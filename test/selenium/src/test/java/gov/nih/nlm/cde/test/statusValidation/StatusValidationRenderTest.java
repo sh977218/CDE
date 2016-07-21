dd1ca4f1a43a330ccc082b33f068531c986f497f
Jakub Flaska <jakub.flaska@nih.gov>
2016-07-21 15:25:44 -0400

    tests

--- test/selenium/src/test/java/gov/nih/nlm/cde/test/statusValidation/StatusValidationRenderTest.java
+++ test/selenium/src/test/java/gov/nih/nlm/cde/test/statusValidation/StatusValidationRenderTest.java
@@ -38,4 +38,30 @@ public class StatusValidationRenderTest extends BaseClassificationTest {
         textPresent("Distance from Closest Margin");
         textPresent("Neoadjuvant Therapy Specify");
     }
+
+    @Test
+    public void setValidRules(){
+        mustBeLoggedInAs(nlm_username, nlm_password);
+        findElement(By.id("username_link")).click();
+        findElement(By.id("user_org_authority")).click();
+        findElement(By.linkText("Status Validation Rules")).click();
+        findElement(By.id("addRule")).click();
+
+        new Select(driver.findElement(By.id("org"))).selectByVisibleText("TEST");
+        findElement(By.id("ruleName")).sendKeys("Test Rule #1");
+        new Select(driver.findElement(By.id("field"))).selectByVisibleText("properties.key");
+        new Select(driver.findElement(By.id("targetStatus"))).selectByVisibleText("Recorded");
+        findElement(By.id("regex")).sendKeys("non-existent");
+        new Select(driver.findElement(By.id("occurence"))).selectByVisibleText("All Elements");
+        findElement(by.id("saveRule")).click();
+        goToCdeSearch();
+        findElement(By.id("browseOrg-TEST")).click();
+
+        findElement(By.id("export")).click();
+        findElement(By.id("exportValidRule")).click();
+        findElement(By.id("selectStatus")).click();
+        findElement(By.id("recorded")).click();
+        findElement(By.id("exportVR")).click();
+        textPresent("Test Rule #1");
+    }
 }
