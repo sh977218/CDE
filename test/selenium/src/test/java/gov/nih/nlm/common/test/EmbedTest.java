366990f2b121133a748c943e7a9deeb7bbd8c54a
ludetc <ludetc@mail.nih.gov>
2016-07-25 15:35:35 -0400

    adding test, not done

--- test/selenium/src/test/java/gov/nih/nlm/common/test/EmbedTest.java
+++ test/selenium/src/test/java/gov/nih/nlm/common/test/EmbedTest.java
@@ -1,13 +1,17 @@
 package gov.nih.nlm.common.test;
 
 import gov.nih.nlm.system.NlmCdeBaseTest;
+import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
 public class EmbedTest extends NlmCdeBaseTest {
 
     @Test
     public void embedNinds() {
-
+        mustBeLoggedInAs(ninds_username, password);
+        clickElement(By.id("username_link"));
+        clickElement(By.linkText("Account Management"));
+        clickElement(By.id("embeddingTab"));
     }
 
 }
