41b1423acaf0643a86e73b62b28b243256857aee
ludetc <ludetc@mail.nih.gov>
2016-07-25 13:27:55 -0400

    skeleton test

---
+++ test/selenium/src/test/java/gov/nih/nlm/common/test/EmbedTest.java
@@ -0,0 +1,13 @@
+package gov.nih.nlm.common.test;
+
+import gov.nih.nlm.system.NlmCdeBaseTest;
+import org.testng.annotations.Test;
+
+public class EmbedTest extends NlmCdeBaseTest {
+
+    @Test
+    public void embedNinds() {
+
+    }
+
+}
