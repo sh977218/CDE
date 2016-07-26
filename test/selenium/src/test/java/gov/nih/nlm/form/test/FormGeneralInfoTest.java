c8233373a96e6a09cfb6428b311f0535f269f096
Peter Huang <shi.huang@nih.gov>
2016-07-26 16:42:55 -0400

    fix comment

--- test/selenium/src/test/java/gov/nih/nlm/form/test/FormGeneralInfoTest.java
+++ test/selenium/src/test/java/gov/nih/nlm/form/test/FormGeneralInfoTest.java
@@ -3,13 +3,16 @@ package gov.nih.nlm.form.test;
 import org.testng.annotations.Test;
 
 public class FormGeneralInfoTest extends BaseFormTest {
-    
+
     @Test
     public void formGeneralInformationTest() {
         goToFormByName("Section Inside Section Form");
         textPresent("Date Created:");
+        textPresent("05/09/2016 @ 5:21PM");
         textPresent("Updated:");
+        textPresent("05/11/2016 @ 11:11AM");
         textPresent("Created By:");
+        textPresent("testAdmin");
     }
 
 }
