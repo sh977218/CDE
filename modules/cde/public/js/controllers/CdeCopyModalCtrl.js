05058080018bc50a38c9487edfaeba57e644dc32
Jakub Flaska <jakub.flaska@nih.gov>
2016-08-02 16:25:44 -0400

    fixed

--- modules/cde/public/js/controllers/CdeCopyModalCtrl.js
+++ modules/cde/public/js/controllers/CdeCopyModalCtrl.js
@@ -4,32 +4,34 @@ angular.module('cdeModule').controller('CdeCopyModalCtrl',
     elt.classification = [];
     delete elt.tinyId;
     delete elt._id;
-    //
-    //delete elt.source;
-    //delete elt.sourceId;
-    //delete elt.origin;
-    //delete elt.created;
-    //
-    //delete elt.updated;
-    //delete elt.imported;
-    //delete elt.updatedBy;
-    //delete elt.createdBy;
-    //delete elt.version;
-    //
-    //delete elt.history;
-    //delete elt.changeNote;
-    //delete elt.comments;
-    //
-    //delete elt.forkOf;
-    //delete elt.views;
+
+    delete elt.source;
+    delete elt.sourceId;
+    delete elt.origin;
+    delete elt.created;
+
+    delete elt.updated;
+    delete elt.imported;
+    delete elt.updatedBy;
+    delete elt.createdBy;
+    delete elt.version;
+
+    delete elt.history;
+    delete elt.changeNote;
+    delete elt.comments;
+
+    delete elt.forkOf;
+    delete elt.views;
 
     elt.ids = [];
     elt.registrationState.registrationStatus = "Incomplete";
-    elt.registrationState.administrativeNote = "Copy of: " + $scope.elt.naming[0].designation;
+    elt.registrationState.administrativeNote = "Copy of: " + elt.naming[0].designation;
+
     delete elt.registrationState.administrativeStatus;
     $scope.elt = JSON.parse(JSON.stringify(elt));
     $scope.origName = $scope.elt.naming[0].designation;
     $scope.elt.naming[0].designation = "Copy of: " + $scope.elt.naming[0].designation;
+
     $scope.myOrgs = userResource.userOrgs;
     $controller('CreateCdeAbstractCtrl', {$scope: $scope});
 
