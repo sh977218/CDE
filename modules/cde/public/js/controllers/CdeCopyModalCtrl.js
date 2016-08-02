023b3d97db834d005858c99dcc6674d8cd14926c
Jakub Flaska <jakub.flaska@nih.gov>
2016-08-02 15:53:40 -0400

    small changes

--- modules/cde/public/js/controllers/CdeCopyModalCtrl.js
+++ modules/cde/public/js/controllers/CdeCopyModalCtrl.js
@@ -4,8 +4,28 @@ angular.module('cdeModule').controller('CdeCopyModalCtrl',
     elt.classification = [];
     delete elt.tinyId;
     delete elt._id;
+    //
+    //delete elt.source;
+    //delete elt.sourceId;
+    //delete elt.origin;
+    //delete elt.created;
+    //
+    //delete elt.updated;
+    //delete elt.imported;
+    //delete elt.updatedBy;
+    //delete elt.createdBy;
+    //delete elt.version;
+    //
+    //delete elt.history;
+    //delete elt.changeNote;
+    //delete elt.comments;
+    //
+    //delete elt.forkOf;
+    //delete elt.views;
+
     elt.ids = [];
     elt.registrationState.registrationStatus = "Incomplete";
+    elt.registrationState.administrativeNote = "Copy of: " + $scope.elt.naming[0].designation;
     delete elt.registrationState.administrativeStatus;
     $scope.elt = JSON.parse(JSON.stringify(elt));
     $scope.origName = $scope.elt.naming[0].designation;
