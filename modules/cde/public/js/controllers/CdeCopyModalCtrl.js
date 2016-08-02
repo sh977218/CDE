b09b641ab515355129c751c2912cb8b7c8b6ae06
Jakub Flaska <jakub.flaska@nih.gov>
2016-08-02 16:31:35 -0400

    fixes

--- modules/cde/public/js/controllers/CdeCopyModalCtrl.js
+++ modules/cde/public/js/controllers/CdeCopyModalCtrl.js
@@ -5,34 +5,26 @@ angular.module('cdeModule').controller('CdeCopyModalCtrl',
     elt.registrationState.administrativeNote = "Copy of: " + elt.tinyId;
     delete elt.tinyId;
     delete elt._id;
-
     delete elt.source;
     delete elt.sourceId;
     delete elt.origin;
     delete elt.created;
-
     delete elt.updated;
     delete elt.imported;
     delete elt.updatedBy;
     delete elt.createdBy;
     delete elt.version;
-
     delete elt.history;
     delete elt.changeNote;
     delete elt.comments;
-
     delete elt.forkOf;
     delete elt.views;
-
     elt.ids = [];
     elt.registrationState.registrationStatus = "Incomplete";
-
-
     delete elt.registrationState.administrativeStatus;
     $scope.elt = JSON.parse(JSON.stringify(elt));
     $scope.origName = $scope.elt.naming[0].designation;
     $scope.elt.naming[0].designation = "Copy of: " + $scope.elt.naming[0].designation;
-
     $scope.myOrgs = userResource.userOrgs;
     $controller('CreateCdeAbstractCtrl', {$scope: $scope});
 
