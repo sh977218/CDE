95626ce06966b75b3588e270290e17baa59c2e4e
Peter Huang <shi.huang@nih.gov>
2016-08-03 11:37:14 -0400

    done

--- modules/cde/public/js/controllers/CdeHistoryCtrl.js
+++ modules/cde/public/js/controllers/CdeHistoryCtrl.js
@@ -1,5 +1,5 @@
-angular.module('cdeModule').controller('CdeHistoryCtrl', ['$scope', '$rootScope', '$http',
-    function ($scope, $rootScope, $http) {
+angular.module('cdeModule').controller('CdeHistoryCtrl', ['$scope', 'ClassificationUtil', '$http',
+    function ($scope, ClassificationUtil, $http) {
         $scope.showVersioned = false;
         $scope.showHistory = false;
         $scope.selectedObjs = {length: 0, selected: {}};
@@ -22,26 +22,6 @@ angular.module('cdeModule').controller('CdeHistoryCtrl', ['$scope', '$rootScope'
             return $scope.selectedIds.indexOf(id) > -1;
         };
 
-        $scope.viewDiff = function () {
-            if ($scope.selectedIds.length === 0) {
-                $scope.addAlert("danger", "Select two to compare.");
-            } else {
-                $scope.cdes = [];
-                $rootScope.eltHistoryCompare = {};
-                $scope.priorCdes.forEach(function (o) {
-                    if (o._id === $scope.selectedIds[0]) {
-                        $scope.cdes[0] = o;
-                        $rootScope.eltHistoryCompare.left = o;
-                    }
-                    if (o._id === $scope.selectedIds[1]) {
-                        $scope.cdes[1] = o;
-                        $rootScope.eltHistoryCompare.right = o;
-                    }
-                });
-                $scope.showHistory = true;
-            }
-        };
-
         function loadPriorCdes() {
             if (!$scope.priorCdes) {
                 if ($scope.elt.history && $scope.elt.history.length > 0) {
@@ -57,4 +37,248 @@ angular.module('cdeModule').controller('CdeHistoryCtrl', ['$scope', '$rootScope'
 
         $scope.historyCtrlLoadedPromise.resolve();
 
+        var datatypeCompareMap = {
+            'Value List': {
+                obj: 'permissibleValues', options: {
+                    equal: function (a, b) {
+                        return a.permissibleValue === b.permissibleValue &&
+                            a.valueMeaningName === b.valueMeaningName &&
+                            a.valueMeaningCode === b.valueMeaningCode &&
+                            a.valueMeaningDefinition === b.valueMeaningDefinition &&
+                            a.codeSystemName === b.codeSystemName;
+                    },
+                    sort: function (a, b) {
+                        return a.permissibleValue - b.permissibleValue;
+                    },
+                    title: 'Data Type Value List', hideSame: false,
+                    properties: [{label: 'Value', property: 'permissibleValue'},
+                        {label: 'Code Name', property: 'valueMeaningName'},
+                        {label: 'Code', property: 'valueMeaningCode'},
+                        {label: 'Code System', property: 'valueMeaningDefinition'},
+                        {label: 'Code Description', property: 'codeSystemName'}],
+                    tooltip: ''
+                }
+            },
+            'Text': {
+                obj: 'datatypeText', options: {
+                    title: 'Data Type Text', hideSame: false,
+                    properties: [{label: 'Minimum Length', property: 'minLength'}, {
+                        label: 'Maximum Length',
+                        property: 'maxLength'
+                    }, {label: 'Regular Expression', property: 'regex'}, {label: 'Free text rule', property: 'rule'}],
+                    tooltip: ''
+                }
+            },
+            'Date': {
+                obj: 'datatypeDate',
+                options: {
+                    title: 'Data Type Date', hideSame: false,
+                    properties: [{label: 'Date Format', property: 'format'}],
+                    tooltip: ''
+                }
+            },
+            'Time': {
+                obj: 'datatypeTime',
+                options: {
+                    title: 'Data Type Time', hideSame: false,
+                    properties: [{label: 'Date Format', property: 'format'}],
+                    tooltip: ''
+                }
+            },
+            'Number': {
+                obj: 'datatypeNumber', options: {
+                    title: 'Data Type Number', hideSame: false,
+                    properties: [{label: 'Minimum Value', property: 'minValue'}, {
+                        label: 'Maximum Value',
+                        property: 'maxValue'
+                    }, {label: 'Precision', property: 'precision'}],
+                    tooltip: ''
+                }
+            },
+            'datatypeExternallyDefined': ''
+        };
+
+        $scope.viewDiff = function () {
+            if ($scope.selectedIds.length === 0) {
+                $scope.addAlert("danger", "Select two to compare.");
+            } else {
+                $scope.priorCdes.forEach(function (o) {
+                    if (o._id === $scope.selectedIds[0]) {
+                        $scope.left = o;
+                    }
+                    if (o._id === $scope.selectedIds[1]) {
+                        $scope.right = o;
+                    }
+                });
+                $scope.showHistory = true;
+                ClassificationUtil.sortClassification($scope.left);
+                ClassificationUtil.sortClassification($scope.right);
+                $scope.left.flatClassifications = ClassificationUtil.flattenClassification($scope.left);
+                $scope.right.flatClassifications = ClassificationUtil.flattenClassification($scope.right);
+
+                $scope.leftDatatypeObj = {
+                    obj: $scope.left.valueDomain[datatypeCompareMap[$scope.left.valueDomain.datatype].obj],
+                    p: datatypeCompareMap[$scope.left.valueDomain.datatype].options
+                };
+                $scope.rightDatatypeObj = {
+                    obj: $scope.right.valueDomain[datatypeCompareMap[$scope.right.valueDomain.datatype].obj],
+                    p: datatypeCompareMap[$scope.right.valueDomain.datatype].options
+                };
+
+            }
+        };
+
+        $scope.versionOptions = {
+            title: 'Versions',
+            hideSame: true,
+            tooltip: ''
+        };
+        $scope.statusOptions = {
+            title: 'Status',
+            hideSame: true,
+            tooltip: ''
+        };
+        $scope.stewardOrgNameOptions = {
+            title: 'StewardOrg Name',
+            hideSame: true,
+            tooltip: ''
+        };
+        $scope.namingOptions = {
+            equal: function (a, b) {
+                return a.designation === b.designation &&
+                    a.definition === b.definition &&
+                    a.languageCode === b.languageCode &&
+                    a.context.acceptability === b.context.acceptability &&
+                    a.context.contextName === b.context.contextName;
+            },
+            sort: function (a, b) {
+                return a.designation - b.designation;
+            },
+            tooltip: 'Names are sorted by designation, compared by it\'s all properties',
+            title: 'Names',
+            hideSame: true,
+            properties: [
+                {label: 'Name', property: 'designation'}, {
+                    label: 'Definition',
+                    property: 'definition'
+                }, {label: 'Context', property: 'context.contextName'}
+            ]
+        };
+        $scope.dataElementConceptOptions = {
+            equal: function (a, b) {
+                return a.name === b.name &&
+                    a.origin === b.origin &&
+                    a.originId === b.originId;
+            },
+            sort: function (a, b) {
+                return a.name - b.name;
+            },
+            tooltip: 'dataElementConcept are sorted by name, compared by it\'s all properties',
+            title: 'Concept',
+            hideSame: true,
+            properties: [
+                {label: 'Name', property: 'name'}, {
+                    label: 'Origin',
+                    property: 'origin'
+                }, {label: 'OriginId', property: 'originId'}
+            ]
+        };
+        $scope.propertyConceptOptions = {
+            equal: function (a, b) {
+                return a.name === b.name &&
+                    a.origin === b.origin &&
+                    a.originId === b.originId;
+            },
+            sort: function (a, b) {
+                return a.name - b.name;
+            },
+            tooltip: 'propertyConcept are sorted by name, compared by it\'s all properties',
+            title: 'Property Concept',
+            hideSame: true,
+            properties: [
+                {label: 'Name', property: 'name'}, {
+                    label: 'Origin',
+                    property: 'origin'
+                }, {label: 'OriginId', property: 'originId'}
+            ]
+        };
+        $scope.objectClassConceptOptions = {
+            equal: function (a, b) {
+                return a.name === b.name &&
+                    a.origin === b.origin &&
+                    a.originId === b.originId;
+            },
+            sort: function (a, b) {
+                return a.name - b.name;
+            },
+            tooltip: 'objectClassConcept are sorted by name, compared by it\'s all properties',
+            title: 'ObjectClass Concept',
+            hideSame: true,
+            properties: [
+                {label: 'Name', property: 'name'}, {
+                    label: 'Origin',
+                    property: 'origin'
+                }, {label: 'OriginId', property: 'originId'}
+            ]
+        };
+        $scope.referenceDocumentsOptions = {
+            equal: function (a, b) {
+                return a.title === b.title;
+            },
+            sort: function (a, b) {
+                return a.title - b.title;
+            },
+            title: 'Reference Documents',
+            hideSame: true,
+            properties: [
+                {label: 'Title', property: 'title'},
+                {label: 'URI', property: 'uri'},
+                {
+                    label: 'Provider Org',
+                    property: 'providerOrg'
+                },
+                {label: 'Language Code', property: 'languageCode'},
+                {label: 'Document', property: 'document'}
+            ]
+        };
+        $scope.propertiesOptions = {
+            equal: function (a, b) {
+                return a.key === b.key;
+            },
+            sort: function (a, b) {
+                return a.key - b.key;
+            },
+            title: 'Properties',
+            hideSame: true,
+            properties: [
+                {label: 'Key', property: 'key'},
+                {label: 'Value', property: 'value'}
+            ]
+        };
+        $scope.idsOptions = {
+            equal: function (a, b) {
+                return a.id === b.id &&
+                    a.source === b.source &&
+                    a.version === b.version;
+            },
+            sort: function (a, b) {
+                return a.id - b.id;
+            },
+            title: 'IDs',
+            hideSame: true,
+            properties: [
+                {label: 'Source', property: 'source'},
+                {label: 'ID', property: 'id'},
+                {label: 'Version', property: 'version'}
+            ]
+        };
+        $scope.classificationsOptions = {
+            title: 'Classifications',
+            hideSame: true
+        };
+        $scope.valueDomainDatatypeOptions = {
+            title: 'Value Type',
+            hideSame: false,
+            tooltip: ''
+        };
     }]);
--- modules/system/public/js/compare-primitive-array.js
+++ modules/system/public/js/compare-primitive-array.js
@@ -34,39 +34,46 @@
                         return a.localeCompare(b);
                     }
                 }
-            },
-            primitiveArrayCompare: function (leftArray, rightArray, options) {
                 options.matchCount = 0;
                 options.results = [];
                 options.showTitle = false;
-
+            },
+            primitiveArrayCompare: function (leftArray, rightArray, options) {
+                function findIndexInArray(array, beginRightIndex, item) {
+                    if (!array || array.length === 0 || beginRightIndex === array.length)
+                        return -1;
+                    for (var index = beginRightIndex; index < array.length; index++) {
+                        if (item === array[index]) return index;
+                    }
+                    return -1;
+                }
                 leftArray.sort(options.sort);
                 rightArray.sort(options.sort);
 
-                var rightArrayCopy = angular.copy(rightArray);
                 var beginRightIndex = 0;
                 leftArray.forEach(function (leftItem, leftIndex) {
-                    rightArrayCopy = rightArray.slice(beginRightIndex, rightArray.length);
-                    var foundInRight = rightArrayCopy.indexOf(leftItem);
+                    var foundInRight = findIndexInArray(rightArray, beginRightIndex, leftItem);
                     if (foundInRight === -1) {
                         options.showTitle = true;
                         options.results.push({leftIndex: leftIndex, match: false});
-                        if (leftIndex === leftArray.length - 1) {
-                            for (var j = 0; j < rightArrayCopy.length; j++) {
-                                options.results.push({rightIndex: j + beginRightIndex, match: false});
-                            }
-                        }
                     } else {
                         options.matchCount++;
-                        for (var i = 0; i < foundInRight; i++) {
-                            options.results.push({rightIndex: i + beginRightIndex, match: false});
+                        for (var i = beginRightIndex; i < foundInRight; i++) {
+                            options.showTitle = true;
+                            options.results.push({rightIndex: i, match: false});
                         }
                         options.results.push({
                             leftIndex: leftIndex,
-                            rightIndex: foundInRight + beginRightIndex,
+                            rightIndex: foundInRight,
                             match: true
                         });
-                        beginRightIndex = beginRightIndex + foundInRight + 1;
+                        beginRightIndex = foundInRight + 1;
+                    }
+                    if (leftIndex === leftArray.length - 1) {
+                        for (var j = beginRightIndex; j < rightArray.length; j++) {
+                            options.showTitle = true;
+                            options.results.push({rightIndex: j, match: false});
+                        }
                     }
                 });
             }
