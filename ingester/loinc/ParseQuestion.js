42058f2f568569ecd85d902129e64f300480dcfb
Peter Huang <shi.huang@nih.gov>
2016-08-22 17:07:52 -0400

    half done

--- ingester/loinc/ParseQuestion.js
+++ ingester/loinc/ParseQuestion.js
@@ -2,45 +2,85 @@ var async = require('async');
 var webdriver = require('selenium-webdriver');
 var By = webdriver.By;
 
+/*
+j needs to be call by reference
+*/
 function loadQuestionInformation(panelHierarchy, array, j, cb) {
-    async.forEach(panelHierarchy.elements, function (element, doneOneElement) {
-        if(element.elements.length)
-        var sections = array[j];
-        sections[0].findElement(By.xpath('(table/tbody/tr/td)[1]')).getText().then(function (loincText) {
-            if (loincText.trim() === element['LOINC#']) {
-                sections.shift();
-                async.forEachSeries(sections, function (section, doneOneSection) {
-                    section.findElements(By.xpath('table/tbody/tr[1]/th')).then(function (sectionHeaderTable) {
-                        if (sectionHeaderTable.length > 0) {
-                            sectionHeaderTable[0].getText().then(function (sectionHeaderText) {
-                                var sectionHeader = sectionHeaderText.trim();
-                                if (sectionHeader === 'OBSERVATION ID IN FORM') {
-                                    section.findElement(By.xpath('(table/tbody/tr)[2]')).getText().then(function (text) {
-                                        element['OBSERVATION ID IN FORM'] = text.trim();
+    async.forEachSeries(panelHierarchy.elements, function (element, doneOneElement) {
+        if (element.elements.length === 0) {
+            var sections = array[j];
+            sections[0].findElement(By.xpath('table/tbody/tr/td[1]')).getText().then(function (loincText) {
+                if (loincText.trim() === element['LOINC#']) {
+                    sections.shift();
+                    async.forEachSeries(sections, function (section, doneOneSection) {
+                        section.findElements(By.xpath('table/tbody/tr[1]/th')).then(function (sectionHeaderTable) {
+                            if (sectionHeaderTable.length > 0) {
+                                sectionHeaderTable[0].getText().then(function (sectionHeaderText) {
+                                    var sectionHeader = sectionHeaderText.trim();
+                                    if (sectionHeader === 'OBSERVATION ID IN FORM') {
+                                        section.findElement(By.xpath('table/tbody/tr[2]')).getText().then(function (text) {
+                                            element['OBSERVATION ID IN FORM'] = text.trim();
+                                            doneOneSection();
+                                        })
+                                    } else if (sectionHeader === 'ANSWER CARDINALITY') {
+                                        section.findElement(By.xpath('table/tbody/tr[2]')).getText().then(function (text) {
+                                            element['ANSWER CARDINALITY'] = text.trim();
+                                            doneOneSection();
+                                        })
+                                    } else {
                                         doneOneSection();
-                                    })
-                                } else if (sectionHeader === 'ANSWER CARDINALITY') {
-                                    section.findElement(By.xpath('(table/tbody/tr)[2]')).getText().then(function (text) {
-                                        element['ANSWER CARDINALITY'] = text.trim();
-                                        doneOneSection();
-                                    })
-                                } else {
-                                    doneOneSection();
-                                }
-                            })
-                        } else {
-                            doneOneSection();
-                        }
+                                    }
+                                })
+                            } else {
+                                doneOneSection();
+                            }
+                        })
+                    }, function doneAllSections() {
+                        j++;
+                        doneOneElement();
                     })
-                }, function doneAllSections() {
-                    j++;
-                    doneOneElement();
-                })
-            } else {
-                console.log('loinc # does not match');
-                process.exit(1);
-            }
-        });
+                } else {
+                    console.log('loinc # does not match. Question');
+                    console.log('element["LOINC#"]: ' + element['LOINC#']);
+                    console.log('loincText: ' + loincText.trim());
+                    process.exit(1);
+                }
+            });
+        } else {
+            var sections = array[j];
+            sections[0].findElement(By.xpath('table/tbody/tr/td[1]')).getText().then(function (loincText) {
+                if (loincText.trim() === element['LOINC#']) {
+                    sections.shift();
+                    async.forEachSeries(sections, function (section, doneOneSection) {
+                        section.findElements(By.xpath('table/tbody/tr[1]/th')).then(function (sectionHeaderTable) {
+                            if (sectionHeaderTable.length > 0) {
+                                sectionHeaderTable[0].getText().then(function (sectionHeaderText) {
+                                    var sectionHeader = sectionHeaderText.trim();
+                                    if (sectionHeader === 'FORM CODING INSTRUCTIONS') {
+                                        section.findElement(By.xpath('table/tbody/tr[2]')).getText().then(function (text) {
+                                            element['FORM CODING INSTRUCTIONS'] = text.trim();
+                                            doneOneSection();
+                                        })
+                                    } else {
+                                        doneOneSection();
+                                    }
+                                })
+                            } else {
+                                doneOneSection();
+                            }
+                        })
+                    }, function doneAllSections() {
+                        j++;
+                        loadQuestionInformation(element, array, j, doneOneElement)
+                    })
+                } else {
+                    console.log('loinc # does not match. Section');
+                    console.log('element["LOINC#"]: ' + element['LOINC#']);
+                    console.log('loincText: ' + loincText.trim());
+                    process.exit(1);
+                }
+            });
+        }
     }, function doneAllElements() {
         cb();
     });
