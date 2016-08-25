b5862e3ad1a39ff0dfb4bc0b37d8dd020f205c94
ludetc <ludetc@mail.nih.gov>
2016-08-25 10:35:33 -0400

    fix create form from board

--- test/selenium/src/test/java/gov/nih/nlm/form/test/board/CreateFormFromBoardTest.java
+++ test/selenium/src/test/java/gov/nih/nlm/form/test/board/CreateFormFromBoardTest.java
@@ -19,6 +19,8 @@ public class CreateFormFromBoardTest extends BoardTest {
         findElement(By.id("formVersion")).sendKeys("1.0");
         new Select(findElement(By.id("elt.stewardOrg.name"))).selectByVisibleText("TEST");
         baseClassificationTest.addClassificationToNewCdeMethod(new String[]{"TEST", "Classify Board", "Classif_Board_Sub"});
+        clickElement(By.id("closeModal"));
+        modalGone();
         clickElement(By.id("submit"));
         textPresent("Incomplete");
         textNotPresent("have newer version");
