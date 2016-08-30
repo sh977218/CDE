ec6cf25d32a7e658bf2f23893fb8cfdd55529644
ludetc <ludetc@mail.nih.gov>
2016-08-30 12:05:22 -0400

    fix test

--- test/selenium/src/test/java/gov/nih/nlm/cde/test/MeshTopics.java
+++ test/selenium/src/test/java/gov/nih/nlm/cde/test/MeshTopics.java
@@ -19,6 +19,7 @@ public class MeshTopics extends NlmCdeBaseTest {
         clickElement(By.xpath("//div[a/span/span[.='Disease']]//i[contains(@class, 'fa-link')]"));
         findElement(By.id("mesh.search")).clear();
         findElement(By.id("mesh.search")).sendKeys("NINDS");
+        textPresent("Neurological Disorders");
         clickElement(By.id("addMeshDescButton"));
         clickElement(By.id("closeModal"));
         textPresent("Saved");
