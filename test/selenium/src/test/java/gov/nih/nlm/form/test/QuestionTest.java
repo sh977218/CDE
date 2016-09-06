b9724a9b4f2b7c105e88aef8c5a6ab448da65cdb
ludetc <ludetc@mail.nih.gov>
2016-09-06 13:15:25 -0400

    fix score test

--- test/selenium/src/test/java/gov/nih/nlm/form/test/QuestionTest.java
+++ test/selenium/src/test/java/gov/nih/nlm/form/test/QuestionTest.java
@@ -2,6 +2,7 @@ package gov.nih.nlm.form.test;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -10,6 +11,14 @@ import org.testng.Assert;
 public class QuestionTest extends BaseFormTest {
 
     public void addQuestionToSection(String cdeName, int sectionNumber) {
+        try {
+            addQuestionToSectionUnsafe(cdeName, sectionNumber);
+        } catch (TimeoutException e) {
+            addQuestionToSectionUnsafe(cdeName, sectionNumber);
+        }
+    }
+
+    public void addQuestionToSectionUnsafe(String cdeName, int sectionNumber) {
         findElement(By.id("ftsearch-input")).clear();
         textPresent("", By.id("ftsearch-input"));
         findElement(By.id("ftsearch-input")).sendKeys("\"" + cdeName + "\"");
@@ -34,11 +43,7 @@ public class QuestionTest extends BaseFormTest {
         textPresent(cdeName, By.id("section_drop_area_" + sectionNumber));
 
     }
-
-    public void addQuestionToSectionSafe(String cdeName) {
-        addQuestionToSection(cdeName, 0);
-    }
-
+    
     public void addSectionToSection(int sectionNumFrom, int sectionNumTo) {
         WebElement sourceElt = findElement(By.xpath("//*[@id='section_view_" + sectionNumFrom + "']/div//i[contains(@class,'fa fa-arrows')]"));
         String sourceStr = findElement(By.xpath("//*[@id='section_view_" + sectionNumFrom + "']/div//div[contains(@id,'section_title')]")).getText();
