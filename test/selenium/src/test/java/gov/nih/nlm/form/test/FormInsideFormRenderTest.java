aac38d3aef3a3ac5ae4de4010e5a663b0cc7db9e
Peter Huang <shi.huang@nih.gov>
2016-09-08 17:13:59 -0400

    fix test fail

--- test/selenium/src/test/java/gov/nih/nlm/form/test/FormInsideFormRenderTest.java
+++ test/selenium/src/test/java/gov/nih/nlm/form/test/FormInsideFormRenderTest.java
@@ -24,7 +24,7 @@ public class FormInsideFormRenderTest extends NlmCdeBaseTest {
         clickElement(By.id("export"));
         clickElement(By.id("sdcExport"));
         switchTab(1);
-        textPresent("Outside section form: PROMIS SF v1.0 - Phys. Function 10a");
+        textPresent("PROMIS SF v1.0 - Phys. Function 10a");
         textPresent("Inside section form: PROMIS SF v1.0 - Phys. Function 10a");
         textPresent("Are you able to get on and off the toilet?");
         switchTabAndClose(0);
@@ -36,7 +36,7 @@ public class FormInsideFormRenderTest extends NlmCdeBaseTest {
         textPresent("This form is large and is not automatically displayed.");
         clickElement(By.id("renderPreviewButton"));
         textPresent("PROMIS SF v1.0 - Phys. Function 10a");
-        textPresent("Inside section form: PROMIS SF v1.0 - Phys. Function 10a");
+        textPresent("section contains form");
         textPresent("Are you able to get on and off the toilet?");
     }
 
@@ -46,7 +46,8 @@ public class FormInsideFormRenderTest extends NlmCdeBaseTest {
         clickElement(By.id("description_tab"));
         textPresent("This form is large and is not automatically displayed.");
         clickElement(By.id("renderPreviewButton"));
-        textPresent("Outside section form: PROMIS SF v1.0 - Phys. Function 10a");
+        textPresent("Embedded Form: Outside section form: PROMIS SF v1.0 - Phys. Function 10a");
+        textPresent("section contains form");
         textPresent("Embedded Form: Inside section form: PROMIS SF v1.0 - Phys. Function 10a");
     }
 
