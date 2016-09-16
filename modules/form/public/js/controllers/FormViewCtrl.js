07022b172e447df6f2e9c44f2ccf6b08c85e0694
Peter Huang <shi.huang@nih.gov>
2016-09-16 11:53:37 -0400

    add render html validator

--- modules/form/public/js/controllers/FormViewCtrl.js
+++ modules/form/public/js/controllers/FormViewCtrl.js
@@ -396,19 +396,26 @@ angular.module('formModule').controller
     };
 
     $scope.getCurrentOptions = function (currentContent, previousQuestions, thisQuestion) {
-        var filterFunc = function (e1) {
-            return e1.toLowerCase().indexOf(tokens.unmatched.toLowerCase()) > -1 &&
-                (!thisQuestion || e1.trim().toLowerCase().replace(/"/g, "") !== thisQuestion.label.trim().toLowerCase().replace(/"/g, ""));
-        };
-        var tokens = tokenSplitter(currentContent);
-        if (tokens.length === 0) return $scope.languageOptions("question", previousQuestions).filter(filterFunc);
-        if (tokens.length === 1) return $scope.languageOptions("operator", previousQuestions).map(function (e1) {
-            return currentContent + " " + e1;
-        });
-        if (tokens.length === 2) return $scope.languageOptions("answer", previousQuestions, null, tokens[0]).filter(filterFunc).map(function (e1) {
-            return "\"" + tokens[0] + "\" " + tokens[1] + " " + e1;
-        });
-        $scope.skipLogicError = 'has error';
+        if (!thisQuestion.skipLogic)thisQuestion.skipLogic = {};
+        if (!currentContent) {
+            thisQuestion.skipLogic.skipLogicError = '';
+            thisQuestion.skipLogic.suggestion = '';
+        }
+        else {
+//            $scope.skipLogicError = 'has error';
+            var filterFunc = function (e1) {
+                return e1.toLowerCase().indexOf(tokens.unmatched.toLowerCase()) > -1 &&
+                    (!thisQuestion || e1.trim().toLowerCase().replace(/"/g, "") !== thisQuestion.label.trim().toLowerCase().replace(/"/g, ""));
+            };
+            var tokens = tokenSplitter(currentContent);
+            if (tokens.length === 0) return $scope.languageOptions("question", previousQuestions).filter(filterFunc);
+            if (tokens.length === 1) return $scope.languageOptions("operator", previousQuestions).map(function (e1) {
+                return currentContent + " " + e1;
+            });
+            if (tokens.length === 2) return $scope.languageOptions("answer", previousQuestions, null, tokens[0]).filter(filterFunc).map(function (e1) {
+                return "\"" + tokens[0] + "\" " + tokens[1] + " " + e1;
+            });
+        }
     };
 
 
