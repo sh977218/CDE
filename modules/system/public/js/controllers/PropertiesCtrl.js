78873e3f4d7c2584285a5335a0bfc2b83f58cd61
Jakub Flaska <jakub.flaska@nih.gov>
2016-09-20 10:34:56 -0400

    undo 2

--- modules/system/public/js/controllers/PropertiesCtrl.js
+++ modules/system/public/js/controllers/PropertiesCtrl.js
@@ -1,108 +1,108 @@
 angular.module('systemModule').controller('PropertiesCtrl',
     ['$scope', '$uibModal', '$location', '$timeout', 'OrgHelpers', 'Alert', '$q',
-        function($scope, $modal, $location, $timeout, OrgHelpers, Alert, $q)
-        {
-            var keysLoaded = $q.defer();
+    function($scope, $modal, $location, $timeout, OrgHelpers, Alert, $q)
+{
+    var keysLoaded = $q.defer();
 
-            function refreshContexts(){
-                OrgHelpers.deferred.promise.then(function () {
-                    $scope.allKeys = OrgHelpers.orgsDetailedInfo[$scope.elt.stewardOrg.name].propertyKeys;
-                    keysLoaded.resolve();
-                });
+    function refreshContexts(){
+        OrgHelpers.deferred.promise.then(function () {
+            $scope.allKeys = OrgHelpers.orgsDetailedInfo[$scope.elt.stewardOrg.name].propertyKeys;
+            keysLoaded.resolve();
+        });
+    }
+
+    refreshContexts();
+
+    $scope.$on('elementReloaded', function() {
+        refreshContexts();
+    });
+
+    $scope.openNewProperty = function () {
+        keysLoaded.promise.then(function () {
+
+            if (!$scope.allKeys || $scope.allKeys.length === 0) {
+                Alert.addAlert("warning", "No valid property keys present, have an Org Admin go to Org Management > List Management to add one");
+                return;
             }
 
-            refreshContexts();
-
-            $scope.$on('elementReloaded', function() {
-                refreshContexts();
-            });
-
-            $scope.openNewProperty = function () {
-                keysLoaded.promise.then(function () {
-
-                    if (!$scope.allKeys || $scope.allKeys.length === 0) {
-                        Alert.addAlert("warning", "No valid property keys present, have an Org Admin go to Org Management > List Management to add one");
+            var modalInstance = $modal.open({
+                animation: false,
+                templateUrl: 'newPropertyModalContent.html',
+                controller: 'NewPropertyModalCtrl',
+                resolve: {
+                    elt: function () {
+                        return $scope.elt;
+                    },
+                    module: function () {
+                        return $scope.module;
+                    }
+                }
+            }).result.then(function (newProperty) {
+                for (var i = 0; i < $scope.elt.properties.length; i++) {
+                    if ($scope.elt.properties[i].key === newProperty.key) {
+                        $scope.addAlert("danger", "This property already exists.");
                         return;
                     }
-
-                    var modalInstance = $modal.open({
-                        animation: false,
-                        templateUrl: 'newPropertyModalContent.html',
-                        controller: 'NewPropertyModalCtrl',
-                        resolve: {
-                            elt: function () {
-                                return $scope.elt;
-                            },
-                            module: function () {
-                                return $scope.module;
-                            }
-                        }
-                    }).result.then(function (newProperty) {
-                            for (var i = 0; i < $scope.elt.properties.length; i++) {
-                                if ($scope.elt.properties[i].key === newProperty.key) {
-                                    $scope.addAlert("danger", "This property already exists.");
-                                    return;
-                                }
-                            }
-                            $scope.elt.properties.push(newProperty);
-                            if ($scope.elt.unsaved) {
-                                Alert.addAlert("info", "Property added. Save to confirm.");
-                            } else {
-                                $scope.elt.$save(function (newElt) {
-                                    $location.url($scope.baseLink + newElt.tinyId + "&tab=properties");
-                                    Alert.addAlert("success", "Property Added");
-                                });
-                            }
-                        });
-                });
-
-
-            };
-
-            $scope.removeProperty = function (index) {
-                $scope.elt.properties.splice(index, 1);
+                }
+                $scope.elt.properties.push(newProperty);
                 if ($scope.elt.unsaved) {
-                    $scope.addAlert("info", "Property removed. Save to confirm.");
+                    Alert.addAlert("info", "Property added. Save to confirm.");
                 } else {
                     $scope.elt.$save(function (newElt) {
                         $location.url($scope.baseLink + newElt.tinyId + "&tab=properties");
-                        $scope.elt = newElt;
-                        Alert.addAlert("success", "Property Removed");
+                        Alert.addAlert("success", "Property Added");
                     });
                 }
-            };
+            });
+        });
 
-            $scope.canEditProperty = function () {
-                return $scope.canCurate && !$scope.elt.unsaved;
-            };
 
-            $scope.saveProperty = function() {
-                $timeout(function() {
-                    $scope.elt.$save(function (newElt) {
-                        $location.url($scope.baseLink + newElt.tinyId + "&tab=properties");
-                        $scope.elt = newElt;
-                    });
-                }, 0);
-            };
+    };
+    
+    $scope.removeProperty = function (index) {
+        $scope.elt.properties.splice(index, 1);
+        if ($scope.elt.unsaved) {
+            $scope.addAlert("info", "Property removed. Save to confirm.");
+        } else {
+            $scope.elt.$save(function (newElt) {
+                $location.url($scope.baseLink + newElt.tinyId + "&tab=properties");
+                $scope.elt = newElt;
+                Alert.addAlert("success", "Property Removed");
+            });
+        }
+    };
 
-        }]);
+    $scope.canEditProperty = function () {
+        return $scope.canCurate && !$scope.elt.unsaved;
+    };
+
+    $scope.saveProperty = function() {
+        $timeout(function() {
+            $scope.elt.$save(function (newElt) {
+                $location.url($scope.baseLink + newElt.tinyId + "&tab=properties");
+                $scope.elt = newElt;
+            });
+        }, 0);
+    };
+
+}]);
 
 angular.module('systemModule').controller('NewPropertyModalCtrl',
     ['$scope', '$uibModalInstance', 'module', 'elt', 'OrgHelpers',
-        function ($scope, $modalInstance, module, elt, OrgHelpers) {
+    function ($scope, $modalInstance, module, elt, OrgHelpers) {
 
-            OrgHelpers.deferred.promise.then(function () {
-                $scope.orgPropertyKeys = OrgHelpers.orgsDetailedInfo[$scope.elt.stewardOrg.name].propertyKeys;
-            });
+        OrgHelpers.deferred.promise.then(function () {
+            $scope.orgPropertyKeys = OrgHelpers.orgsDetailedInfo[$scope.elt.stewardOrg.name].propertyKeys;
+        });
 
-            $scope.elt = elt;
-            $scope.newProperty = {};
+        $scope.elt = elt;
+        $scope.newProperty = {};
 
-            $scope.okCreate = function () {
-                $modalInstance.close($scope.newProperty);
-            };
+        $scope.okCreate = function () {
+            $modalInstance.close($scope.newProperty);
+        };
 
-            $scope.cancelCreate = function() {
-                $modalInstance.dismiss("Cancel");
-            };
-        }]);
+        $scope.cancelCreate = function() {
+            $modalInstance.dismiss("Cancel");
+        };
+}]);
