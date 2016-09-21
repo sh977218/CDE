9f2337bc765174933e5c97e5526cbd5263839412
ludetc <ludetc@mail.nih.gov>
2016-09-21 10:37:56 -0400

    fix quickboard compare

--- modules/cde/public/js/controllers/PermissibleValuesCtrl.js
+++ modules/cde/public/js/controllers/PermissibleValuesCtrl.js
@@ -18,11 +18,14 @@ angular.module('cdeModule').controller('PermissibleValuesCtrl', ['$scope', '$tim
         });
     });
 
-    $scope.deferredEltLoaded.promise.then(function(){
+    function loadSrcOptions() {
         Object.keys($scope.srcOptions).forEach(function(k){
             if ($scope.srcOptions[k].selected) lookupAsSource(k);
         });
-    });
+    }
+    if ($scope.deferredEltLoaded) {
+        $scope.deferredEltLoaded.promise.then(loadSrcOptions);
+    }
 
     $scope.srcOptions = {};
     $scope.containsKnownSystem = false;
