f804f2369e786c9839b755c1fde8ba7b22dc1c81
Peter Huang <shi.huang@nih.gov>
2016-09-26 10:52:55 -0400

    added test

---
+++ test/selenium/src/test/java/gov/nih/nlm/system/WelcomePageTest.java
@@ -0,0 +1,32 @@
+package gov.nih.nlm.system;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class WelcomePageTest extends NlmCdeBaseTest {
+
+    @Test
+    public void editHtmlOverView() {
+        String orgDetail = "The cancer Biomedical Informatics Grid (caBIG) was a US government program to develop an open source, open access information network called caGrid for secure data exchange on cancer research. The initiative was developed by the National Cancer Institute (part of the National Institutes of Health) and was maintained by the Center for Biomedical Informatics and Information Technology (CBIIT).";
+        String htmlString = "<html><body><h1>caBIG HTML over view</h1><span>" + orgDetail + "</span></body></html>";
+        mustBeLoggedInAs(nlm_username, nlm_username);
+        clickElement(By.id("username_link"));
+        clickElement(By.linkText("Org Management"));
+        clickElement(By.xpath("//*[@id='orgHtmlOverview-caBIG']//i"));
+        findElement(By.xpath("//*[@id='orgHtmlOverview-caBIG']//form/input")).sendKeys(htmlString);
+        clickElement(By.xpath("//*[@id='orgHtmlOverview-caBIG']//form//button[contains(text(),'Confirm')]"));
+        textPresent("Org has been updated.");
+        closeAlert();
+
+        mustBeLoggedOut();
+        goToCdeSearch();
+        clickElement(By.xpath("//*[@id='search_by_classification_caBIG']/i"));
+        textPresent(orgDetail);
+        goToFormSearch();
+        clickElement(By.xpath("//*[@id='search_by_classification_caBIG']/i"));
+        textPresent(orgDetail);
+    }
+}
+
+
+
