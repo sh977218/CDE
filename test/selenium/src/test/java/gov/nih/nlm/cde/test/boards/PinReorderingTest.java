9369151448be1f9c1ea3fcaa8f59940896f0682a
ludetc <ludetc@mail.nih.gov>
2016-09-29 15:55:11 -0400

    add test

--- test/selenium/src/test/java/gov/nih/nlm/cde/test/boards/PinReorderingTest.java
+++ test/selenium/src/test/java/gov/nih/nlm/cde/test/boards/PinReorderingTest.java
@@ -24,6 +24,28 @@ public class PinReorderingTest extends BoardTest {
         clickElement(By.id("moveDown-1"));
         findElement(By.id("linkToElt_2")).getText().equals("Ethnic Group Category Text");
         findElement(By.id("linkToElt_0")).getText().equals("Medication affecting cardiovascular function type exam day indicator");
+
+        findElement(By.id("linkToElt_19")).getText().equals("Walking ability status");
+        clickElement(By.id("moveDown-19"));
+        textPresent("Saved");
+        closeAlert();
+        findElement(By.id("linkToElt_19")).getText().equals("Urinary tract surgical procedure indicator");
+        clickElement(By.linkText("2"));
+        findElement(By.id("linkToElt_0")).getText().equals("Walking ability status");
+        clickElement(By.id("moveTop-0"));
+
+        textPresent("Saved");
+        closeAlert();
+        findElement(By.id("linkToElt_0")).getText().equals("Urinary tract surgical procedure indicator");
+        clickElement(By.id("moveUp-0"));
+        textPresent("Saved");
+        closeAlert();
+        findElement(By.id("linkToElt_0")).getText().equals("Brief Pain Inventory (BPI) - pain general activity interference scale");
+
+        clickElement(By.linkText("1"));
+        findElement(By.id("linkToElt_0")).getText().equals("Walking ability status");
+        findElement(By.id("linkToElt_19")).getText().equals("Urinary tract surgical procedure indicator");
+
     }
 }
 
