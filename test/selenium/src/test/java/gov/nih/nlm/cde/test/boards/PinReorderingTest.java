1946f618b6b2c567d0477e492455ab19fcef4a5b
ludetc <ludetc@mail.nih.gov>
2016-09-30 09:56:20 -0400

    close alerts in tests

--- test/selenium/src/test/java/gov/nih/nlm/cde/test/boards/PinReorderingTest.java
+++ test/selenium/src/test/java/gov/nih/nlm/cde/test/boards/PinReorderingTest.java
@@ -6,22 +6,27 @@ import org.testng.annotations.Test;
 public class PinReorderingTest extends BoardTest {
 
     @Test
-    public void pin() {
+    public void reorderPins() {
         mustBeLoggedInAs(classifyBoardUser_username,  password);
         goToBoard("Test Pinning Board");
         findElement(By.id("linkToElt_0")).getText().equals("Medication affecting cardiovascular function type exam day indicator");
         clickElement(By.id("moveDown-0"));
+        closeAlert();
         findElement(By.id("linkToElt_0")).getText().equals("Ethnicity USA maternal category");
         driver.navigate().refresh();
         findElement(By.id("linkToElt_0")).getText().equals("Ethnicity USA maternal category");
         clickElement(By.id("moveUp-1"));
+        closeAlert();
         findElement(By.id("linkToElt_0")).getText().equals("Medication affecting cardiovascular function type exam day indicator");
         driver.navigate().refresh();
         findElement(By.id("linkToElt_2")).getText().equals("Ethnic Group Category Text");
         clickElement(By.id("moveTop-2"));
+        closeAlert();
         findElement(By.id("linkToElt_0")).getText().equals("Ethnic Group Category Text");
         clickElement(By.id("moveDown-0"));
+        closeAlert();
         clickElement(By.id("moveDown-1"));
+        closeAlert();
         findElement(By.id("linkToElt_2")).getText().equals("Ethnic Group Category Text");
         findElement(By.id("linkToElt_0")).getText().equals("Medication affecting cardiovascular function type exam day indicator");
 
@@ -33,7 +38,6 @@ public class PinReorderingTest extends BoardTest {
         clickElement(By.linkText("2"));
         findElement(By.id("linkToElt_0")).getText().equals("Walking ability status");
         clickElement(By.id("moveTop-0"));
-
         textPresent("Saved");
         closeAlert();
         findElement(By.id("linkToElt_0")).getText().equals("Urinary tract surgical procedure indicator");
