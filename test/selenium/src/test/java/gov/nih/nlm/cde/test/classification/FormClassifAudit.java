b9fdd2b28314bd77e2dce7b9ed1df94bc297a55a
ludetc <ludetc@mail.nih.gov>
2016-10-17 14:52:51 -0400

    add test

---
+++ test/selenium/src/test/java/gov/nih/nlm/cde/test/classification/FormClassifAudit.java
@@ -0,0 +1,22 @@
+package gov.nih.nlm.cde.test.classification;
+
+import gov.nih.nlm.cde.test.BaseClassificationTest;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class FormClassifAudit extends BaseClassificationTest {
+    
+    @Test
+    public void formClassifAudit() {
+        mustBeLoggedInAs(testAdmin_username, password);
+        goToFormByName("Functional Imaging");
+        clickElement(By.id("classification_tab"));
+        addClassificationMethod(new String[] {"TEST", " Eligibility Criteria"});
+        openClassificationAudit("TEST > Eligibility Criteria");
+        clickElement(By.linkText("Functional Imaging"));
+        switchTab(1);
+        textPresent("Contains data elements collected when an imaging study");
+        switchTabAndClose(0);
+    }
+
+}
