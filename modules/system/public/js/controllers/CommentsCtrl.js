828739e0779a6b94d4358fbf389898f78ef4a701
Peter Huang <shi.huang@nih.gov>
2016-10-17 13:58:29 -0400

    fix text area auto size

--- modules/system/public/js/controllers/CommentsCtrl.js
+++ modules/system/public/js/controllers/CommentsCtrl.js
@@ -1,84 +1,86 @@
 angular.module('systemModule').controller('CommentsCtrl', ['$scope', '$http', 'userResource',
-    function($scope, $http, userResource)
-{
+    function ($scope, $http, userResource) {
 
-    $scope.comment = {};
+        $scope.comment = {};
 
-    $scope.avatarUrls = {};
-    function addAvatar(username) {
-        if (username && !$scope.avatarUrls[username]) {
-            $http.get('/user/avatar/' + username).then(function (res) {
-                $scope.avatarUrls[username] = res.data.length > 0 ? res.data : "/cde/public/assets/img/portrait.png";
-            });
-        }
-    }
-    $scope.deferredEltLoaded.promise.then(function () {
-        $scope.elt.comments.forEach(function (comment) {
-            addAvatar(comment.userName);
-            if (comment.replies) {
-                comment.replies.forEach(function (r) {addAvatar(r.username);})
+        $scope.avatarUrls = {};
+        function addAvatar(username) {
+            if (username && !$scope.avatarUrls[username]) {
+                $http.get('/user/avatar/' + username).then(function (res) {
+                    $scope.avatarUrls[username] = res.data.length > 0 ? res.data : "/cde/public/assets/img/portrait.png";
+                });
             }
+        }
+
+        $scope.deferredEltLoaded.promise.then(function () {
+            $scope.elt.comments.forEach(function (comment) {
+                addAvatar(comment.userName);
+                if (comment.replies) {
+                    comment.replies.forEach(function (r) {
+                        addAvatar(r.username);
+                    })
+                }
+            });
         });
-    });
-    userResource.getPromise().then(function() {
-        addAvatar(userResource.user.username);
-    });
-
-    $scope.canRemoveComment = function(com) {
-        return ((userResource.user._id) && 
-                (userResource.user._id === com.user ||
-                (userResource.user.orgAdmin.indexOf($scope.elt.stewardOrg.name) > -1) ||
-                userResource.user.siteAdmin ) );
-    };
-    $scope.canResolveComment = function (com) {
-        return com.status !== "resolved" && $scope.canRemoveComment(com);
-    };
-    $scope.canReopenComment = function (com) {
-        return com.status === "resolved" && $scope.canRemoveComment(com);
-    };
-
-    $scope.addComment = function() {  
-        $http.post("/comments/" + $scope.module + "/add", {
-            comment: $scope.comment.content
-            , element: {tinyId: $scope.elt.tinyId}
-        }).then(function(res) {
-              $scope.addAlert("success", res.data.message);  
-              $scope.elt = res.data.elt;
-              $scope.comment.content = "";
-        });
-    };
-    
-    $scope.removeComment = function(commentId) {
-        $http.post("/comments/" + $scope.module + "/remove", {
-            commentId: commentId
-            , element: {tinyId: $scope.elt.tinyId}
-        }).then(function (res) {
-            $scope.addAlert("success", res.data.message);
-            $scope.elt = res.data.elt;
-        });
-    };
-
-    $scope.updateCommentStatus = function(commentId, status) {
-        $http.post("/comments/" + $scope.module + "/status/" + status, {
-            commentId: commentId
-            , element: {tinyId: $scope.elt.tinyId}
-        }).then(function (res) {
-            $scope.addAlert("success", res.data.message);
-            $scope.elt = res.data.elt;
-        });
-    };
-
-
-    $scope.replyTo = function(commentId, reply) {
-        $http.post("/comments/" + $scope.module + "/reply", {
-            commentId: commentId,
-            reply: reply
-            , element: {tinyId: $scope.elt.tinyId}
-        }).then(function (res) {
-            $scope.addAlert("success", res.data.message);
-            $scope.elt = res.data.elt;
+        userResource.getPromise().then(function () {
+            addAvatar(userResource.user.username);
         });
 
-    };
-}
+        $scope.canRemoveComment = function (com) {
+            return ((userResource.user._id) &&
+            (userResource.user._id === com.user ||
+            (userResource.user.orgAdmin.indexOf($scope.elt.stewardOrg.name) > -1) ||
+            userResource.user.siteAdmin ) );
+        };
+        $scope.canResolveComment = function (com) {
+            return com.status !== "resolved" && $scope.canRemoveComment(com);
+        };
+        $scope.canReopenComment = function (com) {
+            return com.status === "resolved" && $scope.canRemoveComment(com);
+        };
+
+        $scope.addComment = function () {
+            $http.post("/comments/" + $scope.module + "/add", {
+                comment: $scope.comment.content
+                , element: {tinyId: $scope.elt.tinyId}
+            }).then(function (res) {
+                $scope.addAlert("success", res.data.message);
+                $scope.elt = res.data.elt;
+                $scope.comment.content = "";
+            });
+        };
+
+        $scope.removeComment = function (commentId) {
+            $http.post("/comments/" + $scope.module + "/remove", {
+                commentId: commentId
+                , element: {tinyId: $scope.elt.tinyId}
+            }).then(function (res) {
+                $scope.addAlert("success", res.data.message);
+                $scope.elt = res.data.elt;
+            });
+        };
+
+        $scope.updateCommentStatus = function (commentId, status) {
+            $http.post("/comments/" + $scope.module + "/status/" + status, {
+                commentId: commentId
+                , element: {tinyId: $scope.elt.tinyId}
+            }).then(function (res) {
+                $scope.addAlert("success", res.data.message);
+                $scope.elt = res.data.elt;
+            });
+        };
+
+
+        $scope.replyTo = function (commentId, reply) {
+            $http.post("/comments/" + $scope.module + "/reply", {
+                commentId: commentId,
+                reply: reply
+                , element: {tinyId: $scope.elt.tinyId}
+            }).then(function (res) {
+                $scope.addAlert("success", res.data.message);
+                $scope.elt = res.data.elt;
+            });
+
+        };
+    }
 ]);
