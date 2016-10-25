737abbc8ea1d71f3e1e181dcec639cbd7f296195
Peter Huang <shi.huang@nih.gov>
2016-10-25 11:42:17 -0400

    fix comment

--- test/selenium/src/test/java/gov/nih/nlm/form/test/CopyFormTest.java
+++ test/selenium/src/test/java/gov/nih/nlm/form/test/CopyFormTest.java
@@ -11,16 +11,24 @@ public class CopyFormTest extends BaseClassificationTest {
         mustBeLoggedInAs(ninds_username, password);
         String formName = "Type, Place, Cause and Mechanism of Injury";
         goToFormByName(formName);
+        textPresent("LOINC");
+        textPresent("CHAR");
+        showAllTabs();
+        clickElement(By.id("ids_tab"));
+        textPresent("1234567");
         clickElement(By.id("copyFormBtn"));
         textPresent("Create a copy");
         textPresent("Disease/Injury Related Events");
         clickElement(By.id("saveCopy"));
-        hangon(1);
-        showAllTabs();
         textPresent("Incomplete", By.id("dd_status"));
         textPresent("Copy of: " + formName, By.id("dd_general_name"));
+        textNotPresent("LOINC");
+        textNotPresent("CHAR");
+        showAllTabs();
         clickElement(By.id("status_tab"));
         textPresent("Copy of: XyqIIyrBtx");
+        clickElement(By.id("ids_tab"));
+        textNotPresent("1234567");
     }
 
 }
