4cd4f86f51dbd23bbb07886edc5f7dbb56f32b46
Georgi Ivanov <georgi.ivanov@nih.gov>
2016-11-08 11:39:46 -0500

    create test

---
+++ test/selenium/src/test/java/gov/nih/nlm/form/test/FormHistoryCompareTest.java
@@ -0,0 +1,38 @@
+package gov.nih.nlm.form.test;
+
+import junit.framework.Assert;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class FormHistoryTest extends BaseFormTest {
+
+    @Test
+    public void formHistoryTest() {
+        String formName = "Form History Compare Test";
+        mustBeLoggedInAs(testAdmin_username, password);
+        goToFormByName(formName);
+        showAllTabs();
+        clickElement(By.id("history_tab"));
+        textPresent("List of previous versions");
+        Assert.assertEquals(2, driver.findElements(By.xpath("//*[@id='historyTable']/tbody/tr")).size());
+
+        selectHistoryAndCompare(2,3);
+        findElement(By.xpath("//*[@id='historyCompareLeft_Form Description_1']//*[contains(@class,'unmatchedIcon')]"));
+        findElement(By.xpath("//*[@id='historyCompareLeft_Form Description_1']//*[contains(@class,'unmatchedIcon')]"));
+        findElement(By.xpath("//*[@id='historyCompareRight_Form Description_4']//*[contains(@class,'unmatchedIcon')]"));
+        findElement(By.xpath("//*[@id='historyCompareRight_Form Description_5']//*[contains(@class,'unmatchedIcon')]"));
+
+        selectHistoryAndCompare(1,2);
+        textPresent("Reordered",
+                By.xpath("//*[@id=historyCompareLeft_Form Description_4']//div[contains(@class,'compareMessage')]"));
+        textPresent("Reordered",
+                By.xpath("//*[@id=historyCompareRight_Form Description_2']//div[contains(@class,'compareMessage')]"));
+    }
+
+    protected void selectHistoryAndCompare(Integer leftIndex, Integer rightIndex) {
+        clickElement(By.xpath("//*[@id='historyTable']/tbody/tr[" + leftIndex + "]"));
+        clickElement(By.xpath("//*[@id='historyTable']/tbody/tr[" + rightIndex + "]"));
+        clickElement(By.id("historyCompareBtn"));
+        textPresent("Differences");
+    }
+}
