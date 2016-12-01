75db967a40745c468b520a8f8aae64bc664f557e
Georgi Ivanov <georgi.ivanov@nih.gov>
2016-12-01 14:45:33 -0500

    4 fixed columns with reset

--- modules/form/public/js/cde-native-value-list.js
+++ modules/form/public/js/cde-native-value-list.js
@@ -9,12 +9,6 @@
                     $scope.getLabel = function (pv) {
                         return pv ? (pv.valueMeaningName ? pv.valueMeaningName : pv.permissibleValue) : '';
                     };
-                    $scope.is1column = function (pv) {
-                        return $scope.getLabel(pv).length <= 17;
-                    };
-                    $scope.is2column = function (pv) {
-                        return $scope.getLabel(pv).length > 17 && $scope.getLabel(pv).length <= 38;
-                    }
                 }]
             };
         }])
