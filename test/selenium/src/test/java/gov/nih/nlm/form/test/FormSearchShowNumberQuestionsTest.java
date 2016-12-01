88cb6c9eb953b7e167f4d5ea833bb9f60bd5fe82
ludetc <ludetc@mail.nih.gov>
2016-12-01 09:41:21 -0500

    added test

--- test/selenium/src/test/java/gov/nih/nlm/form/test/FormSearchShowNumberQuestionsTest.java
+++ test/selenium/src/test/java/gov/nih/nlm/form/test/FormSearchShowNumberQuestionsTest.java
@@ -7,9 +7,13 @@ import org.testng.annotations.Test;
 
 public class FormSearchShowNumberQuestionsTest extends NlmCdeBaseTest {
 
+    @Test
     public void formSearchShowNumberQuestionsTest() {
         mustBeLoggedOut();
         searchElt("Classification of Seizures", "form");
-        Assert.assertEquals(findElement(By.id("dd_nQuestion-0")).getText(), "8");
+        Assert.assertEquals(findElement(By.id("searchResult_0")).getText(), "8 Questions");
+
+        searchElt("Form In Form Num Questions", "form");
+        Assert.assertEquals(findElement(By.id("searchResult_0")).getText(), "21 Questions");
     }
 }
