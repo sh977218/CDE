c13e4e0af8d6ad2345fce250b8755fb1bb359aeb
Peter Huang <shi.huang@nih.gov>
2016-12-05 15:17:14 -0500

    fix tests

--- test/selenium/src/test/java/gov/nih/nlm/cde/test/MeshTopics.java
+++ test/selenium/src/test/java/gov/nih/nlm/cde/test/MeshTopics.java
@@ -3,6 +3,7 @@ package gov.nih.nlm.cde.test;
 import gov.nih.nlm.system.NlmCdeBaseTest;
 import org.openqa.selenium.By;
 import org.openqa.selenium.TimeoutException;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class MeshTopics extends NlmCdeBaseTest {
@@ -30,19 +31,37 @@ public class MeshTopics extends NlmCdeBaseTest {
         clickElement(By.linkText("Server Statuses"));
         clickElement(By.id("syncWithMeshButton"));
         hangon(5);
+        Exception e = null;
         // try multiple times to give it time to re-index
         for (int i = 0; i < 2; i++) {
             try {
                 clickElement(By.id("menu_cdes_link"));
                 clickElement(By.linkText("Browse by Topic"));
                 clickElement(By.partialLinkText("Health Care Economics and Organizations"));
+                clickElement(By.id("li-blank-Organizations"));
                 clickElement(By.id("li-blank-Government"));
+                clickElement(By.id("li-blank-Federal Government"));
+                clickElement(By.id("li-blank-United States Government Agencies"));
+                clickElement(By.id("li-blank-United States Dept. of Health and Human Services"));
                 clickElement(By.id("li-blank-National Institute of Neurological Disorders and Stroke"));
                 i = 2;
-            } catch (TimeoutException e) {}
+                e = null;
+            } catch (TimeoutException ex) {
+                e = ex;
+            }
         }
+        Assert.assertNull(e, e.getMessage());
         textPresent("results for All Terms | All Classifications | Health Care > Health Care Economics and Organizat... | All St");
 
+        clickElement(By.id("menu_forms_link"));
+        clickElement(By.linkText("Browse by Topic"));
+        clickElement(By.partialLinkText("Health Care Economics and Organizations"));
+        clickElement(By.id("li-blank-Organizations"));
+        clickElement(By.id("li-blank-Government"));
+        clickElement(By.id("li-blank-Federal Government"));
+        clickElement(By.id("li-blank-United States Government Agencies"));
+        clickElement(By.id("li-blank-United States Dept. of Health and Human Services"));
+        clickElement(By.id("li-blank-National Institute of Neurological Disorders and Stroke"));
     }
 
 }
