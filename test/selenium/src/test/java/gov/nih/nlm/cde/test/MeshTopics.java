8bb04cdd8ad8eb9ec92f3cd6227b54d0eca222c7
Peter Huang <shi.huang@nih.gov>
2016-12-06 16:31:50 -0500

    fix test

--- test/selenium/src/test/java/gov/nih/nlm/cde/test/MeshTopics.java
+++ test/selenium/src/test/java/gov/nih/nlm/cde/test/MeshTopics.java
@@ -2,8 +2,6 @@ package gov.nih.nlm.cde.test;
 
 import gov.nih.nlm.system.NlmCdeBaseTest;
 import org.openqa.selenium.By;
-import org.openqa.selenium.TimeoutException;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class MeshTopics extends NlmCdeBaseTest {
@@ -30,30 +28,43 @@ public class MeshTopics extends NlmCdeBaseTest {
         clickElement(By.linkText("Site Management"));
         clickElement(By.linkText("Server Statuses"));
         clickElement(By.id("syncWithMeshButton"));
-        hangon(5);
-        Exception e = null;
-        // try multiple times to give it time to re-index
-        for (int i = 0; i < 2; i++) {
+        hangon(20);
+        try {
+            hangon(30);
+            clickElement(By.id("menu_cdes_link"));
+            clickElement(By.linkText("Browse by Topic"));
+            textPresent("Health Care Economics and Organizations (11148)");
+        } catch (Exception e) {
+            System.out.println("Fail after one 30 seconds.");
             try {
+                hangon(20);
                 clickElement(By.id("menu_cdes_link"));
                 clickElement(By.linkText("Browse by Topic"));
-                clickElement(By.partialLinkText("Health Care Economics and Organizations"));
-                clickElement(By.id("li-blank-Organizations"));
-                clickElement(By.id("li-blank-Government"));
-                clickElement(By.id("li-blank-Federal Government"));
-                clickElement(By.id("li-blank-United States Government Agencies"));
-                clickElement(By.id("li-blank-United States Dept. of Health and Human Services"));
-                clickElement(By.id("li-blank-National Institutes of Health (U.S.)"));
-                scrollToViewById("status_filter");
-                i = 2;
-                e = null;
-            } catch (TimeoutException ex) {
-                e = ex;
+                textPresent("Health Care Economics and Organizations (11148)");
+            } catch (Exception ex) {
+                System.out.println("Fail after one 20 seconds.");
+                try {
+                    hangon(10);
+                    clickElement(By.id("menu_cdes_link"));
+                    clickElement(By.linkText("Browse by Topic"));
+                    textPresent("Health Care Economics and Organizations (11148)");
+                } catch (Exception exc) {
+                    System.out.println("Fail after one 10 seconds.");
+                }
             }
         }
-        Assert.assertNull(e, e.getMessage());
+        clickElement(By.partialLinkText("Health Care Economics and Organizations"));
+        clickElement(By.id("li-blank-Organizations"));
+        clickElement(By.id("li-blank-Government"));
+        clickElement(By.id("li-blank-Federal Government"));
+        clickElement(By.id("li-blank-United States Government Agencies"));
+        clickElement(By.id("li-blank-United States Dept. of Health and Human Services"));
+        clickElement(By.id("li-blank-National Institutes of Health (U.S.)"));
+        driver.navigate().refresh();
+        scrollToViewById("li-checked-National Institutes of Health (U.S.)");
+        hangon(5);
+        textPresent("National Institute of Neurological Disorders and Stroke (11129)");
         textPresent("results for All Terms | All Classifications | Health Care > Health Care Economics and Organizat... | All St");
-
         clickElement(By.id("menu_forms_link"));
         clickElement(By.linkText("Browse by Topic"));
         clickElement(By.partialLinkText("Health Care Economics and Organizations"));
