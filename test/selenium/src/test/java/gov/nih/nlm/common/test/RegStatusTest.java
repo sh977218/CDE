ba5e672d3ad33dbc9a7359ec46e69eb0cb157922
ludetc <ludetc@mail.nih.gov>
2016-12-08 14:19:23 -0500

    fix mistake

--- test/selenium/src/test/java/gov/nih/nlm/common/test/RegStatusTest.java
+++ test/selenium/src/test/java/gov/nih/nlm/common/test/RegStatusTest.java
@@ -50,7 +50,7 @@ public abstract class RegStatusTest extends CommonTest {
         findElement(By.name("administrativeNote")).sendKeys("Admin Note 1");
         findElement(By.name("unresolvedIssue")).sendKeys("Unresolved Issue 1");
         clickElement(By.id("saveRegStatus"));
-        textPresent("Saved.");
+        textPresent("Saved");
         closeAlert();
         setLowStatusesVisible();
         waitForESUpdate();
