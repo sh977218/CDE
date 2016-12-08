3aa0bd857b7bc63c85fe9d1a8d5937c6ffea11ad
ludetc <ludetc@mail.nih.gov>
2016-12-08 10:07:25 -0500

    add test

---
+++ test/selenium/src/test/java/gov/nih/nlm/form/test/logic/EmptyLogic.java
@@ -0,0 +1,44 @@
+package gov.nih.nlm.form.test.logic;
+
+import gov.nih.nlm.system.NlmCdeBaseTest;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class EmptyLogic extends NlmCdeBaseTest {
+
+    @Test
+    public void emptyLogic() {
+        goToFormByName("Empty Logic");
+        clickElement(By.id("nativeFormRenderLink"));
+
+        // Dates
+        textPresent("Data unknown indicator");
+        findElement(By.xpath("//div[@id='Birth date_0']//input")).sendKeys("1");
+        textPresent("Data unknown indicator");
+        findElement(By.xpath("//div[@id='Birth date_0']//input")).sendKeys("995");
+        textNotPresent("Data unknown indicator");
+        findElement(By.xpath("//div[@id='Birth date_0']//input")).clear();
+        textPresent("Data unknown indicator");
+
+        // Value Lists
+        textPresent("Pulmonary function test not done reason");
+        findElement(By.xpath("//div[@id='Image Acquisition Event Yes No Not Done Indicator_2']//input[@value='No']")).click();
+        textNotPresent("Pulmonary function test not done reason");
+
+        // Numbers
+        textPresent("Pulmonary function test not done other text");
+        findElement(By.xpath("//div[@id='Head injury prior number_0']//input")).sendKeys("0");
+        textNotPresent("Pulmonary function test not done other text");
+        findElement(By.xpath("//div[@id='Head injury prior number_0']//input")).clear();
+        textPresent("Pulmonary function test not done other text");
+
+        // Text
+        textPresent("Perianal problem other text");
+        findElement(By.xpath("//div[@id='Noncompliant Reason Text_0']//input")).sendKeys("abc");
+        textNotPresent("Perianal problem other text");
+        findElement(By.xpath("//div[@id='Noncompliant Reason Text_0']//input")).clear();
+        textPresent("Perianal problem other text");
+
+    }
+
+}
