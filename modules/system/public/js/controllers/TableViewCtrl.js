393a940a5f07f3422dcf0d418373304e10ec86b0
ludetc <ludetc@mail.nih.gov>
2016-12-21 13:30:50 -0500

    added question text column

--- modules/system/public/js/controllers/TableViewCtrl.js
+++ modules/system/public/js/controllers/TableViewCtrl.js
@@ -2,4 +2,12 @@ angular.module('systemModule').controller('TableViewCtrl', ['$scope', 'SearchSet
     SearchSettings.getPromise().then(function(settings) {
         $scope.searchViewSettings = settings;
     });
+
+    $scope.getQuestionTexts = function (cde) {
+        return cde.naming.filter(function (n) {
+            return n.context && n.context.contextName &&
+                n.context.contextName.indexOf("Question Text", n.context.contextName.length - "Question Text".length) !== -1;
+        });
+    };
+
 }]);
