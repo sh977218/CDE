19af5bcc4e9e34ddd6ee74d819d34746fec4755f
Peter Huang <shi.huang@nih.gov>
2016-12-23 15:35:43 -0500

    fix skipLogicDateTest

---
+++ test/selenium/src/test/java/gov/nih/nlm/form/test/logic/SkipLogicDateTest.java
@@ -0,0 +1,67 @@
+package gov.nih.nlm.form.test.logic;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class SkipLogicDateTest extends SkipLogicTest {
+
+    @Test
+    public void skipLogicDateTest() {
+        mustBeLoggedInAs(ninds_username, password);
+        String formName = "Imaging OCT Analysis - Spectralis Report Analysis";
+        goToFormByName(formName);
+        clickElement(By.id("description_tab"));
+        textPresent("Macula volume (OD)");
+
+        startEditQuestionSectionById("question_0_2");
+        String inputXpath1 = locateSkipLogicEditTextareaXpathByQuestionId("question_0_2");
+
+        editSkipLogic(inputXpath1, "\"Indicate date of reference scan\"", 2, 2, true, "Unexpected number of tokens in expression 1");
+        editSkipLogic(inputXpath1, "=", 3, 1, true, "Unexpected number of tokens in expression 2");
+        editSkipLogic(inputXpath1, "\"{{MM/DD/YYYY}}\"", 1, 1, true, "\"{{MM/DD/YYYY}}\" is not a valid date for \"Indicate date of reference scan\".");
+
+        String correctSkipLogicString = "\"Indicate date of reference scan\"=\"10/11/2016\"";
+
+        findElement(By.xpath(inputXpath1)).clear();
+        hangon(1);
+        findElement(By.xpath(inputXpath1)).sendKeys(correctSkipLogicString);
+        textNotPresent("Unexpected number of tokens");
+        textNotPresent("is not a valid date for");
+        saveEditQuestionSectionById("question_0_2");
+
+        startEditQuestionSectionById("question_0_3");
+        findElement(By.xpath(locateSkipLogicEditTextareaXpathByQuestionId("question_0_3"))).sendKeys("\"Indicate date of reference scan\">\"10/11/2016\"");
+        saveEditQuestionSectionById("question_0_3");
+
+        startEditQuestionSectionById("question_0_4");
+        findElement(By.xpath(locateSkipLogicEditTextareaXpathByQuestionId("question_0_4"))).sendKeys("\"Indicate date of reference scan\"<\"10/11/2016\"");
+        saveEditQuestionSectionById("question_0_4");
+
+        saveForm();
+
+        goToFormByName(formName);
+        textPresent("Display Profile");
+        textNotPresent("Macula volume (OD)");
+        textNotPresent("Macula volume (OS)");
+        textNotPresent("Laterality Type");
+        findElement(By.xpath("//*[@id='Indicate date of reference scan_1']//input")).sendKeys("10/11/2016");
+        textPresent("Macula volume (OD)");
+
+        findElement(By.xpath("//*[@id='Indicate date of reference scan_1']//input")).clear();
+        findElement(By.xpath("//*[@id='Indicate date of reference scan_1']//input")).sendKeys("10/12/2016");
+        textPresent("Macula volume (OS)");
+
+        findElement(By.xpath("//*[@id='Indicate date of reference scan_1']//input")).clear();
+        findElement(By.xpath("//*[@id='Indicate date of reference scan_1']//input")).sendKeys("10/10/2016");
+        textPresent("Laterality Type");
+
+        findElement(By.xpath("//*[@id='Indicate date of reference scan_1']//input")).clear();
+        findElement(By.xpath("//*[@id='Indicate date of reference scan_1']//input")).sendKeys("2017");
+        textPresent("Macula volume (OS)");
+
+        findElement(By.xpath("//*[@id='Indicate date of reference scan_1']//input")).clear();
+        findElement(By.xpath("//*[@id='Indicate date of reference scan_1']//input")).sendKeys("2015");
+        textPresent("Laterality Type");
+    }
+
+}
--- test/selenium/src/test/java/gov/nih/nlm/form/test/logic/SkipLogicTest.java
+++ test/selenium/src/test/java/gov/nih/nlm/form/test/logic/SkipLogicTest.java
@@ -62,8 +62,8 @@ public class SkipLogicTest extends BaseFormTest {
     }
 
 
-    private void editSkipLogic(String inputXpath, String textToBePresent, int expectedNumSuggested, int clickNth,
-                               boolean displayError, String errorMessage) {
+    public void editSkipLogic(String inputXpath, String textToBePresent, int expectedNumSuggested, int clickNth,
+                              boolean displayError, String errorMessage) {
         findElement(By.xpath(inputXpath)).sendKeys(Keys.SPACE);
         textPresent(textToBePresent, By.xpath("(//*[contains(@id,'typeahead-')]/a)[" + clickNth + "]"));
         int actualNumSuggested = findElements(By.xpath("(//*[contains(@id,'typeahead-')]/a)")).size();
@@ -71,67 +71,6 @@ public class SkipLogicTest extends BaseFormTest {
         clickElement(By.xpath("(//*[contains(@id,'typeahead-')]/a)[" + clickNth + "]"));
         if (displayError) textPresent(errorMessage);
         else textNotPresent(errorMessage);
-    }
-
-    @Test
-    public void skipLogicDateTest() {
-        mustBeLoggedInAs(ninds_username, password);
-        String formName = "Imaging OCT Analysis - Spectralis Report Analysis";
-        goToFormByName(formName);
-        clickElement(By.id("description_tab"));
-        textPresent("Macula volume (OD)");
-
-        startEditQuestionSectionById("question_0_2");
-        String inputXpath1 = locateSkipLogicEditTextareaXpathByQuestionId("question_0_2");
-
-        editSkipLogic(inputXpath1, "\"Indicate date of reference scan\"", 2, 2, true, "Unexpected number of tokens in expression 1");
-        editSkipLogic(inputXpath1, "=", 3, 1, true, "Unexpected number of tokens in expression 2");
-        editSkipLogic(inputXpath1, "\"{{MM/DD/YYYY}}\"", 1, 1, true, "\"{{MM/DD/YYYY}}\" is not a valid date for \"Indicate date of reference scan\".");
-
-        String correctSkipLogicString = "\"Indicate date of reference scan\"=\"10/11/2016\"";
-
-        findElement(By.xpath(inputXpath1)).clear();
-        hangon(1);
-        findElement(By.xpath(inputXpath1)).sendKeys(correctSkipLogicString);
-        clickElement(By.id("dd_datatype_2"));
-        textNotPresent("Unexpected number of tokens");
-        textNotPresent("is not a valid date for");
-
-        clickElement(By.id("question_accordion_0_3"));
-        textPresent("Optical coherence tomography (OCT) oculus sinister (OS) macula volume measurement");
-        findElement(By.xpath("//*[@id='dd_q_skipLogic_3']/div/input[2]")).sendKeys("\"Indicate date of reference scan\">\"10/11/2016\"");
-
-        clickElement(By.id("question_accordion_0_4"));
-        textPresent("Optical coherence tomography retinal nerve fiber layer thickness laterality type");
-        findElement(By.xpath("//*[@id='dd_q_skipLogic_4']/div/input[2]")).sendKeys("\"Indicate date of reference scan\"<\"10/11/2016\"");
-
-        saveEditQuestionSectionById("question_0_2");
-        saveForm();
-
-        goToFormByName(formName);
-        textPresent("Display Profile");
-        textNotPresent("Macula volume (OD)");
-        textNotPresent("Macula volume (OS)");
-        textNotPresent("Laterality Type");
-        findElement(By.xpath("//*[@id='Indicate date of reference scan_1']//input")).sendKeys("10/11/2016");
-        textPresent("Macula volume (OD)");
-
-        findElement(By.xpath("//*[@id='Indicate date of reference scan_1']//input")).clear();
-        findElement(By.xpath("//*[@id='Indicate date of reference scan_1']//input")).sendKeys("10/12/2016");
-        textPresent("Macula volume (OS)");
-
-        findElement(By.xpath("//*[@id='Indicate date of reference scan_1']//input")).clear();
-        findElement(By.xpath("//*[@id='Indicate date of reference scan_1']//input")).sendKeys("10/10/2016");
-        textPresent("Laterality Type");
-
-        findElement(By.xpath("//*[@id='Indicate date of reference scan_1']//input")).clear();
-        findElement(By.xpath("//*[@id='Indicate date of reference scan_1']//input")).sendKeys("2017");
-        textPresent("Macula volume (OS)");
-
-        findElement(By.xpath("//*[@id='Indicate date of reference scan_1']//input")).clear();
-        findElement(By.xpath("//*[@id='Indicate date of reference scan_1']//input")).sendKeys("2015");
-        textPresent("Laterality Type");
-
     }
 
     @Test
