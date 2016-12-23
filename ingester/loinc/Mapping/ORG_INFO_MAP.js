028790271da9b022fc7c40a5050772d96bd93dfa
Peter Huang <shi.huang@nih.gov>
2016-12-23 11:45:17 -0500

    get PHQ9 from web

--- ingester/loinc/Mapping/ORG_INFO_MAP.js
+++ ingester/loinc/Mapping/ORG_INFO_MAP.js
@@ -2,5 +2,6 @@ exports.map = {
     'AHRQ': {stewardOrgName: 'NLM', classificationOrgName: 'AHRQ', classification: []},
     'eyeGENE': {stewardOrgName: 'NLM', classificationOrgName: 'eyeGENE', classification: []},
     'Newborn Screening': {stewardOrgName: 'NLM', classificationOrgName: 'NLM', classification: ['Newborn Screening']},
-    'ONC': {stewardOrgName: 'NLM', classificationOrgName: 'ONC', classification: []}
+    'ONC': {stewardOrgName: 'NLM', classificationOrgName: 'ONC', classification: []},
+    'PHQ9': {stewardOrgName: 'NLM', classificationOrgName: 'NLM', classification: []}
 };
