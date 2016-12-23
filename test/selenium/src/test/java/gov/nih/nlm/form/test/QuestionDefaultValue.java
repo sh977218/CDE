c59333f7d5cf267b224673c5070fde0067611e0c
Peter Huang <shi.huang@nih.gov>
2016-12-23 11:03:01 -0500

    fix questionDefaultValue

--- test/selenium/src/test/java/gov/nih/nlm/form/test/QuestionDefaultValue.java
+++ test/selenium/src/test/java/gov/nih/nlm/form/test/QuestionDefaultValue.java
@@ -11,21 +11,21 @@ public class QuestionDefaultValue extends BaseFormTest {
         mustBeLoggedInAs(ninds_username, password);
         goToFormByName("History Data Source and Reliability");
         clickElement(By.id("description_tab"));
-        new Select(driver.findElement(By.id("q_default_answer_0"))).selectByVisibleText("Brother");
+        startEditQuestionSectionById("question_0_0");
+        new Select(driver.findElement(By.xpath("//*[@id='question_0_0']//*[contains(@class,'defaultAnswer')]"))).selectByVisibleText("Brother");
+        saveEditQuestionSectionById("question_0_0");
 
-        clickElement(By.id("question_accordion_0_1"));
-        clickElement(By.cssSelector("#q_defaultAnswer_text_1 i"));
+        startEditQuestionSectionById("question_0_1");
+        findElement(By.xpath("//*[@id='question_0_1']//*[contains(@class,'defaultAnswer')]//input")).sendKeys("A default answer!");
+        clickElement(By.xpath("//*[@id='question_0_1']//*[contains(@class,'defaultAnswer')]//button[contains(text(),'Confirm')]"));
+        saveEditQuestionSectionById("question_0_1");
 
-        findElement(By.cssSelector("#q_defaultAnswer_text_1 input")).sendKeys("A default answer!");
-        clickElement(By.cssSelector("#q_defaultAnswer_text_1 .fa-check"));
         saveForm();
 
         goToFormByName("History Data Source and Reliability");
+        textPresent("Default Answer: Brother", By.xpath("//*[@id='question_0_0']"));
+        textPresent("Default Answer: A default answer!", By.xpath("//*[@id='question_0_1']"));
 
-        // this find ensures option Brother is selected.
-        findElement(By.xpath("//div[div/div/label[text()='From whom/ what were the medical history data obtained']]//label[contains(.,'Brother')]"))
-                .findElement(By.cssSelector("input:checked"));
-        
     }
 
 }
