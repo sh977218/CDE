1eb4a9444c9f22d59ab09cdebeedfa8be0f10eb9
ludetc <ludetc@mail.nih.gov>
2016-12-27 16:44:04 -0500

    remove unused

--- modules/board/node-js/app.js
+++ modules/board/node-js/app.js
@@ -21,12 +21,6 @@ var elastic = require('../../cde/node-js/elastic')
 exports.init = function (app, daoManager) {
     daoManager.registerDao(mongo_board);
 
-    app.get('/boards/:userId', exportShared.nocacheMiddleware, function (req, res) {
-        mongo_board.boardsByUserId(req.params.userId, function (result) {
-            res.send(result);
-        });
-    });
-
     app.get('/deBoards/:tinyId', exportShared.nocacheMiddleware, function (req, res) {
         mongo_board.publicBoardsByDeTinyId(req.params.tinyId, function (result) {
             res.send(result);
