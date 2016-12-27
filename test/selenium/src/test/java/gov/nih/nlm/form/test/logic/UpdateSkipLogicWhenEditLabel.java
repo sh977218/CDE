97c1058bec752177d8d4442d716dde8d52d80f29
Peter Huang <shi.huang@nih.gov>
2016-12-27 11:52:26 -0500

    fix updateSkipLogicWhenEditLabelTest

--- test/selenium/src/test/java/gov/nih/nlm/form/test/logic/UpdateSkipLogicWhenEditLabel.java
+++ test/selenium/src/test/java/gov/nih/nlm/form/test/logic/UpdateSkipLogicWhenEditLabel.java
@@ -34,7 +34,7 @@ public class UpdateSkipLogicWhenEditLabel extends BaseFormTest {
         clickElement(By.xpath("//*[@id='Off study date and time_0']//button"));
         findElement(By.xpath("//*[@id='Off study date and time_0']//input")).sendKeys("10/15/2016");
         clickElement(By.xpath("//button[text()='Done']"));
-        clickElement(By.xpath("//*[@id='Off study intervention prematurely indicator_2']//label[text()[contains(., 'No')]]"));
+        clickElement(By.xpath("//*[@id='Did participant subject discontinue intervention before planned end of study?_2']//label[text()[contains(., 'No')]]"));
         textPresent("Reason for premature intervention discontinuation");
     }
 }
