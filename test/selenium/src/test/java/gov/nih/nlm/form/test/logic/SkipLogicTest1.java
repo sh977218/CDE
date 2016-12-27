524d775e3ee83f72da0c75220578c667b52760bd
Peter Huang <shi.huang@nih.gov>
2016-12-27 11:55:13 -0500

    move singlePermissibleValue to new Class

---
+++ test/selenium/src/test/java/gov/nih/nlm/form/test/logic/SkipLogicTest1.java
@@ -0,0 +1,23 @@
+package gov.nih.nlm.form.test.logic;
+
+import gov.nih.nlm.form.test.BaseFormTest;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class SkipLogicTest1 extends BaseFormTest {
+
+    @Test
+    public void singlePermissibleValue() {
+        mustBeLoggedInAs(testAdmin_username, password);
+        String formName = "Cancer Screening Test";
+        goToFormByName(formName);
+        textNotPresent("Female Patient Screening");
+        textNotPresent("Breast Carcinoma Estrogen Receptor Status");
+        findElement(By.xpath("//div[div/label[text()='Frontal Systems Behavior Scale (FrSBE) - Disinhibition " +
+                "subscale T score']]/following-sibling::div//input")).sendKeys("200");
+        textPresent("Patient Gender Category");
+        clickElement(By.xpath("//div[div/label[text()='Patient Gender Category']]/following-sibling::div//*[text()[contains(., 'Female Gender')]]"));
+        textPresent("Female Patient Screening");
+    }
+
+}
