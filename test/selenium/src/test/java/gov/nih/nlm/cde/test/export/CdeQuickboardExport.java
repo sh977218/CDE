5b8a4c2855e01facc8faed60f1e11b03f892333d
ludetc <ludetc@mail.nih.gov>
2017-01-03 15:23:07 -0500

    fix test

--- test/selenium/src/test/java/gov/nih/nlm/cde/test/export/CdeQuickboardExport.java
+++ test/selenium/src/test/java/gov/nih/nlm/cde/test/export/CdeQuickboardExport.java
@@ -33,7 +33,7 @@ public class CdeQuickboardExport extends NlmCdeBaseTest {
 
         String[] expected = {
                 "Name, Question Texts, Value Type, Permissible Values, Nb of Permissible Values, Steward, Used By, Registration Status, Identifiers",
-                "\"Intravesical Protocol Agent Administered Specify\",\"No explain\",\"CHARACTER\",\"\",\"0\",\"CTEP\",\"CTEP\",\"Qualified\",\"caDSR: 2399243 v1; MyOrigin1: MyId1 vMyVersion1; MyOrigin3: MyId3 vMyVersion3\"",
+                "\"Intravesical Protocol Agent Administered Specify\",\"No explain\",\"CHARACTER\",\"\",\"0\",\"CTEP\",\"CTEP\",\"Qualified\",\"caDSR: 2399243 v1",
                 "\"Scale for the Assessment of Positive Symptoms (SAPS) - voice conversing  scale\",\"Like voices commenting, voices conversing are considered a Schneiderian first-rank symptom. They involve hearing two or more voices talking with one another, usually discussing something about the subject. As in the case of voices commenting, they should be scored independently of other auditory hallucinations.  Have you heard two or more voices talking with each other?  What did they say?\",\"Value List\",\"0; 1; 2; 3; 4; 5\",\"6\",\"NINDS\",\"NINDS\",\"Qualified\",\"NINDS: C09512 v3; NINDS Variable Name: SAPSVocConvrsngScale\"",
                 "\"User Login Name java.lang.String\",\"\",\"java.lang.String\",\"\",\"0\",\"caCORE\",\"caBIG; caCORE\",\"Qualified\",\"caDSR: 2223533 v3\""
         };
