9b858fe8b4817df06ed5b05b2c840259a2943da3
Peter Huang <shi.huang@nih.gov>
2017-01-05 11:13:46 -0500

    fix tests

--- test/selenium/src/test/java/gov/nih/nlm/cde/test/MergeTest.java
+++ test/selenium/src/test/java/gov/nih/nlm/cde/test/MergeTest.java
@@ -6,93 +6,106 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.annotations.Test;
 
 public class MergeTest extends NlmCdeBaseTest {
-    
+
     private void checkEverything() {
-        findElement(By.xpath("//input[@ng-model='mergeRequest.mergeFields.ids']")).click();  
-        findElement(By.xpath("//input[@ng-model='mergeRequest.mergeFields.attachments']")).click();  
-        findElement(By.xpath("//input[@ng-model='mergeRequest.mergeFields.properties']")).click();  
-        findElement(By.xpath("//input[@ng-model='mergeRequest.mergeFields.naming']")).click();     
+        clickElement(By.id("mergeFieldIdentifiers"));
+        clickElement(By.id("mergeFieldsNaming"));
+        clickElement(By.id("mergeFieldsProperties"));
+        clickElement(By.id("mergeFieldsAttachments"));
+        clickElement(By.id("mergeFieldsSources"));
+        clickElement(By.id("mergeFieldsReferenceDocuments"));
+        clickElement(By.id("mergeFieldsDataSets"));
+        clickElement(By.id("mergeFieldsDerivationRules"));
     }
-    
-    private void createMergeRequest() { 
+
+    private void createMergeRequest() {
         mustBeLoggedInAs(cabigAdmin_username, password);
         addToCompare("Smoking Cessation Other Method Specify Text", "Smoking History Ind");
-        findElement(By.linkText("Retire & Merge")).click();  
+        clickElement(By.linkText("Retire & Merge"));
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText("Smoking Cessation Other Method Specify Text")));
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText("Smoking History Ind")));
         checkEverything();
-        findElement(By.cssSelector("[ng-click='sendMergeRequest()']")).click(); 
+        clickElement(By.cssSelector("[ng-click='sendMergeRequest()']"));
         hangon(1);
     }
-    
-    
+
+
     private void acceptMergeRequest() {
         mustBeLoggedInAs(ctepCurator_username, password);
         gotoInbox();
-        findElement(By.cssSelector(".accordion-toggle")).click();  
+        clickElement(By.cssSelector(".accordion-toggle"));
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText("Smoking Cessation Other Method Specify Text")));
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText("Smoking History Ind")));
         textPresent("Free-text field to capture another method used to help stop smoking that is not already specified or mentioned.");
         textPresent("3279225");
-        findElement(By.cssSelector("[ng-click='showMergeApproveDialog(message)']")).click();
+        clickElement(By.cssSelector("[ng-click='showMergeApproveDialog(message)']"));
         findElement(By.cssSelector("[ng-model='elt.version']")).sendKeys(".2");
         textNotPresent("number has already been used");
-        findElement(By.cssSelector("#confirmNewVersion")).click();         
+        clickElement(By.cssSelector("#confirmNewVersion"));
         waitForESUpdate();
     }
-    
-    private void checkResult() {     
-        findElement(By.cssSelector(".accordion-toggle")).click(); 
+
+    private void checkResult() {
+        clickElement(By.cssSelector(".accordion-toggle"));
         if (!browser.equals("ie")) {
-            findElement(By.linkText("Smoking History Ind")).click(); 
+            clickElement(By.linkText("Smoking History Ind"));
             switchTabAndClose(1);
         } else goToCdeByName("Smoking History Ind");
-        findElement(By.linkText("Classification")).click();
+        clickElement(By.linkText("Classification"));
         textPresent("Health Survey");
         textPresent("Cancer Related Risks");
-        findElement(By.linkText("Naming")).click();
+        clickElement(By.linkText("Naming"));
         textPresent("Free-text field to capture another method used to help stop smoking that is not already specified or mentioned.");
         showAllTabs();
-        findElement(By.linkText("Identifiers")).click();
+        clickElement(By.linkText("Identifiers"));
         textPresent("3279225");
     }
-    
+
     @Test
     public void mergeMineMineEverything() {
         mustBeLoggedInAs(ctepCurator_username, password);
         addToCompare("Common Toxicity Criteria Adverse Event Colitis Grade", "Common Toxicity Criteria Adverse Event Hypophosphatemia Grade");
-        findElement(By.id("retireMerge-0")).click();
+        clickElement(By.id("retireMerge-0"));
         textPresent("Common Toxicity Criteria Adverse Event Colitis Grade");
         checkEverything();
-        findElement(By.id("sendMergeRequest")).click();
+        clickElement(By.id("sendMergeRequest"));
         hangon(2);
         findElement(By.cssSelector("[ng-model='elt.version']")).sendKeys(".2");
         clickElement(By.id("confirmNewVersion"));
         hangon(5);
-        findElement(By.linkText("Naming")).click();
+        clickElement(By.id("naming_tab"));
         textPresent("Common Toxicity Criteria Adverse Event Colitis Grade");
-        findElement(By.linkText("Classification")).click();
+        clickElement(By.id("classification_tab"));
         textPresent("Common Terminology Criteria for Adverse Events v3.0");
         showAllTabs();
-        findElement(By.linkText("Identifiers")).click();
+        clickElement(By.id("ids_tab"));
         textPresent("2005490");
+        clickElement(By.id("properties_tab"));
+        textPresent("Colitis Value");
+        clickElement(By.id("referenceDocument_tab"));
+        textPresent("Colitis ref doc");
+        clickElement(By.id("dataSet_tab"));
+        textPresent("Colitis note");
+        clickElement(By.id("derivationRules_tab"));
+        textPresent("Biomarker Gene Staining Method Score");
+
     }
-    
+
     @Test
     public void mergeMineTheirsClassificationsOnly() {
         mustBeLoggedInAs(cabigAdmin_username, password);
         addToCompare("Diagnosis Change Date java.util.Date", "Form Element End Date java.util.Date");
         hangon(1);
-        findElement(By.linkText("Retire & Merge")).click();
+        clickElement(By.linkText("Retire & Merge"));
         textPresent("Fields to be Imported");
-        findElement(By.cssSelector("[ng-click='sendMergeRequest()']")).click();
+        clickElement(By.cssSelector("[ng-click='sendMergeRequest()']"));
         hangon(1);
-        findElement(By.linkText("Classification")).click();
+        clickElement(By.linkText("Classification"));
         textPresent("caBIG");
         textPresent("caLIMS2");
         textPresent("gov.nih.nci.calims2.domain.inventory");
     }
-  
+
     @Test
     public void mergeMineTheirsEverything() {
         createMergeRequest();
