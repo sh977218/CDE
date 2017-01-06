9915d2194e572500262de5421a74f12ee296c64b
Peter Huang <shi.huang@nih.gov>
2017-01-06 11:47:25 -0500

    fix remove NCI classification script, cannot use stream when modify content of stream

--- ingester/nci/RemoveNciClassification.js
+++ ingester/nci/RemoveNciClassification.js
@@ -1,25 +1,25 @@
+var async = require('async');
 var mongo_cde = require('../../modules/cde/node-js/mongo-cde');
 var DataElement = mongo_cde.DataElement;
 
 var cdeCount = 0;
-var stream = DataElement.find({'classification.stewardOrg.name': 'NCI', archived: null}).stream();
-stream.on('data', function (cde) {
-    stream.pause();
-    cde.classification = cde.get('classification').filter(function (c) {
-        return c.stewardOrg.name !== 'NCI';
-    });
-    cde.markModified('classification');
-    cde.save(function (err) {
-        if (err) throw err;
-        cdeCount++;
-        console.log('cdeCount: ' + cdeCount);
-        stream.resume();
-    })
-});
-stream.on('error', function (error) {
-    if (error) throw error;
-    process.exit(1);
-});
-stream.on('end', function () {
-    console.log('Finished all.');
+DataElement.find({'classification.stewardOrg.name': 'NCI', archived: null}).exec(function (err, cdes) {
+    if (err) {
+        throw err;
+    } else {
+        async.forEach(cdes, function (cde, doneOneCde) {
+            cde.classification = cde.get('classification').filter(function (c) {
+                return c.stewardOrg.name !== 'NCI';
+            });
+            cde.markModified('classification');
+            cde.save(function (err) {
+                if (err) throw err;
+                cdeCount++;
+                console.log('cdeCount: ' + cdeCount);
+                doneOneCde();
+            })
+        }, function doneAllCdes() {
+            console.log('Finished all.');
+        })
+    }
 });
