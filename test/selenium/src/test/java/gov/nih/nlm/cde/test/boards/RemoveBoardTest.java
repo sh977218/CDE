1b8301cb84460f37152639c9d000389b24062dfd
Peter Huang <shi.huang@nih.gov>
2017-01-10 16:39:34 -0500

    split borad managementTest

---
+++ test/selenium/src/test/java/gov/nih/nlm/cde/test/boards/RemoveBoardTest.java
@@ -0,0 +1,19 @@
+package gov.nih.nlm.cde.test.boards;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class RemoveBoardTest extends BoardTest {
+    @Test
+    public void removeBoard() {
+        mustBeLoggedInAs(boardUser, password);
+        String boardName = "Remove me board";
+        gotoMyBoards();
+        clickElement(By.xpath("//*[@data-id='boardDiv_" + boardName + "']//*[contains(@id,'removeBoard-')]"));
+        textPresent("Confirm Delete");
+        clickElement(By.xpath("//*[@data-id='boardDiv_" + boardName + "']//*[contains(@id,'confirmRemove-')]"));
+        textPresent("Done");
+        closeAlert();
+        textNotPresent(boardName);
+    }
+}
