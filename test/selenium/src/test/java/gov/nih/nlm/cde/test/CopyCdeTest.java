94d4569a645f9252cec3a1f6977cb4ac34b974f2
Peter Huang <shi.huang@nih.gov>
2017-01-11 11:40:23 -0500

    fix some tests

--- test/selenium/src/test/java/gov/nih/nlm/cde/test/CopyCdeTest.java
+++ test/selenium/src/test/java/gov/nih/nlm/cde/test/CopyCdeTest.java
@@ -16,7 +16,7 @@ public class CopyCdeTest extends BaseClassificationTest {
         clickElement(By.id("saveCopy"));
         hangon(1);
         showAllTabs();
-        textPresent("Incomplete", By.id("dd_status"));
+        textPresent("Incomplete", By.id("registrationStatus"));
         textPresent("Copy of: Medication affecting cardiovascular function type exam day indicator", By.id("dd_general_name"));
         clickElement(By.id("status_tab"));
         textPresent("Copy of: xug6J6R8fkf");
