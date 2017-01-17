068b1f9ad8d50aa941f863028c1b08a15906d31d
Peter Huang <shi.huang@nih.gov>
2017-01-17 11:40:54 -0500

    x

--- ingester/mergeNamingWithTags.js
+++ ingester/mergeNamingWithTags.js
@@ -17,7 +17,40 @@ async.forEach(collections, function (collection, doneOneCollection) {
         if (findError) throw findError;
         else {
             async.forEach(records, function (record, doneOneRecord) {
-
+                var newNamingArray = [];
+                record.get('naming').forEach(function (naming) {
+                    if (newNamingArray.filter(function (newName) {
+                            return newName.designation === naming.designation &&
+                                newName.definition === naming.definition &&
+                                newName.definitionFormat === naming.definitionFormat &&
+                                newName.languageCode === naming.languageCode &&
+                                newName.source === naming.source
+                        }).length > 0) {
+                        newNamingArray.forEach(function (newName) {
+                            if (newName.designation === naming.designation &&
+                                newName.definition === naming.definition &&
+                                newName.definitionFormat === naming.definitionFormat &&
+                                newName.languageCode === naming.languageCode &&
+                                newName.source === naming.source) {
+                                var existingTag = false;
+                                newName.tags.forEach(function (t) {
+                                    
+                                })
+                            }
+                        })
+                    } else {
+                        newNamingArray.push({
+                            designation: naming.designation,
+                            definition: naming.definition,
+                            definitionFormat: naming.definitionFormat,
+                            languageCode: naming.languageCode,
+                            tags: [{
+                                tag: naming.context.contextName
+                            }],
+                            source: naming.source
+                        })
+                    }
+                });
                 recordsCount++;
                 console.log('recordsCount: ' + recordsCount);
                 doneOneRecord();
