6c1981cc6bffb045a2226e4a459c6143eed33da5
Peter Huang <shi.huang@nih.gov>
2017-01-20 14:32:08 -0500

    change context to tags

--- test/selenium/src/test/java/gov/nih/nlm/form/test/export/FormXMLExport.java
+++ test/selenium/src/test/java/gov/nih/nlm/form/test/export/FormXMLExport.java
@@ -17,15 +17,16 @@ public class FormXMLExport extends NlmCdeBaseTest {
 
         clickElement(By.id("export"));
         String url = findElement(By.id("nihXml")).getAttribute("href");
-        String response = get(url).asString();
+        String response = get(url).asString().replaceAll("\\s+", "");
 
-
-        Assert.assertTrue(response.replaceAll("\\s+", "").contains(("<naming>\n" +
+        String shouldContain = ("<naming>\n" +
                 "<designation>Parenchymal Imaging</designation>\n" +
                 "<definition>\n" +
                 "Contains data elements collected when an imaging study is performed to measure parenchyma; data recorded attempt to divide the strokes into ischemic or hemorrhagic subtypes, as distinction of hemorrhage versus infarction is the initial critical branch point in acute stroke triage. (Examples of CDEs included: Acute infarcts present; Planimetic acute ischemic lesion volume; and Acute hematoma present)\n" +
                 "</definition>\n" +
-                "</naming>").replaceAll("\\s+", "")));
+                "</naming>").replaceAll("\\s+", "");
+        if (!response.contains(shouldContain)) {
+            Assert.fail("response:\n" + response + "\nshouldContain:\n" + shouldContain);
+        }
     }
-
 }
