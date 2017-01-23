9bf18aea0b9a5c7202b8020ae9fb9a0090761475
ludetc <ludetc@mail.nih.gov>
2017-01-23 10:13:50 -0500

    script written

--- scripts/removeDupPvs.js
+++ scripts/removeDupPvs.js
@@ -2,7 +2,6 @@ var mongo_cde = require('../modules/cde/node-js/mongo-cde'),
     async = require('async')
     ;
 
-
 var idList = [];
 
 var stream = mongo_cde.getStream({$where: "this.valueDomain.permissibleValues.length > 2",
@@ -25,10 +24,25 @@ stream.on('close', function () {
             var pvSet = new Set(cde.valueDomain.permissibleValues.map(
                 pv => pv.permissibleValue + "--" + pv.valueMeaningName));
             if (pvSet.size !== cde.valueDomain.permissibleValues.length) {
+                var ex = [];
                 console.log("found dup: " + cde.tinyId);
-                i++;
+                cde.valueDomain.permissibleValues = cde.valueDomain.permissibleValues.filter(pv => {
+                    if (ex.indexOf(pv.permissibleValue + "--" + pv.valueMeaningName) > -1) return false;
+                    else {
+                        ex.push(pv.permissibleValue + "--" + pv.valueMeaningName);
+                        return true;
+                    }
+                });
+                cde.changeNote = "Removed duplicate PV";
+                mongo_cde.update(cde, {username: "batchloader"}, function (err) {
+                    if (err) {
+                        console.log(err);
+                        process.exit(1);
+                    }
+                    oneDone();
+                    i++;
+                });
             }
-            oneDone();
         });
     }, function (err) {
         console.log("All Done");
