a35ec829502a65d6d8d13792f97abcc18b7fc4dc
Peter Huang <shi.huang@nih.gov>
2017-01-23 15:35:42 -0500

    fix export tests

--- test/selenium/src/test/java/gov/nih/nlm/cde/test/export/ExportTest.java
+++ test/selenium/src/test/java/gov/nih/nlm/cde/test/export/ExportTest.java
@@ -2,7 +2,6 @@ package gov.nih.nlm.cde.test.export;
 
 import gov.nih.nlm.system.NlmCdeBaseTest;
 import org.openqa.selenium.By;
-import org.openqa.selenium.TimeoutException;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -19,9 +18,17 @@ import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
 public class ExportTest extends NlmCdeBaseTest {
 
     @Test
-    public void searchExport() {
+    public void cdeSearchExport() {
         mustBeLoggedOut();
-        loadDefaultSettings();
+        clickElement(By.id("searchSettings"));
+        clickElement(By.id("uom"));
+        clickElement(By.id("naming"));
+        clickElement(By.id("administrativeStatus"));
+        clickElement(By.id("source"));
+        clickElement(By.id("updated"));
+        clickElement(By.id("saveSettings"));
+        textPresent("Settings saved!");
+        closeAlert();
 
         goToCdeSearch();
         clickElement(By.id("browseOrg-NINDS"));
@@ -35,42 +42,10 @@ public class ExportTest extends NlmCdeBaseTest {
         closeAlert();
         closeAlert();
 
-        String[] expected = {
-                "Name, Question Texts, Value Type, Permissible Values, Nb of Permissible Values, Steward, Used By, Registration Status, Identifiers",
-                "\"Movement Disorder Society - Unified Parkinson's Disease Rating Scale (MDS UPDRS) - light headedness on stand score\",\"LIGHT HEADEDNESS ON STANDING\",\"Value List\",\"0; 1; 2; 3; 4\",\"5\",\"NINDS\",\"NINDS\",\"Qualified\",\"NINDS: C09971 v3; NINDS Variable Name: MDSUPDRSLiteHeadStndngScore\"",
-                "\"Unified Parkinson's Disease Rating Scale (UPDRS) - symptomatic orthostasis indicator\",\"Does the patient have symptomatic orthostasis?\",\"Value List\",\"0; 1\",\"2\",\"NINDS\",\"NINDS\",\"Qualified\",\"NINDS: C09927 v3; NINDS Variable Name: UPDRSSymOrtInd\""
-        };
+        clickElement(By.id("export"));
+        textPresent("CSV Export");
+        clickElement(By.id("csvExport"));
 
-        try {
-            String actual = new String(Files.readAllBytes(Paths.get(downloadFolder + "/SearchExport.csv")));
-            for (String s : expected) {
-                if (!actual.contains(s)) {
-                    Files.copy(
-                            Paths.get(downloadFolder + "/SearchExport.csv"),
-                            Paths.get(tempFolder + "/ExportTest-searchExport.csv"), REPLACE_EXISTING);
-                    Assert.fail("missing line in export : " + s);
-                }
-            }
-        } catch (IOException e) {
-            Assert.fail("Exception reading SearchExport.csv");
-        }
-        clickElement(By.id("searchSettings"));
-        clickElement(By.id("uom"));
-        clickElement(By.id("naming"));
-        clickElement(By.id("administrativeStatus"));
-        clickElement(By.id("source"));
-        clickElement(By.id("updated"));
-        clickElement(By.id("saveSettings"));
-        textPresent("Settings saved!");
-        closeAlert();
-
-        try {
-            clickElement(By.id("export"));
-            clickElement(By.id("csvExport"));
-        } catch (TimeoutException e) {
-            clickElement(By.id("export"));
-            clickElement(By.id("csvExport"));
-        }
         textPresent("export is being generated");
         textPresent("Export downloaded.");
         closeAlert();
@@ -78,8 +53,8 @@ public class ExportTest extends NlmCdeBaseTest {
 
         String[] expected2 = {
                 "Name, Question Texts, Other Names, Value Type, Permissible Values, Nb of Permissible Values, Unit of Measure, Steward, Used By, Registration Status, Administrative Status, Identifiers, Source, Updated",
-                "\"Movement Disorder Society - Unified Parkinson's Disease Rating Scale (MDS UPDRS) - light headedness on stand score\",\"\",\"Value List\",\"0; 1; 2; 3; 4\",\"5\",\"\",\"NINDS\",\"NINDS\",\"Qualified\",\"\",\"NINDS: C09971 v3; NINDS Variable Name: MDSUPDRSLiteHeadStndngScore\",\"NINDS\",\"\"",
-                "\"Unified Parkinson's Disease Rating Scale (UPDRS) - right hand movements scale\",\"\",\"Value List\",\"0; 1; 2; 3; 4\",\"5\",\"\",\"NINDS\",\"NINDS\",\"Qualified\",\"\",\"NINDS: C09909 v3; NINDS Variable Name: UPDRSRtHndMovScale\",\"NINDS\",\"\""
+                "\"Movement Disorder Society - Unified Parkinson's Disease Rating Scale (MDS UPDRS) - light headedness on stand score\",\"LIGHT HEADEDNESS ON STANDING\",\"Movement Disorder Society - Unified Parkinson's Disease Rating Scale (MDS UPDRS) - light headedness on stand score\",\"Value List\",\"0; 1; 2; 3; 4\",\"5\",\"\",\"NINDS\",\"NINDS\",\"Qualified\",\"\",\"NINDS: C09971 v3; NINDS Variable Name: MDSUPDRSLiteHeadStndngScore\",\"NINDS\",\"\",\n",
+                "\"Unified Parkinson's Disease Rating Scale (UPDRS) - right hand movements scale\",\"Hand Movements Patient opens and closes hands in rapid succession.\",\"Unified Parkinson's Disease Rating Scale (UPDRS) - right hand movements scale\",\"Value List\",\"0; 1; 2; 3; 4\",\"5\",\"\",\"NINDS\",\"NINDS\",\"Qualified\",\"\",\"NINDS: C09909 v3; NINDS Variable Name: UPDRSRtHndMovScale\",\"NINDS\",\"\""
         };
 
         try {
@@ -93,8 +68,8 @@ public class ExportTest extends NlmCdeBaseTest {
                     FileTime createdDate = Files.readAttributes(Paths.get(tempFolder + "/ExportTest-searchExport.csv"), BasicFileAttributes.class).creationTime();
                     SimpleDateFormat df = new SimpleDateFormat("MM/dd/yyyy");
                     df.setTimeZone(TimeZone.getTimeZone("EST"));
-                    System.out.print("SearchExport crated on : " + df.format(searchExportCreatedDate.toMillis()));
-                    System.out.print("ExportTest-searchExport.csv on : " + df.format(createdDate.toMillis()));
+                    System.out.println("SearchExport crated on : " + df.format(searchExportCreatedDate.toMillis()));
+                    System.out.println("ExportTest-searchExport.csv on : " + df.format(createdDate.toMillis()));
                     Assert.fail("missing line in export : " + s + "\n---Actual: " + actual);
                 }
             }
