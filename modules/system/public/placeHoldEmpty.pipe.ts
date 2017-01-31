9823ac82e2e15db6cab95a021f8bd6d65ed95fcc
Georgi Ivanov <georgi.ivanov@nih.gov>
2017-01-31 16:02:10 -0500

    build fix

--- modules/system/public/placeHoldEmpty.pipe.ts
+++ modules/system/public/placeHoldEmpty.pipe.ts
@@ -2,8 +2,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({name: 'placeholder'})
 export class PlaceHoldEmptyPipe implements PipeTransform {
-    transform(input: number) {
-        if (input === undefined || input === null || isNaN(input))
+    transform(input: string) {
+        if (input === undefined || input === null || input === "NaN")
             return "N/A";
         else
             return input;
