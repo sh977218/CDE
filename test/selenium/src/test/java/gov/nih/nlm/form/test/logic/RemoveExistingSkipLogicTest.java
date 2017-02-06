2014997bd526aa979a79472cc2cb709a95bf9e70
Peter Huang <shi.huang@nih.gov>
2017-02-06 14:33:33 -0500

    add tests

---
+++ test/selenium/src/test/java/gov/nih/nlm/form/test/logic/RemoveExistingSkipLogicTest.java
@@ -0,0 +1,41 @@
+package gov.nih.nlm.form.test.logic;
+
+import gov.nih.nlm.form.test.BaseFormTest;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class RemoveExistingSkipLogicTest extends BaseFormTest {
+
+    @Test
+    public void removeExistingSkipLogicTest() {
+        String formName = "Remove SkipLogic Form";
+        String questionSL = "\"Quality of Life - Write task list difficulty scale\" =\"5\"";
+        String sectionSL = "\"Quality of Life - Write task list difficulty scale\" =";
+        String inFormSL = "\"Patient Gender Code\" =\"Males\"";
+        mustBeLoggedInAs(testAdmin_username, password);
+        goToFormByName(formName);
+        clickElement(By.xpath("description_tab"));
+
+        textPresent(questionSL, By.xpath("//*[@id='question_0_1']//div[contains(@ng-if,'skipLogic')]"));
+        textPresent(sectionSL, By.xpath("//*[@id='dd_s_skipLogic_2']//input[2]"));
+        textPresent(inFormSL, By.xpath("//*[@id='dd_s_skipLogic_3']//input[2]"));
+
+        startEditQuestionSectionById("question_0_1");
+        findElement(By.xpath("//*[@id='question_0_1']//textarea[2]")).clear();
+        saveEditQuestionSectionById("question_0_1");
+
+        startEditQuestionSectionById("section_0_2");
+        findElement(By.xpath("//*[@id='dd_s_skipLogic_2']//input[2]")).clear();
+        saveEditQuestionSectionById("section_0_2");
+
+        startEditQuestionSectionById("inform_0_3");
+        findElement(By.xpath("//*[@id='dd_s_skipLogic_3']//input[2]")).clear();
+        saveEditQuestionSectionById("inform_0_3");
+        saveForm();
+
+        goToFormByName(formName);
+        textNotPresent(questionSL);
+        textNotPresent(sectionSL);
+        textNotPresent(inFormSL);
+    }
+}
