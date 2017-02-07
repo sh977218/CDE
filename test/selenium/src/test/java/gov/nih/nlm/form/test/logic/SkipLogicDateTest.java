43551822e28928b0a2b6d44afd824dc13fd90b85
ludetc <ludetc@mail.nih.gov>
2017-02-07 15:13:34 -0500

    fix skip logic test

--- test/selenium/src/test/java/gov/nih/nlm/form/test/logic/SkipLogicDateTest.java
+++ test/selenium/src/test/java/gov/nih/nlm/form/test/logic/SkipLogicDateTest.java
@@ -2,6 +2,7 @@ package gov.nih.nlm.form.test.logic;
 
 import gov.nih.nlm.form.test.BaseFormTest;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.testng.annotations.Test;
 
 public class SkipLogicDateTest extends BaseFormTest {
@@ -58,15 +59,21 @@ public class SkipLogicDateTest extends BaseFormTest {
         findElement(By.xpath("//*[@id='Indicate date of reference scan_1']//input")).sendKeys("10/11/2016");
         textPresent("Macula volume (OD)");
 
+        ((JavascriptExecutor)driver).executeScript ("document.getElementByName('q2').removeAttribute('readonly',0);");
+
+        findElement(By.xpath("//*[@id='Indicate date of reference scan_1']//input")).clear();
         findElement(By.xpath("//*[@id='Indicate date of reference scan_1']//input")).sendKeys("10/12/2016");
         textPresent("Macula volume (OS)");
 
+        findElement(By.xpath("//*[@id='Indicate date of reference scan_1']//input")).clear();
         findElement(By.xpath("//*[@id='Indicate date of reference scan_1']//input")).sendKeys("10/10/2016");
         textPresent("Laterality Type");
 
+        findElement(By.xpath("//*[@id='Indicate date of reference scan_1']//input")).clear();
         findElement(By.xpath("//*[@id='Indicate date of reference scan_1']//input")).sendKeys("2017");
         textPresent("Macula volume (OS)");
 
+        findElement(By.xpath("//*[@id='Indicate date of reference scan_1']//input")).clear();
         findElement(By.xpath("//*[@id='Indicate date of reference scan_1']//input")).sendKeys("2015");
         textPresent("Laterality Type");
     }
