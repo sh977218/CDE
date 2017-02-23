cf6df982392f2eb07e53168a19dc05a1e9a462f1
ludetc <ludetc@mail.nih.gov>
2017-02-23 14:54:00 -0500

    temp

---
+++ modules/cde/public/components/concepts.component.ts
@@ -0,0 +1,69 @@
+import { Http } from "@angular/http";
+import { Component, Inject, Input, ViewChild } from "@angular/core";
+import { ModalDirective } from "ng2-bootstrap/modal";
+
+@Component({
+    selector: "cde-concepts",
+    templateUrl: "./concepts.component.html"
+})
+
+
+export class ConceptsComponent {
+
+    @ViewChild("childModal") public childModal:ModalDirective;
+    @Input( ) public elt: any;
+
+    conceptConfigurations = {
+        "dataElementConcept": {display: "Data Element Concept", path: "dataElementConcept.concepts.name"},
+        "objectClass": {display: "Object Class", path: "objectClass.concepts.name"},
+        "property": {display: "Property", path: "property.concepts.name"}
+    };
+
+    openNewConcept () {
+        this.childModal.show();
+    };
+
+    dataElementConceptRemoveConcept (index) {
+        this.elt.dataElementConcept.concepts.splice(index, 1);
+        this.elt.unsaved = true;
+    };
+
+    objectClassRemoveConcept (index) {
+        this.elt.objectClass.concepts.splice(index, 1);
+        this.elt.unsaved = true;
+    };
+
+    propertyRemoveConcept (index) {
+        this.elt.property.concepts.splice(index, 1);
+        this.elt.unsaved = true;
+    };
+
+    removeConcept (type, i) {
+        this[type + "RemoveConcept"](i);
+    };
+
+    relatedCdes (concept, type) {
+        // @TODO
+        // $location.url('/cde/search?q=' + $scope.conceptConfigurations[type].path + ':"' + concept + '"');
+    };
+
+    newConcept = {origin: "LOINC", type: "dec"};
+
+    okCreate () {
+        if (!this.elt.dataElementConcept) this.elt.dataElementConcept = {};
+        if (this.newConcept.type === "dec") {
+            if (!this.elt.dataElementConcept.concepts) this.elt.dataElementConcept.concepts = [];
+            this.elt.dataElementConcept.concepts.push(this.newConcept);
+        } else if (this.newConcept.type === "prop") {
+            if (!this.elt.property.concepts) this.elt.property.concepts = [];
+            this.elt.property.concepts.push(this.newConcept);
+        } else if (this.newConcept.type === "oc") {
+            if (!this.elt.objectClass.concepts) this.elt.objectClass.concepts = [];
+            this.elt.objectClass.concepts.push(this.newConcept);
+        }
+        this.elt.unsaved = true;
+        this.childModal.hide();
+    };
+
+
+}
---
+++ modules/cde/public/js/cde.module.ts
@@ -0,0 +1,20 @@
+import { CommonModule } from "@angular/common";
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { PaginationModule } from "ng2-bootstrap";
+import { ModalModule } from "ng2-bootstrap";
+
+import { upgradeAdapter } from "../../../upgrade";
+import { ConceptsComponent } from "../components/concepts.component";
+
+@NgModule({
+    declarations: [
+        ConceptsComponent
+        ],
+    providers: [],
+    imports: [CommonModule, FormsModule, ModalModule.forRoot(), PaginationModule.forRoot()],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+
+export class CdeModule {
+}
