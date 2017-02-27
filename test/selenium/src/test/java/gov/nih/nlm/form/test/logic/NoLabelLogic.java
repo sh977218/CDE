c7c3386a1296d7bd4bd68ba6fa62dcf56b3d63ce
ludetc <ludetc@mail.nih.gov>
2017-02-27 15:08:35 -0500

    added test

---
+++ test/selenium/src/test/java/gov/nih/nlm/form/test/logic/NoLabelLogic.java
@@ -0,0 +1,31 @@
+package gov.nih.nlm.form.test.logic;
+
+import gov.nih.nlm.form.test.BaseFormTest;
+import gov.nih.nlm.system.NlmCdeBaseTest;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class NoLabelLogic extends BaseFormTest {
+
+    @Test
+    public void noLabelLogic() {
+        mustBeLoggedInAs(nlm_username, nlm_password);
+        goToFormByName("No Label Logic");
+        textPresent("Undifferentiated/Indeterminant/Intersex");
+        textPresent("Walking speed value");
+        clickElement(By.id("description_tab"));
+        clickElement(By.cssSelector("#question_0_1 .fa-pencil"));
+        findElement(By.xpath(locateSkipLogicEditTextareaXpathByQuestionId("question_0_1"))).sendKeys("\"Gender type\" = \"Unknown\"");
+        saveForm();
+        textPresent("Saved.");
+        closeAlert();
+        textPresent("Show if: \"Gender type\" = \"Unknown\"");
+        clickElement(By.id("general_tab"));
+        textPresent("Walking speed value");
+        clickElement(By.xpath("//label[contains(., 'Printable Logic')]"));
+        textNotPresent("Walking speed value");
+        clickElement(By.xpath("//input[@value='Unknown']"));
+        textPresent("Walking speed value");
+    }
+
+}
