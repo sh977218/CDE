ba186ed732f7c8d71c829f6d8a92a29284576857
Peter Huang <shi.huang@nih.gov>
2017-03-03 15:39:30 -0500

    add tests and update bson

--- test/selenium/src/test/java/gov/nih/nlm/cde/test/valueDomain/PvValidatorTest.java
+++ test/selenium/src/test/java/gov/nih/nlm/cde/test/valueDomain/PvValidatorTest.java
@@ -3,16 +3,13 @@ package gov.nih.nlm.cde.test.valueDomain;
 import gov.nih.nlm.cde.test.BaseClassificationTest;
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class PvValidatorTest extends BaseClassificationTest {
 
-    public void addPv(String pv, String name, String code) {
-        addPv(pv, name, code, null);
-    }
-
     public void addPv(String pv, String name, String code, String codeSystem) {
-        findElement(By.id("addPv")).click();
+        clickElement(By.id("addPv"));
         findElement(By.id("permissibleValueInput")).sendKeys(pv);
 
         if (name != null) {
@@ -31,17 +28,17 @@ public class PvValidatorTest extends BaseClassificationTest {
     }
 
     public void changeField(String which, String to) {
-        findElement(By.xpath("//td[@id='" + which + "']//i")).click();
+        clickElement(By.xpath("//td[@id='" + which + "']//i"));
         findElement(By.xpath("//td[@id='" + which + "']//input")).clear();
         findElement(By.xpath("//td[@id='" + which + "']//input")).sendKeys(to);
-        findElement(By.cssSelector("#" + which + " .fa-check")).click();
+        clickElement(By.cssSelector("#" + which + " .fa-check"));
     }
 
     @Test
     public void pvValidation() {
         mustBeLoggedInAs("selenium", password);
         goToCdeByName("PvValidatorCde");
-        clickElement(By.linkText("Permissible Values"));
+        clickElement(By.id("pvs_tab"));
         textNotPresent("There are validation errors");
 
         changeField("pv-0", "pv2");
@@ -53,7 +50,7 @@ public class PvValidatorTest extends BaseClassificationTest {
         textNotPresent("There are validation errors");
         wait.until(ExpectedConditions.elementToBeClickable(By.id("openSave")));
 
-        addPv("pv5", "name1", "code5");
+        addPv("pv5", "name1", "code5", "LOINC");
         textPresent("There are validation errors. Duplicate Code Name");
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("pv-4-notValid")));
         wait.until(ExpectedConditions.invisibilityOfElementLocated(By.id("openSave")));
@@ -61,7 +58,7 @@ public class PvValidatorTest extends BaseClassificationTest {
         textNotPresent("There are validation errors");
         wait.until(ExpectedConditions.elementToBeClickable(By.id("openSave")));
 
-        addPv("pv5", "name1", "code2");
+        addPv("pv5", "name1", "code2", "NCi");
         textPresent("There are validation errors. Duplicate Code");
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("pv-4-notValid")));
         wait.until(ExpectedConditions.invisibilityOfElementLocated(By.id("openSave")));
@@ -69,6 +66,12 @@ public class PvValidatorTest extends BaseClassificationTest {
         textNotPresent("There are validation errors");
         wait.until(ExpectedConditions.elementToBeClickable(By.id("openSave")));
 
+        clickElement(By.id("addPv"));
+        findElement(By.id("permissibleValueInput")).sendKeys("pv6");
+        findElement(By.id("valueMeaningCodeInput")).sendKeys("code6");
+        Assert.assertEquals(findElement(By.id("createNewPv")).isEnabled(), false);
+        findElement(By.id("codeSystemInput")).sendKeys("MESH");
+        Assert.assertEquals(findElement(By.id("createNewPv")).isEnabled(), true);
     }
 
 }
