acce33f4b6f76fadcf630e460f8cf43544c6dab6
Georgi Ivanov <georgi.ivanov@nih.gov>
2017-03-17 13:15:47 -0400

    fix tests

--- modules/system/public/components/adminItem/identifiers.component.ts
+++ modules/system/public/components/adminItem/identifiers.component.ts
@@ -1,5 +1,5 @@
-import {Component, Inject, Input, ViewChild} from "@angular/core";
-import {ModalDirective} from "ng2-bootstrap/modal";
+import { Component, Inject, Input, ViewChild } from "@angular/core";
+import { ModalDirective } from "ng2-bootstrap/modal";
 import "rxjs/add/operator/map";
 
 @Component({
