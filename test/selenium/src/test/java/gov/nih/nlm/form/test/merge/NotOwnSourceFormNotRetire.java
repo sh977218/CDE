5833cd549da09c77fe8b5017aecc14f94c7c8420
Peter Huang <shi.huang@nih.gov>
2017-03-20 11:12:37 -0400

    fix alignment

--- test/selenium/src/test/java/gov/nih/nlm/form/test/merge/NotOwnSourceFormNotRetire.java
+++ test/selenium/src/test/java/gov/nih/nlm/form/test/merge/NotOwnSourceFormNotRetire.java
@@ -1,5 +1,6 @@
 package gov.nih.nlm.form.test.merge;
 
+import gov.nih.nlm.system.EltIdMaps;
 import gov.nih.nlm.system.NlmCdeBaseTest;
 import org.openqa.selenium.By;
 import org.testng.annotations.Test;
@@ -11,8 +12,10 @@ public class NotOwnSourceFormNotRetire extends NlmCdeBaseTest {
         mustBeLoggedInAs(nlm_username, nlm_password);
         String form1 = "Patient Health Questionnaire - 9 (PHQ-9) Depression Scale";
         String form2 = "PHQ-9 quick depression assessment panel [Reported.PHQ]";
-        addFormToQuickBoard(form1);
-        addFormToQuickBoard(form2);
+        clickElement(By.id("menu_forms_link"));
+        findElement(By.id("ftsearch-input")).sendKeys(EltIdMaps.eltMap.get(form1));
+        clickElement(By.id("menu_forms_link"));
+        findElement(By.id("ftsearch-input")).sendKeys(EltIdMaps.eltMap.get(form2));
         goToQuickBoardByModule("form");
         clickElement(By.id("qb_form_compare"));
         clickElement(By.xpath("//*[@class='leftObj']/*[contains(@class,'mergeForm')]"));
