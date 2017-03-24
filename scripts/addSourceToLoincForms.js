c3b3d51a979ce9e55f1807220eb9c62915c7ca45
ludetc <ludetc@mail.nih.gov>
2017-03-24 11:40:49 -0400

    done

--- scripts/addSourceToLoincForms.js
+++ scripts/addSourceToLoincForms.js
@@ -2,23 +2,25 @@ const mongo_form = require('../modules/form/node-js/mongo-form'),
     async = require('async')
 ;
 
+let count = 0;
+
 mongo_form.Form.find({"classification.stewardOrg.name": "PhenX", archived: null,
     "ids.source": "LOINC"}, (err, pForms) => {
 
         async.eachSeries(pForms, (pForm, oneDone) => {
-            if (!pForms.sources) {
-                pForms.sources = [];
+            if (!pForm.sources) {
+                pForm.sources = [];
             }
-            if (!pForms.sources.find((src) => src.sourceName === 'LOINC')) {
-                pForms.sources.push({
+            if (!pForm.sources.find((src) => src.sourceName === 'LOINC')) {
+                pForm.sources.push({
                     sourceName: "LOINC",
                     copyright: {
                         valueFormat: "html",
                         value: "<a href='http://loinc.org/terms-of-use' target='_blank'>Terms of Use</a>"
                     }
                 });
-                pForms.save(() => {
-                    
+                pForm.save(() => {
+                    console.log("done " + ++count);
                     oneDone();
                 });
             } else {
