dce61b82cf89656944b7618cfec0685a1a2e2bf1
Georgi Ivanov <georgi.ivanov@nih.gov>
2017-03-30 16:54:54 -0400

    fix tests

--- test/selenium/src/test/java/gov/nih/nlm/form/test/NativeTableTest.java
+++ test/selenium/src/test/java/gov/nih/nlm/form/test/NativeTableTest.java
@@ -14,11 +14,11 @@ public class NativeTableTest extends BaseFormTest {
 
         textPresent("Patient Family Member Order Name", By.xpath("//thead//th[@rowspan='3']"));
         textPresent("Condition", By.xpath("//thead//th[@colspan='3']"));
-        Assert.assertEquals(driver.findElements(By.xpath("//thead//th[@colspan='3']")).size(), 2);
-        Assert.assertEquals(driver.findElements(By.xpath("//thead//th[@ng-reflect-text-content='Education level']")).size(), 2);
         findElement(By.xpath("//div[@class='native-table-cell']/label[@ng-reflect-text-content='2.']"));
         findElement(By.xpath("//div[contains(@class,'native-table-cell')]/label/span[contains(@ng-reflect-text-content,'1st Grade')]"));
         findElement(By.xpath("//div[contains(@class,'native-table-cell')]/label[text()=' year']"));
+        Assert.assertEquals(2, driver.findElements(By.xpath("//thead//th[@colspan='3']")).size());
+        Assert.assertEquals(2, driver.findElements(By.xpath("//thead//th[@ng-reflect-text-content='Education level']")).size());
 
         clickElement(By.id("description_tab"));
         textPresent("Repeats: 5 times", By.xpath("//div[@id='section_0']//span[contains(@class,'label-primary')]"));
