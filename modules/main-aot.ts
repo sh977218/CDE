81a5f88b08bd173040ff2724c8ddad0b23a8cae4
Georgi Ivanov <georgi.ivanov@nih.gov>
2017-04-03 13:23:21 -0400

    Revert "temp"
    This reverts commit b9a92d0cd7ec4165adc17944e11e772c82647901.

---
+++ modules/main-aot.ts
@@ -0,0 +1,14 @@
+import "core-js/client/shim";
+import "zone.js";
+import "reflect-metadata";
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+import { enableProdMode } from "@angular/core";
+enableProdMode();
+
+import { CdeAppModule } from "./app.module";
+import { upgradeAdapter } from "./upgrade";
+
+import "./upgrade-imports";
+
+platformBrowserDynamic().bootstrapModule(CdeAppModule/*, options*/);
+upgradeAdapter.bootstrap(document.body, ["cdeAppModule"], {strictDi: true});
