7d574908765b03637dddcf3f78508e334f40ad83
Peter Huang <shi.huang@nih.gov>
2017-04-06 16:15:25 -0400

    temp

--- modules/cde/public/js/cdeDiff.js
+++ modules/cde/public/js/cdeDiff.js
@@ -1,4 +1,4 @@
-angular.module('cdeModule').factory('CdeDiffPopulate', function() {
+angular.module('cdeModule').factory('CdeDiffPopulate', function () {
     var CdeDiffPopulate = this;
     CdeDiffPopulate.pathFieldMap = {
         1: [
@@ -18,79 +18,125 @@ angular.module('cdeModule').factory('CdeDiffPopulate', function() {
             , {fieldName: "Permissible Values - Text", path: ["valueDomain", "datatypeText"]}
             , {fieldName: "Permissible Values - Integer", path: ["valueDomain", "datatypeInteger"]}
             , {fieldName: "Permissible Values - Number", path: ["valueDomain", "datatypeNumber"]}
-            , {fieldName: "Permissible Values - Date", path: ["valueDomain", "datatypeDate"]} 
-            , {fieldName: "Permissible Values - Value List", path: ["valueDomain", "datatypeValueList"]} 
-            , {fieldName: "Unit of Measure", path: ["valueDomain", "uom"]} 
-            , {fieldName: "Permissible Values - VSAC Mapping", path: ["dataElementConcept", "conceptualDomain"]} 
-            , {fieldName: "Permissible Values - Externaly Defined", path: ["valueDomain", "datatypeExternallyDefined"]} 
-            , {fieldName: "Permissible Values" , path: ["valueDomain", "permissibleValues"]} 
-            , {fieldName: "Permissible Values - Float" , path: ["valueDomain", "datatypeFloat"]} 
-            
-            
-        ]        
+            , {fieldName: "Permissible Values - Date", path: ["valueDomain", "datatypeDate"]}
+            , {fieldName: "Permissible Values - Value List", path: ["valueDomain", "datatypeValueList"]}
+            , {fieldName: "Unit of Measure", path: ["valueDomain", "uom"]}
+            , {fieldName: "Permissible Values - VSAC Mapping", path: ["dataElementConcept", "conceptualDomain"]}
+            , {fieldName: "Permissible Values - Externaly Defined", path: ["valueDomain", "datatypeExternallyDefined"]}
+            , {fieldName: "Permissible Values", path: ["valueDomain", "permissibleValues"]}
+            , {fieldName: "Permissible Values - Float", path: ["valueDomain", "datatypeFloat"]}
+
+
+        ]
         , 3: [
-            {fieldName: "Naming - Other Name", path: ["naming",-1,"designation"]}
-            , {fieldName: "Naming - Other Definition", path: ["naming",-1,"definition"]}        
-            , {fieldName: "Primary Name", path: ["naming",0,"designation"]}
-            , {fieldName: "Primary Definition", path: ["naming",0,"definition"]}
-            , {fieldName: "Permissible Values - Text - Regular Expression", path: ["valueDomain", "datatypeText", "regex"]}
+            {fieldName: "Naming - Other Name", path: ["naming", -1, "designation"]}
+            , {fieldName: "Naming - Other Definition", path: ["naming", -1, "definition"]}
+            , {fieldName: "Primary Name", path: ["naming", 0, "designation"]}
+            , {fieldName: "Primary Definition", path: ["naming", 0, "definition"]}
+            , {
+                fieldName: "Permissible Values - Text - Regular Expression",
+                path: ["valueDomain", "datatypeText", "regex"]
+            }
             , {fieldName: "Permissible Values - Text - Freetext Rule", path: ["valueDomain", "datatypeText", "rule"]}
-            , {fieldName: "Permissible Values - Text - Maximum Length", path: ["valueDomain", "datatypeText", "maxLength"]}
-            , {fieldName: "Permissible Values - Text - Minimum Length", path: ["valueDomain", "datatypeText", "minLength"]}
-            , {fieldName: "Permissible Values - Integer - Maximum Value", path: ["valueDomain", "datatypeInteger", "maxValue"]}
-            , {fieldName: "Permissible Values - Integer - Minimum Value", path: ["valueDomain", "datatypeInteger", "minValue"]}
-            , {fieldName: "Permissible Values - Number - Maximum Value", path: ["valueDomain", "datatypeNumber", "maxValue"]}
-            , {fieldName: "Permissible Values - Number - Minimum Value", path: ["valueDomain", "datatypeNumber", "minValue"]}
-            , {fieldName: "Permissible Values - Date - Format", path: ["valueDomain", "datatypeDate", "format"]}     
-            , {fieldName: "Permissible Values - Value List - Datatype", path: ["valueDomain", "datatypeValueList", "datatype"]}   
-            , {fieldName: "Permissible Values - Properties - Value", path: ["properties", -1, "value"]}   
-            , {fieldName: "Naming - Other Definition - Format", path: ["naming", -1, "definitionFormat"]}            
-            , {fieldName: "Permissible Values - Float - Minimum Value", path: ["valueDomain", "datatypeFloat", "minValue"]}
-            , {fieldName: "Permissible Values - Float - Maximum Value", path: ["valueDomain", "datatypeFloat", "maxValue"]}
+            , {
+                fieldName: "Permissible Values - Text - Maximum Length",
+                path: ["valueDomain", "datatypeText", "maxLength"]
+            }
+            , {
+                fieldName: "Permissible Values - Text - Minimum Length",
+                path: ["valueDomain", "datatypeText", "minLength"]
+            }
+            , {
+                fieldName: "Permissible Values - Integer - Maximum Value",
+                path: ["valueDomain", "datatypeInteger", "maxValue"]
+            }
+            , {
+                fieldName: "Permissible Values - Integer - Minimum Value",
+                path: ["valueDomain", "datatypeInteger", "minValue"]
+            }
+            , {
+                fieldName: "Permissible Values - Number - Maximum Value",
+                path: ["valueDomain", "datatypeNumber", "maxValue"]
+            }
+            , {
+                fieldName: "Permissible Values - Number - Minimum Value",
+                path: ["valueDomain", "datatypeNumber", "minValue"]
+            }
+            , {fieldName: "Permissible Values - Date - Format", path: ["valueDomain", "datatypeDate", "format"]}
+            , {
+                fieldName: "Permissible Values - Value List - Datatype",
+                path: ["valueDomain", "datatypeValueList", "datatype"]
+            }
+            , {fieldName: "Permissible Values - Properties - Value", path: ["properties", -1, "value"]}
+            , {fieldName: "Naming - Other Definition - Format", path: ["naming", -1, "definitionFormat"]}
+            , {
+                fieldName: "Permissible Values - Float - Minimum Value",
+                path: ["valueDomain", "datatypeFloat", "minValue"]
+            }
+            , {
+                fieldName: "Permissible Values - Float - Maximum Value",
+                path: ["valueDomain", "datatypeFloat", "maxValue"]
+            }
             , {fieldName: "Permissible Values - Float - Precision", path: ["valueDomain", "datatypeFloat", "precision"]}
             , {fieldName: "Properties - Value - Format", path: ["properties", -1, "valueFormat"]}
             , {fieldName: "Properties - Key", path: ["properties", -1, "key"]}
             , {fieldName: "Identifiers - Source", path: ["ids", -1, "source"]}
             , {fieldName: "Identifiers - ID", path: ["ids", -1, "id"]}
-            , {fieldName: "Identifiers - Version", path: ["ids", -1, "version"]}  
+            , {fieldName: "Identifiers - Version", path: ["ids", -1, "version"]}
             , {fieldName: "Attachments - File ID", path: ["attachments", -1, "fileid"]}
             , {fieldName: "Attachments - Filename", path: ["attachments", -1, "filename"]}
             , {fieldName: "Attachments - Upload Date", path: ["attachments", -1, "uploadDate"]}
-            , {fieldName: "Attachments - File Size", path: ["attachments", -1, "filesize"]} 
+            , {fieldName: "Attachments - File Size", path: ["attachments", -1, "filesize"]}
         ]
         , 4: [
             {fieldName: "Permissible Values", path: ["valueDomain", "permissibleValues", -1, "permissibleValue"]}
             , {fieldName: "Concepts - Data Element - Name", path: ["dataElementConcept", "concepts", -1, "name"]}
             , {fieldName: "Concepts - Data Element - Origin", path: ["dataElementConcept", "concepts", -1, "origin"]}
-            , {fieldName: "Concepts - Data Element - Origin ID", path: ["dataElementConcept", "concepts", -1, "originId"]}
-            , {fieldName: "Permissible Values - Code Name", path: ["valueDomain", "permissibleValues", -1, "valueMeaningName"]}
-            , {fieldName: "Permissible Values - Value", path: ["valueDomain", "permissibleValues", -1, "permissibleValue"]}
-            , {fieldName: "Permissible Values - Code", path: ["valueDomain", "permissibleValues", -1, "valueMeaningCode"]}         
-            , {fieldName: "Permissible Values - Code System", path: ["valueDomain", "permissibleValues", -1, "codeSystemName"]}
+            , {
+                fieldName: "Concepts - Data Element - Origin ID",
+                path: ["dataElementConcept", "concepts", -1, "originId"]
+            }
+            , {
+                fieldName: "Permissible Values - Code Name",
+                path: ["valueDomain", "permissibleValues", -1, "valueMeaningName"]
+            }
+            , {
+                fieldName: "Permissible Values - Value",
+                path: ["valueDomain", "permissibleValues", -1, "permissibleValue"]
+            }
+            , {
+                fieldName: "Permissible Values - Code",
+                path: ["valueDomain", "permissibleValues", -1, "valueMeaningCode"]
+            }
+            , {
+                fieldName: "Permissible Values - Code System",
+                path: ["valueDomain", "permissibleValues", -1, "codeSystemName"]
+            }
             , {fieldName: "Naming - Primary Name - Context", path: ["naming", 1, "context", "contextName"]}
             , {fieldName: "Naming - Other Name - Context", path: ["naming", -1, "context", "contextName"]}
             , {fieldName: "Properties - Object Class - Name", path: ["objectClass", "concepts", -1, "name"]}
-            
+
         ]
     };
-    
-    CdeDiffPopulate.nullsToBottom = function(obj) {
+
+    CdeDiffPopulate.nullsToBottom = function (obj) {
         return (angular.isDefined(obj.updated) ? 0 : 1);
-    };    
-    
-    CdeDiffPopulate.comparePaths = function(patternPath,realPath){
+    };
+
+    CdeDiffPopulate.comparePaths = function (patternPath, realPath) {
         var equal = true;
-        patternPath.forEach(function(el,i){
-            if (typeof el === "number" && el===-1) return;
-            if (el===realPath[i]) equal = equal && true;
-            else equal = false;
-        });
+        if (patternPath)
+            patternPath.forEach(function (el, i) {
+                if (typeof el === "number" && el === -1) return;
+                if (el === realPath[i]) equal = equal && true;
+                else equal = false;
+            });
         return equal;
     };
-    
-    CdeDiffPopulate.makeHumanReadable = function(change){
+
+    CdeDiffPopulate.makeHumanReadable = function (change) {
         if (!change) return;
-        this.stringify = function(obj) {
+        this.stringify = function (obj) {
             try {
                 if (typeof obj === "string") return obj;
                 if (typeof obj === "number") return obj;
@@ -103,41 +149,42 @@ angular.module('cdeModule').factory('CdeDiffPopulate', function() {
                 return "";
             }
         };
-        this.stringifyClassif = function(obj) {
+        this.stringifyClassif = function (obj) {
             if (obj && obj.elements) return " > " + obj.name + this.stringifyClassif(obj.elements[0]);
             else return "";
-        };                
-        if (change.kind==="E") {
+        };
+        if (change.kind === "E") {
             change.modificationType = "Modified Field";
             change.newValue = change.rhs;
             change.previousValue = change.lhs;
         }
-        if (change.kind==="N") {
+        if (change.kind === "N") {
             change.modificationType = "New Item";
             change.newValue = this.stringify(change.rhs);
-        }                
-        if (change.kind==="A" && change.item.kind==="N") {
-            change.modificationType = "New Item";
-            change.newValue = this.stringify(change.item.rhs);                  
         }
-        if (change.kind==="A" && change.item.kind==="D") {
+        if (change.kind === "A" && change.item.kind === "N") {
+            change.modificationType = "New Item";
+            change.newValue = this.stringify(change.item.rhs);
+        }
+        if (change.kind === "A" && change.item.kind === "D") {
             change.modificationType = "Item Deleted";
             change.newValue = this.stringify(change.item.lhs);
-        }            
-        if (change.kind==="D") {
+        }
+        if (change.kind === "D") {
             change.modificationType = "Item Deleted";
             change.previousValue = this.stringify(change.lhs);
-        }                   
+        }
         if (change.path[0] === "classification") {
             change.fieldName = "Classification";
             if (change.item && change.item.lhs) change.newValue = this.stringifyClassif(change.item.lhs);
             if (change.item && change.item.rhs) change.newValue = this.stringifyClassif(change.item.rhs);
             return;
         }
-        CdeDiffPopulate.pathFieldMap[change.path.length].forEach(function(pathPair){
-            if (CdeDiffPopulate.comparePaths(pathPair.path, change.path)) change.fieldName = pathPair.fieldName;
-        });
-    };        
-         
+        if (CdeDiffPopulate.pathFieldMap[change.path.length])
+            CdeDiffPopulate.pathFieldMap[change.path.length].forEach(function (pathPair) {
+                if (CdeDiffPopulate.comparePaths(pathPair.path, change.path)) change.fieldName = pathPair.fieldName;
+            });
+    };
+
     return CdeDiffPopulate;
 });
--- modules/system/public/components/siteAdmin/logAudit/logAudit.component.ts
+++ modules/system/public/components/siteAdmin/logAudit/logAudit.component.ts
@@ -1,47 +1,63 @@
 import { Http } from "@angular/http";
-import { Component, Inject } from "@angular/core";
+import { Component, Inject, OnInit } from "@angular/core";
 import "rxjs/add/operator/map";
 
+//import moment = require("moment");
+import * as moment from "moment";
 
 @Component({
     selector: "cde-log-audit",
     templateUrl: "./logAudit.component.html"
 })
 
-export class LogAuditComponent {
-
-    gridLogEvents: any[];
-    gridOptions: any;
-    search: any;
+export class LogAuditComponent implements OnInit {
+    gridLogEvents: any[] = [];
+    search: any = {
+        currentPage: 0
+    };
     table: any;
     totalItems: number;
     itemsPerPage: number;
 
-    constructor(private http: Http, @Inject ("Alert") private Alert) {
-        this.gridLogEvents = [];
-        this.search = {currentPage: 0};
+    constructor(private http: Http,
+                @Inject("Alert") private Alert) {
     }
 
-    searchLogs () {
+    ngOnInit(): void {
+        this.searchLogs();
+    }
+
+    searchLogs() {
         this.gridLogEvents = [];
-        this.search = {currentPage: 0};
-
-        this.http.post("/logs", {query: this.search}).map(res => res.json()).subscribe((res) => {
-            this.totalItems = res.count;
-            this.itemsPerPage = res.itemsPerPage;
-            res.logs.forEach(log => {
-                if (log !== undefined) {
-                    this.gridLogEvents.push({
-                        date: new Date(log.date).toLocaleString()
-                        , ip: log.remoteAddr
-                        , url: log.url
-                        , method: log.method
-                        , status: log.httpStatus
-                        , respTime: log.responseTime
+        let fromDate, toDate;
+        if (this.search.fromDate) {
+            fromDate = moment(this.search.fromDate + moment().format("Z")).toISOString();
+        }
+        if (this.search.toDate) {
+            toDate = moment(this.search.toDate + moment().format("Z")).toISOString();
+        }
+        //noinspection TypeScriptValidateTypes
+        this.http.post("/logs", {
+            query: this.search,
+            fromDate: fromDate,
+            toDate: toDate
+        }).map(res => res.json()).subscribe(
+            res => {
+                this.totalItems = res.count;
+                this.itemsPerPage = res.itemsPerPage;
+                if (res.logs)
+                    res.logs.forEach(log => {
+                        if (log !== undefined) {
+                            this.gridLogEvents.push({
+                                date: new Date(log.date).toLocaleString(),
+                                ip: log.remoteAddr,
+                                url: log.url,
+                                method: log.method,
+                                status: log.httpStatus,
+                                respTime: log.responseTime
+                            });
+                        }
                     });
-                }
             });
-        });
     };
-
 }
