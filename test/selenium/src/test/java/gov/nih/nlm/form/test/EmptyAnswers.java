0b8176fd7a39acaafe18b3a111ad51b2a2477bd3
ludetc <ludetc@mail.nih.gov>
2017-04-12 14:16:40 -0400

    Temp

---
+++ test/selenium/src/test/java/gov/nih/nlm/form/test/EmptyAnswers.java
@@ -0,0 +1,19 @@
+package gov.nih.nlm.form.test;
+
+import gov.nih.nlm.system.NlmCdeBaseTest;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class EmptyAnswers extends NlmCdeBaseTest {
+
+    @Test
+    public void emptyAnswers() {
+        mustBeLoggedInAs(ninds_username, password);
+        goToFormByName("Stroke Symptoms/Comorbid Events");
+        clickElement(By.id("description_tab"));
+        clickElement(By.cssSelector("#question_0_0 .fa-pencil"));
+        clickElement(By.xpath("//*"));
+
+    }
+
+}
