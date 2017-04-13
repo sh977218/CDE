5cfde7503f8865e981c7da447dbaec022de96626
Peter Huang <shi.huang@nih.gov>
2017-04-13 14:50:30 -0400

    x

--- modules/admin/public/components/classification.component.ts
+++ modules/admin/public/components/classification.component.ts
@@ -1,23 +1,28 @@
-import { Component, Input, ViewChild, Inject } from "@angular/core";
+import { Component, Input, ViewChild, Inject, OnInit } from "@angular/core";
 import { NgbModalRef, NgbModal, NgbActiveModal, NgbModalModule } from "@ng-bootstrap/ng-bootstrap";
 @Component({
     selector: "cde-admin-item-classification",
     providers: [NgbActiveModal],
     templateUrl: "./classification.component.html"
 })
-export class ClassificationComponent {
+export class ClassificationComponent implements OnInit {
     @ViewChild("newClassificationContent") public newClassificationContent: NgbModalModule;
     @Input() public elt: any;
+    public myOrgs: any;
     public modalRef: NgbModalRef;
 
     constructor(public modalService: NgbModal,
                 public activeModal: NgbActiveModal,
                 @Inject("Alert") private alert,
+                @Inject("userResource") private userService,
                 @Inject("isAllowedModel") public isAllowedModel,
                 @Inject("OrgHelpers") private orgHelpers) {
-
     }
 
+    ngOnInit(): void {
+        this.myOrgs = this.userService.userOrgs;
+    }
+    
     openClassificationModal() {
         this.modalRef = this.modalService.open(this.newClassificationContent, {size: "lg"});
         this.modalRef.result.then(result => {
