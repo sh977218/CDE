7a4953fe02a86e5b5604bd5e92d4f9a6c2236990
ludetc <ludetc@mail.nih.gov>
2017-04-19 14:07:59 -0400

    fix test

--- test/selenium/src/test/java/gov/nih/nlm/cde/test/attachments/MaliciousAttachment.java
+++ test/selenium/src/test/java/gov/nih/nlm/cde/test/attachments/MaliciousAttachment.java
@@ -11,27 +11,24 @@ public class MaliciousAttachment  extends BaseAttachmentTest {
     private void tryAgain(int ttl) {
         if (ttl == 0) Assert.fail("Fail to recognize virus");
 
+        String cde = "Stasis dilation upper urinary tract indicator";
+        goToCdeByName(cde);
 
         clickElement(By.id("attachments_tab"));
         textPresent("Upload more files");
         ((JavascriptExecutor) driver).executeScript("$(\"input[type='file']\").show();");
         findElement(By.id("fileToUpload")).sendKeys("T:\\CDE\\data\\fakeVirus.txt");
-//        clickElement(By.id("doUploadButton"));
         try {
             textPresent("The file probably contains a virus");
         } catch (Exception e) {
             // this randomly fails every 5 times.
-            driver.get(driver.getCurrentUrl());
             tryAgain(ttl - 1);
         }
     }
 
     @Test
     public void uploadMaliciousAttachment() {
-        String cde = "Stasis dilation upper urinary tract indicator";
         mustBeLoggedInAs(ninds_username, password);
-        goToCdeByName(cde);
-
         tryAgain(3);
     }
 
