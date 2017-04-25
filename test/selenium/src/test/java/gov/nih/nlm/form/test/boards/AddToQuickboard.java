3dfee241480360b8fdc434acd79d677312920262
ludetc <ludetc@mail.nih.gov>
2017-04-25 15:23:17 -0400

    done

---
+++ test/selenium/src/test/java/gov/nih/nlm/form/test/boards/AddToQuickboard.java
@@ -0,0 +1,22 @@
+package gov.nih.nlm.form.test.boards;
+
+import gov.nih.nlm.cde.test.boards.BoardTest;
+import gov.nih.nlm.system.NlmCdeBaseTest;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class AddToQuickboard extends BoardTest {
+
+    @Test
+    public void addToQuickBoard() {
+        mustBeLoggedInAs(testAdmin_username, password);
+        goToBoard("Form Board");
+        textPresent("Quick Board (0)");
+        clickElement(By.id("addToCompare_0"));
+        textPresent("Quick Board (1)");
+        clickElement(By.id("menu_qb_link"));
+        clickElement(By.id("qb_form_tab"));
+        clickElement(By.linkText("cdeCompare1"));
+    }
+
+}
