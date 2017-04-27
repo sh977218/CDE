bfb5f8aefa408fefcdf3947cae4d5740d0db7999
ludetc <ludetc@mail.nih.gov>
2017-04-27 15:26:02 -0400

    script done

--- scripts/fixNciPvs.js
+++ scripts/fixNciPvs.js
@@ -1,5 +1,4 @@
 const mongo_form = require('../modules/form/node-js/mongo-form'),
-    mongo_cde = require('../modules/cde/node-js/mongo-cde'),
     async = require('async')
 ;
 
@@ -8,15 +7,36 @@ let count = 0;
 mongo_form.Form.find({"classification.stewardOrg.name": "NCI", archived: false}, (err, pForms) => {
 
     async.eachSeries(pForms, (pForm, oneFormDone) => {
+        pForm.formElements.forEach(section => {
+           section.formElements.forEach(question => {
+               question = question.question;
+               if (question.answers) {
 
-        async.eacheries(pForms.formElements, (section, oneSectionDone) => {
-           async.eachSeries(section.formElements, (question, oneQuestionDone) => {
+                   question.answers = question.answers.filter((a, pos) => {
+                       return question.answers.findIndex(fa => fa.permissibleValue === a.permissibleValue) === pos;
+                   });
 
-           }, () => oneSectionDone());
-        }, () => {
-            console.log(++count);
-            oneDone();
+                   if (question.cde.permissibleValues) {
+                       question.cde.permissibleValues = question.cde.permissibleValues.filter((a, pos) => {
+                           return question.cde.permissibleValues.findIndex(fa => fa.permissibleValue === a.permissibleValue) === pos;
+                       });
+                   }
+
+                   question.answers.forEach(a => {
+                       let pv = question.cde.permissibleValues.find(e => e.permissibleValue === a.permissibleValue);
+                       if (!pv) {
+                           console.log("No PV for form: " + pForm.tinyId + " and CDE: " + question.cde.tinyId +
+                               " and pv: " + a.permissibleValue);
+                       } else {
+                           a.valueMeaningCode = pv.valueMeaningCode;
+                           a.codeSystemName = undefined;
+                           a.codeSystemVersion = undefined;
+                       }
+                   });
+               }
+           });
         });
+        pForm.save(oneFormDone);
     }, () => {
         console.log("all done");
         process.exit(0);
