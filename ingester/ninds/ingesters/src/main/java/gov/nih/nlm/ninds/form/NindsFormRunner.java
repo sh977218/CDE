e9680ed103cee4fbbe3e3b07d55e68367c1e2fde
Peter Huang <shi.huang@nih.gov>
2017-05-04 09:50:32 -0400

    update loader page

--- ingester/ninds/ingesters/src/main/java/gov/nih/nlm/ninds/form/NindsFormRunner.java
+++ ingester/ninds/ingesters/src/main/java/gov/nih/nlm/ninds/form/NindsFormRunner.java
@@ -8,8 +8,8 @@ import java.util.concurrent.Executors;
 public class NindsFormRunner {
     public static void main(String[] args) throws IOException, AWTException {
         int nbOfThread = 1;
-        int startingPage = 18;
-        int endingPages = 18;
+        int startingPage = 19;
+        int endingPages = 19;
 //        int endingPages = Constants.TOTAL_PAGE;
 
         ExecutorService executor1 = Executors.newFixedThreadPool(nbOfThread);
