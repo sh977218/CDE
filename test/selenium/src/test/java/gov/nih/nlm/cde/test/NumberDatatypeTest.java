fdb42069118e5986aec9a251a5d1831d14f4bc57
Peter Huang <shi.huang@nih.gov>
2017-05-19 16:58:48 -0400

    fix numberDatatypeTest

--- test/selenium/src/test/java/gov/nih/nlm/cde/test/NumberDatatypeTest.java
+++ test/selenium/src/test/java/gov/nih/nlm/cde/test/NumberDatatypeTest.java
@@ -2,7 +2,6 @@ package gov.nih.nlm.cde.test;
 
 import gov.nih.nlm.system.NlmCdeBaseTest;
 import org.openqa.selenium.By;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 
@@ -13,19 +12,18 @@ public class NumberDatatypeTest extends NlmCdeBaseTest {
         String cdeName = "Resource Utilization Group Version IV (RUG IV) - alpha-numeric code";
         goToCdeByName(cdeName);
         clickElement(By.id("pvs_tab"));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("editDatatype")));
-        clickElement(By.id("editDatatype"));
-        new Select(findElement(By.id("valueTypeSelect"))).selectByVisibleText("Number");
-        clickElement(By.id("confirmDatatype"));
+        clickElement(By.xpath("//*[@id='datatypeSelect']//i"));
+        new Select(findElement(By.xpath("//*[@id='datatypeSelect']//select"))).selectByVisibleText("Number");
+        clickElement(By.xpath("//*[@id='datatypeSelect']//button[contains(@class,'fa fa-check')]"));
 
-        clickElement(By.xpath("//div[@id='numberMinValue']//i[@title='Edit']"));
-        clickElement(By.xpath("//div[@id='numberMaxValue']//i[@title='Edit']"));
-        findElement(By.xpath("//div[@id='numberMinValue']//input")).sendKeys("123");
-        findElement(By.xpath("//div[@id='numberMaxValue']//input")).sendKeys("456");
-        clickElement(By.cssSelector("#numberMinValue .fa-check"));
-        clickElement(By.cssSelector("#numberMaxValue .fa-check"));
+        clickElement(By.xpath("//*[@id='datatypeNumberMin']//i[contains(@class,'fa fa-edit')]"));
+        findElement(By.xpath("//*[@id='datatypeNumberMin']//input")).sendKeys("123");
+        clickElement(By.xpath("//*[@id='datatypeNumberMin']//button[contains(@class,'fa fa-check')]"));
+
+        clickElement(By.xpath("//*[@id='datatypeNumberMax']//i[contains(@class,'fa fa-edit')]"));
+        findElement(By.xpath("//*[@id='datatypeNumberMax']//input")).sendKeys("456");
+        clickElement(By.xpath("//*[@id='datatypeNumberMax']//button[contains(@class,'fa fa-check')]"));
         newCdeVersion();
-
 
         clickElement(By.id("history_tab"));
         selectHistoryAndCompare(1, 2);
@@ -34,14 +32,14 @@ public class NumberDatatypeTest extends NlmCdeBaseTest {
         textPresent("Number", By.xpath("//*[@id='historyCompareLeft_Value Type']"));
 
         clickElement(By.id("pvs_tab"));
-        clickElement(By.xpath("//div[@id='numberMinValue']//i[@title='Edit']"));
-        clickElement(By.xpath("//div[@id='numberMaxValue']//i[@title='Edit']"));
-        findElement(By.xpath("//div[@id='numberMinValue']//input")).clear();
-        findElement(By.xpath("//div[@id='numberMaxValue']//input")).clear();
-        findElement(By.xpath("//div[@id='numberMinValue']//input")).sendKeys("789");
-        findElement(By.xpath("//div[@id='numberMaxValue']//input")).sendKeys("987");
-        clickElement(By.cssSelector("#numberMinValue .fa-check"));
-        clickElement(By.cssSelector("#numberMaxValue .fa-check"));
+        clickElement(By.xpath("//*[@id='datatypeNumberMin']//i[contains(@class,'fa fa-edit')]"));
+        findElement(By.xpath("//*[@id='datatypeNumberMin']//input")).sendKeys("789");
+        clickElement(By.xpath("//*[@id='datatypeNumberMin']//button[contains(@class,'fa fa-check')]"));
+
+        clickElement(By.xpath("//*[@id='datatypeNumberMax']//i[contains(@class,'fa fa-edit')]"));
+        findElement(By.xpath("//*[@id='datatypeNumberMax']//input")).sendKeys("987");
+        clickElement(By.xpath("//*[@id='datatypeNumberMax']//button[contains(@class,'fa fa-check')]"));
+
         newCdeVersion();
 
         goToCdeByName(cdeName);
