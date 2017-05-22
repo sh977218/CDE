d32c8540cedee91864dde14498496bfb9878f716
Peter Huang <shi.huang@nih.gov>
2017-05-22 16:01:39 -0400

    fix vmDefinitions

--- test/selenium/src/test/java/gov/nih/nlm/cde/test/permissibleValue/VMDefinitionTest.java
+++ test/selenium/src/test/java/gov/nih/nlm/cde/test/permissibleValue/VMDefinitionTest.java
@@ -4,7 +4,7 @@ import gov.nih.nlm.system.NlmCdeBaseTest;
 import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
-public class VMDefinition extends NlmCdeBaseTest {
+public class VMDefinitionTest extends NlmCdeBaseTest {
 
     @Test
     public void vmDefinitions() {
