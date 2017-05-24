50c8c05a7eec2121cbe6916558406823bc277089
Peter Huang <shi.huang@nih.gov>
2017-05-24 15:24:26 -0400

    add tests

---
+++ test/selenium/src/test/java/gov/nih/nlm/cde/test/classification/DeleteRootClassificationTest.java
@@ -0,0 +1,19 @@
+package gov.nih.nlm.cde.test.classification;
+
+import gov.nih.nlm.cde.test.BaseClassificationTest;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class DeleteRootClassificationTest extends BaseClassificationTest {
+
+    @Test
+    public void deleteRootClassification() {
+        String formName = "PROMIS SF v2.0 - Companionship 6a";
+        mustBeLoggedInAs(nlm_username, nlm_password);
+        goToFormByName(formName);
+        clickElement(By.id("classification_tab"));
+        textPresent("PROMIS / Neuro-QOL");
+        clickElement(By.xpath("//*[@id='PROMIS Instruments-unclassifyBtn']"));
+        textNotPresent("PROMIS / Neuro-QOL");
+    }
+}
