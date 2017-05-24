e609019634b0270bf80f52b72f3320b7edfb1554
Peter Huang <shi.huang@nih.gov>
2017-05-24 13:47:54 -0400

    add migration PhenX_changed schema

--- ingester/createMigrationConnection.js
+++ ingester/createMigrationConnection.js
@@ -99,8 +99,13 @@ exports.MigrationRedcapModel = migrationConn.model('MigrationRedcapModel', new S
     strict: false,
     collection: 'Redcap'
 }));
+exports.MigrationPhenXChanged = migrationConn.model('PhenX_changed', new Schema({}, {
+    strict: false,
+    collection: 'PhenX_changed'
+}));
 exports.PhenxURL = "https://www.phenxtoolkit.org/index.php?pageLink=browse.measures&tree=off";
 exports.PHENX_ZIP_BASE_FOLDER = 's:/MLB/CDE/phenx/www.phenxtoolkit.org/toolkit_content/redcap_zip/all';
+
 
 // MIGRATION REFERENCE COLLECTION
 exports.MigrationPhenxToLoincMappingModel = migrationConn.model('MigrationPhenxToLoincMapping', new Schema({}, {
