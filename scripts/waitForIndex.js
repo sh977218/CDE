e66aa34f86e45ad3857e893e17b00be01b01273b
Ludet <ludetc@nih.gov>
2017-06-12 14:33:02 -0400

    fix prot test

--- scripts/waitForIndex.js
+++ scripts/waitForIndex.js
@@ -4,7 +4,8 @@ const request = require('request');
 setInterval(() => {
     console.log("Waiting for indexing to be complete");
     request.get("http://localhost:3001/status/cde", (err, res, body) => {
-       if (body.indexOf("ERROR") === -1 && body.indexOf("false") === -1 && body.indexOf("indices") > 0) {
+       if (body.indexOf("ERROR") === -1 && body.indexOf("false") === -1 &&
+           body.indexOf('indices":[]') === -1 && body.indexOf('indices') > 0) {
             console.log("indexing complete, status returned: ");
             console.log(body);
            process.exit(0);
--- test/protractor/tests/testUtils.js
+++ test/protractor/tests/testUtils.js
@@ -14,6 +14,7 @@ module.exports = {
         element(by.id("passwd")).clear();
         element(by.id("passwd")).sendKeys(password);
         element(by.id("login_button")).click();
+        expect(element(by.id("login_link")).getText()).toContain(usernameStr);
     },
 
     mustBeLoggedInAs: function (username, password) {
