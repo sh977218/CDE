1098b0acd67b307bebad6772fb7bad876b09d9e7
Peter Huang <shi.huang@nih.gov>
2017-06-12 16:43:20 -0400

    keep fixing tests

--- test/protractor/tests/formComments.spec.js
+++ test/protractor/tests/formComments.spec.js
@@ -2,9 +2,10 @@ const utils = require('./testUtils');
 
 describe('Form Comments', () => {
 
-    it ('should blah blah', () => {
+    it('should blah blah', done => {
         browser.get("");
         utils.mustBeLoggedInAs("nlm", "nlm");
+        done();
     });
 
 
--- test/selenium/src/test/java/gov/nih/nlm/form/test/FormHistoryCompareTest.java
+++ test/selenium/src/test/java/gov/nih/nlm/form/test/FormHistoryCompareTest.java
@@ -15,21 +15,12 @@ public class FormHistoryCompareTest extends BaseFormTest {
         clickElement(By.id("history_tab"));
         textPresent("List of previous versions");
         Assert.assertEquals(4, driver.findElements(By.xpath("//*[@id='historyTable']/tbody/tr[td]")).size());
-
         selectHistoryAndCompare(2, 3);
         textPresent("\"Neoadjuvant Therapy\" = \"Yes (specify type)\"", By.xpath("//*[@id='Form Elements_3']//ins"));
+        clickElement(By.id("closeHistoryCompareModal"));
 
         clickElement(By.xpath("//*[@id='historyTable']/tbody/tr[2]"));
         selectHistoryAndCompare(1, 2);
-
-        String boarderCss = findElement(By.xpath("//*[@id='Form Elements_2']")).getCssValue("border-left");
-        Assert.assertTrue(boarderCss.contains("#fad000"));
-    }
-
-    protected void selectHistoryAndCompare(Integer leftIndex, Integer rightIndex) {
-        clickElement(By.xpath("//*[@id='historyTable']/tbody/tr[" + leftIndex + "]"));
-        clickElement(By.xpath("//*[@id='historyTable']/tbody/tr[" + rightIndex + "]"));
-        clickElement(By.id("historyCompareBtn"));
-        textPresent("Changes");
+        textPresent("\"Neoadjuvant Therapy\" = \"Yes (specify type)\"", By.xpath("//*[@id='Form Elements_2']//div[contains(@class,'arrayObjReorder')]"));
     }
 }
