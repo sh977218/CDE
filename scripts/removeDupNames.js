bbef3be60c780277712441ca8826fecf9e56cf25
Ludet <ludetc@nih.gov>
2017-06-21 09:34:43 -0400

    temp

--- scripts/removeDupNames.js
+++ scripts/removeDupNames.js
@@ -1,4 +1,4 @@
-const mongo_cde = require('../modules/cde/node-js/mongo-cde')
+const mongo_cde = require('../modules/cde/node-js/mongo-cde'),
     _ = require('lodash')
 ;
 
@@ -7,22 +7,27 @@ let stream = mongo_cde.getStream({archived: false,
     source: 'NINDS'
 });
 
-stream.on('data', (cde) => {
+let count = 0;
+
+stream.on('data', cde => {
+    if (count++ % 100 === 0) console.log(count);
     stream.pause();
 
     let changed = false;
 
-    let flatNames = cde.naming.map(n => n.designation + "<<==>>" + n.definition);
+    let flatNames = cde.naming.map(n => n.designation + "<<==>>" + (n.definition?n.definition:""));
 
     flatNames.forEach((fn, i) => {
         let changed = false;
         if (flatNames.indexOf(fn) !== i) {
+            // console.log("found dup: " + fn);
             let dupInd = flatNames.indexOf(fn);
             let mergeTags = false;
             if (!cde.naming[dupInd].source || cde.naming[dupInd].source === "") {
                 cde.naming[dupInd].source = cde.naming[i].source;
                 mergeTags = true
-            } else if (!cde.naming[i].source || cde.naming[i].source === "" || cde.naming[i].source === cde.naming[dupInd].source) {
+            } else if (!cde.naming[i].source || cde.naming[i].source === ""
+                || cde.naming[i].source === cde.naming[dupInd].source) {
                 mergeTags = true;
             }
 
@@ -35,8 +40,9 @@ stream.on('data', (cde) => {
     });
 
     if (changed) {
+        console.log("CDE: " + cde.naming[0].designation);
         cde.naming = cde.naming.filter(n => n !== "");
-        cde.save(() => stream.resume());
+        cde.save(stream.resume);
     } else {
         stream.resume();
     }
