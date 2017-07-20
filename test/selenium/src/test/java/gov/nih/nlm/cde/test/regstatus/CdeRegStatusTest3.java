78a02b2ff41b2c1d6f0ecd1d6e6ee658edc74655
Peter Huang <shi.huang@nih.gov>
2017-07-20 13:41:51 -0400

    add tests

--- test/selenium/src/test/java/gov/nih/nlm/cde/test/regstatus/CdeRegStatusTest3.java
+++ test/selenium/src/test/java/gov/nih/nlm/cde/test/regstatus/CdeRegStatusTest3.java
@@ -3,11 +3,6 @@ package gov.nih.nlm.cde.test.regstatus;
 import org.testng.annotations.Test;
 
 public class CdeRegStatusTest3 extends CdeRegStatusTest {
-
-    @Test
-    public void cancelRegStatus() {
-        cancelRegStatus("Form Form Element Administered Item Modified By java.lang.String", cabigAdmin_username);
-    }   
     
     @Test
     public void cantEditStatusIfPendingChanges() {
