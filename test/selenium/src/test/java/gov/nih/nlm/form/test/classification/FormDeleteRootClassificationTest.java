07810b24ef9deeb8d2b9ec3f62b95c55c49743f7
Peter Huang <shi.huang@nih.gov>
2017-07-21 16:10:44 -0400

    add some classification tests

---
+++ test/selenium/src/test/java/gov/nih/nlm/form/test/classification/FormDeleteRootClassificationTest.java
@@ -0,0 +1,20 @@
+package gov.nih.nlm.form.test.classification;
+
+import gov.nih.nlm.system.NlmCdeBaseTest;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class FormDeleteRootClassificationTest extends NlmCdeBaseTest {
+
+    @Test
+    public void formDeleteRootClassification() {
+        String formName = "PROMIS SF v2.0 - Companionship 6a";
+        mustBeLoggedInAs(nlm_username, nlm_password);
+        goToFormByName(formName);
+        clickElement(By.id("classification_tab"));
+        textPresent("PROMIS / Neuro-QOL");
+        clickElement(By.xpath("//*[@id='PROMIS Instruments-unclassifyBtn']"));
+        clickElement(By.id("confirmDeleteClassificationBtn"));
+        textNotPresent("PROMIS / Neuro-QOL");
+    }
+}
