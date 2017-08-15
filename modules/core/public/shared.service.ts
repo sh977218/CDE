fb057594861d82aa099a05247618a9c1465d55f0
Peter Huang <shi.huang@nih.gov>
2017-08-15 14:02:29 -0400

    removed some ng1 compare js & htmlj

--- modules/core/public/shared.service.ts
+++ modules/core/public/shared.service.ts
@@ -1,6 +1,5 @@
 import * as authorizationShared from "system/shared/authorizationShared";
 import * as classificationShared from "system/shared/classificationShared";
-import * as compareShared from "system/shared/compareShared";
 import * as formShared from "form/shared/formShared";
 import * as exportShared from "system/shared/exportShared";
 import * as regStatusShared from "system/shared/regStatusShared";
@@ -8,7 +7,6 @@ import * as regStatusShared from "system/shared/regStatusShared";
 export class SharedService {
     static readonly auth = authorizationShared;
     static readonly classificationShared = classificationShared;
-    static readonly compareShared = compareShared;
     static readonly exportShared = exportShared;
     static readonly formShared = formShared;
     static readonly regStatusShared = regStatusShared;
