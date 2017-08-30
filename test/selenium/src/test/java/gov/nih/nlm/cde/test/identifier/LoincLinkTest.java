085d1388a6a198f484256b4bac5b45e057135b41
Peter Huang <shi.huang@nih.gov>
2017-08-30 13:43:30 -0400

    done

--- test/selenium/src/test/java/gov/nih/nlm/cde/test/identifier/LoincLinkTest.java
+++ test/selenium/src/test/java/gov/nih/nlm/cde/test/identifier/LoincLinkTest.java
@@ -14,6 +14,7 @@ public class LoincLinkTest extends NlmCdeBaseTest {
         findElement(By.name("source")).sendKeys("LOINC");
         findElement(By.name("id")).sendKeys("59362-4");
         clickElement(By.id("createNewIdentifierBtn"));
+        textPresent("saved.");
         closeAlert();
         findElement(By.linkText("59362-4"));
     }
