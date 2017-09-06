6fc9b7c656452f8dc57d6e6a554aab8659fc6d65
Ludet <ludetc@nih.gov>
2017-09-06 09:15:22 -0400

    fix test

--- test/selenium/src/test/java/gov/nih/nlm/cde/test/OrgAdminTasks.java
+++ test/selenium/src/test/java/gov/nih/nlm/cde/test/OrgAdminTasks.java
@@ -43,7 +43,7 @@ public class OrgAdminTasks extends BaseClassificationTest {
         new Select(findElement(By.id("newOrgAdminOrgName"))).selectByVisibleText("caBIG");
         findElement(By.id("newOrgAdminUsername")).sendKeys("userToPromote");
         findElement(By.id("newOrgAdminSubmit")).click();
-        textPresent("Saved");
+        textPresent("Removed");
         closeAlert();
         textPresent("userToPromote");
         orgLength = driver.findElements(By.xpath("//td[starts-with(@id, 'existingOrgAdminOrgName-')]")).size();
@@ -59,7 +59,7 @@ public class OrgAdminTasks extends BaseClassificationTest {
                 }
             }
         }
-        textPresent("Saved");
+        textPresent("Removed");
         textNotPresent("userToPromote");
     }
     
