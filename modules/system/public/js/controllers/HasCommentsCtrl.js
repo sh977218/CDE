9851cc1a7b41c52375afe4f2b3a4d1ab852bf68b
Ludet <ludetc@nih.gov>
2017-09-06 10:56:57 -0400

    remove unused ctrls

--- modules/system/public/js/controllers/HasCommentsCtrl.js
+++
@@ -1,8 +0,0 @@
-angular.module('systemModule').controller('HasCommentsCtrl', ['$scope', '$http', function ($scope, $http) {
-
-    $scope.deferredEltLoaded.promise.then(function() {
-        $http.get('/comments/eltId/' + $scope.getEltId()).then(function onSuccess(response) {
-            $scope.hasComments = response.data.length > 0;
-        }).catch(function onError() {});
-    });
-}]);
