932c1684c1dc08e89bb4383b6aeb16e464f69fc9
Peter Huang <shi.huang@nih.gov>
2017-09-11 16:48:14 -0400

    removed import

--- test/selenium/src/test/java/gov/nih/nlm/form/test/description/FormCopySectionTest.java
+++ test/selenium/src/test/java/gov/nih/nlm/form/test/description/FormCopySectionTest.java
@@ -3,13 +3,9 @@ package gov.nih.nlm.form.test.description;
 import gov.nih.nlm.form.test.BaseFormTest;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
-
-import java.awt.*;
-import java.awt.event.KeyEvent;
 
 public class FormCopySectionTest extends BaseFormTest {
     @Test
