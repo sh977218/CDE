13d626be84bcc2ee82d74ac8063eb1d9124892e2
Peter Huang <shi.huang@nih.gov>
2017-09-11 14:56:15 -0400

    update bson

--- test/selenium/src/test/java/gov/nih/nlm/form/test/description/FormCopySectionTest.java
+++ test/selenium/src/test/java/gov/nih/nlm/form/test/description/FormCopySectionTest.java
@@ -3,6 +3,7 @@ package gov.nih.nlm.form.test.description;
 import gov.nih.nlm.form.test.BaseFormTest;
 import gov.nih.nlm.system.NlmCdeBaseTest;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
@@ -11,11 +12,13 @@ public class FormCopySectionTest extends BaseFormTest {
     @Test
     public void formCopySection() {
         String form1 = "Loinc Widget Test Form";
-        String form2 = "Section Test Form";
+        String form2 = "Copy Section Form Test";
         mustBeLoggedInAs(nlm_username, nlm_username);
         goToFormByName(form1);
         clickElement(By.id("description_tab"));
-        clickElement(By.xpath("//*[@id='section_1']//*[contains(@class,'copySection')]"));
+        clickElement(By.xpath("//*[@id='section_1']//i[contains(@class,'copySection')]"));
+        findElement(By.cssSelector("body")).sendKeys(Keys.CONTROL + "t");
+        hangon(5);
         switchTab(1);
         goToFormByName(form2);
         clickElement(By.id("description_tab"));
