8f817333b5d3f08a70bf1f5646fdde0b4c929ca6
Ludet <ludetc@nih.gov>
2017-10-12 14:52:53 -0400

    temp

--- modules/cde/public/components/statusReport/cdeStatusReport.component.ts
+++ modules/cde/public/components/statusReport/cdeStatusReport.component.ts
@@ -1,7 +1,7 @@
-import { Component, Inject, Input, OnInit } from "@angular/core";
-import { ExportService } from "../../../../core/public/export.service";
-import { OrgHelperService } from "../../../../core/public/orgHelper.service";
-import { UserService } from "../../../../core/public/user.service";
+import { Component, OnInit } from "@angular/core";
+import { ExportService } from "core/public/export.service";
+import { OrgHelperService } from "core/public/orgHelper.service";
+import { UserService } from "core/public/user.service";
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
