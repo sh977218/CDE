ecbafb3295e9e73db49f51d2821c645ffd6ceb9e
Ludet <ludetc@nih.gov>
2017-10-16 09:33:59 -0400

    add test and fix build

--- modules/main.ts
+++ modules/main.ts
@@ -4,12 +4,12 @@ import "../node_modules/zone.js/dist/long-stack-trace-zone.js";
 import "reflect-metadata";
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 
-import { CdeAppModule } from '_app/app.module';
-
 // required for ng2-select2
 require('expose-loader?select2!select2');
 
 import { enableProdMode } from "@angular/core";
+import { CdeAppModule } from '_app/app.module';
+
 if (PRODUCTION)
     enableProdMode();
 
