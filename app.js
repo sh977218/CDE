c7499b6864e09b431043bb9bc2615f0877a5722c
Ludet <ludetc@nih.gov>
2017-10-16 15:42:59 -0400

    fix 404 / swagger

--- app.js
+++ app.js
@@ -219,7 +219,7 @@ app.use('/robots.txt', express.static(path.join(__dirname, '/modules/system/publ
 // final route -> 404
 app.use((req, res, next) => {
     // swagger does something i dont get. This will let swagger work
-    if (req.originalUrl === "/docs" || req.originalUrl.indexOf("/docs/" === 0)) {
+    if (req.originalUrl === "/docs" || req.originalUrl === "/api-docs" || req.originalUrl.indexOf("/docs/") === 0) {
         return next();
     }
     res.render('index', 'system', {config: config, loggedIn: !!req.user, version: 'version'});
