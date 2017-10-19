17c2f82401417a251dae2242a89a9c87557406fa
Peter Huang <shi.huang@nih.gov>
2017-10-19 11:04:39 -0400

    fix more tests

--- test/selenium/src/test/java/gov/nih/nlm/form/test/InfiniteScrollDescription.java
+++ test/selenium/src/test/java/gov/nih/nlm/form/test/InfiniteScrollDescription.java
@@ -10,20 +10,10 @@ public class InfiniteScrollDescription extends NlmCdeBaseTest {
     public void infiniteScrollDescription() {
         mustBeLoggedInAs("ninds", password);
         goToFormByName("Food Frequency Questionnaire (FFQ)");
+        goToGeneralDetail();
         textPresent("The Food Frequency Questionnaire (FFQ) consists of a list of foods with little descriptive detail, and the respondent answers questions about the frequency of each food on the list.");
         goToFormDescription();
         textPresent("Do you currently take multi-vitamins?(Please report other individual vitamins in question 2)");
         textPresent("Not counting multi-vitamins, do you currently take Calcium or Dolomite (including Tums)?");
-//        textNotPresent("Not counting multi-vitamins, do you currently take Calcium or Dolomite (including Tums)?");
-//
-//        try {
-//            scrollContainerDownBy(findElement(By.tagName("tree-viewport")), 500);
-//            textPresent("Not counting multi-vitamins, do you currently take Calcium or Dolomite (including Tums)?");
-//        } catch (Exception e) {
-//            for (int i = 0; i < 10; i++) {
-//                scrollContainerDownBy(findElement(By.tagName("tree-viewport")), 500);
-//            }
-//            textPresent("Not counting multi-vitamins, do you currently take Calcium or Dolomite (including Tums)?");
-//        }
     }
 }
