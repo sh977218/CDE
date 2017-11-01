0200024fae35912037faa8629ebf7cef938a62c8
Peter Huang <shi.huang@nih.gov>
2017-11-01 16:50:41 -0400

    tmp

--- modules/form/public/tabs/description/formDescriptionQuestion.component.ts
+++ modules/form/public/tabs/description/formDescriptionQuestion.component.ts
@@ -27,7 +27,8 @@ export class FormDescriptionQuestionComponent implements OnInit {
 
     constructor(public formService: FormService,
                 private http: Http,
-                public modalService: NgbModal) {}
+                public modalService: NgbModal) {
+    }
 
     ngOnInit() {
         this.question = this.node.data;
@@ -56,79 +57,79 @@ export class FormDescriptionQuestionComponent implements OnInit {
 
     openUpdateCdeVersion(question) {
         this.http.get('/de/' + question.question.cde.tinyId).map((res: Response) => res.json())
-            .subscribe((response) => {
-                this.formService.convertCdeToQuestion(response, (newQuestion) => {
-                    this.updateCdeVersion = (() => {
-                        let modal: any = {
-                            currentQuestion: question,
-                            newQuestion: newQuestion
-                        };
-                        let currentQuestion = question;
-
-                        newQuestion.question.editable = currentQuestion.question.editable;
-                        newQuestion.question.instructions = currentQuestion.question.instructions;
-                        newQuestion.question.invisible = currentQuestion.question.invisible;
-                        newQuestion.question.multiselect = currentQuestion.question.multiselect;
-                        newQuestion.question.required = currentQuestion.question.required;
-                        newQuestion.question.skipLogic = currentQuestion.question.skipLogic;
-                        newQuestion.repeat = currentQuestion.repeat;
-
-                        this.http.get("/de/" + newQuestion.question.cde.tinyId).map((res: Response) => res.json())
-                            .subscribe((newCde) => {
-                                let cdeUrl = currentQuestion.question.cde.tinyId +
-                                    (currentQuestion.question.cde.version ? "/" + currentQuestion.question.cde.version : "");
-                                this.http.get("/de/" + cdeUrl).map((res: Response) => res.json())
-                                    .subscribe((oldCde) => {
-                                        modal.bLabel = !_.isEqual(newCde.naming, oldCde.naming);
-                                    });
-                                let found = false;
-                                newCde.naming.forEach(function (result) {
-                                    if (result.designation === currentQuestion.label) found = true;
-                                });
-                                if (found) newQuestion.label = currentQuestion.label;
-                            });
-
-                        modal.bCde = true;
-                        modal.bDatatype = currentQuestion.question.datatype !== newQuestion.question.datatype;
-                        modal.bUom = !_.isEqual(currentQuestion.question.uoms, newQuestion.question.uoms);
-
-                        if (newQuestion.question.datatype === "Number") {
-                            if (currentQuestion.question.datatype === "Number" &&
-                                currentQuestion.question.datatypeNumber &&
-                                newQuestion.question.datatypeNumber) {
-                                modal.bNumberMin = currentQuestion.question.datatypeNumber.minValue
-                                    !== newQuestion.question.datatypeNumber.minValue;
-                                modal.bNumberMax = currentQuestion.question.datatypeNumber.maxValue
-                                    !== newQuestion.question.datatypeNumber.maxValue;
-                            } else {
-                                modal.bNumberMin = modal.bNumberMax = true;
-                            }
-                        }
-                        if (newQuestion.question.datatype === "Value List") {
-                            if (currentQuestion.question.datatype === "Value List") {
-                                modal.bValuelist = !_.isEqual(currentQuestion.question.cde.permissibleValues,
-                                    newQuestion.question.cde.permissibleValues);
-                                if (!modal.bValuelist) newQuestion.question.answers = currentQuestion.question.answers;
-
-                                if (currentQuestion.question.defaultAnswer && newQuestion.question.answers.filter(a => a.permissibleValue === currentQuestion.question.defaultAnswer).length > 0)
-                                    newQuestion.question.defaultAnswer = currentQuestion.question.defaultAnswer;
-                            } else {
-                                modal.bValuelist = true;
-                            }
-                        }
-                        modal.bDefault = _.toString(currentQuestion.question.defaultAnswer) !== _.toString(newQuestion.question.defaultAnswer);
-
-                        return modal;
-                    })();
-
-                    let self = this;
-
-                    this.modalService.open(this.updateCdeVersionTmpl).result.then(function () {
-                        question.question = newQuestion.question;
-                        question.label = newQuestion.label;
-                        self.stageElt.emit();
+            .subscribe(response => {
+                this.formService.convertCdeToQuestion(response, newQuestion => {
+                    this.updateCdeVersionFn(newQuestion, question, () => {
+                        this.modalService.open(this.updateCdeVersionTmpl).result.then(() => {
+                            question.question = newQuestion.question;
+                            question.label = newQuestion.label;
+                            this.stageElt.emit();
+                        }, () => {
+                        });
                     });
                 });
             });
     }
+
+    updateCdeVersionFn(newQuestion, question, cb) {
+        let modal: any = {
+            currentQuestion: question,
+            newQuestion: newQuestion
+        };
+        let currentQuestion = question;
+
+        newQuestion.question.editable = currentQuestion.question.editable;
+        newQuestion.question.instructions = currentQuestion.question.instructions;
+        newQuestion.question.invisible = currentQuestion.question.invisible;
+        newQuestion.question.multiselect = currentQuestion.question.multiselect;
+        newQuestion.question.required = currentQuestion.question.required;
+        newQuestion.question.skipLogic = currentQuestion.question.skipLogic;
+        newQuestion.repeat = currentQuestion.repeat;
+
+        modal.bCde = true;
+        modal.bDatatype = currentQuestion.question.datatype !== newQuestion.question.datatype;
+        modal.bUom = !_.isEqual(currentQuestion.question.uoms, newQuestion.question.uoms);
+
+        if (newQuestion.question.datatype === "Number") {
+            if (currentQuestion.question.datatype === "Number" &&
+                currentQuestion.question.datatypeNumber &&
+                newQuestion.question.datatypeNumber) {
+                modal.bNumberMin = currentQuestion.question.datatypeNumber.minValue
+                    !== newQuestion.question.datatypeNumber.minValue;
+                modal.bNumberMax = currentQuestion.question.datatypeNumber.maxValue
+                    !== newQuestion.question.datatypeNumber.maxValue;
+            } else {
+                modal.bNumberMin = modal.bNumberMax = true;
+            }
+        }
+        if (newQuestion.question.datatype === "Value List") {
+            if (currentQuestion.question.datatype === "Value List") {
+                modal.bValuelist = !_.isEqual(currentQuestion.question.cde.permissibleValues,
+                    newQuestion.question.cde.permissibleValues);
+                if (!modal.bValuelist) newQuestion.question.answers = currentQuestion.question.answers;
+
+                if (currentQuestion.question.defaultAnswer && newQuestion.question.answers.filter(a => a.permissibleValue === currentQuestion.question.defaultAnswer).length > 0)
+                    newQuestion.question.defaultAnswer = currentQuestion.question.defaultAnswer;
+            } else {
+                modal.bValuelist = true;
+            }
+        }
+        modal.bDefault = _.toString(currentQuestion.question.defaultAnswer) !== _.toString(newQuestion.question.defaultAnswer);
+
+        this.http.get("/de/" + newQuestion.question.cde.tinyId).map((res: Response) => res.json())
+            .subscribe(newCde => {
+                let cdeUrl = currentQuestion.question.cde.tinyId +
+                    (currentQuestion.question.cde.version ? "/" + currentQuestion.question.cde.version : "");
+                this.http.get("/de/" + cdeUrl).map((res: Response) => res.json())
+                    .subscribe((oldCde) => {
+                        modal.bLabel = !_.isEqual(newCde.naming, oldCde.naming);
+                        cb();
+                    });
+                let found = false;
+                newCde.naming.forEach(result => {
+                    if (result.designation === currentQuestion.label) found = true;
+                });
+                if (found) newQuestion.label = currentQuestion.label;
+            });
+    };
 }
