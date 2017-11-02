866a588f32952b569a6aadc8988ac0d7ec36196e
Peter Huang <shi.huang@nih.gov>
2017-11-02 17:32:32 -0400

    fix tests

--- test/selenium/src/test/java/gov/nih/nlm/form/test/displayProfile/DisplayProfilesTest.java
+++ test/selenium/src/test/java/gov/nih/nlm/form/test/displayProfile/DisplayProfilesTest.java
@@ -14,6 +14,30 @@ public class DisplayProfilesTest extends BaseFormTest {
         String formName = "PROMIS SF v1.1 - Anger 5a";
         mustBeLoggedInAs(nlm_username, nlm_password);
         goToFormByName(formName);
+        goToFormDescription();
+        clickElement(By.xpath("//div[@id='question_0_3']//i[contains(@class,'fa-pencil')]"));
+        clickElement(By.xpath("//div[@id='question_0_3']//div[text()='Invisible:']/following-sibling::div/input"));
+
+        clickElement(By.id("displayProfiles_tab"));
+        createDisplayProfile(0, "Matrix and Values", true, true, true, true, "Follow-up", 1, false);
+        createDisplayProfile(1, "Matrix No Values", true, false, false, false, "Dynamic", 6, true);
+        createDisplayProfile(2, "No Matrix No Values", false, false, false, false, "Follow-up", 1, false);
+        createDisplayProfile(3, "No Matrix No Values Wider", false, false, false, false, "Follow-up", 5, false);
+        scrollToTop();
+        Assert.assertEquals(driver.findElements(By.xpath("//div[@id='profile_0']//table//input[@type='radio']")).size(), 10);
+        textPresent("1", By.xpath("//div[@id='profile_0']//table/tbody/tr[1]/td[6]/span"));
+        Assert.assertTrue(
+                findElement(By.xpath("//div[@id='profile_2']//div[@id='Education level USA type_1']//label/span[text()='1st Grade']")).getLocation().y + 8 <
+                        findElement(By.xpath("//div[@id='profile_2']//div[@id='Education level USA type_1']//label/span[text()='2nd Grade']")).getLocation().y
+        );
+        scrollToViewById("profile_3");
+        Assert.assertEquals(
+                findElement(By.xpath("//*[@id='profile_3']//*[*[text()='Education level USA type']]//*/span[text()='1st Grade']")).getLocation().y,
+                findElement(By.xpath("//*[@id='profile_3']//*[*[text()='Education level USA type']]//*/span[text()='5th Grade']")).getLocation().y
+        );
+        newFormVersion();
+
+        goToFormByName(formName);
         textPresent("In the past 7 days");
         textPresent("I felt annoyed");
         textPresent("1", By.xpath("//div[@id='formRenderSection_In the past 7 days']//table/tbody/tr[1]/td[2]"));
@@ -29,6 +53,29 @@ public class DisplayProfilesTest extends BaseFormTest {
         textNotPresent("1", By.xpath("//table"));
         textPresent("I was grouchy");
 
+        selectDisplayProfileByName("No Matrix No Values");
+        hangon(1);
+        assertNoElt(By.xpath("//div[@id='formRenderSection_In the past 7 days']//table//input[@type='radio']"));
+        assertNoElt(By.xpath("//select[@ng-model='question.question.answer']"));
+        Assert.assertTrue(findElement(By.xpath("//div[@id='I was irritated more than people knew_0']//*/span[text()[contains(., 'Never')]]")).getLocation().y + 8 <
+                findElement(By.xpath("//div[@id='I was irritated more than people knew_0']//*/span[text()[contains(., 'Rarely')]]")).getLocation().y
+        );
+
+        selectDisplayProfileByName("No Matrix No Values Wider");
+        hangon(1);
+        Assert.assertEquals(findElement(By.xpath("//*[*[text()='I was irritated more than people knew']]//*/span[text()='Never']")).getLocation().y,
+                findElement(By.xpath("//*[*[text()='I was irritated more than people knew']]//*/span[text()='Always']")).getLocation().y
+        );
+        clickElement(By.id("displayProfiles_tab"));
+
+        for (int i = 0; i < 4; i++) {
+            clickElement(By.id("removeDisplayProfile-0"));
+            clickElement(By.id("confirmRemoveDisplayProfile-0"));
+        }
+
+        newFormVersion();
+        goToGeneralDetail();
+        textNotPresent("Display Profile:");
     }
 
 }
