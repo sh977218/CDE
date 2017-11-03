f872ab53c396d08f95c8d245498fecde028ee440
Peter Huang <shi.huang@nih.gov>
2017-11-03 10:11:12 -0400

    revert >>>

--- modules/form/public/tabs/description/formDescription.component.ts
+++ modules/form/public/tabs/description/formDescription.component.ts
@@ -24,20 +24,20 @@ import { copySectionAnimation } from 'form/public/tabs/description/copySectionAn
     templateUrl: "formDescription.component.html",
     animations: [copySectionAnimation],
     styles: [`
-        :host > > > .panel {
+        :host >>> .panel {
             margin-bottom: 1px;
         }
 
-        :host > > > .tree-children {
+        :host >>> .tree-children {
             padding-left: 0;
         }
 
-        :host > > > .node-drop-slot {
+        :host >>> .node-drop-slot {
             height: 10px;
             margin-bottom: 1px;
         }
 
-        :host > > > .panel-badge-btn {
+        :host >>> .panel-badge-btn {
             color: white;
             background-color: #333;
         }
