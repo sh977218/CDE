a9fff7e0edda3f9197ddeb6d7eddb5e6cc18cf1a
Ludet <ludetc@nih.gov>
2017-12-11 15:33:10 -0500

    add test

--- test/selenium/src/test/java/gov/nih/nlm/cde/test/AutoCompletionWelcomeTest.java
+++ test/selenium/src/test/java/gov/nih/nlm/cde/test/AutoCompletionWelcomeTest.java
@@ -10,15 +10,12 @@ public class AutoCompletionWelcomeTest extends NlmCdeBaseTest {
     @Test
     public void AutoCompletionWelcome() {
         goToSearch("cde");
-        String search_input = "Winter will be cold";
-        findElement(By.id("ftsearch-input")).sendKeys(search_input);
-        clickElement(By.id("search.submit"));
-        textPresent("results for Winter will be cold");
-        waitForESUpdate();
-        goToSearch("cde");
-        findElement(By.id("ftsearch-input")).clear();
-        findElement(By.id("ftsearch-input")).sendKeys("Wint");
-        String search_string = findElement(By.xpath("//div[@id='searchDiv']//ngb-highlight[1]")).getText();
-        Assert.assertTrue(search_string.contains(search_input.toLowerCase()));
+        findElement(By.id("ftsearch-input")).sendKeys("ff broc");
+        Assert.assertEquals(findElement(By.xpath("//div[@id='searchDiv']//ngb-highlight[1]")).getText(), "ffq broccoli");
+
+        goToSearch("form");
+        findElement(By.id("ftsearch-input")).sendKeys("prom iso");
+        Assert.assertEquals(findElement(By.xpath("//div[@id='searchDiv']//ngb-highlight[1]")).getText(), "promis isolation");
     }
+
 }
