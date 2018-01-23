957e29ef072bae0933c3f64e9efbce0c7712f40f
Huang <huangs8@nih.gov>
2018-01-23 15:44:25 -0500

    revert 699

--- modules/search/searchBase.component.ts
+++ modules/search/searchBase.component.ts
@@ -26,6 +26,7 @@ export const searchStyles: string = `
     .treeItemText {
         font-size: 80%;
         word-wrap: break-word;
+        word-break: break-all;
     }
     .treeParent {
         line-height: 1;
