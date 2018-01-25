36fe01708b1d460ea6993c7672b0d61d32b7da97
Ludet <ludetc@nih.gov>
2018-01-25 10:51:16 -0500

    fix test

--- test/selenium/src/test/java/gov/nih/nlm/form/test/export/FormCdeExport.java
+++ test/selenium/src/test/java/gov/nih/nlm/form/test/export/FormCdeExport.java
@@ -22,9 +22,9 @@ public class FormCdeExport extends NlmCdeBaseTest {
         checkAlert("Export downloaded");
 
         String[] expected = {
-                "Traumatic brain injury symptom category,TBI symptom or sign category,Value List,Physical; Sleep; Cognitive; Emotional; Other,NINDS,NINDS,Qualified,NINDS: C01056 v3; NINDS Variable Name: TBISympCat",
-                "Insomnia Severity Index (ISI) - difficulty falling asleep measurement,Difficulty falling asleep,Value List,0; 1; 2; 3; 4,NINDS,NINDS,Qualified,NINDS: C09377 v3; NINDS Variable Name: ISIDifFalAslpMeasr",
-                "Insomnia Severity Index (ISI) - Total score,Total Score,Number,,NINDS,NINDS,Qualified,NINDS: C21126 v1; NINDS Variable Name: ISITotalScore"
+                "\"Traumatic brain injury symptom other text\",\"Other, Specify:\",\"Text\",\"\",\"NINDS\",\"NINDS\",\"Qualified\",\"NINDS: C18400 v3; NINDS Variable Name: TBISympOthrTxt\",",
+                "\"Person Gender Text Type\",\"Patient gender; Gender; Gender of a Person; Recipient gender:; What is your gender?; Newborn Gender\",\"Value List\",\"Female; Male; Unknown; Unspecified\",\"caBIG\",\"caBIG; DCP; NHLBI; SPOREs; TEST; CCR; CDC/PHIN; ECOG-ACRIN; NICHD; AECC; LCC; USC/NCCC; NHC-NCI; PBTC; CITN; OHSU Knight; DCI\",\"Standard\",\"caDSR: 2200604 v3\",",
+                "\"Insomnia Severity Index (ISI) - worry distress measurement\",\"How worried distressed are you about your current sleep problem?\",\"Value List\",\"0; 1; 2; 3; 4\",\"NINDS\",\"NINDS\",\"Qualified\",\"NINDS: C09382 v3; NINDS Variable Name: ISIWorDistMeasr\""
         };
         try {
             String actual = new String(Files.readAllBytes(Paths.get(downloadFolder + "/FormCdes-Xy910Dz6f.csv")));
