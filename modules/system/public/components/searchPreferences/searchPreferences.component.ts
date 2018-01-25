1ff045ed1e514a5c96ecdb5a4a482023219dd8b8
Huang <huangs8@nih.gov>
2018-01-25 10:21:46 -0500

    x

--- modules/system/public/components/searchPreferences/searchPreferences.component.ts
+++ modules/system/public/components/searchPreferences/searchPreferences.component.ts
@@ -1,16 +1,16 @@
-import { Component } from "@angular/core";
-import { Http } from "@angular/http";
+import { Component } from '@angular/core';
+import { Http } from '@angular/http';
 import { ElasticService } from '_app/elastic.service';
 import { AlertService } from '_app/alert/alert.service';
 
 @Component({
-    selector: "cde-search-preferences",
-    templateUrl: "searchPreferences.component.html"
+    selector: 'cde-search-preferences',
+    templateUrl: 'searchPreferences.component.html'
 })
 
 export class SearchPreferencesComponent {
     searchSettings: any;
-    allIdentifiers = [];
+    allIdentifiers = ['NINDS', 'NINDS Variable Name', 'caDSR', 'GRDR', 'Assessment Center', 'LOINC', '(LOINC)', 'UMLS', 'TESTOrg'];
     public options = {
         multiple: true,
         tags: true
@@ -20,21 +20,23 @@ export class SearchPreferencesComponent {
                 public esService: ElasticService,
                 private alert: AlertService) {
         this.searchSettings = this.esService.searchSettings;
+/*
         this.http.get('/identifiersSource').map(res => res.json()).subscribe(res => {
             this.allIdentifiers = res.map(r => {
                 return {id: r, text: r};
             });
         }, err => this.alert.addAlert('danger', err));
+*/
     }
 
     saveSettings() {
         this.esService.saveConfiguration(this.searchSettings);
-        this.alert.addAlert("success", "Settings saved!");
+        this.alert.addAlert('success', 'Settings saved!');
         window.history.back();
     };
 
     cancelSettings() {
-        this.alert.addAlert("warning", "Cancelled...");
+        this.alert.addAlert('warning', 'Cancelled...');
         window.history.back();
     };
 
@@ -43,12 +45,12 @@ export class SearchPreferencesComponent {
         Object.keys(defaultSettings).forEach(key => {
             this.searchSettings[key] = defaultSettings[key];
         });
-        this.alert.addAlert("info", "Default settings loaded. Press Save to persist them.");
+        this.alert.addAlert('info', 'Default settings loaded. Press Save to persist them.');
     };
 
 
     changedIdentifier(searchSettings, data: { value: string[] }) {
-       // searchSettings.tableViewFields.identifiers = data.value;
+        // searchSettings.tableViewFields.identifiers = data.value;
     }
 
 }
