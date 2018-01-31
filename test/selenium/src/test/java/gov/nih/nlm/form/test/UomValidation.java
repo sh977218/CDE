e06a1ef0964fe9a61bfcb24f117f79b7bc8c76fb
Huang <huangs8@nih.gov>
2018-01-31 16:41:06 -0500

    fix tests

--- test/selenium/src/test/java/gov/nih/nlm/form/test/UomValidation.java
+++ test/selenium/src/test/java/gov/nih/nlm/form/test/UomValidation.java
@@ -22,14 +22,16 @@ public class UomValidation extends BaseFormTest {
 
         startEditQuestionById("question_0_3");
         questionEditAddUom("question_0_3", "kilogram");
+        questionEditRemoveUom("question_0_3", "meter");
         saveEditQuestionById("question_0_3");
+        textNotPresent("meter");
         findElement(By.xpath("//*[contains(@class,'badge-danger')]//*[text()='kilogram']"));
         textPresent("replaced with kg");
 
         goToPreview();
         findElement(By.xpath("//input[@id='If Yes, what are the number of CAG repeats on the larger allele_3_box']")).sendKeys("1.25");
         clickElement(By.xpath("(//div[@id='If Yes, what are the number of CAG repeats on the larger allele_3']//input[@name='q4_uom'])[1]"));
-        Assert.assertEquals(findElement(By.xpath("//input[@id='If Yes, what are the number of CAG repeats on the larger allele_3_box']")).getAttribute("title"),"1.25");
+        Assert.assertEquals(findElement(By.xpath("//input[@id='If Yes, what are the number of CAG repeats on the larger allele_3_box']")).getAttribute("title"), "1.25");
         clickElement(By.xpath("(//div[@id='If Yes, what are the number of CAG repeats on the larger allele_3']//input[@name='q4_uom'])[2]"));
         wait.until(ExpectedConditions.attributeToBe(
                 By.xpath("//input[@id='If Yes, what are the number of CAG repeats on the larger allele_3_box']"),
