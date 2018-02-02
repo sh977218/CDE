f859abc106bf0c328732f20f45e016ca87e78216
Huang <huangs8@nih.gov>
2018-02-02 13:31:18 -0500

    revert

--- modules/_app/app.component.ts
+++ modules/_app/app.component.ts
@@ -43,6 +43,7 @@ export class CdeAppComponent implements OnInit {
         ($ as any).feedback({
             ajaxURL: '/feedback/report',
             html2canvasURL: '/static/formdata.js',
+            postBrowserInfo: true,
             postHTML: false,
             postURL: true,
             proxy: undefined,
