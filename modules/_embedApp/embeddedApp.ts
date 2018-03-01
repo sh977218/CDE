322282c064cfa7d7a1b36dadf9ff35d784ba6af6
Ludet <ludetc@nih.gov>
2018-03-01 14:32:17 -0500

    fix build

--- modules/_embedApp/embeddedApp.ts
+++ modules/_embedApp/embeddedApp.ts
@@ -3,8 +3,7 @@ if (!PRODUCTION) require('../../node_modules/zone.js/dist/long-stack-trace-zone.
 // reflect-metadata needed before enableProdMode for IE
 import 'reflect-metadata';
 
-import '../node_modules/font-awesome/css/font-awesome.css';
-import '../node_modules/font-awesome-animation/dist/font-awesome-animation.css';
+import 'font-awesome/css/font-awesome.css';
 
 // path to node_modules required to override module/components/bootstrap
 import '../../node_modules/bootstrap/dist/css/bootstrap.css';
