2a17335163c7496d3488b034fc07157fbd3e5d28
Huang <huangs8@nih.gov>
2018-04-02 11:33:10 -0400

    done

--- test/selenium/src/test/java/gov/nih/nlm/form/test/BaseFormTest.java
+++ test/selenium/src/test/java/gov/nih/nlm/form/test/BaseFormTest.java
@@ -193,7 +193,7 @@ public class BaseFormTest extends FormCommentTest {
 
 
     protected void createDisplayProfile(int index, String name, boolean matrix, boolean displayValues, boolean instructions,
-                                        boolean numbering, String dispType, int numberOfColumns, boolean displayInvisible, int maxNumberAnswers) {
+                                        boolean numbering, String displayType, int numberOfColumns, boolean displayInvisible, int answerDropdownLimit) {
         textPresent("Add Profile");
         clickElement(By.id("addDisplayProfile"));
         clickElement(By.xpath("//*[@id='profileNameEdit_" + index + "']//i[@title='Edit']"));
@@ -204,15 +204,15 @@ public class BaseFormTest extends FormCommentTest {
         if (displayValues) clickElement(By.id("displayValues_" + index));
         if (!instructions) clickElement(By.id("displayInstructions_" + index));
         if (!numbering) clickElement(By.id("displayNumbering_" + index));
-        if (!"Follow-up".equals(dispType)) clickElement(By.id("displayType_" + index));
+        if (!"Follow-up".equals(displayType)) clickElement(By.id("displayType_" + index));
 
         new Select(findElement(By.id("nc_" + index))).selectByVisibleText(String.valueOf(numberOfColumns));
 
         if (displayInvisible) clickElement(By.id("displayInvisible_" + index));
 
-        if (maxNumberAnswers >= 0) {
-            findElement(By.id("displayMaxNumberAnswers_" + index)).clear();
-            findElement(By.id("displayMaxNumberAnswers_" + index)).sendKeys(String.valueOf(maxNumberAnswers));
+        if (answerDropdownLimit > 0) {
+            findElement(By.id("displayAnswerDropdownLimit_" + index)).clear();
+            findElement(By.id("displayAnswerDropdownLimit_" + index)).sendKeys(String.valueOf(answerDropdownLimit));
         }
     }
 
