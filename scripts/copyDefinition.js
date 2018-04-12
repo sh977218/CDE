8ef0cbc4b13a2a5aecfee9e35cd0e3b287f0c073
Huang <huangs8@nih.gov>
2018-04-12 16:57:54 -0400

    finished script

--- scripts/copyDefinition.js
+++ scripts/copyDefinition.js
@@ -3,27 +3,36 @@ const DataElement = require('../server/cde/mongo-cde').DataElement;
 const Form = require('../server/form/mongo-form').Form;
 const mongo_data = require('../server/system/mongo-data');
 
-let count = 0;
-
 function run() {
-    let DAOs = [DataElement, Form];
-//    let DAOs = [DataElement];
-    DAOs.forEach(dao => {
-        let cursor = dao.find({}).cursor();
+    let DAOs = [
+        {
+            name: 'de',
+            count: 0,
+            dao: DataElement
+        },
+        {
+            name: 'form',
+            count: 0,
+            dao: Form
+        }
+    ];
+
+    DAOs.forEach(DAO => {
+        let cursor = DAO.dao.find({}).cursor();
         cursor.eachAsync(elt => {
             return new Promise((resolve, reject) => {
                 let definitions = mongo_data.copyDefinition(elt.naming);
                 elt.definitions = definitions;
                 elt.save(err => {
                     if (err) reject(err);
-                    count++;
-                    console.log("count: " + count);
+                    DAO.count++;
+                    console.log(DAO.name + "Count: " + DAO.count);
                     resolve();
                 });
             });
         }).then(err => {
             if (err) throw err;
-            console.log("Finished all. count: " + count);
+            console.log("Finished all.");
             process.exit(1);
         });
     })
