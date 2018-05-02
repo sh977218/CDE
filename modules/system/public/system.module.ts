c1e98c7fc7aa1ac5ca1ca78096ff3b43ddae88fa
Huang <huangs8@nih.gov>
2018-05-02 15:59:12 -0400

    upgrade user mgt ng-select

--- modules/system/public/system.module.ts
+++ modules/system/public/system.module.ts
@@ -2,7 +2,7 @@ import { CommonModule } from "@angular/common";
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
-import { Select2Module } from 'ng2-select2';
+import { NgSelectModule } from '@ng-select/ng-select';
 
 import { OrgAdminComponent } from 'system/public/components/siteAdmin/orgAdmin/orgAdmin.component';
 import { UsersMgtComponent } from 'system/public/components/siteAdmin/usersMgt/usersMgt.component';
@@ -13,7 +13,7 @@ import { WidgetModule } from 'widget/widget.module';
         CommonModule,
         FormsModule,
         NgbModule,
-        Select2Module,
+        NgSelectModule,
         // core
         WidgetModule,
         // internal
