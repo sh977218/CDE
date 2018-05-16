66028cf71a4cd9644164d524117f69ceae305371
Ludet <ludetc@nih.gov>
2018-05-16 15:36:40 -0400

    fix tests

--- test/selenium/src/test/java/gov/nih/nlm/cde/test/search/CuratorLowStatusSearch.java
+++ test/selenium/src/test/java/gov/nih/nlm/cde/test/search/CuratorLowStatusSearch.java
@@ -13,13 +13,13 @@ public class CuratorLowStatusSearch extends NlmCdeBaseTest {
         goToCdeSearch();
         findElement(By.id("ftsearch-input")).clear();
         findElement(By.id("ftsearch-input")).sendKeys("java");
-        clickElement(By.cssSelector("i.fa-search"));
+        clickElement(By.xpath("//mat-icon[. = 'search']"));
         textPresent("Qualified (");
         textNotPresent("Candidate (");
         setVisibleStatus("minStatus-Candidate");
         findElement(By.id("ftsearch-input")).clear();
         findElement(By.id("ftsearch-input")).sendKeys("java");
-        clickElement(By.cssSelector("i.fa-search"));
+        clickElement(By.xpath("//mat-icon[. = 'search']"));
         findElement(By.id("regstatus-Candidate"));
         textPresent("caCORE (");
 
@@ -28,7 +28,7 @@ public class CuratorLowStatusSearch extends NlmCdeBaseTest {
         goToCdeSearch();
         findElement(By.id("ftsearch-input")).clear();
         findElement(By.id("ftsearch-input")).sendKeys("java");
-        clickElement(By.cssSelector("i.fa-search"));
+        clickElement(By.xpath("//mat-icon[. = 'search']"));
         textPresent("caCORE (");
         clickElement(By.id("regstatus-Candidate"));
         textNotPresent("caCORE (");
