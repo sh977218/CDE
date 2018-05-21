d9c873c1acd7add3c60f0667c889c2f4f59fbd72
Huang <huangs8@nih.gov>
2018-05-21 10:10:02 -0400

    fix tests

--- test/selenium/src/test/java/gov/nih/nlm/system/NlmCdeBaseTest.java
+++ test/selenium/src/test/java/gov/nih/nlm/system/NlmCdeBaseTest.java
@@ -1034,7 +1034,7 @@ public class NlmCdeBaseTest {
     protected void loadDefaultTableViewSettings() {
         clickElement(By.id("list_gridView"));
         openTableViewPreferenceModal();
-        clickElement(By.id("loadDefaultSettings"));
+        clickElement(By.id("loadDefaultTableViewSettingsBtn"));
         checkAlert("Default settings loaded");
         closeTableViewPreferenceModal();
     }
