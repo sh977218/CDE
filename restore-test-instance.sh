e63df7bd44cc3aff97e104b2623a76d1c430f26d
Huang <huangs8@nih.gov>
2018-05-24 16:14:13 -0400

    update test script

--- restore-test-instance.sh
+++ restore-test-instance.sh
@@ -8,8 +8,6 @@ db_password='password'
 
 target='{"count":0,"_shards":{"total":1,"successful":1,"failed":0}}'
 
-mongo test deploy/dbInit.js -u $db_user -p $db_password
-
 #wait for empty
 
 COUNTER=0
@@ -24,19 +22,8 @@ while [ $COUNTER -lt 2 ]; do
     fi
 done
 
-mongo cde-logs-test deploy/logInit.js -u $db_user -p $db_password
-
-mongorestore --drop -d test -c dataelements test/data/cdedump/dataelements.bson -u $db_user -p $db_password
-mongorestore --drop -d test -c dataelementdrafts test/data/cdedump/dataelementdrafts.bson -u $db_user -p $db_password
-mongorestore --drop -d test -c forms test/data/formDump/forms.bson -u $db_user -p $db_password
-mongorestore --drop -d test -c formdrafts test/data/formDump/formdrafts.bson -u $db_user -p $db_password
-mongorestore --drop -d test -c pinningBoards test/data/cdedump/pinningBoards.bson -u $db_user -p $db_password
-mongorestore --drop -d test -c orgs test/data/cdedump/orgs.bson -u $db_user -p $db_password
-mongorestore --drop -d test -c meshclassifications test/data/meshclassifications.bson -u $db_user -p $db_password
-mongorestore --drop -d test -c comments test/data/comments.bson -u $db_user -p $db_password
-
-mongo test test/createLargeBoard.js -u $db_user -p $db_password
-mongo test test/createManyBoards.js -u $db_user -p $db_password
+mongorestore -u $db_user -p $db_password --drop -d test test/data/test/
+mongorestore -u $db_user -p $db_password --drop -d cde-logs-test test/data/cde-logs-test/
 
 echo "deleting es index."
 gulp es
