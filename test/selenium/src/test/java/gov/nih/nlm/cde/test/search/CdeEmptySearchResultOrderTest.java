a211c1e23d0da7b8d4c9c4f97b460f1a97d578af
Huang <huangs8@nih.gov>
2018-05-24 13:54:35 -0400

    add tests

---
+++ test/selenium/src/test/java/gov/nih/nlm/cde/test/search/CdeEmptySearchResultOrderTest.java
@@ -0,0 +1,17 @@
+package gov.nih.nlm.cde.test.search;
+
+import gov.nih.nlm.system.NlmCdeBaseTest;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class CdeEmptySearchResultOrderTest extends NlmCdeBaseTest {
+
+    @Test
+    public void cdeEmptySearchResultOrder() {
+        goToCdeSearch();
+        clickElement(By.id("search_by_classification_GRDR"));
+        textPresent("Qualified", By.id("registrationStatus_0"));
+        textPresent("Recorded", By.id("registrationStatus_1"));
+    }
+
+}
---
+++ test/selenium/src/test/java/gov/nih/nlm/form/test/search/FormEmptySearchResultOrderTest.java
@@ -0,0 +1,18 @@
+package gov.nih.nlm.form.test.search;
+
+import gov.nih.nlm.system.NlmCdeBaseTest;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class FormEmptySearchResultOrderTest extends NlmCdeBaseTest {
+
+    @Test
+    public void cdeEmptySearchResultOrder() {
+        goToCdeSearch();
+        clickElement(By.id("search_by_classification_TEST"));
+        textPresent("Qualified", By.id("registrationStatus_0"));
+        textPresent("Recorded", By.id("registrationStatus_1"));
+        textPresent("Recorded", By.id("registrationStatus_17"));
+    }
+
+}
