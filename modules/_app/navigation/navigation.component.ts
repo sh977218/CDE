7b879f7136a5e650d9671b55254b385f6550e327
Ludet <ludetc@nih.gov>
2018-06-11 15:37:14 -0400

    remove unused code

--- modules/_app/navigation/navigation.component.ts
+++ modules/_app/navigation/navigation.component.ts
@@ -3,8 +3,6 @@ import { LoginService } from '_app/login.service';
 import { QuickBoardListService } from '_app/quickBoardList.service';
 import { UserService } from '_app/user.service';
 import { canCreateForms, canOrgAuthority, isOrgAdmin, isOrgCurator, isSiteAdmin } from 'shared/system/authorizationShared';
-import { DomSanitizer } from "@angular/platform-browser";
-import { MatIconRegistry } from "@angular/material";
 
 @Component({
     selector: 'cde-navigation',
@@ -44,19 +42,10 @@ export class NavigationComponent {
     isOrgCurator = isOrgCurator;
     isSiteAdmin = isSiteAdmin;
 
-    settingsSvg = "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"20\" height=\"20\" viewBox=\"0 0 20 20\">\n" +
-        "    <path fill=\"none\" d=\"M0 0h20v20H0V0z\"/>\n" +
-        "    <path d=\"M15.95 10.78c.03-.25.05-.51.05-.78s-.02-.53-.06-.78l1.69-1.32c.15-.12.19-.34.1-.51l-1.6-2.77c-.1-.18-.31-.24-.49-.18l-1.99.8c-.42-.32-.86-.58-1.35-.78L12 2.34c-.03-.2-.2-.34-.4-.34H8.4c-.2 0-.36.14-.39.34l-.3 2.12c-.49.2-.94.47-1.35.78l-1.99-.8c-.18-.07-.39 0-.49.18l-1.6 2.77c-.1.18-.06.39.1.51l1.69 1.32c-.04.25-.07.52-.07.78s.02.53.06.78L2.37 12.1c-.15.12-.19.34-.1.51l1.6 2.77c.1.18.31.24.49.18l1.99-.8c.42.32.86.58 1.35.78l.3 2.12c.04.2.2.34.4.34h3.2c.2 0 .37-.14.39-.34l.3-2.12c.49-.2.94-.47 1.35-.78l1.99.8c.18.07.39 0 .49-.18l1.6-2.77c.1-.18.06-.39-.1-.51l-1.67-1.32zM10 13c-1.65 0-3-1.35-3-3s1.35-3 3-3 3 1.35 3 3-1.35 3-3 3z\"/>\n" +
-        "</svg>";
-
     constructor(public userService: UserService,
                 public quickBoardService: QuickBoardListService,
                 public loginSvc: LoginService,
-                iconRegistry: MatIconRegistry,
-                sanitizer: DomSanitizer
-    ) {
-        iconRegistry.addSvgIconLiteral('settings', sanitizer.bypassSecurityTrustHtml(this.settingsSvg));
-    }
+    ) {}
 
     isPageActive(viewLocation) {
         return viewLocation === window.location.pathname;
