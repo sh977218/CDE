01f283f7795207514f053b58020d6f4e879074c1
Huang <huangs8@nih.gov>
2018-06-21 15:26:05 -0400

    done

--- server/system/parseConfig.js
+++ server/system/parseConfig.js
@@ -9,16 +9,20 @@ databaseNames.forEach(databaseName => {
     let uriOptions = [];
     if (database.options.replicaSet)
         uriOptions.push('replicaSet=' + database.options.replicaSet);
-    if (database.options.ssl)
+    if (database.options.ssl) {
         uriOptions.push('ssl=true');
+        try {
+            if (database.sslCAPath)
+                database.options.sslCA = [fs.readFileSync(database.sslCAPath)];
+            if (database.sslCertPath)
+                database.options.sslCert = fs.readFileSync(database.sslCertPath);
+        } catch (e) {
+            console.log(e);
+        }
+    }
     database.uri = "mongodb://" + database.username + ":" + database.password + "@" + hosts + "/" + database.db;
     if (uriOptions) database.uri = database.uri + '?' + uriOptions.join('&');
-    if (database.options.sslCA)
-        database.options.sslCA = [fs.readFileSync(database.options.sslCA)];
-    if (database.options.sslCA && database.options.sslCert)
-        database.options.sslCert = fs.readFileSync(database.options.sslCert);
 });
-
 Object.keys(config).forEach(function (key) {
     exports[key] = config[key];
 });
