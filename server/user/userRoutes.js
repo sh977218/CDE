a0a3c9f2cc47ed13dd706cb02d0cd8f3231f71cf
Huang <huangs8@nih.gov>
2018-07-06 15:02:04 -0400

    reformat

--- server/user/userRoutes.js
+++ server/user/userRoutes.js
@@ -9,47 +9,35 @@ exports.module = function (roleConfig) {
 
     router.get('/', [exportShared.nocacheMiddleware], (req, res) => {
         if (!req.user) return res.send({});
-        userDb.byId(req.user._id, handleError({
-            res: res,
-            origin: "/user"
-        }, user => {
+        userDb.byId(req.user._id, handleError({res, origin: "/user"}, user => {
             if (!user) return res.status(404).send();
             res.send(user);
         }))
     });
 
     router.post('/', (req, res) => {
-        userDb.updateUser(req.user._id, req.body, handleError({
-            res: res,
-            origin: "/user"
-        }, user => {
+        userDb.updateUser(req.user._id, req.body, handleError({res, origin: "/user"}, user => {
             if (!user) return res.status(404).send();
             res.send(user);
         }))
     });
 
     router.get('/avatar/:username', (req, res) => {
-        userDb.avatarByUsername(req.params.username, handleError({
-            res: res,
-            origin: "/avatar/:username"
-        }, avatar => {
+        userDb.avatarByUsername(req.params.username, handleError({res, origin: "/avatar/:username"}, avatar => {
             if (!avatar) return res.status(404).send();
             res.send(avatar);
         }))
     });
 
     router.get('/mailStatus', [authorization.loggedInMiddleware], (req, res) => {
-        mongo_data.mailStatus(req.user, handleError({
-            res: res,
-            origin: "/mailStatus"
-        }, mails => res.send({count: mails.length})));
+        mongo_data.mailStatus(req.user, handleError({res, origin: "/mailStatus"}, mails => {
+            if (!mails) return res.status(404).send();
+            res.send({count: mails.length})
+        }));
     });
 
     router.get('/searchUsers/:username?', roleConfig.search, (req, res) => {
-        userDb.usersByUsername(req.params.username, handleError({
-                res: res,
-                origin: "/searchUsers"
-            }, users => {
+        userDb.usersByUsername(req.params.username, handleError({res, origin: "/searchUsers"}, users => {
                 if (!users) return res.status(404).send();
                 res.send({users: users})
             })
@@ -57,20 +45,15 @@ exports.module = function (roleConfig) {
     });
 
     router.post('/addUser', roleConfig.manage, (req, res) => {
-        userDb.byUsername(req.body.username, handleError({
-            res: res,
-            origin: "/addUser"
-        }, existingUser => {
+        let username = req.body.username;
+        userDb.byUsername(username, handleError({res, origin: "/addUser"}, existingUser => {
             if (existingUser) return res.status(409).send("Duplicated username");
             let newUser = {
-                username: req.body.username.toLowerCase(),
+                username: username.toLowerCase(),
                 password: "umls",
                 quota: 1024 * 1024 * 1024
             };
-            userDb.save(newUser, handleError({
-                res: res,
-                origin: "/addUser"
-            }, savedUser => res.send(savedUser.username + " added.")));
+            userDb.save(newUser, handleError({res, origin: "/addUser"}, () => res.send(username + " added.")));
         }))
     });
     return router;
