ab9eacfb83c4989d02104f0f10d454fb6dbb7af0
Huang <huangs8@nih.gov>
2018-07-16 12:04:29 -0400

    try it

--- server/notification/notificationDb.js
+++ server/notification/notificationDb.js
@@ -2,5 +2,14 @@ const clientError = require('../log/schemas').clientErrorSchema;
 const logError = require('../log/schemas').logErrorSchema;
 
 exports.getNumberServerError = (user, callback) => {
-
+    if (user.notificationDate.serverLogDate) {
+        logError.count({}, callback);
+    }
+    else logError.count({date: {$lt: user.notificationDate.serverLogDate}}, callback)
+};
+exports.getNumberClientError = (user, callback) => {
+    if (user.notificationDate.clientLogDate) {
+        clientError.count({}, callback);
+    }
+    else clientError.count({date: {$lt: user.notificationDate.clientLogDate}}, callback)
 };
--- server/notification/notificationRoutes.js
+++ server/notification/notificationRoutes.js
@@ -1,13 +1,13 @@
-const handleError = require('./dbLogger').handleError;
+const handleError = require('../log/dbLogger').handleError;
 const notificationDb = require('./notificationDb');
 
 exports.module = function (roleConfig) {
     const router = require('express').Router();
     router.get('/serverError', (req, res) => {
-        notificationDb.getNumberServerError(req.user, handleError({req, res}, () => {
-        }))
+        notificationDb.getNumberServerError(req.user, handleError({req, res}, result => res.send(result)))
     });
     router.get('/clientError', (req, res) => {
+        notificationDb.getNumberClientError(req.user, handleError({req, res}, result => res.send(result)))
     });
 
     return router;
