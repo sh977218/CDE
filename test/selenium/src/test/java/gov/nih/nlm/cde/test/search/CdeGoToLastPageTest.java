3bad98413a2e88e69e3d92076687b57526624513
Huang <huangs8@nih.gov>
2018-07-19 13:24:55 -0400

    add tests

---
+++ test/selenium/src/test/java/gov/nih/nlm/cde/test/search/CdeGoToLastPageTest.java
@@ -0,0 +1,16 @@
+package gov.nih.nlm.cde.test.search;
+
+import gov.nih.nlm.system.NlmCdeBaseTest;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class CdeGoToLastPageTest extends NlmCdeBaseTest {
+    @Test
+    public void cdeGoToLastPage() {
+        goToCdeSearch();
+        clickElement(By.id("search_by_classification_NHLBI"));
+        textPresent("29 results for");
+        findElement(By.xpath("//*[@id='goToPage']//input")).sendKeys("2");
+        textPresent("21 - 29 of 29");
+    }
+}
