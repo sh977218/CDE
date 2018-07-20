86996ddadce5ff086f9b4da254f13e3b9ff2da8c
Huang <huangs8@nih.gov>
2018-07-20 10:28:54 -0400

    refactory tests

---
+++ test/selenium/src/test/java/gov/nih/nlm/board/form/AddToQuickboard.java
@@ -0,0 +1,21 @@
+package gov.nih.nlm.board.form;
+
+import gov.nih.nlm.board.cde.BoardTest;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class AddToQuickboard extends BoardTest {
+
+    @Test
+    public void addToQuickBoard() {
+        mustBeLoggedInAs(formboarduser, password);
+        goToBoard("TestQuickboard");
+        textPresent("QUICK BOARD (0)");
+        clickElement(By.id("addToCompare_0"));
+        textPresent("QUICK BOARD (1)");
+        clickElement(By.id("menu_qb_link"));
+        clickElement(By.id("formQuickBoard"));
+        clickElement(By.linkText("Walking Speed"));
+    }
+
+}
