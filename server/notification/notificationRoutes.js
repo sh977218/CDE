c15d12ed8b2b9c1d4e18b2447557c87800f5f160
Huang <huangs8@nih.gov>
2018-07-25 13:34:44 -0400

    fix format

--- server/notification/notificationRoutes.js
+++ server/notification/notificationRoutes.js
@@ -6,8 +6,9 @@ exports.module = function (roleConfig) {
     const router = require('express').Router();
 
     router.get('/', roleConfig.errorLog, (req, res) => {
-        if (!req.user || !authorizationShared.isSiteAdmin(req.user))
+        if (!req.user || !authorizationShared.isSiteAdmin(req.user)) {
             return res.send({serverErrorCount: 0, clientErrorCount: 0});
+        }
         notificationDb.getNumberServerError(req.user, handleError({req, res}, serverErrorCount => {
             notificationDb.getNumberClientError(req.user, handleError({req, res}, clientErrorCount =>
                 res.send({serverErrorCount, clientErrorCount})))
