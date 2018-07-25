f12af034a1d75b5e17881c7de115e70b4512500b
Huang <huangs8@nih.gov>
2018-07-25 17:33:31 -0400

    add tests

---
+++ test/selenium/src/test/java/gov/nih/nlm/cde/test/search/CdeMaxPageTest.java
@@ -0,0 +1,20 @@
+package gov.nih.nlm.cde.test.search;
+
+import gov.nih.nlm.system.NlmCdeBaseTest;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class CdeMaxPageTest extends NlmCdeBaseTest {
+    @Test
+    public void cdeGoToLastPage() {
+        mustBeLoggedInAs(nlm_username, nlm_password);
+        goToCdeSearch();
+        clickElement(By.id("search_by_classification_NHLBI"));
+        int result = getNumberOfResults();
+        textPresent(result + " results for");
+        findElement(By.xpath("//*[@id='goToPage']//input")).clear();
+        hangon(1);
+        findElement(By.xpath("//*[@id='goToPage']//input")).sendKeys("5");
+        checkAlert("Invalid page: 5");
+    }
+}
