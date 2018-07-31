f4f16c9dfa4af10513e556bd21dfd43e771b3092
Huang <huangs8@nih.gov>
2018-07-31 14:18:08 -0400

    x

--- modules/home/tour.service.ts
+++ modules/home/tour.service.ts
@@ -58,7 +58,7 @@ const searchResultSteps: Array<any> = [
         title: " 10/42 Search Result",
         element: "#resultList",
         content: "Browsing can return hundreds of elements sorted by relevance.",
-        placement: "bottom"
+        placement: "top"
     },
     {
         title: " 11/42 Classification Filter",
@@ -276,7 +276,6 @@ const formSteps = [
 
 @Injectable()
 export class TourService {
-    static started = false;
     static steps = navigationSteps.concat(searchResultSteps).concat(cdeSteps).concat(formSteps);
 
     static clickAndGoNext(tour, clickWhat: string, waitForWhat: string, cb) {
@@ -301,7 +300,6 @@ export class TourService {
         });
         tour.init();
         tour.start(true);
-        this.started = true;
     }
 
 }
