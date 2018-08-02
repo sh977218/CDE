26bd9ce83af131a1901a1f8083a6c2ad33ac95e0
Huang <huangs8@nih.gov>
2018-08-02 16:33:25 -0400

    try new gulp

--- gulpfile.js
+++ gulpfile.js
@@ -20,9 +20,7 @@ require('es6-promise').polyfill();
 
 gulp.task('npm', function _npm() {
     run('node --version').exec();
-    console.log('*************\nStart Running npm config ls -l\n****************');
-    run('npm config ls -l').exec();
-    console.log('*************\nEnd   Running npm config ls -l\n****************');
+    run('npm cache verify').exec();
     return gulp.src(['./package.json'])
         .pipe(install());
 });
