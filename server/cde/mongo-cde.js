7f2f8fe4bb99364dc6c0b20fb127f3798b092528
Huang <huangs8@nih.gov>
2018-08-14 13:20:55 -0400

    revert

--- server/cde/mongo-cde.js
+++ server/cde/mongo-cde.js
@@ -308,7 +308,9 @@ exports.query = function (query, callback) {
 };
 
 exports.transferSteward = function (from, to, callback) {
-    DataElement.update({'stewardOrg.name': from}, {$set: {'stewardOrg.name': to}}, {multi: true}).exec(callback);
+    DataElement.update({'stewardOrg.name': from}, {$set: {'stewardOrg.name': to}}, {multi: true}).exec(function (err, result) {
+        callback(err, result.nModified);
+    });
 };
 
 let auditModifications = function (oldDe, newDe, user) {
