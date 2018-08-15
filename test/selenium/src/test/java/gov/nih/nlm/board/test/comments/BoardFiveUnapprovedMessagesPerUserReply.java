eee70dfa1d7de0b2faeb576c9dc2e0a9747ffb9b
Huang <huangs8@nih.gov>
2018-08-15 16:28:28 -0400

    add tests

--- test/selenium/src/test/java/gov/nih/nlm/board/test/comments/BoardFiveUnapprovedMessagesPerUserReply.java
+++ test/selenium/src/test/java/gov/nih/nlm/board/test/comments/BoardFiveUnapprovedMessagesPerUserReply.java
@@ -1,21 +1,24 @@
-package gov.nih.nlm.form.test.comments;
+package gov.nih.nlm.board.test.comments;
 
 import gov.nih.nlm.system.NlmCdeBaseTest;
 import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
-public class FormFiveUnapprovedMessagesPerUser extends NlmCdeBaseTest {
+public class BoardFiveUnapprovedMessagesPerUserReply extends NlmCdeBaseTest {
+
 
     @Test
-    public void formFiveUnapprovedMessagesPerUserTest() {
-        String formName = "PROMIS SF v2.0 - Instrumental Support 4a";
+    public void boardFiveUnapprovedMessagesPerUserReplyTest() {
+        String boardName = "Depression";
         String reply = "This reply will never be seen.";
         mustBeLoggedInAs(unapprovedMessage_username, password);
-        goToFormByName(formName);
+        goToBoard(boardName);
         goToDiscussArea();
         findElement(By.id("newReplyTextArea_0")).sendKeys(reply);
         clickElement(By.id("replyBtn_0"));
         textNotPresent(reply);
-        checkAlert("You cannot do this.");
+        checkAlert("You have too many unapproved messages.");
     }
+
+
 }
