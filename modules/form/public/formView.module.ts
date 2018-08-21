02b001ffee2204bb5e7ffeef756abfe55d0d1458
Huang <huangs8@nih.gov>
2018-08-21 11:29:20 -0400

    fix build

--- modules/form/public/formView.module.ts
+++ modules/form/public/formView.module.ts
@@ -51,7 +51,7 @@ const appRoutes: Routes = [
         RouterModule.forChild(appRoutes),
         NgSelectModule,
         HotkeyModule.forRoot(),
-        TreeModule.forRoot(),
+        TreeModule,
         MatButtonModule,
         MatCardModule,
         MatCheckboxModule,
