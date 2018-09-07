9793f955341ca85676d39026adeb6ff9f170d7b7
Ludet <ludetc@nih.gov>
2018-09-07 10:06:34 -0400

    remove many fa-

--- modules/_app/main.ts
+++ modules/_app/main.ts
@@ -4,7 +4,7 @@ if (!PRODUCTION) require('../../node_modules/zone.js/dist/long-stack-trace-zone.
 import 'reflect-metadata';
 
 // path to node_modules required to override module/components/bootstrap
-import '../../node_modules/font-awesome/css/font-awesome.css';
+// import '../../node_modules/font-awesome/css/font-awesome.css';
 import '../../node_modules/font-awesome-animation/dist/font-awesome-animation.css';
 
 import '../../node_modules/bootstrap/dist/css/bootstrap.css';
