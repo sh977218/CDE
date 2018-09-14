781e5d3a32f002dcee54300cb4379aedeee75dc0
Huang <huangs8@nih.gov>
2018-09-14 16:08:37 -0400

    fix build

--- server/system/parseConfig.js
+++ server/system/parseConfig.js
@@ -5,19 +5,21 @@ let databaseNames = ['log', 'appData', 'migration'];
 
 databaseNames.forEach(databaseName => {
     let database = config.database[databaseName];
-    let hosts = config.database.servers.map(srv => srv.host + ":" + srv.port).join(",");
-    let uriOptions = [];
-    if (database.options.replicaSet)
-        uriOptions.push('replicaSet=' + database.options.replicaSet);
-    if (database.options.ssl) {
-        uriOptions.push('ssl=true');
-        if (database.sslCAPath)
-            database.options.sslCA = [fs.readFileSync(database.sslCAPath)];
-        if (database.sslCertPath)
-            database.options.sslCert = fs.readFileSync(database.sslCertPath);
+    if (database) {
+        let hosts = config.database.servers.map(srv => srv.host + ":" + srv.port).join(",");
+        let uriOptions = [];
+        if (database.options.replicaSet)
+            uriOptions.push('replicaSet=' + database.options.replicaSet);
+        if (database.options.ssl) {
+            uriOptions.push('ssl=true');
+            if (database.sslCAPath)
+                database.options.sslCA = [fs.readFileSync(database.sslCAPath)];
+            if (database.sslCertPath)
+                database.options.sslCert = fs.readFileSync(database.sslCertPath);
+        }
+        database.uri = "mongodb://" + database.username + ":" + database.password + "@" + hosts + "/" + database.db;
+        if (uriOptions) database.uri = database.uri + '?' + uriOptions.join('&');
     }
-    database.uri = "mongodb://" + database.username + ":" + database.password + "@" + hosts + "/" + database.db;
-    if (uriOptions) database.uri = database.uri + '?' + uriOptions.join('&');
 });
 Object.keys(config).forEach(function (key) {
     exports[key] = config[key];
