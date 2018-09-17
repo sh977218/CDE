76c5efb074bdbed5f8d6f9f239813e02e425e347
Huang <huangs8@nih.gov>
2018-09-17 16:07:01 -0400

    finished

--- ingester/loinc/LOADER/migrateLoincById.js
+++ ingester/loinc/LOADER/migrateLoincById.js
@@ -1,10 +1,15 @@
 const MigrationLoincModel = require('../../createMigrationConnection').MigrationLoincModel;
 const loincLoader = require('./../Website/loincLoader');
 
-const loincId = '89070-7';
-const orgName = 'External Forms';
-
 async function run() {
+    if (process.argv.length !== 4) {
+        console.log('pass loinc id into argv. number argv ' + argv.length);
+        process.exit(1);
+    }
+    let loincId = process.argv[2];
+    console.log('***********Start loading loinc Id ' + loincId);
+    let orgName = process.argv[3];
+    console.log('***********Into org ' + orgName);
     let loinc = await MigrationLoincModel.findOne({loincId: loincId});
     let newForm = await loincLoader.runOneForm(loinc.toObject(), orgName);
     console.log('existingForm: ' + newForm);
