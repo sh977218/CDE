2a2389e80f860d1e23e1cc0e5993ae6b67342e2f
Huang <huangs8@nih.gov>
2018-09-21 10:18:03 -0400

    revert

--- ingester/ninds/Loader/updateNindsCdes.js
+++ ingester/ninds/Loader/updateNindsCdes.js
@@ -11,41 +11,45 @@ let changed = 0;
 let created = 0;
 let same = 0;
 
-async function run() {
-    MigrationDataElement.find({}).cursor().eachAsync(migrationCde => {
-        return new Promise(async (resolve, reject) => {
-            let idObj = _.find(migrationCde.ids, o => o.source === 'NINDS');
-            let migrationId = idObj.id;
-            let migrationVersion = idObj.version;
-            let cond = {
-                "stewardOrg.name": "NINDS",
-                "archived": false,
-                "registrationState.registrationStatus": {$ne: "Retired"}
-            };
-            let existingCdes = await DataElement.find(cond)
-                .elemMatch("ids", {source: 'NINDS', id: migrationId, version: migrationVersion}).exec();
-            if (existingCdes.length > 1) throw new Error('found more than 1 ' + migrationId);
-            if (existingCdes.length === 0) {
-                await new DataElement(migrationCde.toObject()).save();
-                console.log('created: ' + ++created);
-            } else {
-                let existingCde = existingCdes[0];
-                let diff = MergeCDE.compareCdes(migrationCde, existingCde);
-                if (!_.isEmpty(diff)) {
-                    MergeCDE.mergeCde(migrationCde, existingCde);
-                    await mongo_cde.updatePromise(existingCde, user);
-                    console.log('changed: ' + ++changed);
-                } else console.log('same: ' + ++same);
-            }
-            await migrationCde.remove();
-            resolve();
-        });
-    })
-}
+doOne = migrationCde => {
+    return new Promise(async (resolve, reject) => {
+        let idObj = _.find(migrationCde.ids, o => o.source === 'NINDS');
+        let migrationId = idObj.id;
+        let migrationVersion = idObj.version;
+        let cond = {
+            "stewardOrg.name": "NINDS",
+            "archived": false,
+            "registrationState.registrationStatus": {$ne: "Retired"}
+        };
+        let existingCdes = await DataElement.find(cond)
+            .elemMatch("ids", {source: 'NINDS', id: migrationId, version: migrationVersion}).exec();
+        if (existingCdes.length > 1) throw new Error('found more than 1 ' + migrationId);
+        if (existingCdes.length === 0) {
+            await new DataElement(migrationCde.toObject()).save();
+            console.log('created: ' + ++created);
+        } else {
+            let existingCde = existingCdes[0];
+            let diff = MergeCDE.compareCdes(migrationCde, existingCde);
+            if (!_.isEmpty(diff)) {
+                MergeCDE.mergeCde(migrationCde, existingCde);
+                await mongo_cde.updatePromise(existingCde, user);
+                console.log('changed: ' + ++changed);
+            } else console.log('same: ' + ++same);
+        }
+        await migrationCde.remove();
+        resolve();
+    });
+};
 
-run().then(() => {
-    console.log('changed: ' + changed + ' created: ' + created + ' same: ' + same);
-    process.exit(1);
-}, err => {
-    throw err;
-});
+function run() {
+    MigrationDataElement.find({}).cursor().eachAsync(async migrationCde => {
+        await doOne(migrationCde);
+    }).then(() => {
+        console.log('changed: ' + changed + ' created: ' + created + ' same: ' + same);
+        process.exit(1);
+    }, err => {
+        throw err;
+    });
+};
+
+run();
