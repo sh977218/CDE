24727ce1ba1e396e240636afc1aa0a4c206ebe29
Ludet <ludetc@nih.gov>
2018-10-01 10:26:11 -0400

    temp

--- modules/adminItem/public/components/classification/classificationView.component.ts
+++ modules/adminItem/public/components/classification/classificationView.component.ts
@@ -1,12 +1,11 @@
-import { Component, ViewChild, Input, Output, EventEmitter } from "@angular/core";
+import { Component, ViewChild, Input, Output, EventEmitter, TemplateRef } from "@angular/core";
 import { IActionMapping } from "angular-tree-component/dist/models/tree-options.model";
-import { NgbModalRef, NgbModal, NgbActiveModal, NgbModalModule } from "@ng-bootstrap/ng-bootstrap";
 import _noop from 'lodash/noop';
-
 import { UserService } from '_app/user.service';
 import { IsAllowedService } from 'core/isAllowed.service';
 import { OrgHelperService } from 'core/orgHelper.service';
 import { isSiteAdmin } from 'shared/system/authorizationShared';
+import { MatDialog } from '@angular/material';
 
 const actionMapping: IActionMapping = {
     mouse: {
@@ -19,14 +18,12 @@ const actionMapping: IActionMapping = {
 
 @Component({
     selector: "cde-classification-view",
-    providers: [NgbActiveModal],
     templateUrl: "./classificationView.component.html"
 })
 export class ClassificationViewComponent {
-    @ViewChild("deleteClassificationContent") public deleteClassificationContent: NgbModalModule;
+    @ViewChild("deleteClassificationContent") public deleteClassificationContent: TemplateRef<any>;
     @Input() elt;
     @Output() confirmDelete = new EventEmitter();
-    public modalRef: NgbModalRef;
     deleteClassificationString;
     orgHelperLoaded = false;
 
@@ -39,7 +36,7 @@ export class ClassificationViewComponent {
         actionMapping: actionMapping
     };
 
-    constructor(public modalService: NgbModal,
+    constructor(public dialog: MatDialog,
                 public isAllowedModel: IsAllowedService,
                 protected userService: UserService,
                 private orgHelperService: OrgHelperService) {
@@ -58,16 +55,14 @@ export class ClassificationViewComponent {
             _treeNode = _treeNode.parent;
             if (!_treeNode.data.virtual) deleteClassificationArray.unshift(_treeNode.data.name);
         }
-        this.modalRef = this.modalService.open(this.deleteClassificationContent);
-        this.modalRef.result.then(result => {
+        this.dialog.open(this.deleteClassificationContent).afterClosed().subscribe(result => {
             if (result === "confirm") {
                 this.confirmDelete.emit({
                     deleteClassificationArray: deleteClassificationArray,
                     deleteOrgName: deleteOrgName
                 });
             }
-        }, () => {
-        });
+        }, () => {});
     }
 
     searchByClassificationParams(node, orgName) {
