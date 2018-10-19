ed807cac5b3242bf0b2307603d62b20bc0e10960
Huang <huangs8@nih.gov>
2018-10-19 10:45:55 -0400

    fix definition

--- ingester/nci/Shared/ParseDefinitions.js
+++ ingester/nci/Shared/ParseDefinitions.js
@@ -3,18 +3,5 @@ exports.parseDefinitions = nciCde => {
         definition: nciCde.PREFERREDDEFINITION[0],
         tags: []
     }];
-    if (nciCde.REFERENCEDOCUMENTSLIST[0].REFERENCEDOCUMENTSLIST_ITEM) {
-        let designationTags = ["Application Standard Question Text", "Preferred Question Text", "Alternate Question Text"];
-        nciCde.REFERENCEDOCUMENTSLIST[0].REFERENCEDOCUMENTSLIST_ITEM.forEach(refDoc => {
-            let tagIndex = designationTags.indexOf(refDoc.DocumentType[0]);
-            if (tagIndex > -1) {
-                definitions.push({
-                    definition: refDoc.Name[0],
-                    tags: [refDoc.DocumentType[0]]
-                });
-            }
-        });
-    }
-
     return definitions;
 };
