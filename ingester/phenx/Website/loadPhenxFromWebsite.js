4b9f33f63f5e291421efd0cd56d092f5c37e3aa3
Huang <huangs8@nih.gov>
2018-11-06 10:39:25 -0500

    update measurement

--- ingester/phenx/Website/loadPhenxFromWebsite.js
+++ ingester/phenx/Website/loadPhenxFromWebsite.js
@@ -2,7 +2,6 @@ const webdriver = require('selenium-webdriver');
 
 let baseUrl = require('../../createMigrationConnection').PhenxURL;
 let MeasureModel = require('../../createMigrationConnection').MeasureModel;
-let ProtocolModel = require('../../createMigrationConnection').ProtocolModel;
 let ParseMeasure = require('./ParseMeasure');
 
 let measureCount = 0;
@@ -12,25 +11,29 @@ async function doLoadPhenxMeasure() {
     await driver.get(baseUrl);
     let measureXpath = "//*[@id='phenxTooltip']//following-sibling::table/tbody/tr/td/div/div/a[2]";
     let measureLinks = await driver.findElements(webdriver.By.xpath(measureXpath));
-    //measureLinks.splice(200);
+    //measureLinks.splice(0, 20);
+    console.log("There are " + measureLinks.length + " measurements to load.");
     for (let measureLink of measureLinks) {
-        console.log("There are " + measureLinks.length + " measurements to load.");
         let browserIdText = await measureLink.findElement(webdriver.By.css('span')).getText();
-        let hrefText = await measureLink.getAttribute('href');
-        let measure = await ParseMeasure.parseMeasure(hrefText.trim());
-        measure['href'] = hrefText.trim();
-        measure['browserId'] = browserIdText.replace('#', '').trim();
-        await new MeasureModel(measure).save();
+        let browserId = browserIdText.replace('#', '').trim();
+        let existingMeasure = await MeasureModel.findOne({browserId: browserId});
+        if (!existingMeasure) {
+            let hrefText = await measureLink.getAttribute('href');
+            let measure = await ParseMeasure.parseMeasure(hrefText.trim());
+            measure['href'] = hrefText.trim();
+            measure['browserId'] = browserId;
+            await new MeasureModel(measure).save();
+        }
         measureCount++;
         console.log("measureCount: " + measureCount);
     }
 }
 
 async function run() {
-    await MeasureModel.remove({});
-    console.log('Removed all doc in migration measure collection');
-    await ProtocolModel.remove({});
-    console.log('Removed all doc in migration protocol collection');
+    /*
+        await MeasureModel.remove({});
+        console.log('Removed all doc in migration measure collection');
+    */
     await doLoadPhenxMeasure();
     console.log('Finished grab all measures from PhenX website');
 }
