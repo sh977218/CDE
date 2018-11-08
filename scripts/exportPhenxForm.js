15350827e1b070f240bd51774598e86b1575e1ba
Huang <huangs8@nih.gov>
2018-11-08 17:10:51 -0500

    add scrip

--- scripts/exportPhenxForm.js
+++ scripts/exportPhenxForm.js
@@ -4,11 +4,26 @@ const generate = require('csv-generate');
 const Form = require('../server/form/mongo-form').Form;
 
 async function run() {
-    let wstream = fs.createWriteStream('./phenx.csv');
-    let forms = await Form.find().lean();
-    generate({
-        columns: [], length: 3
-    }).pipe(wstream);
+    let filePath = './phenx.csv';
+    let header = ['NLM Id', 'PhenX Id', 'URL'];
+    let csvHeader = header.join(',') + '\n';
+    let forms = await Form.find({
+        'archived': false,
+        'ids.source': 'PhenX',
+        'registrationState.registrationStatus': {$ne: 'Retired'}
+    }).lean();
+    let rows = forms.map(form => {
+        let fields = [form.tinyId, form.ids[0].id, 'https://cde.nlm.nih.gov/formView?tinyId=' + form.tinyId];
+        return fields.join(',');
+    });
+    let data = csvHeader + rows.join('\n');
+    fs.writeFile(filePath, data, function (err) {
+        if (err) {
+            console.log(err);
+            process.exit(1);
+        }
+        console.log('PhenX.csv is saved.');
+    });
 }
 
 
