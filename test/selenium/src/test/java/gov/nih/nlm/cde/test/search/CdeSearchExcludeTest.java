f358a557ab6f0d91cfd8cc2063499613acb85aab
Huang <huangs8@nih.gov>
2018-12-06 10:26:33 -0500

    fix tests

--- test/selenium/src/test/java/gov/nih/nlm/cde/test/search/CdeSearchExcludeTest.java
+++ test/selenium/src/test/java/gov/nih/nlm/cde/test/search/CdeSearchExcludeTest.java
@@ -13,7 +13,7 @@ public class CdeSearchExcludeTest extends NlmCdeBaseTest {
         textPresent("caBIG");
         clickElement(By.id("excludeFilterModeToggle"));
         clickElement(By.id("classif-caBIG"));
-        textNotPresent("caBIG");
+        textNotPresent("caBIG", By.id("resultList"));
     }
 
     @Test
@@ -23,25 +23,25 @@ public class CdeSearchExcludeTest extends NlmCdeBaseTest {
         textPresent("caBIG");
         clickElement(By.id("excludeFilterModeToggle"));
         clickElement(By.id("classif-caBIG"));
-        textNotPresent("caBIG");
+        textNotPresent("caBIG", By.id("resultList"));
         clickElement(By.id("classif-ONC"));
-        textNotPresent("ONC");
+        textNotPresent("ONC", By.id("resultList"));
     }
 
     @Test
     public void searchAllExclude() {
         goToCdeSearch();
         clickElement(By.id("browseOrg-TEST"));
-        textPresent("caBIG");
-        textPresent("ONC");
-        textPresent("ECOG-ACRIN");
-        textPresent("NHC-NCI");
+        textPresent("caBIG", By.id("resultList"));
+        textPresent("ONC", By.id("resultList"));
+        textPresent("ECOG-ACRIN", By.id("resultList"));
+        textPresent("NHC-NCI", By.id("resultList"));
         clickElement(By.id("excludeFilterModeToggle"));
         clickElement(By.id("exludeAllOrgs"));
-        textNotPresent("caBIG");
-        textNotPresent("ONC");
-        textNotPresent("ECOG-ACRIN");
-        textNotPresent("NHC-NCI");
+        textNotPresent("caBIG", By.id("resultList"));
+        textNotPresent("ONC", By.id("resultList"));
+        textNotPresent("ECOG-ACRIN", By.id("resultList"));
+        textNotPresent("NHC-NCI", By.id("resultList"));
     }
-    
+
 }
