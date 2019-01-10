6824473c7af1849ca9d236fb929866b006119480
Huang <huangs8@nih.gov>
2019-01-10 17:12:05 -0500

    fixed

--- server/system/timestamped.js
+++ server/system/timestamped.js
@@ -8,7 +8,11 @@ module.exports = function timestamped(model, noPost) {
             model.remove({_id: id}, handleError({res, origin: 'timestamped delete ' + model}, cb));
         },
         find: (res, crit, cb) => {
-            model.find(crit, handleError({res, origin: 'timestamped find ' + model}, cb));
+            if (crit._id && !mongoose.Types.ObjectId.isValid(crit._id)) {
+                callback(undefined, undefined);
+            } else {
+                model.find(crit, handleError({res, origin: 'timestamped find ' + model}, cb));
+            }
         },
         get: (res, id, cb) => {
             model.findOne({_id: id}, handleError({res, origin: 'timestamped get ' + model}, cb));
