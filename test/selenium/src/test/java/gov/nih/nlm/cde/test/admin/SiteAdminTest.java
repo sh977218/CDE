c8de330c5b4a4a46e780bc0cd24ad7af5c715a05
Huang <huangs8@nih.gov>
2019-01-17 16:19:15 -0500

    fix tests

--- test/selenium/src/test/java/gov/nih/nlm/cde/test/admin/SiteAdminTest.java
+++ test/selenium/src/test/java/gov/nih/nlm/cde/test/admin/SiteAdminTest.java
@@ -44,7 +44,7 @@ public class SiteAdminTest extends NlmCdeBaseTest {
         clickElement(By.id("username_link"));
         clickElement(By.linkText("Org Management"));
         clickElement(By.xpath("//div[. = 'Org Admins']"));
-        clickElement(By.xpath("//tr/td[contains(text(),'" + test_username + "')]/..//mat-icon[normalize-space() = 'delete_outline']"));
+        clickElement(By.xpath("//*[contains(text(),'" + test_username + "')]/..//mat-icon[normalize-space() = 'delete_outline']"));
         textNotPresent(test_username);
     }
 
