80ad990fa724e9f63a4aef0cc0fbd785fcb2e5bd
Ludet <ludetc@nih.gov>
2019-01-31 11:42:04 -0500

    add tests

---
+++ test/selenium/src/test/java/gov/nih/nlm/cde/test/CDEDateRangeSearch.java
@@ -0,0 +1,37 @@
+package gov.nih.nlm.cde.test;
+
+import gov.nih.nlm.system.NlmCdeBaseTest;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class CDEDateRangeSearch extends NlmCdeBaseTest {
+
+    @Test
+    public void cdeDateRangeSearch() {
+        goToCdeSearch();
+        findElement(By.id("ftsearch-input")).sendKeys("created:<2015-05-14");
+        clickElement(By.id("search.submit"));
+        textPresent("853 results");
+
+        findElement(By.id("ftsearch-input")).clear();
+        findElement(By.id("ftsearch-input")).sendKeys("created:<2015-05-13");
+        textPresent("No results were found.");
+
+        findElement(By.id("ftsearch-input")).clear();
+        findElement(By.id("ftsearch-input")).sendKeys("updated:<2015-09-21");
+        textPresent("No results were found.");
+
+        findElement(By.id("ftsearch-input")).clear();
+        findElement(By.id("ftsearch-input")).sendKeys("updated:<2015-09-22");
+        textPresent("9880 results");
+
+        findElement(By.id("ftsearch-input")).clear();
+        findElement(By.id("ftsearch-input")).sendKeys("imported:<2014-12-10");
+        textPresent("No results were found.");
+
+        findElement(By.id("ftsearch-input")).clear();
+        findElement(By.id("ftsearch-input")).sendKeys("imported:<2014-12-11");
+        textPresent("458 results");
+    }
+
+}
---
+++ test/selenium/src/test/java/gov/nih/nlm/form/test/FormDateRangeSearch.java
@@ -0,0 +1,37 @@
+package gov.nih.nlm.form.test;
+
+import gov.nih.nlm.system.NlmCdeBaseTest;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class FormDateRangeSearch extends NlmCdeBaseTest {
+
+    @Test
+    public void formDateRangeSearch() {
+        goToCdeSearch();
+        findElement(By.id("ftsearch-input")).sendKeys("created:<2015");
+        clickElement(By.id("search.submit"));
+        textPresent("1 results");
+
+        findElement(By.id("ftsearch-input")).clear();
+        findElement(By.id("ftsearch-input")).sendKeys("created:<2014");
+        textPresent("No results were found.");
+
+        findElement(By.id("ftsearch-input")).clear();
+        findElement(By.id("ftsearch-input")).sendKeys("updated:<2015");
+        textPresent("No results were found.");
+
+        findElement(By.id("ftsearch-input")).clear();
+        findElement(By.id("ftsearch-input")).sendKeys("updated:<2016");
+        textPresent("12 results");
+
+        findElement(By.id("ftsearch-input")).clear();
+        findElement(By.id("ftsearch-input")).sendKeys("imported:<2015");
+        textPresent("No results were found.");
+
+        findElement(By.id("ftsearch-input")).clear();
+        findElement(By.id("ftsearch-input")).sendKeys("imported:<2016");
+        textPresent("2 results");
+    }
+
+}
