ebb567a6482f6724865dc9260bbba09bd3d502af
Ludet <ludetc@nih.gov>
2019-02-20 13:17:09 -0500

    fix index

--- server/system/connections.js
+++ server/system/connections.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const logger = require('./noDbLogger');
 
 let establishedConns = {};
-
 exports.establishConnection = function (dbConfig) {
     let uri = dbConfig.uri;
 
