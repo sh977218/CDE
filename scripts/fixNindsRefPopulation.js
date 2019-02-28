79a3bfaafc6fb6f65fdf118973e935bd90f82782
Huang <huangs8@nih.gov>
2019-02-28 14:26:09 -0500

    fix script

--- scripts/fixNindsRefPopulation.js
+++ scripts/fixNindsRefPopulation.js
@@ -2,13 +2,31 @@ const mongo_cde = require('../server/cde/mongo-cde');
 const DataElement = mongo_cde.DataElement;
 const DataElementSource = mongo_cde.DataElementSource;
 
+const mongo_form = require('../server/form/mongo-form');
+const Form = mongo_form.Form;
+const FormSource = mongo_form.FormSource;
+
 const classificationShared = require('esm')(module)('../shared/system/classificationShared');
 
-let count = 0;
+let deCount = 0;
+let formCount = 0;
 
-let cursor = DataElement.find({archived: false, 'classification.stewardOrg.name': 'NINDS'}).cursor();
+/*
+ not archived
+ not Preclinical
+ not retired
+ but in NINDS
+*/
+let cond = {
+    'archived': false,
+    'registrationState.registrationStatus': {$ne: 'Retired'},
+    'classification.stewardOrg.name': 'NINDS',
+    'classification.elements.name': {$ne: 'Preclinical TBI'}
+};
 
-cursor.eachAsync(function (cde) {
+let deCursor = DataElement.find(cond).cursor();
+
+deCursor.eachAsync(function (cde) {
     return new Promise(function (resolve) {
         let cdeObject = cde.toObject();
         let id = '';
@@ -16,46 +34,99 @@ cursor.eachAsync(function (cde) {
             if (i.source === 'NINDS') id = i.id;
         });
         if (!id) {
-            console.log(cdeObject.tinyId + ' does not have ninds id.');
+            console.log('de ' + cdeObject.tinyId + ' does not have ninds id.');
             process.exit(1);
         } else {
             DataElementSource.findOne({'ids.id': id}, (err, source) => {
                 if (err) {
                     console.log(err);
                     process.exit(1);
+                } else if (!source) {
+                    console.log('de ' + id + ' not found in source');
+                    resolve();
                 } else {
                     let sourceObject = source.toObject();
-                    if (cdeObject.referenceDocuments.length !== sourceObject.referenceDocuments.length) {
-                        console.log(cdeObject.tinyId + ' ref is not same');
-                        process.exit(1);
-                    } else {
-                        cde.referenceDocuments = sourceObject.referenceDocuments;
-                    }
+                    cde.referenceDocuments = sourceObject.referenceDocuments;
                     sourceObject.classification.forEach(c => {
                         c.elements.forEach(e => {
                             if (e.name === 'Population') {
                                 e.elements.forEach(p => {
                                     let populationToAdd = ['Population'];
                                     populationToAdd.push(p.name);
-                                    classificationShared.classifyItem(item, "NINDS", populationToAdd);
+                                    classificationShared.classifyItem(cde, "NINDS", populationToAdd);
                                 })
                             }
                         })
-                    })
+                    });
 
+                    cde.classification = cde.classification.filter(c => c.elements && c.elements.length > 0);
+                    cde.save(e => {
+                        if (e) {
+                            console.log(e);
+                            process.exit(1);
+                        } else {
+                            deCount++;
+                            if (deCount % 500 === 0) console.log('deCount: ' + deCount);
+                            resolve();
+                        }
+                    })
                 }
             })
         }
     });
 });
 
-cursor.on('close', function () {
-    console.log("Finished all. count: " + count);
-    process.exit(1);
-});
-cursor.on('error', function (err) {
-    console.log("error: " + err);
-    process.exit(1);
+
+let formCursor = Form.find(cond).cursor();
+
+formCursor.eachAsync(function (form) {
+    return new Promise(function (resolve) {
+        let formObject = form.toObject();
+        let id = '';
+        formObject.ids.forEach(i => {
+            if (i.source === 'NINDS') id = i.id;
+        });
+        if (!id) {
+            console.log('form ' + formObject.tinyId + ' does not have ninds id.');
+            process.exit(1);
+        } else {
+            FormSource.findOne({'ids.id': id}, (err, source) => {
+                if (err) {
+                    console.log(err);
+                    process.exit(1);
+                } else if (!source) {
+                    console.log('form ' + id + ' not found in source');
+                    resolve();
+                } else {
+                    let sourceObject = source.toObject();
+                    form.referenceDocuments = sourceObject.referenceDocuments;
+                    sourceObject.classification.forEach(c => {
+                        c.elements.forEach(e => {
+                            if (e.name === 'Population') {
+                                e.elements.forEach(p => {
+                                    let populationToAdd = ['Population'];
+                                    populationToAdd.push(p.name);
+                                    classificationShared.classifyItem(form, "NINDS", populationToAdd);
+                                })
+                            }
+                        })
+                    });
+
+                    form.classification = form.classification.filter(c => c.elements && c.elements.length > 0);
+                    form.save(e => {
+                        if (e) {
+                            console.log(e);
+                            process.exit(1);
+                        } else {
+                            formCount++;
+                            if (formCount % 500 === 0) console.log('formCount: ' + formCount);
+                            resolve();
+                        }
+                    })
+                }
+            })
+        }
+    });
 });
 
 
