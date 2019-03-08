9b7751c8ce6d8aa9cc58e317428280ad5e33b7a7
Huang <huangs8@nih.gov>
2019-03-08 11:00:04 -0500

    fix all

--- ingester/ninds/CDE/MergeCDE.js
+++ ingester/ninds/CDE/MergeCDE.js
@@ -1,29 +1,24 @@
-const _ = require('lodash');
-
-const classificationShared = require('esm')(module)('../../../shared/system/classificationShared');
-
-
-mergeSources = (newSources, existingSources) => {
+function mergeSources(newSources, existingSources) {
     let otherSources = existingSources.filter(s => s.sourceName !== 'NINDS');
     let result = newSources.concat(otherSources);
     return result;
 };
 
-mergeProperties = (newProperties, existingProperties) => {
+function mergeProperties(newProperties, existingProperties) {
     let otherSources = existingProperties.filter(s => s.source !== 'NINDS');
     let result = newProperties.concat(otherSources);
     return result;
 };
 
-mergeReferenceDocuments = (newReferenceDocuments, existingReferenceDocuments) => {
+function mergeReferenceDocuments(newReferenceDocuments, existingReferenceDocuments) {
     let otherSources = existingReferenceDocuments.filter(s => s.source !== 'NINDS');
     let result = newReferenceDocuments.concat(otherSources);
     return result;
 };
 
-let idKeys = ['NINDS', 'NINDS Variable Name', 'NINDS caDSR', 'csDSR'];
-mergeIds = (newIds, existingIds) => {
-    let otherSources = existingIds.filter(s => idKeys.indexOf(s.source) == -1);
+function mergeIds(newIds, existingIds) {
+    let idKeys = ['NINDS', 'NINDS Variable Name', 'NINDS caDSR', 'caDSR'];
+    let otherSources = existingIds.filter(s => idKeys.indexOf(s.source) === -1);
     let result = newIds.concat(otherSources);
     return result;
 };
