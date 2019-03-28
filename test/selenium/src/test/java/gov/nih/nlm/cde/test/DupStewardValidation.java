495e51d1259b505b4ad3773b6868188eb2b17a56
ludetc <ludetc@gmail.com>
2019-03-28 13:37:08 -0400

    fix test

--- test/selenium/src/test/java/gov/nih/nlm/cde/test/DupStewardValidation.java
+++ test/selenium/src/test/java/gov/nih/nlm/cde/test/DupStewardValidation.java
@@ -9,7 +9,7 @@ public class DupStewardValidation extends NlmCdeBaseTest {
     @Test
     public void dupStewardValidation() {
         mustBeLoggedInAs(nlm_username, nlm_password);
-        goToCdeByName("Addition Dimension");
+        goToCdeByName("Additional Dimension");
         goToNaming();
         clickElement(By.cssSelector(".mat-chip-remove"));
         clickElement(By.id("openSave"));
