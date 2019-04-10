13c87273073b65e9d0596cbd8639dad9cae9b934
ludetc <ludetc@gmail.com>
2019-04-10 15:40:42 -0400

    temp

--- test/selenium/src/test/java/gov/nih/nlm/system/NlmCdeBaseTest.java
+++ test/selenium/src/test/java/gov/nih/nlm/system/NlmCdeBaseTest.java
@@ -2,6 +2,16 @@ package gov.nih.nlm.system;
 
 import com.paulhammant.ngwebdriver.NgWebDriver;
 import org.apache.commons.io.FileUtils;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.CookieStore;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.protocol.HttpClientContext;
+import org.apache.http.impl.client.BasicCookieStore;
+import org.apache.http.impl.nio.client.CloseableHttpAsyncClient;
+import org.apache.http.message.BasicNameValuePair;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
@@ -30,6 +40,7 @@ import java.nio.file.Paths;
 import java.nio.file.attribute.PosixFilePermission;
 import java.util.*;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
@@ -254,7 +265,46 @@ public class NlmCdeBaseTest implements USERNAME, MAP_HELPER {
     }
 
     protected void mustBeLoggedInAs(String username, String password) {
-        doLogin(username, password);
+//        doLogin(username, password);
+
+        CloseableHttpAsyncClient httpclient = HttpAsyncClients.createDefault();
+
+
+        HttpClientContext localContext = HttpClientContext.create();
+        CookieStore cookieStore = new BasicCookieStore();
+
+        HttpGet httpget = new HttpGet("https://www.hepsiburada.com/ayagina-gelsin/giris");
+        System.out.println("Executing request " + httpget.getRequestLine());
+
+        httpclient.start();
+
+        // Pass local context as a parameter
+        Future<HttpResponse> future = httpclient.execute(httpget, localContext, null);
+
+        // Please note that it may be unsafe to access HttpContext instance
+        // while the request is still being executed
+
+        System.out.println("Shutting down");
+
+        HttpPost httpPost = new HttpPost("https://www.hepsiburada.com/ayagina-gelsin/Customer/Login");
+        List<NameValuePair> params = new ArrayList<NameValuePair>();
+        params.add(new BasicNameValuePair("em", "swtestacademy@mailinator.com"));
+        params.add(new BasicNameValuePair("p", "Qwerty_123"));
+        httpPost.setEntity(new UrlEncodedFormEntity(params));
+        future = httpclient.execute(httpPost,localContext,null);
+        HttpResponse response = future.get();
+        System.out.println("Response: " + response.getStatusLine());
+        List<Cookie> cookies = cookieStore.getCookies();
+        cookies = cookieStore.getCookies();
+
+        org.openqa.selenium.Cookie c;
+        for (int i = 0; i < cookies.size(); i++) {
+            System.out.println("Local cookie: " + cookies.get(i));
+            c = new org.openqa.selenium.Cookie(cookies.get(i).getName(),cookies.get(i).getValue());
+            driver.manage().addCookie(c);
+        }
+
+
         goToCdeSearch();
     }
 
