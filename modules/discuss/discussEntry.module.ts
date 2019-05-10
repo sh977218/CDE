d74475e816375f3601f19ddda9b3fec9f206770f
Huang <huangs8@nih.gov>
2019-05-10 11:45:43 -0400

    fix build

---
+++ modules/discuss/discussEntry.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { DiscussModule } from 'discuss/discuss.module';
+import { LatestCommentsComponent } from 'discuss/components/latestComments/latestComments.component';
+
+
+const appRoutes: Routes = [
+    {path: '', component: LatestCommentsComponent, data: {commentsUrl: 'server/discuss/orgComments'}},
+];
+
+@NgModule({
+    imports: [
+        RouterModule.forChild(appRoutes),
+        // internal
+        DiscussModule,
+    ],
+    declarations: [],
+    entryComponents: [],
+    exports: [],
+    schemas: []
+})
+export class DiscussEntryModule {
+}
