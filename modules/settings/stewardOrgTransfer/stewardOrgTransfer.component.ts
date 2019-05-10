f42113facb4c8ed73baec764dc2ad0da6c3b1783
Huang <huangs8@nih.gov>
2019-05-10 15:23:52 -0400

    fix build

--- modules/settings/stewardOrgTransfer/stewardOrgTransfer.component.ts
+++ modules/settings/stewardOrgTransfer/stewardOrgTransfer.component.ts
@@ -5,13 +5,17 @@ import { AlertService } from 'alert/alert.service';
 import { UserService } from '_app/user.service';
 import { IsAllowedService } from 'non-core/isAllowed.service';
 
+class TRANSFER_STEWARD_OB {
+    from: string = '';
+    to: string = '';
+}
 
 @Component({
     selector: 'cde-steward-org-transfer',
     templateUrl: './stewardOrgTransfer.component.html'
 })
 export class StewardOrgTransferComponent {
-    transferStewardObj = {};
+    transferStewardObj: TRANSFER_STEWARD_OB = new TRANSFER_STEWARD_OB();
 
     constructor(private alert: AlertService,
                 private http: HttpClient,
@@ -24,10 +28,10 @@ export class StewardOrgTransferComponent {
     transferSteward() {
         this.http.post('/transferSteward', this.transferStewardObj, {responseType: 'text'}).subscribe(r => {
             this.alert.addAlert('success', r);
-            this.transferStewardObj = {};
+            this.transferStewardObj = new TRANSFER_STEWARD_OB();
         }, () => {
             this.alert.addAlert('danger', 'An error occurred.');
-            this.transferStewardObj = {};
+            this.transferStewardObj = new TRANSFER_STEWARD_OB();
         });
     }
 }
