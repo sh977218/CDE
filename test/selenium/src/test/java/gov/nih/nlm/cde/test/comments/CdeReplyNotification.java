0493a5a3c987b8e5edb0d9c8d98ca633f0ae6786
ludetc <ludetc@gmail.com>
2019-05-10 14:04:59 -0400

    temp

--- test/selenium/src/test/java/gov/nih/nlm/cde/test/comments/CdeReplyNotification.java
+++ test/selenium/src/test/java/gov/nih/nlm/cde/test/comments/CdeReplyNotification.java
@@ -1,8 +1,6 @@
 package gov.nih.nlm.cde.test.comments;
 
 import gov.nih.nlm.system.NlmCdeBaseTest;
-import org.openqa.selenium.By;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.annotations.Test;
 
 public class CdeReplyNotification extends NlmCdeBaseTest {
