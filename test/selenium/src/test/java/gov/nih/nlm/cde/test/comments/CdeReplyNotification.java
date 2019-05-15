ba221b9b3c1382b69e8e10fde4526fac806c28b4
ludetc <ludetc@gmail.com>
2019-05-15 10:44:02 -0400

    fix notif test

--- test/selenium/src/test/java/gov/nih/nlm/cde/test/comments/CdeReplyNotification.java
+++ test/selenium/src/test/java/gov/nih/nlm/cde/test/comments/CdeReplyNotification.java
@@ -27,14 +27,14 @@ public class CdeReplyNotification extends NlmCdeBaseTest {
 
         clickElement(By.cssSelector("cde-notifications"));
         WebElement taskItem = findElement(By.cssSelector(".notificationDrawerContent div.taskItem"));
-        Assert.assertEquals(taskItem.getCssValue("background-color"), "rgb(209, 236, 241)");
+        Assert.assertEquals(taskItem.getCssValue("background-color"), "rgb(209, 236, 241, 1)");
 
         clickElement(By.cssSelector("div.taskItem"));
         textPresent("Cigarette Average Daily Pack Use Count");
 
         clickElement(By.cssSelector("cde-notifications"));
         taskItem = findElement(By.cssSelector(".notificationDrawerContent div.taskItem"));
-        Assert.assertEquals(taskItem.getCssValue("background-color"), "rgb(255, 255, 255)");
+        Assert.assertEquals(taskItem.getCssValue("background-color"), "rgb(255, 255, 255, 1)");
     }
 
 }
