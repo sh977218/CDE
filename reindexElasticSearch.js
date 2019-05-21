6d31fce9d403279fe865455f78ddb5e9e1480d76
Huang <huangs8@nih.gov>
2019-05-21 14:14:00 -0400

    done

--- reindexElasticSearch.js
+++ reindexElasticSearch.js
@@ -1,15 +1,16 @@
 const esInit = require('./server/system/elasticSearchInit');
 const elastic = require('./server/system/elastic');
-
-let allReindex = esInit.indices.map(i => {
-    console.log('Inject Index: ' + i.indexName);
-    return new Promise((resolve, reject) => {
-        elastic.reIndex(i, err => {
-            if (err) reject();
-            else resolve();
+(function () {
+    let allReindex = esInit.indices.map(i => {
+        console.log('Inject Index: ' + i.indexName);
+        return new Promise((resolve, reject) => {
+            elastic.reIndex(i, err => {
+                if (err) reject();
+                else resolve();
+            })
         })
-    })
-});
-Promise.all(allReindex)
-    .then(() => process.exit(0))
-    .catch(() => process.exit(1));
+    });
+    Promise.all(allReindex)
+        .then(() => process.exit(0))
+        .catch(() => process.exit(1));
+})();
