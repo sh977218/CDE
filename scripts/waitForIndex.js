c978e487e3e7912b40b73d8f1411b9a256037aeb
Huang <huangs8@nih.gov>
2019-05-23 14:08:44 -0400

    fix ed

--- scripts/waitForIndex.js
+++ scripts/waitForIndex.js
@@ -13,12 +13,17 @@ let indexInt = setInterval(() => {
                 clearInterval(indexInt);
 
                 // too slow, dont wait. wait in test.
-                request.post('http://localhost:3001/syncLinkedForms', {}, () => {});
+                request.post('http://localhost:3001/syncLinkedForms', {}, () => {
+                });
                 request.post('http://localhost:3001/server/mesh/syncWithMesh', {}, () => {
                     let meshInterval = setInterval(() => {
                         request.get('http://localhost:3001/server/mesh/syncWithMesh', (err, res, body) => {
-                            body = JSON.parse(body);
-                            console.log(body);
+                            try {
+                                body = JSON.parse(body);
+                            } catch (e) {
+                                console.log(e);
+                                console.log(body);
+                            }
                             if (body.dataelement.done === body.dataelement.total &&
                                 body.form.done === body.form.total
                             ) {
