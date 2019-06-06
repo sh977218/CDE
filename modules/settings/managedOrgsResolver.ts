0a0f12916b08a94445c64a9068d67bc38b32118c
Huang <huangs8@nih.gov>
2019-06-06 16:54:08 -0400

    fix build

--- modules/settings/managedOrgsResolver.ts
+++ modules/settings/managedOrgsResolver.ts
@@ -2,19 +2,19 @@ import { Injectable } from '@angular/core';
 import { Resolve, Router } from '@angular/router';
 
 import { Observable } from 'rxjs/Observable';
-import { Article } from 'core/article/article.model';
 import { catchError } from 'rxjs/operators';
 import { EMPTY } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { Organization } from 'shared/models.model';
 
 @Injectable()
-export class ManagedOrgsResolver implements Resolve<Observable<Article>> {
+export class ManagedOrgsResolver implements Resolve<Observable<Organization>> {
     constructor(private router: Router,
                 private http: HttpClient) {
     }
 
     resolve() {
-        return this.http.get<Article>('/managedOrgs')
+        return this.http.get<Organization>('/managedOrgs')
             .pipe(catchError(() => {
                 this.router.navigate(["/404"]);
                 return EMPTY;
