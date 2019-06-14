a2dfab6cd5ad1edbaa39cb47685ee6d4851eb4d1
Huang <huangs8@nih.gov>
2019-06-14 15:49:19 -0400

    update bson

---
+++ test/selenium/src/test/java/gov/nih/nlm/cde/test/comments/CdeResolveReply.java
@@ -0,0 +1,16 @@
+package gov.nih.nlm.cde.test.comments;
+
+import gov.nih.nlm.system.NlmCdeBaseTest;
+import org.testng.annotations.Test;
+
+public class CdeResolveReply extends NlmCdeBaseTest {
+
+    @Test
+    public void cdeResolveReplyTest() {
+        String cdeName = "In the past 7 days, when I was in pain I protected the part of my body that hurt";
+        mustBeLoggedInAs(reguser_username, password);
+        goToCdeByName(cdeName);
+        resolveComment("another comment about Naming");
+    }
+
+}
