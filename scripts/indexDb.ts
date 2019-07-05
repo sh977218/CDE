cd9588b645adfd0c8aef3c11d5e42e0cfda9a8b1
Huang <huangs8@nih.gov>
2019-07-05 11:12:59 -0400

    fix build

---
+++ scripts/indexDb.ts
@@ -0,0 +1,9 @@
+import { initEs } from '../server/system/elastic';
+import { syncWithMesh } from '../server/mesh/elastic';
+
+console.log('Elastic Index Running');
+initEs(() => {
+    console.log("Done indexing collections");
+    console.log("Syncing with Mesh");
+    syncWithMesh(() => process.exit(0));
+});
