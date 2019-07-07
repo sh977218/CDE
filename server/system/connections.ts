b6ea250e709c497cb3a24da127c15866dc4586e0
Huang <huangs8@nih.gov>
2019-07-06 21:34:00 -0400

    rename ingester/shared

--- server/system/connections.ts
+++ server/system/connections.ts
@@ -1,18 +1,19 @@
 import { createConnection } from 'mongoose';
 import { noDbLogger } from './noDbLogger';
 
-let establishedConns = {};
+let establishedConnections = {};
+
 export function establishConnection(dbConfig) {
     let uri = dbConfig.uri;
 
-    if (establishedConns[uri]) {
-        return establishedConns[uri];
+    if (establishedConnections[uri]) {
+        return establishedConnections[uri];
     }
 
-    establishedConns[uri] = createConnection(uri, dbConfig.options)
+    establishedConnections[uri] = createConnection(uri, dbConfig.options)
         .once('open', () => noDbLogger.info("Connection open to " + dbConfig.db))
         .on('error', error => noDbLogger.info("Error connection open to " + error))
         .on('reconnected', () => noDbLogger.info("Connection open to " + dbConfig.db));
 
-    return establishedConns[uri];
+    return establishedConnections[uri];
 }
