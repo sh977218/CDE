4892891387d9954e33524cafdbb94c6914bc6698
Huang <huangs8@nih.gov>
2019-07-08 11:29:23 -0400

    fix phenx form

--- ingester/phenx/Form/ParseAttachments.ts
+++ ingester/phenx/Form/ParseAttachments.ts
@@ -1,115 +1,28 @@
-const fs = require('fs');
-const md5File = require('md5-file');
+import { existsSync } from 'fs';
+import { find } from 'lodash';
+import { parseFormElements as parseFeLoinc } from '../../loinc/Form/ParseFormElements';
+import { map as orgMapping } from '../../loinc/Mapping/ORG_INFO_MAP';
+import { parseFormElements as parseFeRedcap } from './redCap/ParseRedCap';
 
-const mongo_data = require('../../../server/system/mongo-data');
-const gfs = mongo_data.gfs;
+const zipFolder = 's:/MLB/CDE/PhenX/www.phenxtoolkit.org/toolkit_content/redcap_zip/';
 
-const batchloader = require('../../shared/updatedByLoader').batchloader;
-
-const toolkit_content = 's:/MLB/CDE/phenx/original-phenxtoolkit.rti.org/toolkit_content';
-const pdfFolder = toolkit_content + '/PDF/';
-const zipFolder = toolkit_content + '/redcap_zip/';
-
-addAttachment = (fileName, filePath, fileType) => {
-    return new Promise(async (resolve, reject) => {
-        let fileSize = fs.statSync(filePath).size;
-        let attachment = {
-            fileid: null,
-            filename: fileName,
-            filetype: fileType,
-            uploadDate: Date.now(),
-            comment: [],
-            scanned: 'scanned',
-            filesize: fileSize,
-            uploadedBy: batchloader
-        };
-
-        md5File(filePath, async (err, md5) => {
-            if (err) {
-                console.log('MD5 failed. ' + err);
-                process.exit(1);
-            }
-            gfs.findOne({md5: md5}, (err, existingFile) => {
-                if (err) {
-                    console.log('Error gsf find ' + err);
-                    process.exit(1);
-                }
-                if (!existingFile) {
-                    let streamDescription = {
-                        filename: fileName,
-                        mode: 'w',
-                        content_type: fileType,
-                        scanned: 'scanned',
-                        metadata: {
-                            status: 'approved'
-                        }
-                    };
-                    let writestream = gfs.createWriteStream(streamDescription);
-                    writestream.on('close', function (newFile) {
-                        attachment.fileid = newFile._id;
-                        resolve(attachment);
-                    });
-                    let attachmentStream = fs.createReadStream(filePath);
-                    attachmentStream.pipe(writestream);
-                } else {
-                    attachment.fileid = existingFile._id;
-                    resolve(attachment);
-                }
-            });
+export async function parseFormElements(protocol, attachments, newForm) {
+    let loinc = find(protocol.standards, standard => standard.Source === 'LOINC');
+    if (loinc) {
+        let formElements = await parseFeLoinc(loinc, orgMapping['PhenX']);
+        formElements.unshift({
+            "elementType": "section",
+            "instructions": {
+                value: protocol.specificInstructions
+            },
+            "formElements": []
         });
-
-    })
-};
-
-doPdf = async (pdfFileName, pdfFilePath) => {
-    return await addAttachment(pdfFileName, pdfFilePath, 'pdf');
-};
-
-doImg = async imgFolder => {
-    let attachments = [];
-
-    let imgSubFolders = fs.readdirSync(imgFolder);
-    for (let imgSubFolder of imgSubFolders) {
-        let imgSubFolderExist = fs.existsSync(imgFolder + '/' + imgSubFolder);
-        if (imgSubFolderExist) {
-            let imgFiles = fs.readdirSync(imgFolder);
-            for (let imgFile of imgFiles) {
-                let fileType = 'jpg';
-                let imgFilePath = imgFolder + '/' + imgSubFolder + '/' + imgFile + '.' + fileType;
-                let imgFileExist = fs.existsSync(imgFilePath);
-                if (!imgFileExist) {
-                    fileType = 'png';
-                    imgFilePath = imgFolder + '/' + imgSubFolder + '/' + imgFile + '.' + fileType;
-                    imgFileExist = fs.existsSync(imgFilePath);
-                }
-                if (imgFileExist) {
-                    let attachment = await addAttachment(imgFile, imgFilePath, fileType);
-                    if (attachment) attachments.push(attachment);
-                }
-            }
+        newForm.formElements = formElements;
+    } else {
+        let protocolId = protocol.protocolID;
+        let zipFile = zipFolder + 'PX' + protocolId + '.zip';
+        if (existsSync(zipFile)) {
+            await parseFeRedcap(protocol, attachments, newForm);
         }
     }
-    return attachments;
-};
-
-exports.parseAttachments = async protocol => {
-    let attachments = [];
-    let protocolId = protocol.protocolId;
-
-    let pdfFileName = 'PX' + protocolId + '.pdf';
-    let pdfFilePath = pdfFolder + pdfFileName;
-    let pdfFileExist = fs.existsSync(pdfFilePath);
-    if (pdfFileExist) {
-        let pdfAttachment = await doPdf(pdfFileName, pdfFilePath);
-        if (pdfAttachment)
-            attachments.push(pdfAttachment);
-    }
-
-    let imgFolderPath = zipFolder + 'PX' + protocolId + '/attachments';
-    let imgFolderExist = fs.existsSync(imgFolderPath);
-    if (imgFolderExist) {
-        let zipAttachments = await doImg(imgFolderPath);
-        attachments = attachments.concat(zipAttachments);
-    }
-    return attachments;
-};
+}
