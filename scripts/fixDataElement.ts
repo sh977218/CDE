0941f1223429668fb009e46994e3e03547b0b921
Huang <huangs8@nih.gov>
2019-07-11 16:06:49 -0400

    fix

--- scripts/fixDataElement.ts
+++ scripts/fixDataElement.ts
@@ -4,13 +4,40 @@ process.on('unhandledRejection', function (error) {
     console.log(error);
 });
 
+function fixEmptyPermissibleValue(cde) {
+    let cdeObj = cde.toObject();
+    cdeObj.valueDomain.permissibleValues.forEach(pv => {
+        if (!pv.permissibleValue) pv.permissibleValue = pv.valueMeaningName;
+    });
+    cde.valueDomain.permissibleValues = cdeObj.valueDomain.permissibleValues.filter(pv => !pv.permissibleValue);
+    console.log('fixed.');
+}
+
+function fixError(error, cde) {
+    error.errors.forEach(e => {
+        if (e.dataPath.indexOf('.valueDomain.permissibleValues') !== -1) {
+            if (e.message === 'should NOT be shorter than 1 characters') {
+                fixEmptyPermissibleValue(cde);
+            }
+        } else {
+            console.log('other error.');
+        }
+    });
+}
+
 (function () {
     let cdeCount = 0;
     DataElement.find({
-        archived: false
+        lastMigrationScript: {$ne: 'fixDataElement'},
+        archived: false,
+        'registrationState.registrationStatus': {$ne: "Retired"}
     }).cursor().eachAsync(async (cde: any) => {
-        await cde.save().catch(e => {
-            throw `${cde.tinyId} ${e}`;
+        cde.lastMigrationScript = 'fixDataElement';
+        await cde.save().catch(error => {
+            fixError(error, cde);
+            cde.save().catch(error2 => {
+                throw `${cde.tinyId} ${error2}`;
+            })
         });
         cdeCount++;
         console.log(`cdeCount: ${cdeCount}`);
