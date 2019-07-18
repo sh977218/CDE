6c76c26d0a8569d8c789e342e3b80269d9a25af8
ludetc <ludetc@gmail.com>
2019-07-18 15:40:18 -0400

    fix gulp

---
+++ scripts/indexDb.ts
@@ -0,0 +1,20 @@
+import { syncLinkedFormsByTinyId } from 'server/form/formsvc';
+import { syncWithMesh } from 'server/mesh/elastic';
+import { initEs } from 'server/system/elastic';
+
+initEs(() => {
+    console.log('Done indexing collections');
+    console.log('Syncing with Mesh');
+    Promise.all([
+        new Promise(resolve => {
+            syncWithMesh(resolve);
+        }),
+        syncLinkedFormsByTinyId('myg51_nyXg'),
+        syncLinkedFormsByTinyId('7J69yuhyme'),
+        syncLinkedFormsByTinyId('QJmc1OnyQe'),
+        syncLinkedFormsByTinyId('QkZ91d2yXx'),
+        syncLinkedFormsByTinyId('QJdxq1unyQl'),
+        syncLinkedFormsByTinyId('mJR9Jd2JQx'),
+        syncLinkedFormsByTinyId('Xyl_qyuhJ7e'),
+    ]).then(() => process.exit(0));
+});
