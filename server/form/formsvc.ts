6ec0688f9a97aef35d7c0e50e55b9ea7693fd64f
ludetc <ludetc@gmail.com>
2019-07-18 12:23:55 -0400

    six linked forms

--- server/form/formsvc.ts
+++ server/form/formsvc.ts
@@ -408,52 +408,64 @@ export function originalSourceByTinyIdSourceName(req, res) {
 
 export let syncLinkedFormsProgress: any = {done: 0, total: 0};
 
+async function extractedSyncLinkedForms(cde) {
+    let esResult = await elastic.esClient.search({
+        index: config.elastic.formIndex.name,
+        q: cde.tinyId,
+        size: 200
+    });
+
+    const linkedForms: any = {
+        Retired: 0,
+        Incomplete: 0,
+        Candidate: 0,
+        Recorded: 0,
+        Qualified: 0,
+        Standard: 0,
+        'Preferred Standard': 0,
+        forms: []
+    };
+
+    esResult.hits.hits.forEach(h => {
+        linkedForms.forms.push({
+            tinyId: h._source.tinyId,
+            registrationStatus: h._source.registrationState.registrationStatus,
+            primaryName: h._source.primaryNameCopy
+        });
+        linkedForms[h._source.registrationState.registrationStatus]++;
+    });
+
+    linkedForms.Standard += linkedForms['Preferred Standard'];
+    linkedForms.Qualified += linkedForms.Standard;
+    linkedForms.Recorded += linkedForms.Qualified;
+    linkedForms.Candidate += linkedForms.Recorded;
+    linkedForms.Incomplete += linkedForms.Candidate;
+    linkedForms.Retired += linkedForms.Incomplete;
+
+    elastic.esClient.update({
+        index: config.elastic.index.name,
+        type: 'dataelement',
+        id: cde.tinyId,
+        body: {doc: {linkedForms: linkedForms}}
+    });
+    syncLinkedFormsProgress.done++;
+}
+
+export function syncLinkedFormsByTinyId(tinyId) {
+    return new Promise( resolve => {
+        mongo_cde.byTinyId(tinyId, async cde => {
+            await extractedSyncLinkedForms(cde);
+        });
+    });
+}
+
 export async function syncLinkedForms() {
     let t0 = Date.now();
     syncLinkedFormsProgress = {done: 0, total: 0};
     const cdeCursor = mongo_cde.getStream({archived: false});
     syncLinkedFormsProgress.total = await mongo_cde.count({archived: false});
     for (let cde = await cdeCursor.next(); cde != null; cde = await cdeCursor.next()) {
-        let esResult = await elastic.esClient.search({
-            index: config.elastic.formIndex.name,
-            q: cde.tinyId,
-            size: 200
-        });
-
-        const linkedForms: any = {
-            Retired: 0,
-            Incomplete: 0,
-            Candidate: 0,
-            Recorded: 0,
-            Qualified: 0,
-            Standard: 0,
-            "Preferred Standard": 0,
-            forms: []
-        };
-
-        esResult.hits.hits.forEach(h => {
-            linkedForms.forms.push({
-                tinyId: h._source.tinyId,
-                registrationStatus: h._source.registrationState.registrationStatus,
-                primaryName: h._source.primaryNameCopy
-            });
-            linkedForms[h._source.registrationState.registrationStatus]++;
-        });
-
-        linkedForms.Standard += linkedForms["Preferred Standard"];
-        linkedForms.Qualified += linkedForms.Standard;
-        linkedForms.Recorded += linkedForms.Qualified;
-        linkedForms.Candidate += linkedForms.Recorded;
-        linkedForms.Incomplete += linkedForms.Candidate;
-        linkedForms.Retired += linkedForms.Incomplete;
-
-        elastic.esClient.update({
-            index: config.elastic.index.name,
-            type: "dataelement",
-            id: cde.tinyId,
-            body: {doc: {linkedForms: linkedForms}}
-        });
-        syncLinkedFormsProgress.done++;
+        await extractedSyncLinkedForms(cde);
     }
     syncLinkedFormsProgress.timeTaken = Date.now() - t0;
 }
