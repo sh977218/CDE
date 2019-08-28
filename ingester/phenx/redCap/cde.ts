699bd385ce7026185de34eb36cf96657e7a990dd
Huang <huangs8@nih.gov>
2019-08-28 17:17:11 -0400

    fix build

--- ingester/phenx/redCap/cde.ts
+++ ingester/phenx/redCap/cde.ts
@@ -4,10 +4,10 @@ import { parseValueDomain } from 'ingester/phenx/redCap/parseValueDomain';
 import { parseIds } from 'ingester/phenx/redCap/parseIds';
 import { generateTinyId } from 'server/system/mongo-data';
 import { BATCHLOADER, created } from 'ingester/shared/utility';
-import { parseClassificationa } from 'ingester/phenx/redCap/parseClassification';
+import { parseClassification } from 'ingester/phenx/redCap/parseClassification';
 
 export async function createRedCde(row, protocol, newForm) {
-    const classification = parseClassificationa(protocol);
+    const classification = parseClassification(protocol);
     const designations = parseDesignations(row);
     const valueDomain = parseValueDomain(row);
     const ids = parseIds(row, newForm);
