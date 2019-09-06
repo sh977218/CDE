27f68ba6051b9c77d50dfd38e923f334511fcde8
ludetc <ludetc@gmail.com>
2019-09-06 15:35:00 -0400

    fix tests

--- test/selenium/src/test/java/gov/nih/nlm/cde/test/attachments/CdeAttachmentTest.java
+++ test/selenium/src/test/java/gov/nih/nlm/cde/test/attachments/CdeAttachmentTest.java
@@ -1,11 +1,16 @@
 package gov.nih.nlm.cde.test.attachments;
 
 import gov.nih.nlm.common.test.BaseAttachmentTest;
+import io.restassured.http.ContentType;
 import io.restassured.http.Cookie;
 import io.restassured.response.Response;
 import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Random;
 
 import static io.restassured.RestAssured.given;
 
@@ -57,26 +62,35 @@ public class CdeAttachmentTest extends BaseAttachmentTest {
 
 
     @Test
-    public void checkErrors() {
+    public void attachmentErrors() throws IOException {
         mustBeLoggedInAs(reguser_username, password);
         Cookie myCookie = getCurrentCookie();
         given().cookie(myCookie).body("").post(baseUrl + "/server/attachment/cde/add").then().statusCode(400);
 
-        Response resp = given().cookie(myCookie).body("{\"id\": \"5aa6dd751217150ae0a19800\"")
+        Random rand = new Random();
+        final File tempFile = File.createTempFile("attachmentErrorSample_" + rand.nextInt(10000000), ".tmp");
+        tempFile.deleteOnExit();
+
+        Response resp = given().cookie(myCookie)
+                .contentType("multipart/form-data")
+                .multiPart("uploadedFiles", tempFile)
+                .multiPart("id", "5aa6dd751217150ae0a19800")
                 .post(baseUrl + "/server/attachment/cde/add");
         Assert.assertEquals(resp.getStatusCode(), 401);
-        Assert.assertTrue(resp.getBody().toString().contains("You do not own"));
+        Assert.assertTrue(resp.getBody().print().contains("You do not own"));
 
 
-        resp = given().cookie(myCookie).body("{\"id\": \"5aa6dd751217150ae0a19800\"")
+        resp = given().contentType(ContentType.JSON).cookie(myCookie)
+                .body("{\"id\": \"5aa6dd751217150ae0a19800\"}")
                 .post(baseUrl + "/server/attachment/cde/remove");
         Assert.assertEquals(resp.getStatusCode(), 401);
-        Assert.assertTrue(resp.getBody().toString().contains("You do not own"));
+        Assert.assertTrue(resp.getBody().print().contains("You do not own"));
 
-        resp = given().cookie(myCookie).body("{\"id\": \"5aa6dd751217150ae0a19800\"")
+        resp = given().cookie(myCookie).contentType(ContentType.JSON)
+                .body("{\"id\": \"5aa6dd751217150ae0a19800\"}")
                 .post(baseUrl + "/server/attachment/cde/setDefault");
         Assert.assertEquals(resp.getStatusCode(), 401);
-        Assert.assertTrue(resp.getBody().toString().contains("You do not own"));
+        Assert.assertTrue(resp.getBody().print().contains("You do not own"));
 
     }
 
