a84416bfa761e143d566855c5e3ebfd62323a0f5
ludetc <ludetc@gmail.com>
2019-09-06 14:20:57 -0400

    feature done

--- modules/adminItem/public/components/sources/sources.component.ts
+++ modules/adminItem/public/components/sources/sources.component.ts
@@ -1,11 +1,28 @@
 import { Component, Input } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CdeForm } from 'shared/form/form.model';
 
 @Component({
-    selector: 'cde-admin-item-sources',
+    selector: 'cde-admin-item-sources[elt]',
     templateUrl: './sources.component.html'
 })
 export class SourcesComponent {
-    @Input() elt: any;
+    @Input() set elt(e: CdeForm) {
+        e.sources.forEach(async s => {
+            const url = `/originalSource/${e.elementType}/${s.sourceName}/${e.tinyId}`;
+            let result = true;
+            await this.http.head(url).toPromise().catch(() => result = false);
+            this.sourceUrls[url] = result;
+        });
+        this._elt = e;
+    }
+    get elt() {
+        return this._elt;
+    }
+    private _elt!: CdeForm;
 
-    allowSources = ['NINDS', 'caDSR', 'PhenX', 'LOINC'];
+    sourceUrls: any = {};
+
+    constructor(private http: HttpClient) {}
+
 }
