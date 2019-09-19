59996a28f0e510ebfbc99a8f0f5e812056e165cf
Huang <huangs8@nih.gov>
2019-09-19 16:55:05 -0400

    add nativeRender

---
+++ server/nativeRender/nativeRenderRouters.ts
@@ -0,0 +1,32 @@
+import { renderFile } from 'ejs';
+import { is } from 'useragent';
+
+/* for IE Opera Safari, emit polyfill.js */
+function isModernBrowser(req) {
+    const ua = is(req.headers['user-agent']);
+    return ua.chrome || ua.firefox || ua.edge;
+}
+
+export function module() {
+    const router = require('express').Router();
+    let version = 'local-dev';
+    try {
+        version = require('./version.js').version;
+    } catch (e) {
+    }
+
+    let nativeRenderHtml = '';
+    renderFile('modules/_nativeRenderApp/nativeRenderApp.ejs', {isLegacy: false, version}, (err, str) => {
+        nativeRenderHtml = str;
+    });
+
+    let nativeRenderLegacyHtml = '';
+    renderFile('modules/_nativeRenderApp/nativeRenderApp.ejs', {isLegacy: true, version}, (err, str) => {
+        nativeRenderLegacyHtml = str;
+    });
+    router.get('/', (req, res) => {
+        res.send(isModernBrowser(req) ? nativeRenderHtml : nativeRenderLegacyHtml);
+    });
+    return router;
+
+}
