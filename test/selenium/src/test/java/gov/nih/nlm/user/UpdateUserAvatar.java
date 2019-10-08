8479f839c74f8c3dc984fc37f2d732327667d418
Huang <huangs8@nih.gov>
2019-10-08 17:01:14 -0400

    add tests

---
+++ test/selenium/src/test/java/gov/nih/nlm/user/UpdateUserAvatar.java
@@ -0,0 +1,19 @@
+package gov.nih.nlm.user;
+
+import gov.nih.nlm.system.NlmCdeBaseTest;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class UpdateUserAvatar extends NlmCdeBaseTest {
+    @Test
+    void updateUserAvatar() {
+        String avatarUrl = "/cde/public/assets/img/min/nih-cde-logo-simple.png";
+        mustBeLoggedInAs(nlm_username, nlm_password);
+        goToUsers();
+        searchUsername(empty_avatar_username);
+        clickElement(By.id("searchUsersSubmit"));
+        clickElement(By.id("user_avatar_0"));
+        findElement(By.xpath("//*[@id='user_avatar_0']//input")).sendKeys(avatarUrl);
+        findElement(By.xpath("//*[@id='user_avatar_0']//img"));
+    }
+}
