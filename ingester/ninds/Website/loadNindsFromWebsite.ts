3a2d5d8c34008cff9e3ce889b9175b9209f5c4e4
Huang <huangs8@nih.gov>
2019-10-28 16:25:58 -0400

    fix loader

--- ingester/ninds/Website/loadNindsFromWebsite.ts
+++ ingester/ninds/Website/loadNindsFromWebsite.ts
@@ -8,7 +8,7 @@ const URL_PREFIX = 'https://www.commondataelements.ninds.nih.gov/';
 const DEFAULT_XPATH = "//*[@id='Data_Standards']/a/following-sibling::table/tbody/tr//td";
 
 // tslint:disable-next-line:max-line-length
-// document count xpath: div[div[p[button[normalize-space(text())='Expand All']]]]/div[@class='view-content']/div[@class='view-grouping']/div[@class='view-grouping-content']/div/table/tbody/tr
+// document count xpath: //div[div[p[button[normalize-space(text())='Expand All']]]]/div[@class='view-content']/div[@class='view-grouping']/div[@class='view-grouping-content']/div/table/tbody/tr
 const DISORDERS: any = [
     {
         disorderName: 'General (For all diseases)',
@@ -106,37 +106,46 @@ const DISORDERS: any = [
     }*/
 ];
 
-async function doCdesHeader(headerDiv: any) {
-    let disorderName = '';
+async function doCdesHeader(ninds: any, headerDiv: any) {
+    let diseaseName = '';
     let subDomainName = '';
-    let paginationInfo = await headerDiv.getText();
-    const elements = await headerDiv.findElements('./');
+    let crfName = '';
+    const fullText = await headerDiv.getText();
+    const elements = await headerDiv.findElements(By.xpath('./*'));
     for (const element of elements) {
         const text = await element.getText();
-        if (indexOf(text, 'Disease: ') !== -1) {
-            disorderName = replace(text, 'Disease: ', '');
-            paginationInfo = replace(paginationInfo, text, '');
+        const diseaseIndex = text.indexOf('Disease: ');
+        if (diseaseIndex !== -1) {
+            diseaseName = replace(text, 'Disease: ', '');
         }
-        if (indexOf(text, 'Sub-Domain: ') !== -1) {
+        const subDomainIndex = text.indexOf('Sub-Domain: ');
+        if (subDomainIndex !== -1) {
             subDomainName = replace(text, 'Sub-Domain: ', '');
-            paginationInfo = replace(paginationInfo, text, '');
+        }
+        const crfIndex = text.indexOf('CRF: ');
+        if (crfIndex !== -1) {
+            crfName = replace(text, 'CRF: ', '');
         }
     }
-    return {disorderName, subDomainName, paginationInfo};
+    ninds.diseaseName = diseaseName;
+    ninds.subDomainName = subDomainName;
+    ninds.crfName = crfName;
+    ninds.fullText = fullText;
 }
 
-async function doCdesTable(cdeTableElement: any) {
-    const cdes = [];
+async function doCdesTable(ninds: any, cdeTableElement: any) {
+    ninds.cdes = [];
     const keys = [];
     const ths = await cdeTableElement.findElements(By.xpath('./thead/tr/th'));
     for (const th of ths) {
         const key = await th.getText();
-        keys.push(key.trim());
+        const keyReplace = key.replace('(e.g., Core)', '');
+        keys.push(keyReplace.trim());
     }
     const trs = await cdeTableElement.findElements(By.xpath('./tbody/tr'));
     for (const tr of trs) {
         const cde: any = {};
-        const tds = await tr.findElements(By.xpath('/td'));
+        const tds = await tr.findElements(By.xpath('./td'));
 
         // tslint:disable-next-line:prefer-for-of
         for (let i = 0; i < tds.length; i++) {
@@ -144,21 +153,20 @@ async function doCdesTable(cdeTableElement: any) {
             const value = await td.getText();
             cde[keys[i]] = value.trim();
         }
-        cdes.push(cde);
+        ninds.cdes.push(cde);
     }
-    return cdes;
 }
 
-async function doCdes(url: string) {
+async function doCdes(ninds: any, url: string) {
     const driver = await new Builder().forBrowser('chrome').build();
     await driver.get(url);
     const mainDiv = await driver.findElement(By.xpath("//section[@id='block-system-main']/div"));
     const headerDiv = await mainDiv.findElement(By.xpath("./div[@class='view-header']"));
-    const ninds: any = doCdesHeader(headerDiv);
+    await doCdesHeader(ninds, headerDiv);
     const contentDiv = await mainDiv.findElement(By.xpath("./div[@class='view-content']"));
     const cdeTableElement = await contentDiv.findElement(By.xpath('./div/table'));
-    const cdes = await doCdesTable(cdeTableElement);
-    ninds.cdes = cdes;
+    await doCdesTable(ninds, cdeTableElement);
+    await driver.close();
     return ninds;
 }
 
@@ -169,32 +177,52 @@ async function doDomainName(domainElement: any) {
     return domainName;
 }
 
-async function doDomainTable(domainElement: any, domainName: string) {
+async function doDomainTable(disorder: any, domainElement: any, domainName: string) {
     const domainTable = await domainElement.findElement(By.xpath("./div[@class='view-grouping-content']/div/table"));
     const trElements = await domainTable.findElements(By.xpath('./tbody/tr'));
     for (const trElement of trElements) {
         const tds = await trElement.findElements(By.xpath('./td'));
         const formName = await tds[0].getText();
-        const formIdElement = await tds[0].findElement(By.xpath('./a'));
-        const formId = await formIdElement.getAttribute('title');
-        const aElement = await tds[0].findElement(By.xpath('./a'));
-        const cdeUrl = await aElement.getAttribute('href');
-        const ninds = await doCdes(cdeUrl);
-        ninds.domainName = domainName;
-        ninds.formName = formName;
-        ninds.formId = formId;
-        await new NindsModel(ninds).save();
+        const formIdElements = await tds[0].findElements(By.xpath('./a'));
+        let formId = '';
+        if (formIdElements.length === 1) {
+            formId = await formIdElements[0].getAttribute('title');
+        } else {
+            console.log('No formId found.');
+            process.exit(1);
+        }
+        const ninds: any = {
+            url: disorder.url,
+            domainName,
+            formName,
+            formId
+        };
+        const existingNinds = await NindsModel.findOne(ninds);
+        if (existingNinds) {
+            console.log(`${disorder.url}  ${formId} ${formName} has loaded. Skipping...`);
+        } else {
+            const aElements = await tds[1].findElements(By.xpath('./a'));
+            if (aElements.length === 1) {
+                const cdeUrl = await aElements[0].getAttribute('href');
+                await doCdes(ninds, cdeUrl);
+            } else {
+                console.log(`${formId} has no Cdes.`);
+            }
+            await new NindsModel(ninds).save();
+        }
     }
 }
 
 async function doDisorder(disorder: any) {
     const driver = await new Builder().forBrowser('chrome').build();
+    await driver.get(disorder.url);
+    driver.findElement(By.xpath("//button[normalize-space(text())='Expand All']")).click();
     // tslint:disable-next-line:max-line-length
     const domainDivXpath = "//div[div[p[button[normalize-space(text())='Expand All']]]]/div[@class='view-content']/div[@class='view-grouping']";
     const domainElements = await driver.findElements(By.xpath(domainDivXpath));
     for (const domainElement of domainElements) {
         const domainName = await doDomainName(domainElement);
-        await doDomainTable(domainElement, domainName);
+        await doDomainTable(disorder, domainElement, domainName);
     }
 }
 
