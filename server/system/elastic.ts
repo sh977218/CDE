a9ff63c5c47b54538c1d50ca4aeb6a4a90372475
ludetc <ludetc@gmail.com>
2019-11-05 11:39:08 -0500

    add coverage

--- server/system/elastic.ts
+++ server/system/elastic.ts
@@ -833,7 +833,7 @@ export const queryMostViewed = {
                     bool: {
                         should: [
                             {term: {'registrationState.registrationStatus': 'Standard'}},
-                            {term: {'registrationState.registrationStatus': 'Qualified'}}
+                            {term: {'registrationState.registrallOrgNamesationStatus': 'Qualified'}}
                         ]
                     }
                 }
