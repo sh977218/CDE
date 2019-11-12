27d8adfc83e6b3788d713f93c424bda22ad6c2df
ludetc <ludetc@gmail.com>
2019-11-12 08:41:48 -0500

    temp

--- app.ts
+++ app.ts
@@ -51,11 +51,10 @@ import { startServer } from 'server/system/ioServer';
 import { errorLogger, expressLogger } from 'server/system/logging';
 import * as daoManager from 'server/system/moduleDaoManager';
 import { sessionStore } from 'server/system/mongo-data';
-import { banHackers, banIp, blockBannedIps } from 'server/system/trafficFilterSvc';
+import { banHackers, blockBannedIps } from 'server/system/trafficFilterSvc';
 import { module as userModule } from 'server/user/userRoutes';
 import { module as utsModule } from 'server/uts/utsRoutes';
 import { isOrgAuthority, isOrgCurator } from 'shared/system/authorizationShared';
-import { consoleLog } from 'server/log/dbLogger';
 
 const config = Config as any;
 const domain = Domain.create();
@@ -264,6 +263,8 @@ if (config.expressLogFile) {
 
 let connections = 0;
 setInterval(() => connections = 0, 60000);
+
+console.log('HELLO Peter !!!!!');
 
 app.use((req, res, next) => {
     const maxLogsPerMinute = config.maxLogsPerMinute || 1000;
