c22b2fdf511466075fa02866d947ab1cd6a3a00b
ludetc <ludetc@gmail.com>
2019-12-02 10:52:55 -0500

    fix scroll hist test

--- test/selenium/src/test/java/gov/nih/nlm/system/ScrollHistoryTest.java
+++ test/selenium/src/test/java/gov/nih/nlm/system/ScrollHistoryTest.java
@@ -33,7 +33,10 @@ public class ScrollHistoryTest extends NlmCdeBaseTest {
         clickElement(By.id(elementId));
         findElement(By.className("mobileViewH1"));
 
-        searchEltAny("Test of Memory Malingering (TOMM) - Trial 1 question 10 score", "cde");
+        goToCdeSearch();
+        findElement(By.id("ftsearch-input")).clear();
+        findElement(By.id("ftsearch-input")).sendKeys("Test of Memory Malingering (TOMM) - Trial 1 question 10 score");
+        clickElement(By.id("search.submit"));
         textPresent("Test of Memory Malingering (TOMM) - Trial 1 question 10 score");
         scrollToViewById(elementId);
         hangon(1);
