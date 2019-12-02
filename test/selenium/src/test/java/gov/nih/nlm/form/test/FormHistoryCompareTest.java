1796007f24e6c74d3a0990ebffd4b68e4df538d0
Huang <huangs8@nih.gov>
2019-12-02 14:09:40 -0500

    fix tests

--- test/selenium/src/test/java/gov/nih/nlm/form/test/FormHistoryCompareTest.java
+++ test/selenium/src/test/java/gov/nih/nlm/form/test/FormHistoryCompareTest.java
@@ -14,7 +14,7 @@ public class FormHistoryCompareTest extends BaseFormTest {
 
         goToHistory();
         textPresent("List of previous versions");
-        Assert.assertEquals(4, driver.findElements(By.xpath("//*[@id='historyTable']/tbody/tr[td]")).size());
+        Assert.assertEquals(4, driver.findElements(By.xpath("//*[@id='historyTable']//tr[td]")).size());
         selectHistoryAndCompare(2, 3);
         textPresent("Neoadjuvant Therapy", By.xpath("//*[@id='Form Elements_1']//div[contains(@class,'arrayObjEdit')]//div[contains(@class,'skipLogic.condition')]"));
         textPresent("Yes (specify type)", By.xpath("//*[@id='Form Elements_1']//div[contains(@class,'arrayObjEdit')]//div[contains(@class,'skipLogic.condition')]"));
