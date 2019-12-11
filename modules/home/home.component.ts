8cb1c4c060920653ee7d37ec7b17edeab72bdbf0
Peter Huang <peterhuang1113@gmail.com>
2019-12-10 21:05:05 -0500

    try it

--- modules/home/home.component.ts
+++ modules/home/home.component.ts
@@ -10,6 +10,7 @@ import './home.scss';
 })
 export class HomeComponent implements OnInit {
     displayCarousel = (window.screen.width > 575);
+    slideIndex: number = 1;
 
     @HostListener('window:resize', ['$event'])
     onResize(event: UIEvent) {
@@ -31,5 +32,36 @@ export class HomeComponent implements OnInit {
 
     takeATour() {
         TourService.takeATour();
+    }
+
+
+    plusSlides(n: number) {
+        this.showSlides(this.slideIndex += n);
+    }
+
+    currentSlide(n: number) {
+        this.showSlides(this.slideIndex = n);
+    }
+
+    showSlides(n: number) {
+        /*
+                let i;
+                const slides = document.getElementsByClassName('mySlides');
+                const dots = document.getElementsByClassName('dot');
+                if (n > slides.length) {
+                    this.slideIndex = 1;
+                }
+                if (n < 1) {
+                    this.slideIndex = slides.length;
+                }
+                for (i = 0; i < slides.length; i++) {
+                    slides[i].style.display = 'none';
+                }
+                for (i = 0; i < dots.length; i++) {
+                    dots[i].className = dots[i].className.replace(' active', '');
+                }
+                slides[this.slideIndex - 1].style.display = 'block';
+                dots[this.slideIndex - 1].className += ' active';
+        */
     }
 }
