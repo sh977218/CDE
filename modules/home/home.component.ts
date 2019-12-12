b51074c21c11b938c62406dbb925428b3f1af7e9
Huang <huangs8@nih.gov>
2019-12-12 11:55:02 -0500

    revert

--- modules/home/home.component.ts
+++ modules/home/home.component.ts
@@ -10,7 +10,6 @@ import './home.scss';
 })
 export class HomeComponent implements OnInit {
     displayCarousel = (window.screen.width > 575);
-    slideIndex: number = 1;
 
     @HostListener('window:resize', ['$event'])
     onResize(event: UIEvent) {
@@ -28,43 +27,9 @@ export class HomeComponent implements OnInit {
         } else if (this.route.snapshot.queryParams.notifications !== undefined) {
             this.router.navigate(['/home']);
         }
-        this.showSlides(this.slideIndex);
     }
 
     takeATour() {
         TourService.takeATour();
-    }
-
-
-    plusSlides(n: number) {
-        this.showSlides(this.slideIndex += n);
-    }
-
-    currentSlide(n: number) {
-        this.showSlides(this.slideIndex = n);
-    }
-
-    showSlides(n: number) {
-        let i;
-        const slides: any = document.getElementsByClassName('mySlides');
-        const dots: any = document.getElementsByClassName('dot');
-        if (n > slides.length) {
-            this.slideIndex = 1;
-        }
-        if (n < 1) {
-            this.slideIndex = slides.length;
-        }
-        for (i = 0; i < slides.length; i++) {
-            slides[i].style.display = 'none';
-        }
-        for (i = 0; i < dots.length; i++) {
-            dots[i].className = dots[i].className.replace(' active', '');
-        }
-        if (slides.length) {
-            slides[this.slideIndex - 1].style.display = 'block';
-        }
-        if (dots.length) {
-            dots[this.slideIndex - 1].className += ' active';
-        }
     }
 }
