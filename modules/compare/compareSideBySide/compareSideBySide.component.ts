266156a7019add56611b36345defbda0a990d948
Huang <huangs8@nih.gov>
2019-12-26 21:51:35 -0500

    fix

--- modules/compare/compareSideBySide/compareSideBySide.component.ts
+++ modules/compare/compareSideBySide/compareSideBySide.component.ts
@@ -44,8 +44,8 @@ interface Data {
 }
 
 const URL_MAP: {[module: string]: string} = {
-    cde: '/server/de/',
-    form: '/server/form/',
+    cde: '/api/de/',
+    form: '/api/form/',
 };
 
 @Component({
