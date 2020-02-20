7ae10933abc9eeb2d6e073abaf7953829d7759cd
Ludet <ludetc@nih.gov>
2020-02-20 09:56:56 -0500

    first run

--- scripts/spellchecker.ts
+++ scripts/spellchecker.ts
@@ -19,43 +19,62 @@ process.on('unhandledRejection', (error) => {
     console.log(error);
 });
 
-const filename = './spellcheck.whitelist';
-let whitelist;
+const whitelistFile = './scripts/spellcheck.whitelist';
+const blacklistFile = './scripts/spellcheck.blacklist';
+let whitelist = [];
+let blacklist = [];
 async function addToList(term) {
     if (whitelist.indexOf(term === -1)) {
         whitelist.push(term);
-        const toWrite = whitelist.join(';');
-        await fs.writeFileSync(filename, toWrite, 'utf8');
+        await fs.writeFileSync(whitelistFile, whitelist, 'utf8');
     }
 }
 
+const errors = [];
+async function markAsError(term, tinyId) {
+    errors.push({tinyId, term});
+    await fs.writeFileSync('./scripts/spellcheck.output', JSON.stringify(errors), 'utf8');
+    if (blacklist.indexOf(term === -1)) {
+        blacklist.push(term);
+        await fs.writeFileSync(blacklistFile, blacklist, 'utf8');
+    }
+
+}
+
 async function run() {
+    try {
+        whitelist = await fs.readFileSync(whitelistFile, 'utf8').split(',');
+        blacklist = await fs.readFileSync(blacklistFile, 'utf8').split(',');
+    } catch (e) {}
     const cond = {'stewardOrg.name': steward,
         'registrationState.registrationStatus': {$ne: 'Retired'},
             archived: false};
     let cdeCount = await dataElementModel.countDocuments(cond);
     console.log(`TODO: ${cdeCount}`);
-    whitelist = await fs.readFileSync(filename, 'utf8').split(';');
     const cursor = dataElementModel.find(cond).cursor();
-    cursor.eachAsync(async (dataElementSource: any) => {
-        const cdeObj = dataElementSource.toObject();
+    cursor.eachAsync(async (cde: any) => {
         cdeCount--;
 
-        for (const designation of cdeObj.designations) {
-            const terms = designation.designation.replace(/([ '":.,;()\-?/\[\]]+)/g, '§sep§')
-                .split('§sep§');
+        for (const designation of cde.designations) {
+            const terms = designation.designation.replace(/([ '":.,;()\-?/\[\]]+)/g, '§sep§').split('§sep§');
             for (let term of terms) {
                 if (term.toUpperCase() !== term) {
                     term = term.trim().toLowerCase();
                     if (whitelist.indexOf(term) === -1) {
                         const misspelled = !dictionary.spellCheck(term);
                         if (misspelled) {
-                            console.log(`${term} in tinyId: ${cdeObj.tinyId}`);
-                            const ok = await yesno({
-                                question: 'Add to whitelist?'
-                            });
+                            console.log(`${term} in tinyId: ${cde.tinyId}`);
+                            let ok = false;
+                            if (blacklist.indexOf(term) !== -1) {
+                            } else {
+                                ok = await yesno({
+                                    question: 'Add to whitelist?'
+                                });
+                            }
                             if (ok) {
                                 await addToList(term);
+                            } else {
+                                await markAsError(term, cde.tinyId);
                             }
                         }
                     }
