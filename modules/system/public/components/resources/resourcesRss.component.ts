32cfeaad97087a4bfd1478dc467b31e35f4d8fcb
Peter Huang <huangs8@nih.gov>
2022-08-24 20:45:38 -0400

    CDE-2483-restore-formatting-on-pubmed-rss-on-resources

--- modules/system/public/components/resources/resourcesRss.component.ts
+++ modules/system/public/components/resources/resourcesRss.component.ts
@@ -2,15 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'cde-resources-rss',
-    templateUrl: './resourcesRss.component.html',
-    styles: [`
-        .rssHeader {
-            background: rgba(0, 0, 0, .03);
-        }
-    `]
+    templateUrl: './resourcesRss.component.html'
 })
 export class ResourcesRssComponent {
-
     rssFeed: any = {};
-
 }
--- test/selenium/src/test/java/gov/nih/nlm/system/ResourcesPage.java
+++ test/selenium/src/test/java/gov/nih/nlm/system/ResourcesPage.java
@@ -9,7 +9,7 @@ public class ResourcesPage extends BaseAttachmentTest {
     @Test
     public void resourcesPage() {
         String attachmentName = "painLocationInapr.png";
-        String resourceText = "This resources page is under construction";
+        String resourceText = "The NIH CDE Repository is consolidating CDE Resources here on the NIH CDE Repository website";
         mustBeLoggedInAs(nlm_username, nlm_password);
         goToArticles();
         selectArticleByKey("resources");
@@ -32,18 +32,19 @@ public class ResourcesPage extends BaseAttachmentTest {
         clickElement(By.cssSelector(".cke_button__source"));
         clickElement(By.xpath("//mat-icon[. = 'check']"));
         checkAlert("Saved");
+        clickElement(By.cssSelector(".expandTextButton"));
         textPresent(resourceText);
         goToHelp();
         clickElement(By.id("resourcesLink"));
         textPresent(resourceText);
-        textPresent("RSS Feeds Result:");
+        textPresent("Search results for:");
 
-        // again for coverage of rss cachere
+        // again for coverage of rss cache
         goHome();
         goToHelp();
         clickElement(By.id("resourcesLink"));
         textPresent(resourceText);
-        textPresent("RSS Feeds Result");
+        textPresent("Search results for:");
 
     }
 
