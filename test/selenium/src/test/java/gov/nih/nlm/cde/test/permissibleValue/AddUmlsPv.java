05193a0687cbf093979a763b3b9e1c0b94330cf4
Georgi Ivanov <georgi.ivanov@nih.gov>
2022-08-28 22:15:59 -0400

    CDE-2479 permissibleValue.AddUmlsPv fails too often

--- test/selenium/src/test/java/gov/nih/nlm/cde/test/permissibleValue/AddUmlsPv.java
+++ test/selenium/src/test/java/gov/nih/nlm/cde/test/permissibleValue/AddUmlsPv.java
@@ -23,19 +23,16 @@ public class AddUmlsPv extends NlmCdeBaseTest {
         textPresent("C0086287", By.id("pvMeaningCode_5"));
 
         clickElement(By.id("displayNCICodes"));
-        textPresent("Retrieving...");
         textNotPresent("Retrieving...");
         textPresent("Female", By.id("nameAsNCI_5"));
         textPresent("A7570536", By.id("codeAsNCI_5"));
 
         clickElement(By.id("displayLNCCodes"));
-        textPresent("Retrieving...");
         textNotPresent("Retrieving...");
         textPresent("Female", By.id("nameAsLNC_5"));
         textPresent("A24095561", By.id("codeAsLNC_5"));
 
         clickElement(By.id("displaySNOMEDCT_USCodes"));
-        textPresent("Retrieving...");
         textNotPresent("Retrieving...");
         textPresent("Female", By.id("nameAsSCT_5"));
         textPresent("A2881557", By.id("codeAsSCT_5"));
