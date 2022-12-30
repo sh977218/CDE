8ae74e69dc1a4993a0ee0bad3e1237cd60a831e5
Peter Huang <huangs8@nih.gov>
2022-12-30 17:46:30 -0500

    CDE-2541-get-list-and-count-of-all-possible-tags-for-names-and-definitions

---
+++ scripts/tagReport.ts
@@ -0,0 +1,50 @@
+import 'server/globals';
+import { Model } from 'mongoose';
+import { CdeFormDocument, formModel } from 'server/form/mongo-form';
+import { dataElementModel } from 'server/cde/mongo-cde';
+
+process.on('unhandledRejection', (error) => {
+    console.log(error);
+});
+
+async function doOneCollection(collection: Model<CdeFormDocument>) {
+    const cond = {
+        archived: false,
+        'registrationState.registrationStatus': {$nin: ['Retired', 'Incomplete']}
+    }
+    const allNamingTags = await collection.distinct('designations.tags', cond);
+    const namingResult = await Promise.all(allNamingTags.map(async tag => {
+            const query = {'designations.tags': tag, ...cond};
+            const count = await collection.count(query);
+            return {
+                tag,
+                count
+            }
+        })
+    )
+    const allDefinitionTags = await collection.distinct('definitions.tags', cond);
+    const definitionResult = await Promise.all(allDefinitionTags.map(async tag => {
+            const query = {'definitions.tags': tag, ...cond};
+            const count = await collection.count(query);
+            return {
+                tag,
+                count
+            }
+        })
+    )
+    return {namingResult, definitionResult};
+}
+
+function run() {
+    const tasks = [dataElementModel, formModel]
+        .map((model: any) => doOneCollection(model));
+    Promise.all(tasks).then(([a, b]) => {
+        console.log('done');
+        process.exit(0);
+    }, err => {
+        console.log('err ' + err);
+        process.exit(1);
+    });
+}
+
+run();
