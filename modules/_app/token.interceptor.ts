29f6630a527e0c065d185f842b61b1a51cb3d03a
Huang, Peter (NIH/NLM) [C] <shi.huang@nih.gov>
2024-04-29 13:48:42 -0400

    Remove jwt token from local storage when jwt expired

--- modules/_app/token.interceptor.ts
+++ modules/_app/token.interceptor.ts
@@ -1,6 +1,10 @@
 import { Injectable, Injector } from '@angular/core';
 import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
+import { jwtDecode } from 'jwt-decode';
+
+const localStorageJwtTokenKey = 'jwtToken';
 
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
@@ -10,24 +14,36 @@ export class TokenInterceptor implements HttpInterceptor {
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         const http = this.injector.get(HttpClient);
+        const router = this.injector.get(Router);
 
         if (request.url.indexOf('/refreshToken') === -1) {
             /* istanbul ignore next */
             if (performance.now() - this.lastJwtRefresh > 20 * 60 * 1000) {
                 this.lastJwtRefresh = performance.now();
                 http.get<{ jwtToken: string }>(`/server/refreshToken`).subscribe(newJwt => {
-                    localStorage.setItem('jwtToken', newJwt.jwtToken);
+                    localStorage.setItem(localStorageJwtTokenKey, newJwt.jwtToken);
                 });
             }
         }
 
-        if (localStorage.getItem('jwtToken')) {
+        const jwtToken = localStorage.getItem(localStorageJwtTokenKey);
+
+        if (jwtToken) {
             request = request.clone({
                 setHeaders: {
-                    Authorization: `Bearer  ${localStorage.getItem('jwtToken')}`,
+                    Authorization: `Bearer  ${localStorage.getItem(localStorageJwtTokenKey)}`,
                 },
             });
+
+            const decodedToken = jwtDecode(jwtToken);
+            const isJwtTokenExpired = decodedToken.exp || 0 <= new Date().getTime();
+
+            if (isJwtTokenExpired) {
+                localStorage.removeItem(localStorageJwtTokenKey);
+                router.navigate(['/logout']);
+            }
         }
+
         return next.handle(request);
     }
 }
